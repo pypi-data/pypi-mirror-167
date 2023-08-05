dca685f1184161cd294f51340eb6cf156ed76cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package markdown-frames/1.0.6/markdown_frames-1.0.6.tar.gz

---
+++ packages/markdown-frames/markdown_frames-1.0.6.tar.gz/markdown_frames-1.0.6/setup.py
@@ -0,0 +1,37 @@
+"""Setup script for the markdown parsing library."""
+
+from distutils.core import setup
+from pathlib import Path
+
+from setuptools import find_packages
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="markdown_frames",
+    version="1.0.6",
+    packages=find_packages(
+        exclude=(
+            "tests",
+            "docs",
+        )
+    ),
+    install_requires=[],
+    author="Exacaster",
+    author_email="support@exacaster.com",
+    url="https://github.com/exacaster/markdown_frames",
+    description="Markdown tables parsing to pyspark / pandas DataFrames",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Testing",
+    ],
+    extras_require={
+        "pyspark": ["pyspark"],
+        "pandas": ["pandas"],
+    },
+)
