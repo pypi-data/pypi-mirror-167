c5e0e367bf5a4828924ffc242ce8297c81f57714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:29 +0000

    Add package korean-rps/1.0.1/korean_rps-1.0.1-py3-none-any.whl

---
+++ packages/korean-rps/korean_rps-1.0.1-py3-none-any.whl/korean_rps/game.py
@@ -0,0 +1,36 @@
+import random
+
+class Rps:
+  list = ['가위', '바위', '보']
+  data = {0 : True, 1 : False, 2 : '0'}
+
+  def play(self, user : str):
+    if not user in self.list:
+      return None
+
+    bot = random.choice(self.list)
+    if user == bot:
+      stat = 2
+    elif user == '가위' and bot == '바위':
+      stat = 1
+    elif user == '바위' and bot == '보':
+      stat = 1
+    elif user == '보' and bot == '가위':
+      stat = 1
+    else:
+      stat = 0
+    result = {}
+    result['result'] = self.data[stat]
+    result['user'] = user
+    result['bot'] = bot
+    return result
+
+  def emoji(self, text):
+    if text == '가위':
+      return '✌️'
+    elif text == '보':
+      return '🖐️'
+    elif text == '바위':
+      return '✊'
+    else:
+      return None
