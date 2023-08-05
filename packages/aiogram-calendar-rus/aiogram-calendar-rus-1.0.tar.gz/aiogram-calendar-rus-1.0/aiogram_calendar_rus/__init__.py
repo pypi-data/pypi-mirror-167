fcdeaffaa7415e8fdec5b97eea7ca22af1e5fd7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package aiogram-calendar-rus/1.0/aiogram-calendar-rus-1.0.tar.gz

---
+++ packages/aiogram-calendar-rus/aiogram-calendar-rus-1.0.tar.gz/aiogram-calendar-rus-1.0/aiogram_calendar_rus/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+from aiogram_calendar_rus.simple_calendar import calendar_callback as simple_cal_callback, SimpleCalendar
+from aiogram_calendar_rus.dialog_calendar import calendar_callback as dialog_cal_callback, DialogCalendar
---
+++ packages/aiogram-calendar-rus/aiogram-calendar-rus-1.0.tar.gz/aiogram-calendar-rus-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='aiogram-calendar-rus',
+      version='1.0',
+      description='Russian version for aiogram-calendar',
+      packages=['aiogram_calendar_rus'],
+      author_email='nikita.efan.2017@gmail.com',
+      zip_safe=False)
