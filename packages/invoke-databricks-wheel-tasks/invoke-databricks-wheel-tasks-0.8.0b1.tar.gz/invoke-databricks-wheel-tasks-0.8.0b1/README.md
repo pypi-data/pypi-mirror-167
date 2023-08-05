2e62514893610eeb061b1cd4cf478d57f8e4e1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:55 +0000

    Add package invoke-databricks-wheel-tasks/0.8.0b1/invoke-databricks-wheel-tasks-0.8.0b1.tar.gz

---
+++ packages/invoke-databricks-wheel-tasks/invoke-databricks-wheel-tasks-0.8.0b1.tar.gz/invoke-databricks-wheel-tasks-0.8.0b1/README.md
@@ -0,0 +1,166 @@
+# Invoke Databricks Wheel Tasks
+
+Databricks Python Wheel dev tasks in a namespaced collection of tasks to enrich the Invoke CLI task runner.
+
+## Getting Started
+
+```sh
+pip install invoke-databricks-wheel-tasks
+```
+
+This will also install `invoke` and `databricks-cli`.
+
+### Databricks CLI Config
+
+It is assumed you will follow the documentation provided to setup `databricks-cli`.
+
+https://docs.databricks.com/dev-tools/cli/index.html
+
+You'll need to setup a Personal Access Token. Then run the following command:
+
+```sh
+databricks configure --profile yourprofilename --token
+
+Databricks Host (should begin with https://): https://myorganisation.cloud.databricks.com/
+Token: 
+```
+
+Which will create a configuration file in your home directory at `~/.databrickscfg` like:
+
+```sh
+cat ~/.databrickscfg
+
+[yourprofilename]
+host = https://myorganisation.cloud.databricks.com/
+token = dapi0123456789abcdef0123456789abcdef
+jobs-api-version = 2.1
+```
+
+### Invoke Setup
+
+`tasks.py`
+
+```python
+from invoke import task
+from invoke_databricks_wheel_tasks import * # noqa
+
+@task
+def format(c):
+    """Autoformat code for code style."""
+    c.run("black .")
+    c.run("isort .")
+
+@task
+def build(c):
+    """Build wheel."""
+    c.run("rm -rfv dist/")
+    c.run("poetry build -f wheel")
+```
+
+Once your `tasks.py` is setup like this `invoke` will have the extra commands:
+
+```sh
+λ invoke --list
+Available tasks:
+
+  build                  Build wheel.
+  clean                  Clean wheel artifact from DBFS.
+  dbfs-wheel-path        Generate the target path (including wheelname) this wheel should be uploaded to.
+  dbfs-wheel-path-root   Generate the target path (excluding wheelname) this wheel should be uploaded to.
+  define-job             Generate templated Job definition and upsert by Job Name in template.
+  format                 Autoformat code for code style.
+  poetry-wheel-name      Display the name of the wheel file poetry would build.
+  reinstall              Reinstall version of wheel on cluster with a restart.
+  runjob                 Trigger default job associated for this project.
+  upload                 Upload wheel artifact to DBFS.
+```
+
+## The Tasks
+
+### upload
+
+This task will use `dbfs` to empty the upload path and then copy the built wheel from `dist/`.
+This project assumes you're using `poetry` or your wheel build output is located in `dist/`.
+
+If you have other requirements then _pull requests welcome_.
+
+### clean
+
+This tasks will clean up all items on the target `--artifact-path`.
+
+### reinstall
+
+After some trial and error, creating a job which creates a job cluster everytime is roughly 7 minutes.
+
+However if you create an all purpose cluster that you:
+ - Mark the old wheel for uninstall
+ - restart cluster
+ - install updated wheel from dbfs location
+ 
+ This takes roughly 2 minutes which is a much tighter development loop. So these three steps are what `db.reinstall` performs.
+
+### runjob
+
+Assuming you have defined a job, that uses a pre-existing cluster, that has your latest wheel installed, this will create a manual trigger of your job with `job-id`.
+
+The triggering returns a `run-id`, where this `run-id` gets polled until the state gets to an end state.
+
+Then a call to `databricks runs get-output --run-id` happens to retrieve and `error`, `error_trace` and/or `logs` to be emitted to console.
+
+### define-job
+
+You may want to run `invoke --help define-job` to get the help documentation.
+
+There are a few arguments that get abbreviated which we will explain before discussing how they work together.
+ - `--jinja-template` or `-j`: This is a [Jinja2 Template](https://jinja.palletsprojects.com/en/3.1.x/) that must resolve to a valid [Databricks Jobs JSON payload](https://docs.databricks.com/dev-tools/api/latest/jobs.html#) spec.
+ - `--config-file` or `-c`: This is either a JSON or YAML file to define the config, that gets used to parametrise the above `jinja-template`. This `config-file` can also be a Jinja template to inject values that can only be known at runtime like the git feature branch you are currently on. By default it is treated as a plain config file and not a Jinja Template unless `environment-variable` flags are specified (_see next_).
+ - `--environment-variable` or `-e`: This flag can be repeated many times to specify multiple values. It takes a string in the `key=value` format. 
+    - Eg `-e branch=$(git branch --show-current) -e main_wheel=$MAIN -e utils_wheel=$UTILS`
+
+So an example command could look like:
+```sh
+invoke define-job \
+    -j jobs/base-job-template.json.j2 \
+    -c jobs/customer360-etl-job.yaml \
+    -e branch=$(git branch --show-current) \
+    -e main_whl=$(invoke dbfs-wheel-path) \
+    -e utils_whl=$UTILS_DBFS_WHEEL_PATH
+```
+
+Then the `-e` values can get templated into `customer360-etl-job.yml`. Then that YAML file gets parsed and injected into `base-job-template.json.j2`
+
+This will then check the list of Jobs in your workspace, see if a job with the same name exists already and perform a _create or replace job_ operation. This expects the `config-file` to have a key `name` to be able to cross check the list of existing jobs.
+
+**The beauty is that the specifics of `config-file` and `jinja-template` is completely up to you.**
+
+`config-file` is the minimal datastructure you need to configure `jinja-template` and you just use the [Jinja Control Structures](https://jinja.palletsprojects.com/en/3.1.x/templates/#list-of-control-structures) (`if-else`, `for-loop`, etc) to traverse it and populate `jinja-template`.
+
+
+# Contributing
+
+At all times, you have the power to fork this project, make changes as you see fit and then:
+
+```sh
+pip install https://github.com/user/repository/archive/branch.zip
+```
+[Stackoverflow: pip install from github branch](https://stackoverflow.com/a/24811490/622276)
+
+That way you can run from your own custom fork in the interim or even in-house your work and simply use this project as a starting point. That is totally ok.
+
+However if you would like to contribute your changes back, then open a Pull Request "across forks".
+
+Once your changes are merged and published you can revert to the canonical version of `pip install`ing this package.
+
+If you're not sure how to make changes or if you should sink the time and effort, then open an Issue instead and we can have a chat to triage the issue.
+
+
+# Resources
+
+ - [`pyinvoke`](https://pyinvoke.org)
+ - [`databricks-cli`](https://docs.databricks.com/dev-tools/cli/index.html)
+
+# Prior Art
+
+ - https://github.com/Smile-SA/invoke-sphinx
+ - https://github.com/Dashlane/dbt-invoke
+
---
+++ packages/invoke-databricks-wheel-tasks/invoke-databricks-wheel-tasks-0.8.0b1.tar.gz/invoke-databricks-wheel-tasks-0.8.0b1/invoke_databricks_wheel_tasks/utils/databricks.py
@@ -0,0 +1,188 @@
+# Standard Library
+import json
+import os
+import time
+from functools import lru_cache
+from pprint import pprint as pp
+from typing import Any, Dict, List, Optional, Union
+
+# Third Party
+import invoke
+
+from .git import git_current_branch
+from .poetry import poetry_project_name, poetry_wheelname
+
+POLL_DELAY = 5
+
+
+@lru_cache(maxsize=None)
+def default_dbfs_artifact_path(branch_name: Optional[str] = None) -> str:
+    """Helper to construct a default artifact path to upload to in DBFS."""
+    if branch_name is None:
+        branch_name = git_current_branch()
+    return f"dbfs:/FileStore/wheels/{branch_name}/{poetry_project_name()}/"
+
+
+@lru_cache(maxsize=None)
+def default_dbfs_wheel_path(branch_name: Optional[str] = None) -> str:
+    """Helper to construct a default artifact path to upload to in DBFS."""
+    return f"{default_dbfs_artifact_path(branch_name)}{poetry_wheelname()}"
+
+
+def list_jobs(profile: Optional[str] = None) -> Dict[str, str]:
+    """List Jobs from databricks workspace.
+
+    Returns a dictionary with job names as keys and job IDs as values.
+    """
+    profile_flag = f"--profile {profile}" if profile else ""
+    result = invoke.run(f"databricks {profile_flag} jobs list --output JSON", hide=True)
+    run_status = json.loads(result.stdout)
+    jobs = {e["settings"]["name"]: e["job_id"] for e in run_status["jobs"]}
+    return jobs
+
+
+def create_or_reset_job(
+    json_payload: Dict[str, Any], profile: Optional[str] = None, job_id: Optional[str] = None
+) -> Any:
+    """Create or Reset a Databricks Job definition.
+
+    - Takes a parsed JSON payload dictionary,
+    - Encodes it into a temporary JSON file
+    - Runs the `create` or `reset` command depending on the presence of `job_id`
+    - cleans up temporary file
+    - returns response
+
+    Payload must comply with:
+    https://docs.databricks.com/dev-tools/api/latest/jobs.html#
+    """
+    profile_flag = f"--profile {profile}" if profile else ""
+    json_filename = "temp_job_file.json"
+    if os.path.exists(json_filename):
+        os.remove(json_filename)
+
+    with open(json_filename, "w") as f:
+        json.dump(json_payload, f)
+
+    if job_id:
+        result = invoke.run(f"databricks {profile_flag} jobs reset --job-id {job_id} --json-file {json_filename}")
+    else:
+        result = invoke.run(f"databricks {profile_flag} jobs create --json-file {json_filename}")
+
+    os.remove(json_filename)
+    return result.stdout
+
+
+def wait_for_cluster_status(
+    c: invoke.Context,
+    profile: Optional[str],
+    cluster_id: Optional[str],
+    target_status: List[str] = ["RUNNING"],
+    failure_status: List[str] = [
+        "ERROR",
+        "UNKNOWN",
+    ],  # https://docs.databricks.com/dev-tools/api/latest/clusters.html#clusterstate
+) -> None:
+    """Poll cluster status until in desired state."""
+    current_status = None
+    profile_flag = f"--profile {profile}" if profile else ""
+
+    while current_status not in target_status:
+        result = c.run(f"databricks {profile_flag} clusters events --cluster-id {cluster_id} --output JSON", hide=True)
+        latest_event = json.loads(result.stdout)["events"][0]
+        pp(latest_event["type"])
+        current_status = latest_event["type"]
+        if current_status in failure_status:
+            raise ValueError(f"Cluster entered failed state {current_status}... aborting.")
+
+        time.sleep(POLL_DELAY)
+
+
+def wait_for_library_status(
+    c: invoke.Context,
+    profile: Optional[str],
+    cluster_id: Optional[str],
+    wheel: Optional[str],
+    target_status: List[str] = ["INSTALLED"],
+    failure_status: List[str] = [
+        "UNINSTALL_ON_RESTART",
+        "FAILED",
+        "SKIPPED",
+    ],  # https://docs.databricks.com/dev-tools/api/latest/libraries.html#libraryinstallstatus
+) -> None:
+    """Poll library statuses until target library in desired state."""
+    profile_flag = f"--profile {profile}" if profile else ""
+    current_status = None
+
+    while current_status not in target_status:
+        result = c.run(f"databricks {profile_flag} libraries cluster-status --cluster-id {cluster_id}", hide=True)
+        statuses = json.loads(result.stdout)["library_statuses"]
+        filtered_status = [
+            status for status in statuses if "whl" in status["library"] and status["library"]["whl"] == wheel
+        ]
+        pp(filtered_status[0]["status"])
+        current_status = filtered_status[0]["status"]
+        if current_status in failure_status:
+            raise ValueError(f"Library entered failed state {current_status}... aborting.")
+
+        time.sleep(POLL_DELAY)
+
+
+def wait_for_run_status(
+    c: invoke.Context,
+    profile: Optional[str],
+    run_id: Optional[str],
+    target_status: List[str] = ["TERMINATED"],
+    failure_status: List[str] = ["INTERNAL_ERROR", "SKIPPED"],
+) -> None:
+    """Poll run status until in desired state."""
+    profile_flag = f"--profile {profile}" if profile else ""
+    current_status = None
+    url = None
+    run_status = {}
+
+    while current_status not in target_status:
+        result = c.run(f"databricks {profile_flag} runs get --run-id {run_id}", hide=True)
+        run_status = json.loads(result.stdout)
+        if url is None:
+            url = run_status["run_page_url"]
+            print(url)
+
+        current_status = run_status["state"]["life_cycle_state"]
+        print(current_status)
+        if current_status in failure_status:
+            raise ValueError(f"Run entered failed state {current_status}... aborting.")
+
+        time.sleep(POLL_DELAY)
+
+    pp(run_status["state"])
+    print(url)
+    result = c.run(f"databricks {profile_flag} runs get-output --run-id {run_id}", hide=True)
+    output = json.loads(result.stdout)
+
+    for k in ["error", "error_trace", "logs"]:
+        if k in output:
+            print(f"\n========{k.upper()}======\n")
+            print("\n".join(output[k].split("\\n")))
+
+
+def check_conf(
+    c: invoke.Context, value: Optional[str], conf_key: str, should_raise: bool = True
+) -> Optional[Union[invoke.Context, str]]:
+    """Ensure a value is provided from CLI flag value or config."""
+    if value is not None:
+        return value
+
+    key_parts = conf_key.split(".")
+    output = c
+    for part in key_parts:
+        if part in output.keys() and output[part] is not None:
+            output = output[part]
+        else:
+            if should_raise:
+                # Insta-fail if key-path lookups fail
+                raise ValueError(f"Could not resolve a non-null value from {conf_key} or required CLI flag.")
+            else:
+                return None
+
+    # key-path lookups must have passed
+    return output
