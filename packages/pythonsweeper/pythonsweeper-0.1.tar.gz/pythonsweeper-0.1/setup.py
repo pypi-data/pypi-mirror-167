7896cf987284b806232536cea9907b5739f8529c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:09 +0000

    Add package pythonsweeper/0.1/pythonsweeper-0.1.tar.gz

---
+++ packages/pythonsweeper/pythonsweeper-0.1.tar.gz/pythonsweeper-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pythonsweeper',
+    version='0.1',
+    license='MIT',
+    author="Boris Khesin",
+    author_email='rob.falls.do@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Shagulka/pysweeper',
+    keywords='minesweeper',
+    install_requires=[
+          'enum',
+          'random'
+      ],
+
+)
