23fd553b18db2662d9f905527c8f81c382143944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:51 +0000

    Add package hexp/0.0.6/hexp-0.0.6.tar.gz

---
+++ packages/hexp/hexp-0.0.6.tar.gz/hexp-0.0.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name="hexp",
+    version='0.0.6',
+    py_modules=['hexp'],
+    packages=find_packages('bin'),
+    package_dir={'': 'bin'},
+    description="Dump contents of input in hexadecimal.",
+    author="Mano",
+    author_email="shapely_indexes0d@icloud.com",
+    install_requires=[
+        'Click',
+    ],
+    scripts=['bin/hexp'],
+    url="https://github.com/manorajesh/hexp",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+    keywords="hexdump hex hexadecimal",
+    long_description_content_type="text/markdown",
+    long_description=readme(),
+)
