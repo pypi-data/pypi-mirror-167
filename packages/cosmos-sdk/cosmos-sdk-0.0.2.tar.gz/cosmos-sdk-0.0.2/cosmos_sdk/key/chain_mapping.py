1752a6c4cc7e11fea7d5a71864873ad1fda3959b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:46 +0000

    Add package cosmos-sdk/0.0.2/cosmos-sdk-0.0.2.tar.gz

---
+++ packages/cosmos-sdk/cosmos-sdk-0.0.2.tar.gz/cosmos-sdk-0.0.2/cosmos_sdk/key/chain_mapping.py
@@ -0,0 +1,72 @@
+__all__ = ["COIN_TYPES_MAP","COIN_PREFIXES_MAP","COIN_VALIDATOR_PREFIXES_MAP","COIN_VALIDATOR_PUB_PREFIXES_MAP"\
+   "COIN_PUB_PREFIXES_MAP" ]
+
+COIN_TYPES_MAP = {
+    "terra" : 330,
+    "persistence" : 118,
+    "injective" : 60,
+    "osmosis" : 118,
+    "kujira" : 118,
+    "evmos" : 60,
+    "juno" : 118,
+    "fetch" : 118,
+    "secret" : 529,
+    "akash" : 118,
+    "thorchain" : 931
+}
+
+COIN_PREFIXES_MAP = {
+    "terra" : "terra",
+    "injective" : "inj",
+    "persistence" : "persistence",
+    "osmosis" : "osmo",
+    "kujira" : "kujira",
+    "evmos" : "evmos",
+    "juno" : "juno",
+    "fetch" : "fetch",
+    "secret" : "secret",
+    "akash" : "akash",
+    "thorchain" : "thor"
+}
+
+COIN_VALIDATOR_PREFIXES_MAP = {
+    "terra" : "terravaloper",
+    "injective" : "injvaloper",
+    "persistence" : "persistencevaloper",
+    "osmosis" : "osmovaloper",
+    "kujira" : "kujiravaloper",
+    "evmos" : "evmosvaloper",
+    "juno" : "junovaloper",
+    "fetch" : "fetchvaloper",
+    "secret" : "secretvaloper",
+    "akash" : "akashvaloper",
+    "thorchain" : "thorvaloper"
+}
+
+COIN_VALIDATOR_PUB_PREFIXES_MAP = {
+    "terra" : "terravaloperpub",
+    "injective" : "injvaloperpub",
+    "persistence" : "persistencevaloperpub",
+    "osmosis" : "osmovaloperpub",
+    "kujira" : "kujiravaloperpub",
+    "evmos" : "evmosvaloperpub",
+    "juno" : "junovaloperpub",
+    "fetch" : "fetchvaloperpub",
+    "secret" : "secretvaloperpub",
+    "akash" : "akashvaloperpub",
+    "thorchain" : "thorvaloperpub"
+}
+
+COIN_PUB_PREFIXES_MAP = {
+    "terra" : "terrapub",
+    "injective" : "injpub",
+    "persistence" : "persistencepub",
+    "osmosis" : "osmopub",
+    "kujira" : "kujirapub",
+    "evmos" : "evmospub",
+    "juno" : "junopub",
+    "fetch" : "fetchpub",
+    "secret" : "secretpub",
+    "akash" : "akashpub",
+    "thorchain" : "thorpub"
+}
