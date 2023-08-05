28d5780489b7879681de7e20d08c6dc1dee1d8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:01 +0000

    Add package jsl-tmp/4.0.37/jsl_tmp-4.0.37.tar.gz

---
+++ packages/jsl-tmp/jsl_tmp-4.0.37.tar.gz/jsl_tmp-4.0.37/johnsnowlabs/finance.py
@@ -0,0 +1,8 @@
+from johnsnowlabs.abstract_base.lib_resolver import try_import_lib
+
+if try_import_lib('sparknlp_jsl') and try_import_lib('sparknlp'):
+    # from sparknlp_jsl.extensions import finance
+    pass
+
+else:
+    pass
