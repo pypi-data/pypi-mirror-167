6468014dda559cfd152e485e1b2f7ce490eb3792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:02 +0000

    Add package fieldedge-utilities/0.14.2/fieldedge-utilities-0.14.2.tar.gz

---
+++ packages/fieldedge-utilities/fieldedge-utilities-0.14.2.tar.gz/fieldedge-utilities-0.14.2/fieldedge_utilities/tag.py
@@ -0,0 +1,9 @@
+"""Helper functions for converting tags between PEP and JSON styles.
+
+DEPRECATED - merged into `property` module.
+
+Also provides a function for comparing object attributes for equivalence.
+
+"""
+
+from .property import *
