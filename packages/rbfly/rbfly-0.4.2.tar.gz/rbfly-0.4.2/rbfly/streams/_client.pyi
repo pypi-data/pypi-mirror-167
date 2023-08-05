94a4e66116d9383f5757e2ff3061e87ff63153ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:52 +0000

    Add package rbfly/0.4.2/rbfly-0.4.2.tar.gz

---
+++ packages/rbfly/rbfly-0.4.2.tar.gz/rbfly-0.4.2/rbfly/streams/_client.pyi
@@ -0,0 +1,71 @@
+import typing as tp
+
+from ..types import AMQPBody
+from ..amqp import MessageCtx
+from .client import StreamsClient
+from .offset import Offset
+
+class PublisherConstr(tp.Protocol):
+    def __init__(
+        self,
+        client: StreamsClient, stream: str, id: int, name: str, message_id: int
+    ) -> None: ...
+
+    id: int
+    name: str
+    stream: str
+
+class PublisherTrait:
+    stream: str
+    id: int
+    message_id: int
+
+    def __init__(
+        self,
+        client: StreamsClient, stream: str, id: int, name: str, message_id: int
+    ) -> None: ...
+
+    def next_message_id(self) -> int: ...
+
+class Publisher(PublisherTrait):
+    async def send(self, body: AMQPBody) -> None: ...
+
+class PublisherBatch(PublisherTrait):
+    _data: list[bytes]
+
+    def batch(self, body: AMQPBody) -> None: ...
+
+    async def flush(self) -> None: ...
+
+class PublisherBin(PublisherTrait):
+    async def send(self, message: bytes) -> None: ...
+
+class PublisherBinBatch(PublisherTrait):
+    _data: list[bytes]
+
+    def batch(self, message: bytes) -> None: ...
+
+    async def flush(self) -> None: ...
+
+class Subscriber:
+    stream: str
+    subscription_id: int
+    offset: Offset
+    timeout: float
+    amqp: bool
+    message: MessageCtx | None
+
+    def __init__(
+        self,
+        client: StreamsClient,
+        stream: str,
+        subscription_id: int,
+        offset: Offset,
+        timeout: float,
+        amqp: bool,
+    ) -> None:
+        ...
+
+    def __aiter__(self) -> tp.AsyncIterator[MessageCtx]: ...
+
+# vim: sw=4:et:ai
