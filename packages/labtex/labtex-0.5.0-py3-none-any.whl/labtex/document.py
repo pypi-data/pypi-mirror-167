63bcaff2e90ec73566668d4bccbecd37d2dacee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:31 +0000

    Add package labtex/0.5.0/labtex-0.5.0-py3-none-any.whl

---
+++ packages/labtex/labtex-0.5.0-py3-none-any.whl/labtex/document.py
@@ -0,0 +1,197 @@
+from labtex.linear import LinearRegression
+from labtex.measurementlist import MeasurementList
+
+from typing import List, Union
+
+import os
+
+class Document:
+    "A class for LaTeX template document creation with tables and graphs already inserted."
+        # Customise these folders and the templates as you wish.
+    texfolder = "tex/"
+    graphfolder = "figures/"
+
+    template = r"""\documentclass[]{article}
+
+\title{!title}
+\author{!author}
+
+\usepackage{amsmath,amssymb,amsfonts,amsthm,physics,graphicx,geometry,enumitem,booktabs}
+
+\begin{document}
+
+\maketitle
+
+\abstract{
+
+}
+
+\section{Introduction}
+
+\section{Theory}
+
+\section{Method}
+
+\section{Results}
+
+!table
+
+\section{Data Analysis}
+
+!graph
+
+\section{Discussion}
+
+\section*{References}
+
+\end{document}
+"""
+
+
+    tabletemplates = {
+        "default": r"""
+\begin{table}[ht]
+    \centering
+    \caption{!caption}
+    \label{tab:!tablenumber}
+    \begin{tabular}{!columns}
+        \toprule
+        !data
+        \bottomrule
+    \end{tabular}
+\end{table}
+
+!table
+"""
+    }
+
+    graphtemplates = {
+        "default": r"""
+\begin{figure}[ht]
+    \centering
+    \includegraphics[width=!width\textwidth]{!filename.png}
+    \caption{!caption}
+    \label{fig:!graphnumber}
+\end{figure}
+!graph
+"""
+    }
+    
+    def __init__(self,title : str,author : str):
+        "Initialise a LaTeX document with an title and an author."
+        self.title = title
+        self.author = author
+        self.document = Document.template.replace("!title",title).replace("!author",author)
+        self.tablenumber = 0
+        self.graphnumber = 0
+
+    def __repr__(self):
+        return self.document
+
+    def table(self,nameandsymbol : List[str], data : List[MeasurementList], \
+        headers :List[str] = [], caption : str = "",style : str = "sideways"):
+        """
+        Add a table to the LaTeX document. 
+        """
+        assert len(nameandsymbol) == len(data)
+        assert all(len(data[0]) == len(line) for line in data)
+        columns = len(data[0])
+
+        table = Document.tabletemplates["default"]
+        self.tablenumber += 1
+        print(f"labtex: Adding table {self.tablenumber} to Document '{self.title}'.")
+
+        table = table.replace("!tablenumber", str(self.tablenumber))
+        table = table.replace("!caption",caption)
+
+        if not (all(isinstance(line, MeasurementList) for line in data)):
+            raise Exception("Data Error: Data should be a list of MeasurementLists.")
+
+        if(style == "sideways"):
+            # table = table.replace("!columns", "*{" + str(1+columns) + "}c" )
+            table = table.replace("!columns", f"c|{ 'c' * columns}" )
+            if(headers != []):
+                table = table.replace("!data",
+                fr"""{headers[0]} & \multicolumn{{{columns}}}{{c}}{{{headers[1]}}} \\
+            \midrule !data"""
+                )
+            for i in range(len(data)):
+                table = table.replace("!data",
+                fr"""
+            {nameandsymbol[i]}{ data[i].tableprint() } \\ !data"""
+                )
+
+        elif(style == "upright"):
+            table = table.replace("!columns", "*{" + str(len(data)) + "}c" )
+            for i in range(len(data)):
+                table = table.replace("!data",
+                fr"""{nameandsymbol[i]}{data[i].tableprint(novalues=True)} & !data"""
+                )
+            table = table.replace("& !data",
+            r"""\\ 
+            \midrule
+            !data"""
+            )
+            tableprint = [ml.tableprint(nounits=True)[1:].split("&") for ml in data]
+            indexfirst = [ [index[j] for index in tableprint] for j in range(len(tableprint[0]))]
+
+            for index in indexfirst:
+                table = table.replace("!data",
+            rf""" {"&".join([*index])} \\
+            !data""")
+
+        else:
+            raise Exception("Style Error: Only 'sideways' and 'upright' styles are supported.")
+
+
+        table = table.replace("!data","")
+        self.document = self.document.replace("!table",table)
+
+    def graph(self, data : List[MeasurementList], title : str = "", xnameandsymbol : str = "Name, Symbol", \
+        ynameandsymbol : str = "Name, Symbol", caption : str = "", width : float = 0.8, style : str = "default", \
+        showline : bool = True):
+        "Add a graph to the LaTeX document."
+        graph = Document.graphtemplates[style]
+        self.graphnumber += 1
+        graph = graph.replace("!graphnumber",str(self.graphnumber))
+        graph = graph.replace("!caption",caption)
+        graph = graph.replace("!width",str(width))
+
+        if len(data) != 2 or not all(
+            isinstance(listitem, MeasurementList) for listitem in data
+        ):
+            raise Exception("2 MeasurementLists needed for graphing.")
+
+        eq = LinearRegression(*data)
+        filename = f"graph{self.graphnumber}"
+
+        if(Document.graphfolder != '.'): # assuming the graph folder is a subfolder
+            graph = graph.replace("!filename","../" + Document.graphfolder +  filename)
+        else:
+            graph = graph.replace("!filename", Document.graphfolder + filename)
+
+        self.document = self.document.replace("!graph",graph)
+
+        if (not os.path.exists(Document.graphfolder)):
+            os.makedirs(Document.graphfolder)
+        plt = eq.plot(title,xnameandsymbol,ynameandsymbol,showline,self.graphnumber)
+        plt.savefig(Document.graphfolder + filename)
+        print(f"labtex: Wrote to '{Document.graphfolder + filename}.png' and added to Document '{self.title}'.")
+
+    def save(self,filename: str ="labdocument",overwrite: bool = False):
+        "Save the document to 'filename.tex'."
+
+        self.document = self.document.replace("!table","").replace("!graph","")
+
+        if(not os.path.exists(Document.texfolder)):
+            print("labtex: Creating folder '" + Document.texfolder + "'.")
+            os.makedirs(Document.texfolder)
+        if(os.path.exists(Document.texfolder + filename + ".tex")):
+            print(f"labtex: '{Document.texfolder + filename}.tex' already exists. { 'Overwriting.' if overwrite else 'Use `save(...,overwrite=True)` to overwrite.'}")
+            if(not overwrite):
+                return
+
+        with open(Document.texfolder + filename + '.tex','w') as outputfile:
+            outputfile.write(self.document)
+
+        print(f"labtex: Wrote to '{Document.texfolder + filename}.tex'.")
---
+++ packages/labtex/labtex-0.5.0-py3-none-any.whl/labtex/measurement.py
@@ -0,0 +1,295 @@
+import math
+from numbers import Number
+from typing import Union
+from labtex.unit import Unit
+from labtex.unit import factorandbasedims
+
+class Measurement:
+    "Base Class for all single valued measurements with uncertainties and SI units."
+    def __init__(self, value: float, uncertainty: float = 0, unit: Union[Unit,str] = ""):
+        "Create a measurement with a value, uncertainty and SI Unit."
+        self.value = value
+        self.uncertainty = uncertainty
+        self.relativeuncertainty = self.uncertainty / self.value if self.value != 0 else 0
+
+        self.unit = unit if (isinstance(unit,Unit)) else Unit(unit)
+    
+    def __repr__(self):
+        "Print string with sigfigs up to uncertainty."
+        if(self.uncertainty == 0):
+            return str(self.value) + " " + str(self.unit)
+
+        sigdigits = -math.floor(math.log10(self.uncertainty))
+        # To account for rounding up of uncertainty before applying it,
+        # eg. 98 => sigdigits = -1 but round(98,-1) => 100 so we apply again to get -2.
+        sigdigits = -math.floor(math.log10(round(self.uncertainty,sigdigits)))
+        
+        if(sigdigits >= 3): # Factored uncertainty e.g. (... ± 1) × 10^-2 
+            return f"({round(self.value * 10**sigdigits)} ± {round(self.uncertainty * 10**sigdigits)}) × 10^{{{-sigdigits}}} {self.unit}"
+        elif(sigdigits > 0): # Decimal uncertainty e.g. ± 0.1
+            return f"{round(self.value,sigdigits)} ± {round(self.uncertainty,sigdigits)} {self.unit}"
+        elif(sigdigits == 0): # Single digit uncertainty: remove decimal points from the float data type
+            return f"{round(self.value)} ± {round(self.uncertainty)} {self.unit}"
+        else: # Factored uncertainty e.g. (... ± 1) × 10^2
+            return f"({round(self.value * 10**sigdigits)} ± {round(self.uncertainty * 10**sigdigits)}) × 10^{{{-sigdigits}}} {self.unit}"
+    
+    def __add__(self,obj):
+        "Add two measurements."
+
+        # Addition necessitates independent variables
+        if(self == obj):
+            return Measurement(
+            self.value * 2,
+            self.uncertainty * 2,
+            self.unit
+        )
+
+        if(isinstance(obj,Measurement)):
+            if(self.unit == obj.unit):
+                return Measurement(
+                    self.value + obj.value,
+                    math.hypot(self.uncertainty,obj.uncertainty),
+                    self.unit
+            )
+            else:
+                raise Exception("Cannot add measurements with different units.")
+
+        # For a constant with no uncertainty
+        if(isinstance(obj,Number)):
+            return Measurement(
+                self.value + obj,
+                self.uncertainty,
+                self.unit
+            )
+        else:
+            return NotImplemented
+    
+    def __radd__(self,obj):
+        return self.__add__(obj)
+
+    def __neg__(self):
+        "Negate a measurement."
+        return Measurement(
+            -self.value,
+            self.uncertainty,
+            self.unit
+        )
+        
+    def __sub__(self,obj):
+        return self.__add__(-obj)
+        
+    def __rsub__(self,obj):
+        return self.__neg__().__add__(obj)
+
+    def __mul__(self,obj):
+        "Multiply two measurements."
+
+        # Multiplication necessitates independent variables
+        # This occurs if you do: x * x
+        if(self == obj):
+            return Measurement(
+            self.value ** 2,
+            (self.value ** 2) * 2 * self.relativeuncertainty,
+            self.unit ** 2
+        )
+
+        if(isinstance(obj,Measurement)):
+            return Measurement(
+                self.value * obj.value,
+                abs(self.value * obj.value) * math.hypot(self.relativeuncertainty, obj.relativeuncertainty),
+                self.unit * obj.unit
+            )
+
+        if(isinstance(obj,Number)):
+            return Measurement(
+                self.value * obj,
+                self.uncertainty * abs(obj),
+                self.unit
+            )
+        else:
+            return NotImplemented
+            
+    def __rmul__(self,obj):
+        return self.__mul__(obj)
+
+    
+    def __truediv__(self,obj):
+        "Divide two measurements."
+
+        # While nonsensical, this only occurs if you do: x / x
+        if(self == obj):
+            return Measurement(
+            1,
+            0,
+            ""
+        )
+
+        if(isinstance(obj,Measurement)):
+            return Measurement(
+                self.value / obj.value,
+                abs(self.value / obj.value) * math.hypot(self.relativeuncertainty,obj.relativeuncertainty),
+                self.unit / obj.unit
+            )
+
+        if(isinstance(obj,Number)):
+            return Measurement(
+                self.value / obj,
+                self.uncertainty / abs(obj),
+                self.unit
+            )
+        else:
+            return NotImplemented
+
+    def __rtruediv__(self,obj):
+        "Reverse division to accommodate constant / measurement."
+        if(isinstance(obj,Number)):
+            return Measurement(
+                obj / self.value,
+                abs(obj / self.value) * self.relativeuncertainty,
+                obj / self.unit
+            )
+        else:
+            return NotImplemented
+
+    def __pow__(self,obj):
+        "Raising a measurement to a constant power."
+        if(isinstance(obj,Measurement)):
+            if(Unit.unitless(obj.unit)):
+                return Measurement(
+                    self.value ** obj.value,
+                     abs(self.value ** obj.value) * math.sqrt(
+                        (obj.value * self.relativeuncertainty)**2 + (math.log(obj.value) * obj.uncertainty)**2
+                        ),
+                    self.unit ** obj.value
+                )
+            else:
+                raise Exception("Cannot raise a constant to a dimensional quantity. Units: " + str(self.unit))
+        if(isinstance(obj,Number)):
+            return Measurement(
+                self.value ** obj,
+                abs(self.value ** obj * obj) * self.relativeuncertainty,
+                self.unit ** obj
+            )
+        else:
+            return NotImplemented
+    
+    def __rpow__(self,obj):
+        "Reverse power for constant ** measurement."
+        if(isinstance(obj,Number)):
+            if(Unit.unitless(self.unit)):
+                return Measurement(
+                    obj ** self.value,
+                    abs(obj ** self.value  * math.log(obj)) * self.uncertainty,
+                    ""
+                )
+            else:
+                raise Exception("Cannot raise a constant to a dimensional quantity. Units: " + str(self.unit))
+        else:
+            return NotImplemented
+
+    # Static Functions applied to Measurements
+    # All parameters are instances of Measurement
+    @staticmethod
+    def sin(x):
+        "Sine function on a Measurement."
+        if(Unit.unitless(x.unit)):
+            return Measurement(
+            math.sin(x.value),
+            math.fabs(math.cos(x.value)) * x.uncertainty,
+            ""
+            )
+        else:
+            raise Exception(f"Trigonometric functions take in dimensionless quantities. Input has units: {x.unit}")
+
+    @staticmethod
+    def cos(x):
+        "Cosine function on a Measurement."
+        if(Unit.unitless(x.unit)):
+            return Measurement(
+                math.cos(x.value),
+                math.fabs(math.sin(x.value)) * x.uncertainty,
+                ""
+            )
+        else:
+            raise Exception(f"Trigonometric functions take in dimensionless quantities. Input has units: {x.unit}")
+
+    @staticmethod
+    def tan(x):
+        "Tangent function on a Measurement."
+        if(Unit.unitless(x.unit)):
+            return Measurement(
+                math.tan(x.value),
+                x.uncertainty / math.cos(x.value)**2 ,
+                ""
+            )
+        else:
+            raise Exception(f"Trigonometric functions take in dimensionless quantities. Input has units: {x.unit}")
+    
+    @staticmethod
+    def log(x):
+        "Natural log function on a Measurement."
+        if(Unit.unitless(x.unit)):
+            return Measurement(
+                math.log(x.value),
+                math.fabs(x.relativeuncertainty),
+                ""
+            )
+        else:
+            raise Exception(f"Log functions take in dimensionless quantities. Input has units: {x.unit}")
+    
+    @staticmethod
+    def asin(x):
+        "Inverse sine function on a Measurement."
+        if(Unit.unitless(x.unit)):
+            return Measurement(
+                math.asin(x.value),
+                x.uncertainty / (math.sqrt(1 - x.value**2)),
+                ""
+            )
+        else:
+            raise Exception(f"Trigonometric functions take in dimensionless quantities. Input has units: {x.unit}")
+    
+    @staticmethod
+    def acos(x):
+        "Inverse cosine function on a Measurement."
+        if(Unit.unitless(x.unit)):
+             return Measurement(
+                math.acos(x.value),
+                x.uncertainty / (math.sqrt(1 - x.value**2)),
+                ""
+            )
+        else:
+            raise Exception(f"Trigonometric functions take in dimensionless quantities. Input has units: {x.unit}")
+
+    @staticmethod
+    def atan(x):
+        "Inverse tangent function on a Measurement."
+        if(Unit.unitless(x.unit)):
+            return Measurement(
+                math.atan(x.value),
+                x.uncertainty / (math.sqrt(1 + x.value**2)),
+                ""
+            )
+        else:
+            raise Exception(f"Trigonometric functions take in dimensionless quantities. Input has units: {x.unit}")
+ 
+    def to(self, unit : Union[str,Unit]):
+        "Convert the units of a measurement to one with the same dimensions."
+        unit = unit if isinstance(unit,Unit) else Unit(unit)
+        from_factor, from_basedims = factorandbasedims(self.unit)
+        to_factor, to_basedims = factorandbasedims(unit)
+        if(from_basedims == to_basedims):
+            return Measurement(
+                self.value * from_factor / to_factor,
+                self.uncertainty * from_factor / to_factor,
+                unit
+            )
+        else:
+            print(from_factor, from_basedims)
+            print(to_factor, to_basedims)
+            raise Exception(f"Dimension Error: Cannot convert from {self.unit} to {unit} because they have different dimensions.")
+        
+class M(Measurement):
+    "Base Class for all single valued measurements with uncertainties and SI units."
+    pass
+
