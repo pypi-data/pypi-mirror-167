9e486c16bb834a5a58a418fcbd50dde95a980266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:10 +0000

    Add package mmisdk/0.0.3/mmisdk-0.0.3-py3-none-any.whl

---
+++ packages/mmisdk/mmisdk-0.0.3-py3-none-any.whl/mmisdk/qredo/qredo_tansaction.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from typing import Dict, List, Optional
+
+
+class QredoTransactionEvent(BaseModel):
+    id: str
+    timestamp: int
+    status: str
+    message: str
+
+
+class QredoTransaction(BaseModel):
+    txID: str
+    txHash: str
+    status: str
+    timestamps: Dict
+    events: List[QredoTransactionEvent]
+    nonce: int
+    gasLimit: str
+    gasPrice: Optional[str]
+    maxFeePerGas: Optional[str]
+    maxPriorityFeePerGas: Optional[str]
+    from_: str
+    to: str
+    value: str
+    data: str
+    rawTX: str
+    createdBy: str
+    accountID: str
+    network: str
+    chainID: str
