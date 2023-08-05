8e35ffca0866ab42199c68cda13f724c7c92ec54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package GooblerPKG/2.0.4/GooblerPKG-2.0.4.tar.gz

---
+++ packages/GooblerPKG/GooblerPKG-2.0.4.tar.gz/GooblerPKG-2.0.4/gooblerpkg/__init__.py
@@ -0,0 +1,28 @@
+# IMPORTS:
+import os
+import sys
+import re
+import time
+import random
+import json
+import requests
+from config import colors
+
+###################
+### Goobler 2022 ##
+###################
+# Scripts made by Eric #
+
+
+
+# PURE FUNCS:
+
+def start(tkn):
+  resp = requests.get("https://goobler.imango.com.au/api/get_name_using_auth", {"auth": f"{tkn}"}).json()
+  print(f"Starting {resp['user_name']}...")
+  resp2 = requests.post("https://goobler.imango.com.au/api/login/bot_account", {"auth": f"{tkn}"}).json()
+  print(resp['user_name'] + "has logged in.")
+
+def Application(client_id=None, token=None):
+  if token == None:
+    print(colors.FAIL + colors.BOLD + colors.UNDERLINE + "[GooblerAPI] You cannot execute 'MakeApplication' without the positional arguments, token. Remember, the token is the admin password to your application.")
