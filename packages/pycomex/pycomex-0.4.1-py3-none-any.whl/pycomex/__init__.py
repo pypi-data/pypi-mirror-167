735dacbfe1ef8648d2026efe93e74257e8e0d86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:12 +0000

    Add package pycomex/0.4.1/pycomex-0.4.1-py3-none-any.whl

---
+++ packages/pycomex/pycomex-0.4.1-py3-none-any.whl/pycomex/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for pycomex."""
+
+__author__ = """Jonas Teufel"""
+__email__ = "jonseb1998@gmail.com"
+__version__ = "0.4.1"
+
+from pycomex.experiment import Experiment  # noqa
