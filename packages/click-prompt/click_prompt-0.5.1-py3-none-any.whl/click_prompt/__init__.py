ab2649a8af95884f3e282b220333fcc8dab018eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:03 +0000

    Add package click-prompt/0.5.1/click_prompt-0.5.1-py3-none-any.whl

---
+++ packages/click-prompt/click_prompt-0.5.1-py3-none-any.whl/click_prompt/__init__.py
@@ -0,0 +1,11 @@
+"""
+Provides additional interactive command line interface options for the
+click framework using the questionary library
+"""
+from click_prompt.argument import *
+from click_prompt.option import *
+
+from click_prompt.argument_decorators import *
+from click_prompt.option_decorators import *
+
+
