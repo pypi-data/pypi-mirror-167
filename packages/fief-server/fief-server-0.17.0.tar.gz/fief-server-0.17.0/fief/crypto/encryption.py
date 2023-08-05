f9a69458f4956c5696c9f5c92395ae7f6fc7f0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:25 +0000

    Add package fief-server/0.17.0/fief-server-0.17.0.tar.gz

---
+++ packages/fief-server/fief-server-0.17.0.tar.gz/fief-server-0.17.0/fief/crypto/encryption.py
@@ -0,0 +1,26 @@
+import binascii
+
+from cryptography import fernet
+
+
+def generate_key() -> bytes:
+    return fernet.Fernet.generate_key()
+
+
+def is_valid_key(key: bytes) -> bool:
+    try:
+        fernet.Fernet(key)
+    except binascii.Error:
+        return False
+    else:
+        return True
+
+
+def encrypt(value: str, key: bytes) -> str:
+    f = fernet.Fernet(key)
+    return f.encrypt(value.encode("utf-8")).decode("utf-8")
+
+
+def decrypt(value: str, key: bytes) -> str:
+    f = fernet.Fernet(key)
+    return f.decrypt(value.encode("utf-8")).decode("utf-8")
