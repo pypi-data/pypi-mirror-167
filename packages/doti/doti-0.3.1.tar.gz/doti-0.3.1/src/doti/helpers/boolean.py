c720ea5dcbf2b5c057bb557936c5de373dd52174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:15 +0000

    Add package doti/0.3.1/doti-0.3.1.tar.gz

---
+++ packages/doti/doti-0.3.1.tar.gz/doti-0.3.1/src/doti/helpers/boolean.py
@@ -0,0 +1,29 @@
+"""
+Check if a string represents a boolean.
+"""
+
+
+def is_bool(string):
+    """Check if a string represents a boolean."""
+    bools = [
+        "add",
+        "remove",
+        "rm",
+        "stow",
+        "unstow",
+        "true",
+        "false",
+        "yes",
+        "no",
+        "on",
+        "off",
+        "1",
+        "0",
+    ]
+    return string.lower() in bools
+
+
+def is_true(string):
+    """Check if a string represents a true value."""
+    bools = ["add", "stow", "true", "yes", "on", "1"]
+    return string.lower() in bools
---
+++ packages/doti/doti-0.3.1.tar.gz/doti-0.3.1/src/doti/options/settings.py
@@ -0,0 +1,35 @@
+"""
+Return all settings (command-line argument > config file > default)
+"""
+
+from ..helpers.boolean import is_true
+from ..helpers.path import dir_path
+
+
+def get_setting(arg, config_settings, setting):
+    """Return the setting (command-line argument > config file > default)"""
+    if setting == "dotfiles_dir":
+        if arg is not None:
+            return arg[0]
+        return dir_path(config_settings.get(setting, "~/dotfiles"))
+    if setting in ["verbose", "quiet", "root-enable", "root-only", "simulate"]:
+        if arg:
+            return arg
+        return is_true(config_settings.get(setting, "false"))
+    return arg
+
+
+def get_settings(args, config_settings):
+    """Return all settings."""
+    settings = {}
+    settings["dotfiles_dir"] = get_setting(
+        args.dotfiles_dir, config_settings, "dotfiles_dir"
+    )
+    settings["verbose"] = get_setting(args.verbose, config_settings, "verbose")
+    settings["quiet"] = get_setting(args.quiet, config_settings, "quiet")
+    settings["root-enable"] = get_setting(
+        args.root_enable, config_settings, "root-enable"
+    )
+    settings["root-only"] = get_setting(args.root_only, config_settings, "root-only")
+    settings["simulate"] = get_setting(args.simulate, config_settings, "simulate")
+    return settings
