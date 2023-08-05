a0165c32a69cfc315a51daeebb5fd95aa2189a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:47 +0000

    Add package digitalguide/0.0.298/digitalguide-0.0.298-py3-none-any.whl

---
+++ packages/digitalguide/digitalguide-0.0.298-py3-none-any.whl/digitalguide/whatsapp/db_objects.py
@@ -0,0 +1,45 @@
+import mongoengine
+from telegram.constants import MAX_MESSAGE_LENGTH
+import datetime
+
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')
+
+dbname = config["bot"]["bot_name"]
+class WhatsAppUser(mongoengine.Document):
+    ProfileName = mongoengine.StringField(required=True)
+    WaId = mongoengine.StringField(required=True, max_length=50)
+    entry_time = mongoengine.DateTimeField(default=datetime.datetime.utcnow)
+    meta = {'db_alias': dbname}
+
+class WhatsAppInteraction(mongoengine.Document):
+    user = mongoengine.ReferenceField(WhatsAppUser)
+    Latitude = mongoengine.StringField()
+    Longitude = mongoengine.StringField()
+    SmsMessageSid = mongoengine.StringField()
+    NumMedia = mongoengine.StringField()
+    ProfileName = mongoengine.StringField()
+    SmsSid = mongoengine.StringField()
+    WaId = mongoengine.StringField()
+    SmsStatus = mongoengine.StringField()
+    Body = mongoengine.StringField()
+    To = mongoengine.StringField()
+    NumSegments = mongoengine.StringField()
+    MessageSid = mongoengine.StringField()
+    AccountSid = mongoengine.StringField()
+    From = mongoengine.StringField()
+    ApiVersion = mongoengine.StringField()
+    MediaContentType0 = mongoengine.StringField()
+    MediaUrl0 = mongoengine.StringField()
+    MessagingServiceSid = mongoengine.StringField()
+    ReferralNumMedia = mongoengine.StringField()
+    date = mongoengine.DateTimeField(default=datetime.datetime.utcnow)
+    meta = {'db_alias': dbname}
+
+class WhatsAppUserContextState(mongoengine.Document):
+    WaId = mongoengine.StringField(primary_key=True)
+    context = mongoengine.DictField()
+    state = mongoengine.StringField()
+    meta = {'db_alias': dbname}
