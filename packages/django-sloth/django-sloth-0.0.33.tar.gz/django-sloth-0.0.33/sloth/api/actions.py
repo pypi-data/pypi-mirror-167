0d928a2a6b85975a911ba7af238a0b008382b028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:55 +0000

    Add package django-sloth/0.0.33/django-sloth-0.0.33.tar.gz

---
+++ packages/django-sloth/django-sloth-0.0.33.tar.gz/django-sloth-0.0.33/sloth/api/actions.py
@@ -0,0 +1,81 @@
+from sloth import actions
+from django.contrib import auth
+
+
+class DeleteUserRole(actions.Action):
+    class Meta:
+        verbose_name = 'Excluir'
+        style = 'danger'
+
+    def submit(self):
+        self.instance.delete()
+        self.redirect()
+
+
+class InactivateUserRole(actions.Action):
+    class Meta:
+        verbose_name = 'Excluir'
+        style = 'danger'
+
+    def submit(self):
+        self.instance.active = False
+        self.instance.save()
+        self.redirect()
+
+
+class ActivateUserRole(actions.Action):
+    class Meta:
+        verbose_name = 'Ativar'
+        style = 'success'
+
+    def submit(self):
+        self.instance.active = True
+        self.instance.save()
+        self.redirect('.')
+
+    def has_permission(self, user):
+        return user.is_superuser and not self.instance.active
+
+
+class DeactivateUserRole(actions.Action):
+    class Meta:
+        verbose_name = 'Desativar'
+        style = 'warning'
+
+    def submit(self):
+        self.instance.active = False
+        self.instance.save()
+        self.redirect()
+
+    def has_permission(self, user):
+        return user.is_superuser and self.instance.active
+
+
+class LoginAsUser(actions.Action):
+    class Meta:
+        icon = 'unlock'
+        verbose_name = 'Acessar'
+        style = 'warning'
+
+    def submit(self):
+        auth.login(self.request, self.instance)
+        self.redirect('/')
+
+    def has_permission(self, user):
+        return user.is_superuser and self.instance != self.request.user
+
+
+class StopTask(actions.Action):
+    class Meta:
+        icon = 'stop-circle'
+        verbose_name = 'Parar Execução'
+        style = 'danger'
+        confirmation = True
+
+    def submit(self):
+        self.instance.stop()
+        type(self.instance).STOPPED_TASKS.append(self.instance.id)
+        self.redirect(message='Ação realizada com sucesso')
+
+    def has_permission(self, user):
+        return self.instance.in_progress() and (user.is_superuser or self.instance.user == user)
