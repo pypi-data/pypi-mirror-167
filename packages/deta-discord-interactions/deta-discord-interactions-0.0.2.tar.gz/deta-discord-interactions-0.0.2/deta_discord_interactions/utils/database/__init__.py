dfe2ac55d105abc2a6b2e4f9e5e0ba33c910c066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:44 +0000

    Add package deta-discord-interactions/0.0.2/deta-discord-interactions-0.0.2.tar.gz

---
+++ packages/deta-discord-interactions/deta-discord-interactions-0.0.2.tar.gz/deta-discord-interactions-0.0.2/deta_discord_interactions/utils/database/__init__.py
@@ -0,0 +1,14 @@
+from deta_discord_interactions.utils.database.database import Database
+from deta_discord_interactions.utils.database.record import Record, AutoSyncRecord
+from deta_discord_interactions.utils.database.query import Query, Field
+from deta_discord_interactions.utils.database.record import RecordType, Key
+
+__all__ = [
+    "Database",
+    "Record",
+    "AutoSyncRecord",
+    "Query",
+    "Field",
+    "RecordType",
+    "Key",
+]
---
+++ packages/deta-discord-interactions/deta-discord-interactions-0.0.2.tar.gz/deta-discord-interactions-0.0.2/deta_discord_interactions/utils/database/exceptions.py
@@ -0,0 +1,5 @@
+class KeyNotFound(Exception):
+    "Key does not exists in the Deta Base"
+
+class FieldNotFoundError(Exception):
+    "Record field was never set"
