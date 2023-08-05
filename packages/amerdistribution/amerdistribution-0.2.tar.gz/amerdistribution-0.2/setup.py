0903248c76eba9431ba1c68346a1b6cecdc41375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:17 +0000

    Add package amerdistribution/0.2/amerdistribution-0.2.tar.gz

---
+++ packages/amerdistribution/amerdistribution-0.2.tar.gz/amerdistribution-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='amerdistribution',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['amerdistribution'],
+      author="Amer",
+      author_email="amer.deraa@gmail.com",
+      zip_safe=False)
