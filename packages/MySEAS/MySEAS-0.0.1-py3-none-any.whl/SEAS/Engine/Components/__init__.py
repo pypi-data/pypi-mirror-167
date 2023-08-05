1dfa23e41b3104a0b2ce2c2246d5e3406b5ba78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:53 +0000

    Add package MySEAS/0.0.1/MySEAS-0.0.1-py3-none-any.whl

---
+++ packages/MySEAS/MySEAS-0.0.1-py3-none-any.whl/SEAS/Engine/Components/__init__.py
@@ -0,0 +1,7 @@
+from SEAS.Engine.Components.renderRectComp import *
+from SEAS.Engine.Components.transformRectComp import *
+from SEAS.Engine.Components.gravityComp import *
+from SEAS.Engine.Components.renderPolyComp import *
+from SEAS.Engine.Components.transformPolyComp import *
+from SEAS.Engine.Components.collidePolyComp import *
+from SEAS.Engine.Components.characterPolyControllerComp import *
---
+++ packages/MySEAS/MySEAS-0.0.1-py3-none-any.whl/SEAS/Engine/Components/transformPolyComp.py
@@ -0,0 +1,17 @@
+class TransformPoly:
+    def __init__(self, point):
+        # Make so that u can have many points
+        # U might have to update points in update.
+        # Make so points are relavant to other points
+
+
+        # Well do that later
+        self.points = point
+        self.angle = 0
+
+
+    def start(self):
+        pass
+
+    def update(self):
+        pass
