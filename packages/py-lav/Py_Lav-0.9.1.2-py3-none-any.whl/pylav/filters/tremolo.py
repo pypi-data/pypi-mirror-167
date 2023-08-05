94eec15a1217ca2f766f91646e8c82725ffd3430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:42 +0000

    Add package py-lav/0.9.1.2/Py_Lav-0.9.1.2-py3-none-any.whl

---
+++ packages/py-lav/Py_Lav-0.9.1.2-py3-none-any.whl/pylav/filters/tremolo.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+from pylav.filters.utils import FilterMixin
+
+
+class Tremolo(FilterMixin):
+    __slots__ = ("_frequency", "_depth", "_off", "_default")
+
+    def __init__(self, frequency: float, depth: float):
+        super().__init__()
+        self.frequency = frequency
+        self.depth = depth
+        self.off = False
+
+    def to_dict(self) -> dict:
+        return {
+            "frequency": self.frequency,
+            "depth": self.depth,
+            "off": self.off,
+        }
+
+    def to_json(self) -> dict:
+        return {
+            "frequency": self.frequency,
+            "depth": self.depth,
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Tremolo:
+        c = cls(frequency=data["frequency"], depth=data["depth"])
+        c.off = data["off"]
+        return c
+
+    def __repr__(self):
+        return f"<Tremolo: frequency={self.frequency}, depth={self.depth}>"
+
+    @property
+    def frequency(self) -> float:
+        return self._frequency
+
+    @frequency.setter
+    def frequency(self, v: float):
+        if v == -31415926543:
+            self.off = True
+            self._frequency = v
+            return
+        if v <= 0:
+            raise ValueError(f"Frequency must be must be greater than 0, not {v}")
+        self._frequency = v
+        self.off = False
+
+    @property
+    def depth(self) -> float:
+        return self._depth
+
+    @depth.setter
+    def depth(self, v: float):
+        if v == -31415926543:
+            self.off = True
+            self._depth = v
+            return
+        if not (0.0 < v <= 1.0):
+            raise ValueError(f"Depth must be must be 0.0 < x ≤ 1.0, not {v}")
+        self._depth = v
+        self.off = False
+
+    @classmethod
+    def default(cls) -> Tremolo:
+        c = cls(frequency=-31415926543, depth=-31415926543)
+        c.off = True
+        return c
+
+    def get(self) -> dict[str, float]:
+        return (
+            {}
+            if self.off
+            else {
+                "frequency": self.frequency,
+                "depth": self.depth,
+            }
+        )
+
+    def reset(self) -> None:
+        self.frequency = self.depth = -31415926543
+        self.off = True
