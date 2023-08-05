644837e4d8b0e8c64b692c79aa465cb511a89f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:25 +0000

    Add package bowtie-json-schema/0.13.3/bowtie_json_schema-0.13.3.tar.gz

---
+++ packages/bowtie-json-schema/bowtie_json_schema-0.13.3.tar.gz/bowtie_json_schema-0.13.3/implementations/rust-jsonschema/src/main.rs
@@ -0,0 +1,54 @@
+use std::{io, process};
+
+use jsonschema::JSONSchema;
+use serde_json::{json, Result};
+
+fn main() -> Result<()> {
+    let mut started = false;
+
+    for line in io::stdin().lines() {
+        let request: serde_json::Value = serde_json::from_str(&line.expect("No input!"))?;
+        match request["cmd"].as_str().expect("Bad command!") {
+            "start" => {
+                started = true;
+                if request["version"] != 1 {
+                    panic!("Not version 1!")
+                };
+                let response = json!({
+                    "ready": true,
+                    "version": 1,
+                    "implementation": {
+                        "language": "rust",
+                        "name": "jsonschema",
+                        "homepage": "https://docs.rs/jsonschema/latest/jsonschema/",
+                        "issues": "https://github.com/Stranger6667/jsonschema-rs/issues",
+                    }
+                });
+                println!("{}", response.to_string());
+            }
+            "run" => {
+                if !started {
+                    panic!("Not started!")
+                };
+                let case = &request["case"];
+                let compiled = JSONSchema::compile(&case["schema"]).expect("Invalid schema!");
+                let results: Vec<_> = case["tests"]
+                    .as_array()
+                    .expect("Invalid tests!")
+                    .iter()
+                    .map(|test| json!({"valid": compiled.is_valid(&test["instance"])}))
+                    .collect();
+                let response = json!({"seq": &request["seq"], "results": &results});
+                println!("{}", response.to_string());
+            }
+            "stop" => {
+                if !started {
+                    panic!("Not started!")
+                };
+                process::exit(0);
+            }
+            _ => panic!("Unknown command"),
+        }
+    }
+    Ok(())
+}
