81dcc488172da74dfbaf8cf92753289db3a61e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:41 +0000

    Add package actionpack/1.7.9a1/actionpack-1.7.9a1.tar.gz

---
+++ packages/actionpack/actionpack-1.7.9a1.tar.gz/actionpack-1.7.9a1/tests/actionpack/actions/test_call.py
@@ -0,0 +1,41 @@
+import pickle
+
+from unittest import TestCase
+
+from actionpack.action import Result
+from actionpack.actions import Call
+from actionpack.utils import Closure
+from actionpack.utils import pickleable
+
+
+class CallTest(TestCase):
+
+    @classmethod
+    def function(*args, **kwargs) -> tuple:
+        return args[1:], kwargs
+
+    def setUp(self):
+        self.arg = 'arg'
+        self.kwarg = 'kwarg'
+        self.closure = Closure(self.function, self.arg, kwarg=self.kwarg)
+        self.action = Call(closure=self.closure)
+
+    def test_can_Call(self):
+        result = self.action.perform()
+        self.assertIsInstance(result, Result)
+        self.assertEqual(result.value, ((self.arg,), {self.kwarg: self.kwarg}))
+
+    def test_Call_instantiation_accepts_only_Closure_objects(self):
+        result = Call('not a closure').perform()
+        self.assertFalse(result.successful)
+        self.assertIsInstance(result.value, TypeError)
+
+    def test_serializes_as_expected(self):
+        self.assertEqual(repr(self.action), '<Call(function)>')
+
+    def test_can_pickle(self):
+        pickled = pickleable(self.action)
+        unpickled = pickle.loads(pickled)
+
+        self.assertTrue(pickleable(self.action))
+        self.assertEqual(unpickled.__dict__, self.action.__dict__)
