9a489107bf4fb675d4e5280a4a09b7d0d4dac881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:26 +0000

    Add package digitalguide/0.0.292/digitalguide-0.0.292.tar.gz

---
+++ packages/digitalguide/digitalguide-0.0.292.tar.gz/digitalguide-0.0.292/digitalguide/whatsapp/special_actions/listenfrageActions.py
@@ -0,0 +1,46 @@
+from digitalguide.whatsapp.WhatsAppUpdate import WhatsAppUpdate
+
+def whatsapp_loop_list(client, update: WhatsAppUpdate, context,  key, value, doppelte_antwort):
+    if not key in context:
+        context[key] = []
+
+    if value in context[key]:
+        client.messages.create(
+            body=doppelte_antwort,
+            from_=update.To,
+            to=update.From
+        )
+        return "{}_FRAGE".format(key.upper())
+    else:
+        context[key].append(value)
+
+def whatsapp_loop_list_fertig(client, update: WhatsAppUpdate, context, key, answer_id_list, fertig_antwort):
+    if set(answer_id_list).issubset(set(context[key])):
+        client.messages.create(
+            body=fertig_antwort,
+            from_=update.To,
+            to=update.From
+        )
+
+def whatsapp_eval_list(client, update: WhatsAppUpdate, context, answer_id_name_list, poi, response_text):
+    if not poi in context:
+        context[poi] = []
+
+    response_text += "\n"
+
+    for id, name in answer_id_name_list:
+        if id in context[poi]:
+            response_text += "✅ {}\n".format(name)
+        else:
+            response_text += "◽ {}\n".format(name)
+
+    client.messages.create(
+        body=response_text,
+        from_=update.To,
+        to=update.From
+    )
+
+whatsapp_action_functions = {"loop_list": whatsapp_loop_list,
+                             "eval_list": whatsapp_eval_list,
+                             "loop_list_fertig": whatsapp_loop_list_fertig
+                             }
