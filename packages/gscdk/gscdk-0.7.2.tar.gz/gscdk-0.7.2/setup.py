97b7345c342d684ea2fcaef18883295c287d1e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:54 +0000

    Add package gscdk/0.7.2/gscdk-0.7.2.tar.gz

---
+++ packages/gscdk/gscdk-0.7.2.tar.gz/gscdk-0.7.2/setup.py
@@ -0,0 +1,42 @@
+
+import os
+import shutil
+import setuptools
+# from skbuild import setup
+from distutils.core import setup
+
+from distutils.sysconfig import get_python_lib
+import glob
+
+# if os.path.exists('pysrc/tinygo'):
+#     shutil.rmtree('pysrc/tinygo')
+# shutil.copytree('tinygo/build/release/tinygo', 'pysrc/tinygo')
+
+release_files = []
+for root, dirs, files in os.walk("pysrc/tinygo"):
+    for f in files:
+        release_files.append(os.path.join(root.replace('pysrc/', ''), f))    
+
+for root, dirs, files in os.walk("pysrc/templates"):
+    for f in files:
+        release_files.append(os.path.join(root.replace('pysrc/', ''), f))
+
+setup(
+    name="gscdk",
+    version="0.7.2",
+    description="Go Smart Contract Development Kit",
+    author='The UUOSIO Team',
+    license="BSD-3-Clause",
+    url="https://github.com/uuosio/uuosio.gscdk",
+    packages=['gscdk'],
+    package_dir={'gscdk': 'pysrc'},
+    package_data={
+#        "": ["*"],
+        'gscdk': release_files,
+    },
+    setup_requires=['wheel']
+    # scripts=['compiler/build/release/tinygo/bin/eosio-go'],
+    # install_requires=[
+    # ],
+    # include_package_data=True
+)
