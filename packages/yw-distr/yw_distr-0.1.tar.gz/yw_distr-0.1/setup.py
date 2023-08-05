d0435cb67e7b5b11242bd7b89465db3c43857598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:43 +0000

    Add package yw-distr/0.1/yw_distr-0.1.tar.gz

---
+++ packages/yw-distr/yw_distr-0.1.tar.gz/yw_distr-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='yw_distr',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      author = 'Youcheng Wang', 
+      packages=['yw_distr'],
+      zip_safe=False)
