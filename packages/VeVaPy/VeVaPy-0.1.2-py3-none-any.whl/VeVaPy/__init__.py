f2e06fc8a8c831216dd4a3f45a9a2a452de82ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:32 +0000

    Add package VeVaPy/0.1.2/VeVaPy-0.1.2-py3-none-any.whl

---
+++ packages/VeVaPy/VeVaPy-0.1.2-py3-none-any.whl/VeVaPy/__init__.py
@@ -0,0 +1,6 @@
+# File Name: __init__.py
+# Description: Initialization for VeVaPy, leaving blank as it isn't really that
+#  useful#
+# Author: Christopher Parker
+# Created: Mon Jan 24, 2022 | 03:38P EST
+# Last Modified: Mon Sep 12, 2022 | 04:58P EDT
