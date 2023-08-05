b871e7bdb8e02823d228ad8765565bdb59d7abca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:57 +0000

    Add package aga/0.9.1/aga-0.9.1-py3-none-any.whl

---
+++ packages/aga/aga-0.9.1-py3-none-any.whl/aga/checks.py
@@ -0,0 +1,22 @@
+"""Additional checks and filters for problems."""
+from functools import wraps
+from io import StringIO
+from typing import Any, Callable, TypeVar
+from unittest.mock import patch
+
+Output = TypeVar("Output")
+
+
+def with_captured_stdout(
+    func: Callable[..., Output]
+) -> Callable[..., tuple[str, Output]]:
+    """Run func, returning its stdout and normal return value."""
+
+    @wraps(func)
+    def inner(*args: Any, **kwargs: Any) -> tuple[str, Any]:
+        with patch("sys.stdout", new_callable=StringIO) as stdout:
+            func_out = func(*args, **kwargs)
+
+        return stdout.getvalue(), func_out
+
+    return inner
