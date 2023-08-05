235fe25787b8f0d5bfbe5778c4051e327c79fbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package FastCopyFast/0.1/FastCopyFast-0.1.tar.gz

---
+++ packages/FastCopyFast/FastCopyFast-0.1.tar.gz/FastCopyFast-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(r"C:\Users\Gamer\anaconda3\envs\dfdir\_tmp_PyGitUpload_000002\README.md", encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1'
+DESCRIPTION = "3 functions to copy faster"
+
+# Setting up
+setup(
+    name="FastCopyFast",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/FastCopyFast',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=[],
+    keywords=['copy', 'file copy', 'copy faster'],
+    classifiers=['Topic :: System', 'Topic :: System :: Archiving', 'Topic :: System :: Archiving :: Backup', 'Topic :: System :: Archiving :: Compression', 'Topic :: System :: Archiving :: Mirroring', 'Topic :: System :: Archiving :: Packaging', 'Topic :: System :: Filesystems'],
+    install_requires=[],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
