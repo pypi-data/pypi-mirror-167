4348087bab4a8c4473503e547f7a520eee9c0ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:01 +0000

    Add package jsl-tmp/4.0.39/jsl_tmp-4.0.39-py3-none-any.whl

---
+++ packages/jsl-tmp/jsl_tmp-4.0.39-py3-none-any.whl/johnsnowlabs/nlp.py
@@ -0,0 +1,32 @@
+from johnsnowlabs.abstract_base.lib_resolver import try_import_lib
+
+if try_import_lib('sparknlp'):
+    from sparknlp.base import *
+    from sparknlp.annotator import *
+    import sparknlp
+    from sparknlp.pretrained import ResourceDownloader
+    from sparknlp.training import CoNLL
+    from sparknlp.functions import *
+    from sparknlp.pretrained import PretrainedPipeline
+
+if try_import_lib('pyspark'):
+    from pyspark.sql import DataFrame
+    import pyspark.sql.functions as F
+    import pyspark.sql.types as T
+    import pyspark.sql as SQL
+
+    from pyspark.sql import SparkSession
+    from pyspark.ml import Pipeline, PipelineModel
+
+if try_import_lib('warnings'):
+    import warnings
+    warnings.filterwarnings('ignore')
+
+
+
+if try_import_lib('nlu'):
+    from nlu import load, to_nlu_pipe, autocomplete_pipeline, to_pretty_df
+    import nlu as nlu
+
+
+
