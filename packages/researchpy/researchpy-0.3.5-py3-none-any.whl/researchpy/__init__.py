a8ee86c26bf6fb3f60cbf142839c03be2312907f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:31 +0000

    Add package researchpy/0.3.5/researchpy-0.3.5-py3-none-any.whl

---
+++ packages/researchpy/researchpy-0.3.5-py3-none-any.whl/researchpy/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Corey Bryant
+
+Last udpated: 03/25/2022
+
+"""
+
+from .ttest import ttest
+from .summary import *
+from .correlation import *
+from .crosstab import *
+from .difference_test import *
+from .basic_stats import *
+from .utility import *
+from .model import *
+from .signrank import *
+from .predict import *
+from .anova import *
+from .ols import *
+
