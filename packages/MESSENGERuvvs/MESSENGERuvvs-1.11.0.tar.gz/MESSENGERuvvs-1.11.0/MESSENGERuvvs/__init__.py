da40e1e8c0bde1b080f556984cbc8b9c53564981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:39 +0000

    Add package MESSENGERuvvs/1.11.0/MESSENGERuvvs-1.11.0.tar.gz

---
+++ packages/MESSENGERuvvs/MESSENGERuvvs-1.11.0.tar.gz/MESSENGERuvvs-1.11.0/MESSENGERuvvs/__init__.py
@@ -0,0 +1,10 @@
+from MESSENGERuvvs.MESSENGERdata import MESSENGERdata
+from MESSENGERuvvs.MESSENGERviewer import MESSENGERviewer
+from .initialize_MESSENGERdata import initialize_MESSENGERdata
+
+
+name = 'MESSENGERuvvs'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.11.0'
+__date__ = '2022-09-12'
