2f720412a5b0e9ca11e67ac70058fb3c9162983f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:20 +0000

    Add package Paperl/0.0.1a1/Paperl-0.0.1a1-py3-none-any.whl

---
+++ packages/Paperl/Paperl-0.0.1a1-py3-none-any.whl/Paperl/Paperui/Widgets/__init__.py
@@ -0,0 +1,6 @@
+from Paperl.Paperui.Widgets.window import *
+from Paperl.Paperui.Widgets.eventHandle import *
+from Paperl.Paperui.Widgets.windowDevelop import *
+from Paperl.Paperui.Widgets.widget import *
+from Paperl.Paperui.Widgets.button import *
+from Paperl.Paperui.Widgets.constant import *
