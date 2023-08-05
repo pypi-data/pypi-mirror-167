b0acbac4f0eaf19809489b9917c6a6dbb59d9b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:10 +0000

    Add package pythonsweeper/0.1.1/pythonsweeper-0.1.1.tar.gz

---
+++ packages/pythonsweeper/pythonsweeper-0.1.1.tar.gz/pythonsweeper-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pythonsweeper',
+    version='0.1.1',
+    license='MIT',
+    author="Boris Khesin",
+    author_email='rob.falls.do@gmail.com',
+    description='A Python implementation of the classic Minesweeper game',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Shagulka/pysweeper',
+    keywords='minesweeper',
+    install_requires=[
+          'enum',
+          'random'
+      ],
+)
