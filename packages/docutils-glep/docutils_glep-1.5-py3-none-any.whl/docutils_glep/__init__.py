a6a16a2791241b869111d35f459f8a5c244181ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:59 +0000

    Add package docutils-glep/1.5/docutils_glep-1.5-py3-none-any.whl

---
+++ packages/docutils-glep/docutils_glep-1.5-py3-none-any.whl/docutils_glep/__init__.py
@@ -0,0 +1,7 @@
+"""docutils modules & wrapper to process Gentoo Linux Enhancement Proposals"""
+
+from docutils_glep.reader import Reader
+from docutils_glep.html_writer import Writer
+
+
+__version__ = "1.5"
