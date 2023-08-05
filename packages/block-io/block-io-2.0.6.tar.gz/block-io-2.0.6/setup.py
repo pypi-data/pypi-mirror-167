a60e7b5244fc75a484718dfc11f19ab5dd74de24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:14 +0000

    Add package block-io/2.0.6/block-io-2.0.6.tar.gz

---
+++ packages/block-io/block-io-2.0.6.tar.gz/block-io-2.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(name='block-io',
+      version='2.0.6',
+      description='The easiest way to integrate Bitcoin, Dogecoin and Litecoin in your applications. Sign up at Block.io for your API key.',
+      url='https://github.com/BlockIo/block_io-python',
+      author='Atif Nazir',
+      author_email='a@block.io',
+      license='MIT',
+      packages=['block_io'],
+      python_requires='>=3.6, <4',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      install_requires=[
+          'requests>=2.25.0,<3.0',
+          'pycryptodome>=3.9.9,<4.0',
+          'base58>=2.1,<2.2',
+          'bitcoin-utils-fork-minimal==0.4.11.6'
+      ],
+      zip_safe=False)
