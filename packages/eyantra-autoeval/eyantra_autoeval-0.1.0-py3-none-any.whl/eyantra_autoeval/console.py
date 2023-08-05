53683e207df29db95963ea2410554222e9232e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:46 +0000

    Add package eyantra-autoeval/0.1.0/eyantra_autoeval-0.1.0-py3-none-any.whl

---
+++ packages/eyantra-autoeval/eyantra_autoeval-0.1.0-py3-none-any.whl/eyantra_autoeval/console.py
@@ -0,0 +1,32 @@
+# standard imports
+import importlib
+
+# third-party imprts
+import click
+from rich import print
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option("--year", required=True, help="Year.")
+@click.option("--theme", required=True, help="Theme Initials.")
+@click.option("--task", required=True, help="Task Number.")
+def dryrun(year, theme, task):
+    """Dry run evaluation for a task."""
+    print(
+        f"Running evaluation scripts for [bold blue]{year}[/bold blue] "
+        f"theme [bold blue]{theme}[/bold blue] for "
+        f"task [bold blue]{task}[/bold blue]"
+    )
+    evaluator = importlib.import_module(
+        f"eyantra_autoeval.year.y{year}.{theme}.task{task}.evaluator"
+    )
+    evaluator.dryrun()
+
+
+cli.add_command(dryrun)
+
---
+++ packages/eyantra-autoeval/eyantra_autoeval-0.1.0-py3-none-any.whl/eyantra_autoeval/year/y2022/SD/task0/evaluator.py
@@ -0,0 +1,95 @@
+# standard imports
+import os
+import platform
+import shlex
+import subprocess
+import sys
+
+# third-party imprts
+import distro
+from rich import print
+
+
+def is_docker():
+    path = "/proc/self/cgroup"
+    return (
+        os.path.exists("/.dockerenv")
+        or os.path.isfile(path)
+        and any("docker" in line for line in open(path))
+    )
+
+
+def run_shell_command(command):
+    try:
+        return (
+            subprocess.check_output(shlex.split(command)).decode().strip()
+        )  # nosec B603
+    except Exception as err:
+        print(err)
+        return ""
+
+
+def dryrun():
+    result = {}
+
+    distribution = {
+        "machine": platform.machine(),
+        "release": platform.release(),
+        "system": platform.system(),
+        "version": distro.version(best=True),
+        "name": distro.name(),
+    }
+    result["distro"] = distribution
+
+    result["virtualized"] = "hypervisor" in run_shell_command("cat /proc/cpuinfo")
+
+    result["dockerized"] = is_docker()
+
+    result["apt"] = {}
+    result["apt"]["packages"] = {}
+    packages = (
+        subprocess.run(
+            "apt list --installed | awk '{print $1 $2}' | awk -F'\/now' '{print $1 \" \" $2}'",
+            capture_output=True,
+            shell=True,
+        )
+        .stdout.decode()
+        .split("\n")
+    )
+    packages = packages[1:-1]
+    packages = [p.split() for p in packages]
+    result["apt"]["packages"]["installed"] = packages
+
+    packages = run_shell_command("pip3 freeze").split("\n")
+    packages = [p.split("==") for p in packages]
+
+    result["pip3"] = {}
+    result["pip3"]["packages"] = {}
+    result["pip3"]["packages"]["installed"] = packages
+
+    result["ros"] = {}
+    result["ros"]["version"] = run_shell_command("rosversion -d")
+    result["ros"]["packages"] = {}
+    packages = run_shell_command("rosversion -a").split("\n")
+    packages = [p.split(": ") for p in packages]
+    result["ros"]["packages"]["installed"] = run_shell_command("rosversion -a").split(
+        "\n"
+    )
+
+    result["gazebo"] = {}
+    result["gazebo"]["version"] = subprocess.run(
+        "gazebo --version | grep 'Gazebo' | awk '{print $NF}'",
+        capture_output=True,
+        shell=True,
+    ).stdout.decode()
+
+    result["python"] = {}
+    result["python"]["version"] = {}
+    result["python"]["version"]["major"] = sys.version_info.major
+    result["python"]["version"]["minor"] = sys.version_info.minor
+    result["python"]["version"]["micro"] = sys.version_info.micro
+
+    result["gdal"] = {}
+    result["gdal"]["version"] = run_shell_command("gdalinfo --version")
+
+    print(result)
