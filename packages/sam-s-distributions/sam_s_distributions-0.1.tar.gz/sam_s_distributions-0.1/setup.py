2098c6aa2ba8808cd729f2338b0011796b2bd14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:27 +0000

    Add package sam-s-distributions/0.1/sam_s_distributions-0.1.tar.gz

---
+++ packages/sam-s-distributions/sam_s_distributions-0.1.tar.gz/sam_s_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sam_s_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
