2f908c15752a7798ff070b0d9699beff62339699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:47 +0000

    Add package leprikon/2.39.0/leprikon-2.39.0.tar.gz

---
+++ packages/leprikon/leprikon-2.39.0.tar.gz/leprikon-2.39.0/leprikon/api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+
+def l_url(pattern, name):
+    return url(pattern, getattr(views, name), name=name)
+
+
+urlpatterns = [
+    l_url(r"^participants/(?P<journal_id>[0-9]+)/$", "participants"),
+]
