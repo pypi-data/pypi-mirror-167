2662d473a0aa40e1cac7bd3aec04098c0e1b2c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:48 +0000

    Add package OpenFisca-Tools/0.13.8/OpenFisca-Tools-0.13.8.tar.gz

---
+++ packages/OpenFisca-Tools/OpenFisca-Tools-0.13.8.tar.gz/OpenFisca-Tools-0.13.8/README.md
@@ -0,0 +1,3 @@
+# openfisca-tools
+
+This package contains tools used by OpenFisca country packages (primarily -UK and -US), aiding with microsimulation handling, parameter tests and hypothetical situation input.
---
+++ packages/OpenFisca-Tools/OpenFisca-Tools-0.13.8.tar.gz/OpenFisca-Tools-0.13.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="OpenFisca-Tools",
+    version="0.13.8",
+    author="PolicyEngine",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    url="https://github.com/policyengine/openfisca-tools",
+    install_requires=[
+        "OpenFisca-Core",
+        "microdf_python",
+        "numpy",
+        "pandas",
+        "wheel",
+        "h5py",
+        "tables",
+        "google-cloud-storage",
+    ],
+    extras_require={
+        "test": [
+            "OpenFisca-US>=0.35.0",
+            "OpenFisca-UK>=0.12.0",
+        ]
+    },
+    packages=find_packages(),
+)
