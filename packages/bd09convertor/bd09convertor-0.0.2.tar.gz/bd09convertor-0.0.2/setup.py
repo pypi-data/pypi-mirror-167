649a6818f3a28a6aee7b54078693ffb0fe0c8485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:56 +0000

    Add package bd09convertor/0.0.2/bd09convertor-0.0.2.tar.gz

---
+++ packages/bd09convertor/bd09convertor-0.0.2.tar.gz/bd09convertor-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bd09convertor",
+    version="0.0.2",
+    author="Piao Yang",
+    author_email="495384481@qq.com",
+    description="BD09 coordinates conversion tool for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/46319943/BD09Convertor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+
+    install_requires=[],
+)
