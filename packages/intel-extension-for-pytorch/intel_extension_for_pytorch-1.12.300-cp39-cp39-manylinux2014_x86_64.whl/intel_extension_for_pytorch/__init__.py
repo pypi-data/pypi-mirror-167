c84b41a97c05c882070948d6a5b16773eed828be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:58 +0000

    Add package intel-extension-for-pytorch/1.12.300/intel_extension_for_pytorch-1.12.300-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/intel-extension-for-pytorch/intel_extension_for_pytorch-1.12.300-cp39-cp39-manylinux2014_x86_64.whl/intel_extension_for_pytorch/__init__.py
@@ -0,0 +1,32 @@
+import torch
+try:
+    import torchvision
+except ImportError:
+    pass  # skip if torchvision is not available
+
+from .version import __version__
+from .utils import _cpu_isa, _custom_fx_tracer
+_cpu_isa.check_minimal_isa_support()
+
+torch_version = ''
+ipex_version = ''
+import re
+matches = re.match('(\d+\.\d+).*', torch.__version__)
+if matches and len(matches.groups()) == 1:
+  torch_version = matches.group(1)
+matches = re.match('(\d+\.\d+).*', __version__)
+if matches and len(matches.groups()) == 1:
+  ipex_version = matches.group(1)
+if torch_version == '' or ipex_version == '' or torch_version != ipex_version:
+  print('ERROR! Intel® Extension for PyTorch* needs to work with PyTorch {0}.*, but PyTorch {1} is found. Please switch to the matching version and run again.'.format(ipex_version, torch.__version__))
+  exit(127)
+
+from . import cpu
+from . import quantization
+from . import nn
+from . import jit
+from . import profiler
+from . import autocast
+
+from .utils.verbose import verbose
+from .frontend import optimize, enable_onednn_fusion, set_fp32_math_mode, get_fp32_math_mode, FP32MathMode, _set_blas_backend, _is_mkl_blas_backend, _is_dnnl_blas_backend
