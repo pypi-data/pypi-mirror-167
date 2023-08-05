f87b7f8c6ee22c191f25d0c5d0042fe026b8d0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:05 +0000

    Add package oscillator-gym/1.1.1/oscillator-gym-1.1.1.tar.gz

---
+++ packages/oscillator-gym/oscillator-gym-1.1.1.tar.gz/oscillator-gym-1.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['oscillator']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gym>=0.22,<1.0', 'numpy', 'pygame']
+
+setup_kwargs = {
+    'name': 'oscillator-gym',
+    'version': '1.1.1',
+    'description': 'A flexible harmonic oscillator environment for OpenAI Gym',
+    'long_description': None,
+    'author': 'Onno Eberhard',
+    'author_email': 'onnoeberhard@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
