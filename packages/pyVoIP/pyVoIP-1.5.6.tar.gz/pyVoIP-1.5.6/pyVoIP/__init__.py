5edf4537490bbdfe11f981cdf23dbc0f82034a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:47 +0000

    Add package pyVoIP/1.5.6/pyVoIP-1.5.6.tar.gz

---
+++ packages/pyVoIP/pyVoIP-1.5.6.tar.gz/pyVoIP-1.5.6/pyVoIP/__init__.py
@@ -0,0 +1,23 @@
+
+__all__ = ['SIP', 'RTP', 'VoIP']
+
+version_info = (1, 5, 6)
+
+__version__ = ".".join([str(x) for x in version_info])
+
+DEBUG = False
+
+def debug(s, e=None):
+  if DEBUG:
+    print(s)
+  elif e is not None:
+    print(e)
+
+from pyVoIP.RTP import PayloadType
+
+SIPCompatibleMethods = ['INVITE', 'ACK', 'BYE']
+SIPCompatibleVersions = ['SIP/2.0']
+
+RTPCompatibleVersions = [2]
+RTPCompatibleCodecs = [PayloadType.PCMU, PayloadType.PCMA, PayloadType.EVENT]
+
