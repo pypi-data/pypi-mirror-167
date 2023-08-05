d93826cb24ef31e050f5332303546ab9309c085b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:51 +0000

    Add package psyki/0.2.1/psyki-0.2.1-py3-none-any.whl

---
+++ packages/psyki/psyki-0.2.1-py3-none-any.whl/psyki/ski/__init__.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Any, List
+from tensorflow.keras import Model
+from psyki.logic import Formula
+
+
+class Injector(ABC):
+    """
+    An injector allows a sub-symbolic predictor to exploit prior symbolic knowledge.
+    The knowledge is provided via logic rules representation.
+    Usually, after the injection, the predictor must be trained like in a standard ML workflow.
+    """
+    _predictor: Any  # Any class that has methods fit and predict
+
+    @abstractmethod
+    def inject(self, rules: List[Formula]) -> Any:
+        """
+        @param rules: list of logic rules that represents the prior knowledge to be injected.
+        """
+        pass
+
+    @staticmethod
+    def kill(model: Model,
+             class_mapping: dict[str, int],
+             feature_mapping: dict[str, int],
+             fuzzifier: str = 'lukasiewicz') -> Injector:
+        from psyki.ski.kill import LambdaLayer
+        return LambdaLayer(model, class_mapping, feature_mapping, fuzzifier)
+
+    @staticmethod
+    def kins(model: Model,
+             feature_mapping: dict[str, int],
+             fuzzifier: str = 'netbuilder',
+             injection_layer: int = 0) -> Injector:
+        from psyki.ski.kins import NetworkStructurer
+        return NetworkStructurer(model, feature_mapping, fuzzifier, injection_layer)
