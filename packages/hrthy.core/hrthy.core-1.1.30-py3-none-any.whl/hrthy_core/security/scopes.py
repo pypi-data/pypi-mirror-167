c5d3003b34e2bd2f1298541e59e68af993691e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:52 +0000

    Add package hrthy.core/1.1.30/hrthy.core-1.1.30-py3-none-any.whl

---
+++ packages/hrthy.core/hrthy.core-1.1.30-py3-none-any.whl/hrthy_core/security/scopes.py
@@ -0,0 +1,79 @@
+SCOPE_REFRESH_TOKEN = 'refresh.token'  # System
+SCOPE_COMPANY_LIST = 'company.list'  # Superuser
+SCOPE_COMPANY_CREATE = 'company.create'  # Superuser
+SCOPE_COMPANY_UPDATE = 'company.update'  # Superuser
+SCOPE_COMPANY_DELETE = 'company.delete'  # Superuser
+SCOPE_COMPANY_RESTORE = 'company.restore'  # Superuser
+SCOPE_COMPANY_MY_UPDATE = 'company.my.update'  # Admin
+SCOPE_COMPANY_MY_DELETE = 'company.my.delete'  # Admin
+SCOPE_COMPANY_MY_RESTORE = 'company.my.restore'  # Admin
+SCOPE_COMPANY_MYSELF_GET = 'company.myself.get'  # Myself
+
+SCOPE_USER_LIST = 'user.list'  # Superuser
+SCOPE_USER_CREATE = 'user.create'  # Superuser
+SCOPE_USER_UPDATE = 'user.update'  # Superuser
+SCOPE_USER_DELETE = 'user.delete'  # Superuser
+SCOPE_USER_MY_LIST = 'user.my.list'  # Admin
+SCOPE_USER_MY_CREATE = 'user.my.create'  # Admin
+SCOPE_USER_MY_UPDATE = 'user.my.update'  # Admin
+SCOPE_USER_MY_DELETE = 'user.my.delete'  # Admin
+SCOPE_USER_MYSELF_GET = 'user.myself.get'  # Myself
+SCOPE_USER_MYSELF_UPDATE = 'user.myself.update'  # Myself
+SCOPE_USER_MYSELF_DELETE = 'user.myself.delete'  # Myself
+
+SCOPE_ROLE_LIST = 'role.list'  # Superuser
+SCOPE_ROLE_CREATE = 'role.create'  # Superuser
+SCOPE_ROLE_UPDATE = 'role.update'  # Superuser
+SCOPE_ROLE_DELETE = 'role.delete'  # Superuser
+SCOPE_ROLE_MY_ASSIGN_GLOBAL = 'role.my.assign.global'  # Superuser
+SCOPE_ROLE_MY_LIST = 'role.my.list'  # Admin
+SCOPE_ROLE_MY_CREATE = 'role.my.create'  # Admin
+SCOPE_ROLE_MY_UPDATE = 'role.my.update'  # Admin
+SCOPE_ROLE_MY_DELETE = 'role.my.delete'  # Admin
+SCOPE_ROLE_MYSELF_GET = 'role.myself.get'  # Myself
+
+SCOPE_CANDIDATE_LIST = 'candidate.list'  # Superuser
+SCOPE_CANDIDATE_CREATE = 'candidate.create'  # Superuser
+SCOPE_CANDIDATE_UPDATE = 'candidate.update'  # Superuser
+SCOPE_CANDIDATE_DELETE = 'candidate.delete'  # Superuser
+SCOPE_CANDIDATE_MY_LIST = 'candidate.my.list'  # Admin, User
+SCOPE_CANDIDATE_MY_CREATE = 'candidate.my.create'  # Admin, User
+SCOPE_CANDIDATE_MY_UPDATE = 'candidate.my.update'  # Admin, User
+SCOPE_CANDIDATE_MY_DELETE = 'candidate.my.delete'  # Admin, User
+SCOPE_CANDIDATE_MYSELF_GET = 'candidate.myself.get'  # Myself
+SCOPE_CANDIDATE_MYSELF_UPDATE = 'candidate.myself.update'  # Myself
+
+SCOPE_CANDIDATE_ADDITIONAL_FIELDS_LIST = 'candidate.additional_fields.list'  # Superuser
+SCOPE_CANDIDATE_ADDITIONAL_FIELDS_PROTECTED_LIST = 'candidate.additional_fields.protected.list'  # Superuser
+SCOPE_CANDIDATE_ADDITIONAL_FIELDS_UPDATE = 'candidate.additional_fields.update'  # Superuser
+SCOPE_CANDIDATE_ADDITIONAL_FIELDS_PROTECTED_UPDATE = 'candidate.additional_fields.protected.update'  # Superuser
+SCOPE_CANDIDATE_ADDITIONAL_FIELDS_MY_LIST = 'candidate.additional_fields.my.list'  # Admin, User
+SCOPE_CANDIDATE_ADDITIONAL_FIELDS_PROTECTED_MY_LIST = 'candidate.additional_fields.protected.my.list'  # Admin, User
+SCOPE_CANDIDATE_ADDITIONAL_FIELDS_MY_UPDATE = 'candidate.additional_fields.my.update'  # Admin
+SCOPE_CANDIDATE_ADDITIONAL_FIELDS_PROTECTED_MY_UPDATE = 'candidate.additional_fields.protected.my.update'  # Admin
+
+SCOPE_PIPELINE_LIST = 'pipeline.list'  # Superuser
+SCOPE_PIPELINE_CREATE = 'pipeline.create'  # Superuser
+SCOPE_PIPELINE_UPDATE = 'pipeline.update'  # Superuser
+SCOPE_PIPELINE_DELETE = 'pipeline.delete'  # Superuser
+SCOPE_PIPELINE_MY_LIST = 'pipeline.my.list'  # Admin, User
+SCOPE_PIPELINE_MY_CREATE = 'pipeline.my.create'  # Admin, User
+SCOPE_PIPELINE_MY_UPDATE = 'pipeline.my.update'  # Admin, User
+SCOPE_PIPELINE_MY_DELETE = 'pipeline.my.delete'  # Admin, User
+SCOPE_PIPELINE_MYSELF_GET = 'pipeline.myself.get'  # Myself
+
+SCOPE_PIPELINE_CANDIDATE_ASSIGN = 'pipeline.candidate.assign'  # Superuser
+SCOPE_PIPELINE_CANDIDATE_UNASSIGN = 'pipeline.candidate.unassign'  # Superuser
+SCOPE_PIPELINE_CANDIDATE_MY_ASSIGN = 'pipeline.candidate.my.assign'  # Admin, User
+SCOPE_PIPELINE_CANDIDATE_MY_UNASSIGN = 'pipeline.candidate.my.unassign'  # Admin, User
+
+SCOPE_PIPELINE_ADDITIONAL_FIELDS_LIST = 'pipeline.additional_fields.list'  # Superuser
+SCOPE_PIPELINE_ADDITIONAL_FIELDS_UPDATE = 'pipeline.additional_fields.update'  # Superuser
+SCOPE_PIPELINE_ADDITIONAL_FIELDS_MY_LIST = 'pipeline.additional_fields.my.list'  # Admin, User
+SCOPE_PIPELINE_ADDITIONAL_FIELDS_MY_UPDATE = 'pipeline.additional_fields.my.update'  # Admin, User
+
+SCOPE_LICENSE_POOL_LIST = 'license_pool.list'  # Superuser
+SCOPE_LICENSE_POOL_CREATE = 'license_pool.create'  # Superuser
+SCOPE_LICENSE_POOL_UPDATE = 'license_pool.update'  # Superuser
+SCOPE_LICENSE_POOL_DELETE = 'license_pool.delete'  # Superuser
+SCOPE_LICENSE_POOL_MY_LIST = 'license_pool.my.list'  # Admin, User
---
+++ packages/hrthy.core/hrthy.core-1.1.30-py3-none-any.whl/hrthy_core/security/types.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class ScopeType(Enum):
+    GLOBAL = 'global'
+    COMPANY = 'company'
