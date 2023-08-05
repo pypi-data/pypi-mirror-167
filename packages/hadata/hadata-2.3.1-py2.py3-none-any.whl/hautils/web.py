db5a9abd5214da1a45b773d20310b0a330a4effb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:10 +0000

    Add package hadata/2.3.1/hadata-2.3.1-py2.py3-none-any.whl

---
+++ packages/hadata/hadata-2.3.1-py2.py3-none-any.whl/hautils/web.py
@@ -0,0 +1,74 @@
+import traceback
+
+from fastapi.responses import Response
+from hautils.logger import logger
+import json
+from mongoengine import Document, QuerySet, DateTimeField
+from pydantic import BaseModel
+from fastapi import status
+
+
+def mongo_to_dict(content):
+    if not issubclass(type(content), Document) and not issubclass(type(content), dict) and not issubclass(type(content),
+                                                                                                          BaseModel):
+        logger.warn("unsupported object type %s " % (type(content)))
+        raise Exception
+    if issubclass(type(content), Document):
+        response_object = json.loads(content.to_json())
+        response_object["id"] = str(content.id)
+        response_object.pop("_id")
+        for field in type(content)._fields.values():
+            if isinstance(field, DateTimeField):
+                response_object[field.name] = int(content[field.name].timestamp())
+    elif issubclass(type(content), BaseModel):
+        response_object = content.dict()
+    else:
+        logger.debug("format type %s" % (type(content)))
+        response_object = content
+
+    return response_object
+
+
+def json_response(content, http_status=200, pop_fields=list()):
+    try:
+        if issubclass(type(content), QuerySet) or issubclass(type(content), list):
+            response = []
+            for doc in content:
+                response.append(doc_cleanup(doc, pop_fields))
+        else:
+            response = doc_cleanup(content, pop_fields)
+        logger.debug("json dumping type %s" % (type(response)))
+        response = json.dumps(response)
+        logger.info("json encode %s" % (response,))
+        return Response(content=response, status_code=http_status, media_type="application/json")
+    except Exception as e:
+        exception_log(e)
+
+
+def doc_cleanup(doc, pop_fields):
+    response = mongo_to_dict(doc)
+    for field in pop_fields:
+        try:
+            response.pop(field)
+        except Exception as e:
+            logger.error("field %s not in dictionary" % (field,))
+    return response
+
+
+def mongo_to_log(content):
+    try:
+        return json.dumps(mongo_to_dict(content))
+    except Exception as e:
+        return ""
+
+
+def exception_log(e):
+    logger.error(e)
+    logger.debug(traceback.format_exception(type(e), e, e.__traceback__))
+    raise ProcessException(http_status=status.HTTP_500_INTERNAL_SERVER_ERROR, errors=type(e))
+
+
+class ProcessException(Exception):
+    def __init__(self, errors=None, data={}, http_status=status.HTTP_500_INTERNAL_SERVER_ERROR):
+        self.errors = errors
+        self.status = http_status
