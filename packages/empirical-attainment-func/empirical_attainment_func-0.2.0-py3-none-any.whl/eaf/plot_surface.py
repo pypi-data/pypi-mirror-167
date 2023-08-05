7862b06339d02e9648097b620849df14bf695f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:04 +0000

    Add package empirical-attainment-func/0.2.0/empirical_attainment_func-0.2.0-py3-none-any.whl

---
+++ packages/empirical-attainment-func/empirical_attainment_func-0.2.0-py3-none-any.whl/eaf/plot_surface.py
@@ -0,0 +1,241 @@
+from typing import Any, List, Optional, Union
+
+from eaf.utils import (
+    LOGEPS,
+    _change_scale,
+    _check_surface,
+    _get_slighly_expanded_value_range,
+    _step_direction,
+    pareto_front_to_surface,
+)
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+class EmpiricalAttainmentFuncPlot:
+    """
+    The class to plot empirical attainment function.
+
+    Args:
+        true_pareto_sols (Optional[np.ndarray]):
+            The true Pareto solutions if available.
+            It is used to compute the best values of each objective.
+        larger_is_better_objectives (Optional[List[int]]):
+            The indices of the objectives that are better when the values are larger.
+            If None, we consider all objectives are better when they are smaller.
+        log_scale (Optional[List[int]]):
+            The indices of the log scale.
+            For example, if you would like to plot the first objective in the log scale,
+            you need to feed log_scale=[0].
+            In principle, log_scale changes the minimum value of the axes
+            from -np.inf to a small positive value.
+        x_min, x_max, y_min, y_max (float):
+            The lower/upper bounds for each objective if available.
+            It is used to fix the value ranges of each objective in plots.
+    """
+
+    def __init__(
+        self,
+        true_pareto_sols: Optional[np.ndarray] = None,
+        larger_is_better_objectives: Optional[List[int]] = None,
+        log_scale: Optional[List[int]] = None,
+        x_min: float = np.inf,
+        x_max: float = -np.inf,
+        y_min: float = np.inf,
+        y_max: float = -np.inf,
+    ):
+        self.step_dir = _step_direction(larger_is_better_objectives)
+        self.larger_is_better_objectives = (
+            larger_is_better_objectives if larger_is_better_objectives is not None else []
+        )
+        self.log_scale = log_scale if log_scale is not None else []
+        self.x_is_log, self.y_is_log = 0 in self.log_scale, 1 in self.log_scale
+        self._plot_kwargs = dict(
+            larger_is_better_objectives=larger_is_better_objectives,
+            log_scale=log_scale,
+        )
+
+        if true_pareto_sols is not None:
+            self.x_min, self.x_max, self.y_min, self.y_max = _get_slighly_expanded_value_range(true_pareto_sols)
+            self.true_pareto_surf = pareto_front_to_surface(
+                true_pareto_sols,
+                x_min=self.x_min,
+                x_max=self.x_max,
+                y_min=self.y_min,
+                y_max=self.y_max,
+                **self._plot_kwargs,
+            )
+        else:
+            # We cannot plot until we call _transform_surface_list
+            self.true_pareto_surf = None
+            self.x_min = max(LOGEPS, x_min) if 0 in self.log_scale else x_min
+            self.x_max = x_max
+            self.y_min = max(LOGEPS, y_min) if 1 in self.log_scale else y_min
+            self.y_max = y_max
+
+    def _transform_surface_list(self, surfs_list: List[np.ndarray]) -> List[np.ndarray]:
+        for surf in surfs_list:
+            x_min, x_max, y_min, y_max = _get_slighly_expanded_value_range(surf, self.log_scale)
+            self.x_min, self.x_max = min(self.x_min, x_min), max(self.x_max, x_max)
+            self.y_min, self.y_max = min(self.y_min, y_min), max(self.y_max, y_max)
+
+        for surf in surfs_list:
+            lb = LOGEPS if self.x_is_log else -np.inf
+            surf[..., 0][surf[..., 0] == lb] = self.x_min
+            surf[..., 0][surf[..., 0] == np.inf] = self.x_max
+
+            lb = LOGEPS if self.y_is_log else -np.inf
+            surf[..., 1][surf[..., 1] == lb] = self.y_min
+            surf[..., 1][surf[..., 1] == np.inf] = self.y_max
+
+        return surfs_list
+
+    def plot_surface(
+        self, ax: plt.Axes, surf: np.ndarray, color: str, label: str, transform: bool = True, **kwargs: Any
+    ) -> Any:
+        """
+        Plot multiple surfaces.
+
+        Args:
+            ax (plt.Axes):
+                The subplots axes.
+            surf (np.ndarray):
+                The vertices of the empirical attainment surface.
+                The shape must be (X.size, 2).
+            color (str):
+                The color of the plot
+            label (str):
+                The label of the plot.
+            kwargs:
+                The kwargs for scatter.
+        """
+        if len(surf.shape) != 2 or surf.shape[1] != 2:
+            raise ValueError(f"The shape of surf must be (n_points, 2), but got {surf.shape}")
+
+        if transform:
+            surf = self._transform_surface_list(surfs_list=[surf])[0]
+            ax.set_xlim(self.x_min, self.x_max)
+            ax.set_ylim(self.y_min, self.y_max)
+
+        kwargs.update(drawstyle=f"steps-{self.step_dir}")
+        _check_surface(surf)
+        X, Y = surf[:, 0], surf[:, 1]
+        line = ax.plot(X, Y, color=color, label=label, **kwargs)
+        _change_scale(ax, self.log_scale)
+        return line
+
+    def plot_multiple_surface(
+        self,
+        ax: plt.Axes,
+        surfs: Union[np.ndarray, List[np.ndarray]],
+        colors: List[str],
+        labels: List[str],
+        **kwargs: Any,
+    ) -> List[Any]:
+        """
+        Plot multiple surfaces.
+
+        Args:
+            ax (plt.Axes):
+                The subplots axes.
+            surfs (Union[np.ndarray, List[np.ndarray]]):
+                The vertices of the empirical attainment surfaces for each plot.
+                Each element should have the shape of (X.size, 2).
+                If this is an array, then the shape must be (n_surf, X.size, 2).
+            colors (List[str]):
+                The colors of each plot
+            labels (List[str]):
+                The labels of each plot.
+            kwargs:
+                The kwargs for scatter.
+        """
+        lines: List[Any] = []
+        surfs = self._transform_surface_list(surfs)
+        for surf, color, label in zip(surfs, colors, labels):
+            line = self.plot_surface(ax, surf, color, label, transform=False, **kwargs)
+            lines.append(line)
+
+        ax.set_xlim(self.x_min, self.x_max)
+        ax.set_ylim(self.y_min, self.y_max)
+        return lines
+
+    def plot_surface_with_band(
+        self,
+        ax: plt.Axes,
+        surfs: np.ndarray,
+        color: str,
+        label: str,
+        transform: bool = True,
+        **kwargs: Any,
+    ) -> Any:
+        """
+        Plot the surface with a band.
+        Typically, we would like to plot median with the band between
+        25% -- 75% percentile attainment surfaces.
+
+        Args:
+            ax (plt.Axes):
+                The subplots axes.
+            surfs (np.ndarray):
+                The vertices of the empirical attainment surfaces for each level.
+                If surf[i, j, 1] takes np.inf, this is not actually on the surface.
+                The shape is (3, X.size, 2).
+            colors (str):
+                The color of the plot
+            label (str):
+                The label of the plot.
+            kwargs:
+                The kwargs for scatter.
+        """
+        if surfs.shape[0] != 3:
+            raise ValueError(f"plot_surface_with_band requires three levels, but got only {surfs.shape[0]} levels")
+        if transform:
+            surfs = self._transform_surface_list(surfs_list=surfs)
+            ax.set_xlim(self.x_min, self.x_max)
+            ax.set_ylim(self.y_min, self.y_max)
+
+        for surf in surfs:
+            _check_surface(surf)
+
+        X = surfs[0, :, 0]
+        line = ax.plot(X, surfs[1, :, 1], color=color, label=label, drawstyle=f"steps-{self.step_dir}", **kwargs)
+        ax.fill_between(X, surfs[0, :, 1], surfs[2, :, 1], color=color, alpha=0.2, step=self.step_dir, **kwargs)
+        _change_scale(ax, self.log_scale)
+        return line
+
+    def plot_multiple_surface_with_band(
+        self,
+        ax: plt.Axes,
+        surfs_list: Union[np.ndarray, List[np.ndarray]],
+        colors: List[str],
+        labels: List[str],
+        **kwargs: Any,
+    ) -> List[Any]:
+        """
+        Plot multiple surfaces.
+
+        Args:
+            ax (plt.Axes):
+                The subplots axes.
+            surfs_list (Union[np.ndarray, List[np.ndarray]]):
+                The vertices of the empirical attainment surfaces for each plot.
+                Each element should have the shape of (3, X.size, 2).
+                If this is an array, then the shape must be (n_surf, 3, X.size, 2).
+            colors (List[str]):
+                The colors of each plot
+            labels (List[str]):
+                The labels of each plot.
+            kwargs:
+                The kwargs for scatter.
+        """
+        lines: List[Any] = []
+        surfs_list = self._transform_surface_list(surfs_list)
+        for surf, color, label in zip(surfs_list, colors, labels):
+            line = self.plot_surface_with_band(ax, surf, color, label, transform=False, **kwargs)
+            lines.append(line)
+
+        ax.set_xlim(self.x_min, self.x_max)
+        ax.set_ylim(self.y_min, self.y_max)
+        return lines
---
+++ packages/empirical-attainment-func/empirical_attainment_func-0.2.0-py3-none-any.whl/eaf/utils.py
@@ -0,0 +1,171 @@
+from typing import List, Optional, Tuple
+
+from fast_pareto.pareto import _change_directions
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+LOGEPS = 1e-300
+
+
+def _change_scale(ax: plt.Axes, log_scale: Optional[List[int]]) -> None:
+    log_scale = [] if log_scale is None else log_scale
+    if 0 in log_scale:
+        ax.set_xscale("log")
+    if 1 in log_scale:
+        ax.set_yscale("log")
+
+
+def _get_slighly_expanded_value_range(
+    costs: np.ndarray,
+    log_scale: Optional[List[int]] = None,
+) -> Tuple[float, float, float, float]:
+    X = costs[..., 0].flatten()
+    Y = costs[..., 1].flatten()
+    log_scale = log_scale if log_scale is not None else []
+    x_is_log, y_is_log = 0 in log_scale, 1 in log_scale
+
+    X = X[np.isfinite(X) & (X > LOGEPS) if x_is_log else np.isfinite(X)]
+    Y = Y[np.isfinite(Y) & (Y > LOGEPS) if y_is_log else np.isfinite(Y)]
+    (x_min, x_max) = (np.log(X.min()), np.log(X.max())) if x_is_log else (X.min(), X.max())
+    (y_min, y_max) = (np.log(Y.min()), np.log(Y.max())) if y_is_log else (Y.min(), Y.max())
+
+    x_min -= 0.1 * (x_max - x_min)
+    x_max += 0.1 * (x_max - x_min)
+    y_min -= 0.1 * (y_max - y_min)
+    y_max += 0.1 * (y_max - y_min)
+    (x_min, x_max) = (np.exp(x_min), np.exp(x_max)) if x_is_log else (x_min, x_max)
+    (y_min, y_max) = (np.exp(y_min), np.exp(y_max)) if y_is_log else (y_min, y_max)
+    return x_min, x_max, y_min, y_max
+
+
+def pareto_front_to_surface(
+    pareto_front: np.ndarray,
+    larger_is_better_objectives: Optional[List[int]] = None,
+    log_scale: Optional[List[int]] = None,
+    x_min: float = -np.inf,
+    x_max: float = np.inf,
+    y_min: float = -np.inf,
+    y_max: float = np.inf,
+) -> np.ndarray:
+    """
+    Convert Pareto front set to a surface array.
+
+    Args:
+        pareto_front (np.ndarray):
+            The raw pareto front solution with the shape of (n_sols, n_obj).
+        larger_is_better_objectives (Optional[List[int]]):
+            The indices of the objectives that are better when the values are larger.
+            If None, we consider all objectives are better when they are smaller.
+        log_scale (Optional[List[int]]):
+            The indices of the log scale.
+            For example, if you would like to plot the first objective in the log scale,
+            you need to feed log_scale=[0].
+            In principle, log_scale changes the minimum value of the axes
+            from -np.inf to a small positive value.
+        x_min (float):
+            The lower bound of the first objective.
+        x_max (float):
+            The upper bound of the first objective.
+        y_min (float):
+            The er bound of the second objective.
+        y_max (float):
+            The er bound of the second objective.
+
+    Returns:
+        modified_pareto_front (np.ndarray):
+            The modified pareto front solution with the shape of (n_sols + 2, n_obj).
+            The head and tail of this array is now the specified bounds.
+            Also this array is now sorted with respect to the first objective.
+    """
+    if len(pareto_front.shape) != 2:
+        raise ValueError(f"The shape of pareto_front must be (n_sols, n_obj), but got {pareto_front.shape}")
+    if (
+        not np.all(x_min <= pareto_front[:, 0])
+        or not np.all(pareto_front[:, 0] <= x_max)
+        or not np.all(y_min <= pareto_front[:, 1])
+        or not np.all(pareto_front[:, 1] <= y_max)
+    ):
+        raise ValueError(
+            f"pareto_front must be in for [{x_min}, {x_max}] the first objective and "
+            f"[{y_min}, {y_max}] for the second objective, but got {pareto_front}"
+        )
+
+    log_scale = [] if log_scale is None else log_scale
+    larger_is_better_objectives = [] if larger_is_better_objectives is None else larger_is_better_objectives
+    x_min = max(LOGEPS, x_min) if 0 in log_scale else x_min
+    maximize_x = 0 in larger_is_better_objectives
+    y_min = max(LOGEPS, y_min) if 1 in log_scale else y_min
+    maximize_y = 1 in larger_is_better_objectives
+
+    (n_sols, n_obj) = pareto_front.shape
+
+    modified_pf = np.empty((n_sols + 2, n_obj))
+    modified_pf[1:-1] = pareto_front
+    modified_pf[0] = [x_min, y_min] if (maximize_x ^ maximize_y) else [x_min, y_max]
+    modified_pf[-1] = [x_max, y_max] if (maximize_x ^ maximize_y) else [x_max, y_min]
+
+    if len(larger_is_better_objectives) > 0:
+        modified_pf = _change_directions(modified_pf, larger_is_better_objectives=larger_is_better_objectives)
+
+    # Sort by the first objective, then the second objective
+    order = np.lexsort((-modified_pf[:, 1], modified_pf[:, 0]))
+    modified_pf = modified_pf[order]
+
+    if len(larger_is_better_objectives) > 0:
+        modified_pf = _change_directions(modified_pf, larger_is_better_objectives=larger_is_better_objectives)
+    if maximize_x:
+        modified_pf = np.flip(modified_pf, axis=0)
+
+    return modified_pf
+
+
+def _check_surface(surf: np.ndarray) -> np.ndarray:
+    if len(surf.shape) != 2:
+        raise ValueError(f"The shape of surf must be (n_points, n_obj), but got {surf.shape}")
+
+    X = surf[:, 0]
+    if np.any(np.maximum.accumulate(X) != X):
+        raise ValueError("The axis [:, 0] of surf must be an increasing sequence")
+
+
+def _step_direction(larger_is_better_objectives: Optional[List[int]]) -> str:
+    """
+    Check here:
+        https://matplotlib.org/stable/gallery/lines_bars_and_markers/step_demo.html#sphx-glr-gallery-lines-bars-and-markers-step-demo-py
+
+    min x min (post)
+        o...       R
+           :
+           o...
+              :
+              o
+
+    max x max (pre)
+        o
+        :
+        ...o
+           :
+    R      ...o
+
+    min x max (post)
+              o
+              :
+           o...
+           :
+        o...       R
+
+    max x min (pre)
+    R      ...o
+           :
+        ...o
+        :
+        o
+    """
+    if larger_is_better_objectives is None:
+        larger_is_better_objectives = []
+
+    large_f1_is_better = bool(0 in larger_is_better_objectives)
+    return "pre" if large_f1_is_better else "post"
