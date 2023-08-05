8857bece99acdd8555d54c3f2ef4ca0f47910928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:15 +0000

    Add package pyclocklib/0.0.0/pyclocklib-0.0.0.tar.gz

---
+++ packages/pyclocklib/pyclocklib-0.0.0.tar.gz/pyclocklib-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="pyclocklib",
+    packages=["pyclocklib"],
+    author="HM",
+    description='Welcome To Our Very Advanced/Simple pyclocklib We Hope You Find What You Need And Also Find It Useful!',
+    url="https://discord.gg/Rx44WZfGnJ",
+)
