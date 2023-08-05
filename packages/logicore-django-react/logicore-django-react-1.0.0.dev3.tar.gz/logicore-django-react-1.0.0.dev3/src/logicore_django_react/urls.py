d1167c9336c28a49f4563c2d307210e980a37427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package logicore-django-react/1.0.0.dev3/logicore-django-react-1.0.0.dev3.tar.gz

---
+++ packages/logicore-django-react/logicore-django-react-1.0.0.dev3.tar.gz/logicore-django-react-1.0.0.dev3/src/logicore_django_react/urls.py
@@ -0,0 +1,20 @@
+from django.template.context_processors import static
+from django.urls import path, re_path, include
+from django.conf import settings
+from . import views
+
+
+bottom = [
+    re_path(r'^(?P<path>.*)$', views.HomeView.as_view()),
+]
+
+
+if getattr(settings, "FRONTEND_DEV_MODE", False):
+    top = [
+        re_path(r'^(?P<path>.*\.hot-update\.(js|json))$', views.hot_update), # \.[0-9a-z]{20}
+        re_path('^react-static/(?P<path>.+)$', views.react_static),
+    ]
+else:
+    top = [
+        static("/react-static/", document_root=settings.BASE_DIR / "frontend" / "build" / "react-static")
+    ]
