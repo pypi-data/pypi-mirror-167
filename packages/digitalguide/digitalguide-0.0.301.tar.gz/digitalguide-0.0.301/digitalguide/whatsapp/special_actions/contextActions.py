6b8df043a375c4126cc226ba2662fd4c5a3c6a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:04 +0000

    Add package digitalguide/0.0.301/digitalguide-0.0.301.tar.gz

---
+++ packages/digitalguide/digitalguide-0.0.301.tar.gz/digitalguide-0.0.301/digitalguide/whatsapp/special_actions/contextActions.py
@@ -0,0 +1,19 @@
+from digitalguide.whatsapp.WhatsAppUpdate import WhatsAppUpdate
+
+
+def whatsapp_default_name(client, update: WhatsAppUpdate, context):
+    context["name"] = update.ProfileName
+
+
+def whatsapp_save_text_to_context(client, update: WhatsAppUpdate, context, key):
+    context[key] = update.Body
+
+
+def whatsapp_save_value_to_context(client, update: WhatsAppUpdate, context, key, value):
+    context[key] = value
+
+
+whatsapp_action_functions = {"default_name": whatsapp_default_name,
+                           "save_text_to_context": whatsapp_save_text_to_context,
+                           "save_value_to_context": whatsapp_save_value_to_context
+                           }
