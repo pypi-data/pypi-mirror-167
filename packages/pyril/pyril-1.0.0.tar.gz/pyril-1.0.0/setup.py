cf235930b8def4fe006c7b8d5b81305e4f19cd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package pyril/1.0.0/pyril-1.0.0.tar.gz

---
+++ packages/pyril/pyril-1.0.0.tar.gz/pyril-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'pyril',
+    version = '1.0.0',
+    description = 'Mirror package of ril, please install that instead.',
+    install_requires = [
+        'ril',
+    ],
+    long_description='Mirror package of ril, please install that instead.',
+)
