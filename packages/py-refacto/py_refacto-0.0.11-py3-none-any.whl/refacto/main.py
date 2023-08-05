78c18715c0e5079029ea574f4169c1a0534fae91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:27 +0000

    Add package py-refacto/0.0.11/py_refacto-0.0.11-py3-none-any.whl

---
+++ packages/py-refacto/py_refacto-0.0.11-py3-none-any.whl/refacto/main.py
@@ -0,0 +1,23 @@
+import typer
+
+from refacto.server import refacto_server
+
+app = typer.Typer()
+
+DEFAULT_PORT = 8080
+Host: str = typer.Option(default="localhost")
+Port: int = typer.Option(default=DEFAULT_PORT)
+
+
+@app.command()
+def tcp(host: str = Host, port: int = Port) -> None:
+    refacto_server.start_tcp(host=host, port=port)  # type: ignore
+
+
+@app.command()
+def stdio() -> None:
+    refacto_server.start_io()  # type: ignore
+
+
+if __name__ == "__main__":
+    app()
