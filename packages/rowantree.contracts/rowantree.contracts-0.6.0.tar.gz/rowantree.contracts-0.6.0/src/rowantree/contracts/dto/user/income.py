1a626d09356a472f96a6e5166558bdf2a3797555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:58 +0000

    Add package rowantree.contracts/0.6.0/rowantree.contracts-0.6.0.tar.gz

---
+++ packages/rowantree.contracts/rowantree.contracts-0.6.0.tar.gz/rowantree.contracts-0.6.0/src/rowantree/contracts/dto/user/income.py
@@ -0,0 +1,27 @@
+""" UserIncome Definition """
+
+from typing import Optional
+
+from pydantic import BaseModel
+
+from ...store_type import StoreType
+
+
+class UserIncome(BaseModel):
+    """
+    UserIncome DTO
+    Define a user income source.
+
+    Attributes
+    ----------
+    amount: int
+        The amount (number of workers) of the income.
+    name: StoreType
+        The StoreType of the income.
+    description: Optional[str]
+        The optional description of the income.
+    """
+
+    amount: int
+    name: StoreType
+    description: Optional[str]
