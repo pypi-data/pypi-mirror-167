b8d3dca2851895f868e06c3c7deaf221e804ca85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:23 +0000

    Add package nautobot-sfp-inventory/0.4.2/nautobot-sfp-inventory-0.4.2.tar.gz

---
+++ packages/nautobot-sfp-inventory/nautobot-sfp-inventory-0.4.2.tar.gz/nautobot-sfp-inventory-0.4.2/nautobot_sfp_inventory/api/views.py
@@ -0,0 +1,21 @@
+from . import serializers, filters
+from nautobot.core.api.views import ModelViewSet, APIRootView
+from nautobot.extras.api.views import CustomFieldModelViewSet
+from nautobot_sfp_inventory.models import SFPType, SFP
+
+class SFPInventoryRootView(APIRootView):
+
+    def get_view_name(self):
+        return "SFP Inventory"
+
+
+class SFPTypeViewSet(CustomFieldModelViewSet):
+    queryset = SFPType.objects.all()
+    serializer_class = serializers.SFPTypeSerializer
+    filterset_class = filters.SFPTypeFilterSet
+
+
+class SFPViewSet(CustomFieldModelViewSet):
+    queryset = SFP.objects.all()
+    serializer_class = serializers.SFPSerializer
+    filterset_class = filters.SFPFilterSet
