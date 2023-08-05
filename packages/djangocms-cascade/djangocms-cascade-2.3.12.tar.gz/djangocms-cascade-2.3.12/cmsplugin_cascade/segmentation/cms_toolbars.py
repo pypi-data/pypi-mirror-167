e8c0c3931d175be0dc18a51c5d727bee0c682869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:17 +0000

    Add package djangocms-cascade/2.3.12/djangocms-cascade-2.3.12.tar.gz

---
+++ packages/djangocms-cascade/djangocms-cascade-2.3.12.tar.gz/djangocms-cascade-2.3.12/cmsplugin_cascade/segmentation/cms_toolbars.py
@@ -0,0 +1,18 @@
+from django.utils.module_loading import import_string
+from django.utils.translation import gettext_lazy as _
+from cms.toolbar_pool import toolbar_pool
+from cms.toolbar_base import CMSToolbar
+from cmsplugin_cascade import app_settings
+
+
+@toolbar_pool.register
+class SegmentationToolbar(CMSToolbar):
+    def populate(self):
+        if not self.request.user.has_perm('cmsplugin_cascade.view_segmentation'):
+            return
+        menu = self.toolbar.get_or_create_menu('segmentation', _("Segmentation"))
+        for sgm in app_settings.CMSPLUGIN_CASCADE['segmentation_mixins']:
+            SegmentationMixin = import_string(sgm[1])
+            populate_handler = getattr(SegmentationMixin, 'populate_toolbar', None)
+            if callable(populate_handler):
+                populate_handler(menu, self.request)
