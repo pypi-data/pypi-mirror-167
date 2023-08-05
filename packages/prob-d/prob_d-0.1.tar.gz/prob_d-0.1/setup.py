2b12ad0e2c2f5bbf73dc0c315ad6071a50ec97b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:07 +0000

    Add package prob-d/0.1/prob_d-0.1.tar.gz

---
+++ packages/prob-d/prob_d-0.1.tar.gz/prob_d-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='prob_d',
+      version='0.1',
+      description='Binomial distributions and Gaussian distributions',
+      packages=['prob_d'],
+      author='Nishita Jain',
+      zip_safe=False)
