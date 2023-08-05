acfd161dc9e189ae489951069db3247cd383c41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package htag/0.8.4/htag-0.8.4-py3-none-any.whl

---
+++ packages/htag/htag-0.8.4-py3-none-any.whl/htag/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2022 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htag
+# #############################################################################
+
+from .tag import H,Tag,HTagException
+
+__version__ = '0.8.4' # auto-updated from pypoetry.toml
+
+__all__= ["Tag","HTagException"]
+
