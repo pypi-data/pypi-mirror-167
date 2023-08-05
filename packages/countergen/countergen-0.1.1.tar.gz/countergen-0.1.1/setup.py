55a619f7045f93662d457549d8cf8f022e10126a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:52 +0000

    Add package countergen/0.1.1/countergen-0.1.1.tar.gz

---
+++ packages/countergen/countergen-0.1.1.tar.gz/countergen-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="countergen",
+    version="0.1.1",
+    description="A counterfactual dataset generator to evaluate language model.",
+    author="SaferAI",
+    author_email="saferai.audit@gmail.com",
+    packages=["countergen"],
+)
