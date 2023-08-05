3aa146243170729e852ae049ce44679b3b3b3ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:27 +0000

    Add package py-lav/0.9.2.1/Py-Lav-0.9.2.1.tar.gz

---
+++ packages/py-lav/Py-Lav-0.9.2.1.tar.gz/Py-Lav-0.9.2.1/pylav/filters/volume.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from pylav.filters.utils import FilterMixin
+
+
+class Volume(FilterMixin):
+    __slots__ = ("_value", "_off", "_default")
+
+    def __init__(self, value: int | float | Volume):
+        super().__init__()
+        self.off = False
+        if isinstance(value, Volume):
+            self.value = value.value
+            self.off = value.off
+        elif isinstance(value, int):
+            self.value = value / 100
+        else:
+            self.value = value
+
+    def to_dict(self) -> dict[str, float | bool]:
+        return {"volume": self.value, "off": self.off}
+
+    def to_json(self) -> dict[str, float]:
+        return {"volume": self.value}
+
+    @classmethod
+    def from_dict(cls, data: dict[str, float | bool]) -> Volume:
+        c = cls(data["volume"])
+        c.off = data["off"]
+        return c
+
+    def __float__(self):
+        return self.value
+
+    @property
+    def value(self) -> float:
+        return self._value
+
+    @value.setter
+    def value(self, v: float):
+        if v < 0:
+            raise ValueError(f"Volume must be must be greater than or equals to zero, not {v}")
+        v = float(round(v, 2))
+        self._value = v
+
+    @classmethod
+    def default(cls) -> Volume:
+        return cls(value=1.0)
+
+    def increase(self, by: float = 0.05) -> None:
+        self.value += by
+
+    def decrease(self, by: float = 0.05) -> None:
+        self.value -= by
+
+    def reset(self) -> None:
+        self.value = 1.0
+
+    def get(self) -> float:
+        return self.value
+
+    def get_int_value(self) -> int:
+        return min(max(int(round(self.value * 100)), 0), 1000)
+
+    def __repr__(self):
+        return str(self.value)
