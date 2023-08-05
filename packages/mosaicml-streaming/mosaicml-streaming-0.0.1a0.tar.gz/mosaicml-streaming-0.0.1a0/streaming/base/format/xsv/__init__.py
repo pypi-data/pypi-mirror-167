937a783c22b902aac455a83df1dcca943bc673d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package mosaicml-streaming/0.0.1a0/mosaicml-streaming-0.0.1a0.tar.gz

---
+++ packages/mosaicml-streaming/mosaicml-streaming-0.0.1a0.tar.gz/mosaicml-streaming-0.0.1a0/streaming/base/format/xsv/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 MosaicML Streaming authors
+# SPDX-License-Identifier: Apache-2.0
+
+from streaming.base.format.xsv.reader import CSVReader, TSVReader, XSVReader
+from streaming.base.format.xsv.writer import CSVWriter, TSVWriter, XSVWriter
+
+__all__ = ['CSVReader', 'CSVWriter', 'TSVReader', 'TSVWriter', 'XSVReader', 'XSVWriter']
---
+++ packages/mosaicml-streaming/mosaicml-streaming-0.0.1a0.tar.gz/mosaicml-streaming-0.0.1a0/streaming/vision/cifar10.py
@@ -0,0 +1,8 @@
+# Copyright 2022 MosaicML Streaming authors
+# SPDX-License-Identifier: Apache-2.0
+
+from streaming.vision.base import ImageClassDataset
+
+
+class CIFAR10(ImageClassDataset):
+    pass
