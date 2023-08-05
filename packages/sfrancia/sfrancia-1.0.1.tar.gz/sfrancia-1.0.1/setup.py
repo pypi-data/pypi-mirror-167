2c28c7f15ee1431080073877e4c23a7208bdd347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:18 +0000

    Add package sfrancia/1.0.1/sfrancia-1.0.1.tar.gz

---
+++ packages/sfrancia/sfrancia-1.0.1.tar.gz/sfrancia-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name='sfrancia',
+      packages=['sfrancia'],
+      version='1.0.1',
+      description='Shapiro-Francia normality test',
+      url='',
+      download_url='',
+      author='Luiz Paulo Fávero',
+      author_email='lpfavero@usp.br',
+      keywords=['shapiro', 'francia','normality','test'],
+      license='MIT',  # YOUR LICENSE HERE!
+
+      install_requires=['numpy', 'scipy'],  # YOUR DEPENDENCIES HERE
+
+
+      classifiers=[
+          # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: MIT License',  # Your License Here
+          # List Python versions that you support Here
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      )
