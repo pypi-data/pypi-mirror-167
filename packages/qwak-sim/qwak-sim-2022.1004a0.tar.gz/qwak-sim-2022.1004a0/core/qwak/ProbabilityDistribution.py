a2d37c783176f6404d3ab6b322283459e80e2cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:41 +0000

    Add package qwak-sim/2022.1004a0/qwak-sim-2022.1004a0.tar.gz

---
+++ packages/qwak-sim/qwak-sim-2022.1004a0.tar.gz/qwak-sim-2022.1004a0/core/qwak/ProbabilityDistribution.py
@@ -0,0 +1,223 @@
+import warnings
+
+import numpy as np
+
+from qwak.State import State
+
+from qwak.Errors import MissingNodeInput
+
+
+warnings.filterwarnings("ignore")
+
+
+class ProbabilityDistribution:
+    """
+    Class containing the vector containing the probabilities associated with the
+    final state of a continuous-time quantum walk.
+
+    """
+
+    def __init__(self, state: State) -> None:
+        """The dimension of the probability vector will then be loaded from
+        the state inputted by the user.
+        The vector containing the probabilities will be initialized full of zeros
+        with the dimension obtained from the state.
+
+        Parameters
+        ----------
+        state : State
+            State to be converted into a probability.
+        """
+        self._state = state.getStateVec()
+        self._n = state.getDim()
+        self._probVec = np.zeros((self._n, 1))
+
+    def resetProbDist(self) -> None:
+        """Resets the ProbabilityDistribution object."""
+        # TODO: Rethink state attribute
+        self._state = np.zeros((self._n, 1), dtype=complex)
+        self._probVec = np.zeros((self._n, 1))
+
+    def buildProbDist(self, state: State = None) -> None:
+        """Builds the probability vector by multiplying the user inputted
+        amplitude state by its conjugate.
+
+        Parameters
+        ----------
+        state : State, optional
+            _description_, by default None
+        """
+        if state is not None:
+            self._state = state.getStateVec()
+        for st in range(self._n):
+            self._probVec[st] = self._state[st] * \
+                np.conj(self._state[st])
+
+    def setProbDist(self, newProbDist) -> None:
+        """_summary_
+
+        Parameters
+        ----------
+        newProbDist : _type_
+            _description_
+        """
+        self._state = newProbDist.getState()
+        self._n = newProbDist.getDim()
+        self._probVec = newProbDist.getProbVec()
+
+    def getState(self) -> State:
+        """_summary_
+
+        Returns
+        -------
+        State
+            _description_
+        """
+        return self._state
+
+    def setDim(self, newDim):
+        self._n = newDim
+        self._probVec = np.zeros((self._n, 1))
+
+    def getDim(self) -> int:
+        """_summary_
+
+        Returns
+        -------
+        int
+            _description_
+        """
+        return self._n
+
+    def setProbVec(self, newProbVec: np.ndarray) -> None:
+        """Sets the current probability vector to a user inputted one.
+
+        Parameters
+        ----------
+        newProbVec : np.ndarray
+            New probability vector for the distribution.
+        """
+        self._probVec = newProbVec.getProbDist()
+
+    def getProbVec(self) -> np.ndarray:
+        """Gets the probability vector associated with a distribution.
+
+        Returns
+        -------
+        np.ndarray
+            Returns the array of the ProbabilityDistribution object.
+        """
+        return self._probVec.flatten()
+
+    def searchNodeProbability(self, searchNode: int) -> float:
+        """Searches and gets the probability associated with a given node.
+
+        Parameters
+        ----------
+        searchNode : int
+            User inputted node for the search.
+
+        Returns
+        -------
+        float
+            Probability of the searched node.
+        """
+        return self._probVec.item(searchNode)
+
+    def mean(self) -> float:
+        """Gets the mean of the current probability distribution.
+
+        Returns
+        -------
+        float
+            Mean of the ProbabilityDistribution object.
+        """
+        # TODO: This function has been replaced by moment(1).
+        pos = np.arange(0, self._n)
+        m = 0
+        for x in range(self._n):
+            m += pos[x] * self._probVec[x]
+        return float(m)
+
+    def moment(self, k) -> float:
+        """_summary_
+
+        Parameters
+        ----------
+        k : _type_
+            _description_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        pos = np.arange(0, self._n)
+        m = 0
+        for x in range(self._n):
+            m += (pos[x] ** k) * self._probVec[x]
+        return float(m)
+
+    def stDev(self) -> float:
+        """_summary_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        stDev = self.moment(2) - self.moment(1) ** 2
+        if stDev <= 0:
+            return 0
+        return np.sqrt(stDev)
+
+    def survivalProb(self, k0, k1) -> float:
+        """_summary_
+
+        Parameters
+        ----------
+        k0 : _type_
+            _description_
+        k1 : _type_
+            _description_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        survProb = 0
+        try:
+            k0 = int(k0)
+            k1 = int(k1)
+            if k0 == k1:
+                return self._probVec[int(k0)][0]
+            else:
+                for i in range(int(k0), int(k1) + 1):
+                    survProb += self._probVec[i]
+            return survProb[0]
+        except ValueError:
+            raise MissingNodeInput(
+                f"A node number is missing: k0 = {k0}; k1={k1}")
+
+    def __str__(self) -> str:
+        """String representation of the ProbabilityDistribution object.
+
+        Returns
+        -------
+        str
+            String of the ProbabilityDistribution object.
+        """
+        return f"{self._probVec}"
+
+    def __repr__(self) -> str:
+        """Representation of the ProbabilityDistribution object.
+
+        Returns
+        -------
+        str
+            String of the ProbabilityDistribution object.
+        """
+        return f"N: {self._n}\n" \
+               f"State:\n\t{self._state}\n" \
+               f"ProbDist:\n\t{self._probVec}"
---
+++ packages/qwak-sim/qwak-sim-2022.1004a0.tar.gz/qwak-sim-2022.1004a0/core/qwak/QuantumWalk.py
@@ -0,0 +1,223 @@
+from __future__ import annotations
+
+import warnings
+
+import numpy as np
+
+from qwak.Operator import Operator
+from qwak.State import State
+
+warnings.filterwarnings("ignore")
+
+
+class QuantumWalk:
+    """Class that represents the final state containing the amplitudes of a
+    continuous-time quantum walk.
+    """
+
+    def __init__(self, state: State, operator: Operator) -> None:
+        """This object is initialized with a user inputted initial state and
+        operator.
+        The dimension of the quantum walk will then be loaded from the initial
+        state.
+        The final state will contain the amplitudes of the time evolution of
+        the initial state, as a function of the operator. This variable is initialized
+        as an instance of State class.
+
+        Parameters
+        ----------
+        state : State
+            Initial state which will be the basis of the time dependant evolution.
+        operator : Operator
+            Operator which will evolve the initial state.
+        """
+        # TODO: This class has mandatory init conditions. This is not in
+        # line with the rest of the classes.
+        self._n = state.getDim()
+        self._initState = state
+        self._operator = operator
+        self._finalState = State(self._n)
+
+    def buildWalk(self, initState: State = None,
+                  operator: Operator = None) -> None:
+        """Builds the final state of the quantum walk by setting it to the matrix
+        multiplication of the operator by the initial state.
+
+        Parameters
+        ----------
+        initState : State, optional
+            Initial state which will be the basis of the time dependant evolution, by default None.
+        operator : Operator, optional
+            Operator which will evolve the initial state, by default None.
+        """
+        # TODO: Name of init state variable not in line with class init.
+        if initState is not None:
+            self._initState = initState
+        if operator is not None:
+            self._operator = operator
+        self._finalState.setStateVec(
+            np.matmul(
+                self._operator.getOperator(),
+                self._initState.getStateVec()))
+
+    def resetWalk(self):
+        """Resets the components of the QuantumWalk object."""
+        self._operator.resetOperator()
+        self._initState.resetState()
+        self._finalState.resetState()
+
+    def setInitState(self, newInitState: State) -> None:
+        """Sets the initial state of the quantum walk to a new user inputted one.
+
+        Parameters
+        ----------
+        newInitState : State
+            New initial state for the quantum walk.
+        """
+        self._initState.setState(newInitState)
+
+    def getInitState(self) -> State:
+        """Gets the initial state of the quantum walk.
+
+        Returns
+        -------
+        State
+            Initial state of the quantum walk.
+        """
+        return self._initState
+
+    def setDim(self, newDim: int) -> None:
+        """Sets the current quantum walk dimension to a user defined one.
+
+        Parameters
+        ----------
+        newDim : int
+            New QuantumWalk dimension.
+        """
+        self._n = newDim
+        self._finalState = State(self._n)
+
+    def getDim(self) -> int:
+        """Gets the current state dimension.
+
+        Returns
+        -------
+        int
+            QuantumWalk dimension.
+        """
+        return self._n
+
+    def setOperator(self, newOperator: Operator) -> None:
+        """Sets the current operator to a user defined one.
+
+        Parameters
+        ----------
+        newOperator : Operator
+            New quantum walk operator.
+        """
+        self._operator.setOperator(newOperator)
+
+    def getOperator(self) -> Operator:
+        """Gets the current operator.
+
+        Returns
+        -------
+        Operator
+            Current QuantumWalk Operator object.
+        """
+        return self._operator
+
+    def setWalk(self, newWalk: QuantumWalk) -> None:
+        """Sets all the parameters of the current quantum walk to user defined ones.
+
+        Parameters
+        ----------
+        newWalk : QuantumWalk
+            New quantum walk.
+        """
+        self._initState.setState(newWalk.getInitState())
+        self._operator.setOperator(newWalk.getOperator())
+        self._finalState.setState(newWalk.getWalk())
+
+    def getFinalState(self) -> State:
+        """Gets the final state of the QuantumWalk.
+
+        Returns
+        -------
+        State
+            Final state of the QuantumWalk.
+        """
+        return self._finalState
+
+    def getAmpVec(self) -> np.ndarray:
+        """Gets the vector of the final state amplitudes of the  QuantumWalk.
+
+        Returns
+        -------
+        np.ndarray
+            Vector of the final state.
+        """
+        # TODO: This function name is not in line with getFinalState.
+        return self._finalState.getStateVec()
+
+    def searchNodeAmplitude(self, searchNode: int) -> complex:
+        """Searches and gets the amplitude associated with a given node.
+
+        Parameters
+        ----------
+        searchNode : int
+            User inputted node for the search.
+
+        Returns
+        -------
+        complex
+            Amplitude of the search node.
+        """
+        return self._finalState.getStateVec().item(searchNode)
+
+    def invPartRatio(self) -> float:
+        """_summary_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        amplitudes = 0
+        for amp in self._finalState.getStateVec():
+            amplitudes += np.absolute(amp.item(0)) ** 4
+        amplitudes = amplitudes
+        return 1 / amplitudes
+
+    def transportEfficiency(self) -> float:
+        """_summary_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        return 1 - np.trace(self._finalState @ self._finalState.herm())
+
+    def __str__(self) -> str:
+        """String representation of the StaticQuantumwalk class.
+
+        Returns
+        -------
+        str
+            QuantumWalk string.
+        """
+        return f"{self._finalState.getStateVec()}"
+
+    def __repr__(self) -> str:
+        """Representation of the ProbabilityDistribution object.
+
+        Returns
+        -------
+        str
+            String of the ProbabilityDistribution object.
+        """
+        return f"N: {self._n}\n" \
+               f"Init State:\n\t {self._initState}\n" \
+               f"Operator:\n\t{self._operator}\n"\
+               f"Final State:\n\t{self._finalState}"
