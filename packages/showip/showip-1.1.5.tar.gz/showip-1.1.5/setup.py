4f5f4d6aa72a3445ab77f5c396f41ae5cc8b3fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:39 +0000

    Add package showip/1.1.5/showip-1.1.5.tar.gz

---
+++ packages/showip/showip-1.1.5.tar.gz/showip-1.1.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+NAME = "showip"
+DESCRIPTION = "Get information about your IP or any other IP"
+URL = "https://github.com/varvaruk-v/showip"
+EMAIL = ""
+AUTHOR = "Viktor Varvaruk"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "1.1.5"
+
+with open("requirements.txt", encoding="utf-8") as f:
+    REQUIRED = f.readlines()
+
+try:
+    with open("README.md", encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=["showip"],
+    entry_points={
+        "console_scripts": ["showip=showip.cli:main", "myip=showip.cli:main"]
+    },
+    install_requires=REQUIRED,
+    license="MIT",
+)
---
+++ packages/showip/showip-1.1.5.tar.gz/showip-1.1.5/showip/showip.py
@@ -0,0 +1,31 @@
+import requests, sys
+
+def getdata(ip=""):
+    if ip == None or ip == "":
+        ip = ""
+    url = f"http://ip-api.com/json/{ip}?fields=66846719"
+    r = requests.get(url)
+    if r.status_code == 200:
+        data = r.json()
+        if data["status"] == "success":
+            isp = data["as"].split(" ")
+            isp.pop(0)
+            isp = " ".join(isp)
+            if isp == "":
+                isp = data["isp"]
+            data["isp"] = isp
+            if data["reverse"] == "":
+                data["reverse"] = data["query"]
+        else:
+            print(f"[FAIL] {data['message']}")
+            sys.exit()
+        return data
+    else:
+        print("[ERROR] Unknown error. Check your internet connection")
+        sys.exit()
+
+def dns():
+    url = "http://edns.ip-api.com/json"
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.json()
