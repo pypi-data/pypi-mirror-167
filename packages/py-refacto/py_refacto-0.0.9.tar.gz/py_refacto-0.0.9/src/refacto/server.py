bcacf5320cf07d25bb0b8de526aada05fa4d6a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:17 +0000

    Add package py-refacto/0.0.9/py_refacto-0.0.9.tar.gz

---
+++ packages/py-refacto/py_refacto-0.0.9.tar.gz/py_refacto-0.0.9/src/refacto/server.py
@@ -0,0 +1,30 @@
+from pygls.lsp.methods import CODE_ACTION
+from pygls.lsp.types import CodeAction
+from pygls.lsp.types import CodeActionKind
+from pygls.lsp.types import CodeActionOptions
+from pygls.lsp.types import CodeActionParams
+from pygls.server import LanguageServer
+
+from refacto.refactoring_service import RefactoringService
+
+
+class RefactoLanguageServer(LanguageServer):
+    def __init__(self) -> None:
+        super().__init__()
+        self.service = RefactoringService()
+
+
+refacto_server = RefactoLanguageServer()
+
+
+@refacto_server.feature(
+    CODE_ACTION,
+    CodeActionOptions(
+        code_action_kinds=[CodeActionKind.Refactor],
+        work_done_progress=None,
+        resolve_provider=True,
+    ),
+)
+def code_action(server: RefactoLanguageServer, code_action_params: CodeActionParams) -> list[CodeAction]:
+    document = server.workspace.get_document(code_action_params.text_document.uri)
+    return server.service.get_available_refactorings(document=document, code_action_params=code_action_params)
