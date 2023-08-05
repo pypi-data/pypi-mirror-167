4694168282043f1969543fa55e72c01c32f76a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:45 +0000

    Add package wnk-tree-ai/0.0.13/wnk-tree-ai-0.0.13.tar.gz

---
+++ packages/wnk-tree-ai/wnk-tree-ai-0.0.13.tar.gz/wnk-tree-ai-0.0.13/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+"""
+# Commands
+python setup.py sdist
+python -m twine upload  dist/* -u friebertshaeusertraideai -p DJQ3yjk4ark_ynu!una
+# Result
+https://pypi.org/project/pf-test-hello-world/0.0.1/
+"""
+
+# this grabs the requirements from requirements.txt
+
+
+
+REQUIREMENTS = ['treelib', 
+                'numpy', 
+                'pandas',
+                'pydantic']
+
+setup(
+  name='wnk-tree-ai',
+  version='0.0.13',
+  description='Nomenclature for AI Traversal',
+  long_description="Nomenclature for AI Traversal",
+  url='',  
+  author='Philipp Friebertshäuser',
+  author_email='friebertshaeuser@traide.ai',
+  license='MIT', 
+  keywords='wnk', 
+  packages = find_packages(),
+  install_requires=REQUIREMENTS
+)
