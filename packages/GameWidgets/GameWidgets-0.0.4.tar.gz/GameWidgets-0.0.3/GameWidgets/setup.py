685d68635aecb4f07b2430a61f6f1e158ff0e8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:47 +0000

    Add package GameWidgets/0.0.3/GameWidgets-0.0.4.tar.gz

---
+++ packages/GameWidgets/GameWidgets-0.0.4.tar.gz/GameWidgets-0.0.3/GameWidgets/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+  
+with open("GameWidgets/README.md", "r") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="GameWidgets",
+    version="0.0.3",
+    author="Manomay tyagi",
+    author_email="tyagimanomay57@gmail.com",
+    packages=["GameWidgets"],
+    description="Pygame becomes easier",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gituser/test-tackage",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=['pygame']
+)
