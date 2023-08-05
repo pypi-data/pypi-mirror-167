7c69557d4285640005069066641882724f16b110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:43 +0000

    Add package msb-dynamic-crud/0.0.4/msb_dynamic_crud-0.0.4.tar.gz

---
+++ packages/msb-dynamic-crud/msb_dynamic_crud-0.0.4.tar.gz/msb_dynamic_crud-0.0.4/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='msb_dynamic_crud',
+    version='0.0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='A reusable Django app for simply implementing a advance CRUD on any Model',
+    long_description=README,
+    url='https://github.com/mukeshbadgujar/msb-dynamic-crud',
+    author='Mukesh Badgujar',
+    author_email='mukesh.badgujar.in@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
