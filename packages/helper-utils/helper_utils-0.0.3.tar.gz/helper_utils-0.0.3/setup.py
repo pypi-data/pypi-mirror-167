935ec45a3afd10d710a93a69d70f8b02c1a85d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:32 +0000

    Add package helper-utils/0.0.3/helper_utils-0.0.3.tar.gz

---
+++ packages/helper-utils/helper_utils-0.0.3.tar.gz/helper_utils-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+"""
+# Commands
+- change to directory
+python setup.py sdist
+python -m twine upload  dist/* -u friebertshaeusertraideai -p DJQ3yjk4ark_ynu!una
+# Result
+https://pypi.org/project/pf-test-hello-world/0.0.1/
+"""
+
+
+
+REQUIREMENTS = ['numpy==1.22.4', 
+                'pandas==1.4.2']
+
+setup(
+  name='helper_utils',
+  version='0.0.3',
+  description='Helper functions',
+  long_description="Helper functions",
+  url='',  
+  author='Philipp Friebertshäuser',
+  author_email='friebertshaeuser@traide.ai',
+  license='MIT', 
+  keywords='helper', 
+  packages = find_packages(),
+  install_requires=REQUIREMENTS
+)
