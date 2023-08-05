f61373abf862e6bfc2386fd42103d51c36542021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:42 +0000

    Add package mattstools/0.0.6/mattstools-0.0.6.tar.gz

---
+++ packages/mattstools/mattstools-0.0.6.tar.gz/mattstools-0.0.6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mattstools",
+    version="0.0.6",
+    author="Matthew Leigh",
+    author_email="mattcleigh@gmail.com",
+    description="Some common utilities used in my DL projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.cern.ch/mleigh/mattstools",
+    project_urls={"Bug Tracker": "https://gitlab.cern.ch/mleigh/mattstools/issues"},
+    license="MIT",
+    packages=["mattstools"],
+    install_requires=[
+        "dotmap",
+        "geomloss",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "PyYAML",
+        "scikit_learn",
+        "scipy",
+        "setuptools",
+        "torch",
+        "tqdm",
+        "typing_extensions",
+    ],
+)
