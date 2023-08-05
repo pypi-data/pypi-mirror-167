21477f63fc7ffb4db6c0f5d857484763dfa5369b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:23 +0000

    Add package crosshair-tool/0.0.29/crosshair-tool-0.0.29.tar.gz

---
+++ packages/crosshair-tool/crosshair-tool-0.0.29.tar.gz/crosshair-tool-0.0.29/crosshair/libimpl/heapqlib_test.py
@@ -0,0 +1,17 @@
+import heapq
+from typing import List
+
+from crosshair.statespace import CONFIRMED, MessageType
+from crosshair.test_util import check_states
+
+
+def test_heapify():
+    def f(items: List[int]):
+        """
+        pre: len(items) == 3
+        post: _[0] <= _[1]
+        """
+        heapq.heapify(items)
+        return items
+
+    check_states(f, CONFIRMED)
