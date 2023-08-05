bc8df1029a04399c2a4caf3cfe4bacddeaaa8cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package pyclocklib/0.0.1/pyclocklib-0.0.1.tar.gz

---
+++ packages/pyclocklib/pyclocklib-0.0.1.tar.gz/pyclocklib-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = "pyclocklib",
+    version = "0.0.1",
+    author = "HM",
+    author_email = "huntermikesell84@gmail.com",
+    description = ("Very Simple/Advanced PyClock"),
+    license = "BSD",
+    keywords = "None",
+    packages=["pyclocklib"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
