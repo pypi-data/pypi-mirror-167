9e80349d90d7902e7d87645bb01d2735ceda86da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:01 +0000

    Add package kp-compression/0.1.8/kp_compression-0.1.8.tar.gz

---
+++ packages/kp-compression/kp_compression-0.1.8.tar.gz/kp_compression-0.1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='kp_compression',
+    packages=find_packages(include=['kp_compression']),
+    version='0.1.8',
+    description='Library to compress LSTM layers',
+    author='Suyash Saxena and Varun Singh Negi',
+    license='IITD',
+    install_requires=['numpy','torch'],
+    zip_save='true'
+)
