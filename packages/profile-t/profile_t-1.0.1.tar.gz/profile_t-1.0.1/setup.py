70f54e589e7bf57d33aa76dba413ac36ccb79b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:00 +0000

    Add package profile-t/1.0.1/profile_t-1.0.1.tar.gz

---
+++ packages/profile-t/profile_t-1.0.1.tar.gz/profile_t-1.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+	name='profile_t',
+	version='1.0.0',
+	description='Profile t calculation for symbolic expressions',
+	author='Fabricio Olivetti de Franca, Gabriel Kronberger',
+	author_email='folivetti@ufabc.edu.br, Gabriel.Kronberger@fh-hagenberg.at',
+	url='https://github.com/folivetti/profile_t',
+	packages=find_packages(where='./src', exclude=['test']),
+    package_dir={'': 'src'},
+    install_requires=['scipy>=1.9.0', 'numpy>=1.9.0', 'sympy>=1.7.0', 'matplotlib>=2.0.0'],
+	)
