951297352c22c8244fc3c9303ce1130ee6c176ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:30 +0000

    Add package simctg/0.7/simctg-0.7.tar.gz

---
+++ packages/simctg/simctg-0.7.tar.gz/simctg-0.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simctg',
+    version='0.7',
+    license='MIT',
+    author="Yixuan Su",
+    author_email='ys484@outlook.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/yxuansu/SimCTG',
+    keywords='SimCTG Library',
+    install_requires=[
+          'absl-py',
+          'pytest',
+          'sacrebleu==1.4.10',
+          'six',
+          'wheel',
+          'progressbar', 
+          'sklearn', 
+          'torch',
+          'transformers',
+          'pyyaml',
+          'nltk',
+          'sentencepiece',
+          'spacy',
+          'gdown',
+          'seaborn',
+          'matplotlib',
+          'pandas',
+      ],
+
+)
