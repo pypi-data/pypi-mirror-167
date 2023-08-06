85f38591395110eb259cf9e657a2ef1266bbfe92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:43 +0000

    Add package taiyang-huigui/2022.9.14.0/taiyang_huigui-2022.9.14.0-py3-none-any.whl

---
+++ packages/taiyang-huigui/taiyang_huigui-2022.9.14.0-py3-none-any.whl/TaiyangHuigui/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""taiyang-huigui
+https://github.com/apachecn/taiyang-huigui"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.14.0"
