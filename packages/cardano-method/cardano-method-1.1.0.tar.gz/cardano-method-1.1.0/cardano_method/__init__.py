7930b80d991d2696a57611e9358c7069fdd75daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:36 +0000

    Add package cardano-method/1.1.0/cardano-method-1.1.0.tar.gz

---
+++ packages/cardano-method/cardano-method-1.1.0.tar.gz/cardano-method-1.1.0/cardano_method/__init__.py
@@ -0,0 +1,2 @@
+
+from .cubic import CubicEquation
---
+++ packages/cardano-method/cardano-method-1.1.0.tar.gz/cardano-method-1.1.0/cardano_method/test.py
@@ -0,0 +1,7 @@
+
+from cubic import CubicEquation
+
+a = CubicEquation([1, 3, 4, 4])
+
+print(a.answers)
+# [(-2+0j), (-0.5+1.322875j), (-0.5-1.322875j)]
