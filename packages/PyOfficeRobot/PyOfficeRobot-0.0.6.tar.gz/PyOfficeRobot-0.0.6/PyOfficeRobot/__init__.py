a238ec6aabab969f468887d2ceff764af4dd6e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:02 +0000

    Add package PyOfficeRobot/0.0.6/PyOfficeRobot-0.0.6.tar.gz

---
+++ packages/PyOfficeRobot/PyOfficeRobot-0.0.6.tar.gz/PyOfficeRobot-0.0.6/PyOfficeRobot/__init__.py
@@ -0,0 +1,7 @@
+from PyOfficeRobot.api import chat
+from PyOfficeRobot.api import file
+
+print('=' * 30)
+print('本开源项目的源码仓库：https://github.com/CoderWanFeng/PyOfficeRobot')
+print('请+【项目交流群】：http://t.cn/A6SSrID0')
+print('=' * 30)
