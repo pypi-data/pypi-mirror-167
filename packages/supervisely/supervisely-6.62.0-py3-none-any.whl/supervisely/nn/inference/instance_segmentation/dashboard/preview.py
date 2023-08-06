aa068d42f593490c813fa6a53653baf5f48b1313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:58 +0000

    Add package supervisely/6.62.0/supervisely-6.62.0-py3-none-any.whl

---
+++ packages/supervisely/supervisely-6.62.0-py3-none-any.whl/supervisely/nn/inference/instance_segmentation/dashboard/preview.py
@@ -0,0 +1,46 @@
+from select import select
+from supervisely.app.widgets import (
+    Text,
+    Select,
+    Field,
+    OneOf,
+    SelectTeam,
+    SelectWorkspace,
+    SelectProject,
+    SelectItem,
+)
+from supervisely.app.widgets.select_dataset.select_dataset import SelectDataset
+from supervisely.project.project_type import ProjectType
+
+t_url = Text("url")
+t_sly = Text("sly")
+t_upload = Text("upload")
+
+# selector = SelectTeam()
+# selector = SelectWorkspace()
+# selector = SelectProject(compact=False, project_types=[ProjectType.VIDEOS])
+# selector = SelectDataset(compact=False, multiselect=True)
+selector = SelectItem(compact=False)
+
+# select image
+# select dataset
+# select dataset
+# Team->Workspace->Project->Dataset->Image
+# - list images in dataset limit
+# - dialog window?
+
+
+image_source = Select(
+    items=[
+        Select.Item(value="url", label="Image URL", content=selector),
+        Select.Item(value="sly", label="Image in Supervisely", content=t_sly),
+        # Select.Item(value="demo", label="Demo image"), ???
+        Select.Item(value="upload", label="Upload your image", content=t_upload),
+    ],
+)
+
+image_source_field = Field(
+    image_source,
+    "Select image",
+    "Choose one of the image sources and provide test image",
+)
