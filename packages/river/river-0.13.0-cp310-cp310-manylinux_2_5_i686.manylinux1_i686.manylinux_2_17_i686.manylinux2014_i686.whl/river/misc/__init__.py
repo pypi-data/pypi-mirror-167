ca183fc425a6ebd386a8f642114f0a69b405c271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:16 +0000

    Add package river/0.13.0/river-0.13.0-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/river/river-0.13.0-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/river/misc/__init__.py
@@ -0,0 +1,10 @@
+"""Miscellaneous.
+
+This module essentially regroups some implementations that have nowhere else to go.
+
+"""
+from .histogram import Histogram
+from .sdft import SDFT
+from .skyline import Skyline
+
+__all__ = ["Histogram", "SDFT", "Skyline"]
