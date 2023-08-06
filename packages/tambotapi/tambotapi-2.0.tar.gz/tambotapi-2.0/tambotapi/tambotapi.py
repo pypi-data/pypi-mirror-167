48f6390d59858e874e5d1ea7d701ce3ba9c18258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:09 +0000

    Add package tambotapi/2.0/tambotapi-2.0.tar.gz

---
+++ packages/tambotapi/tambotapi-2.0.tar.gz/tambotapi-2.0/tambotapi/tambotapi.py
@@ -0,0 +1,14 @@
+import json
+import logging
+import os
+import time
+
+import requests
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class TamBot:
+    def gay():
+        return True
