e7246935b0a22fd198ac98c1fa22b8603aa6a43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:36 +0000

    Add package sim2bids/1.0.1/sim2bids-1.0.1-py3-none-any.whl

---
+++ packages/sim2bids/sim2bids-1.0.1-py3-none-any.whl/sim2bids/preprocess/preprocess.py
@@ -0,0 +1,21 @@
+IDS = []
+start = 1
+
+
+def create_uuid():
+    global start
+
+    if start in IDS:
+        start += 1
+
+    new = f'sub-0{start}' if start < 10 else f'sub-{start}'
+
+    IDS.append(start)
+    return new
+
+
+def reset_index():
+    global start, IDS
+    start = 1
+    IDS = []
+
