4a806dae4d1dad28d0bea678e858716d25f6ad5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:23 +0000

    Add package assesspy/0.1.1/assesspy-0.1.1.tar.gz

---
+++ packages/assesspy/assesspy-0.1.1.tar.gz/assesspy-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="assesspy",
+    version="0.1.1",
+    description="General purpose Python package for measuring assessment performance",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://gitlab.com/ccao-data-science---modeling/packages/assesspy/",
+    author="CCAO",
+    author_email="assessor.data@cookcountyil.gov",
+    license="GPL-3",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "pyarrow",
+        "numpy",
+        "scipy",
+        "sklearn",
+        "statsmodels"
+        ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"],
+    include_package_data = True,
+    package_data={
+        "": ["*.parquet"],
+    }
+    )
