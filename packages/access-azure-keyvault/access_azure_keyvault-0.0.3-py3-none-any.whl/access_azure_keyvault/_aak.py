693548cdcd32daef1e4859059b1fc5f7daedac3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:39 +0000

    Add package access-azure-keyvault/0.0.3/access_azure_keyvault-0.0.3-py3-none-any.whl

---
+++ packages/access-azure-keyvault/access_azure_keyvault-0.0.3-py3-none-any.whl/access_azure_keyvault/_aak.py
@@ -0,0 +1,17 @@
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+from azure.core.exceptions import AzureError
+
+class LocalException (Exception): pass
+
+def getSecret(keyVaultName, secretName):
+  KVUri = f"https://{keyVaultName}.vault.azure.net"
+
+  try:
+      credential = DefaultAzureCredential()
+      client = SecretClient(vault_url=KVUri, credential=credential)
+      retrieved_secret = client.get_secret(secretName)
+      return retrieved_secret
+  except AzureError as e:
+      raise LocalException(f"AzureError: <{e.__class__.__name__}> <{e}>")
+
