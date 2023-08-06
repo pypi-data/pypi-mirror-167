15f46f5a49ca5aa3988f7bfdef5816a77bd3f85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:01 +0000

    Add package lequangminh/0.0.1/lequangminh-0.0.1.tar.gz

---
+++ packages/lequangminh/lequangminh-0.0.1.tar.gz/lequangminh-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+
+setup(
+    name="lequangminh",
+    version='0.0.1',
+    license='Eclipse Public License 0.0.1',
+    authors=["quangminh591", "loTus01", "BlueRed"],
+    author_email="<quangminh591@gmail.com>",
+    description="by quangminh591, loTus01 and BlueRed",
+    keywords=['cli', 'fade', 'colors', 'terminal', 'tui'],
+    packages=['lequangminh']
+)
