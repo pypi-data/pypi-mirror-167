260ff3bf909d30d640ead6cbb813588271f3124a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:56 +0000

    Add package mattermost-notify/22.9.0/mattermost-notify-22.9.0.tar.gz

---
+++ packages/mattermost-notify/mattermost-notify-22.9.0.tar.gz/mattermost-notify-22.9.0/tests/test_git.py
@@ -0,0 +1,72 @@
+# Copyright (C) 2022 Jaspar Stach <jasp.stac@gmx.de>
+
+import unittest
+from unittest.mock import patch
+
+from mattermost_notify.git import (
+    _linker as linker,
+    parse_args,
+    fill_template,
+)
+
+
+class GitNotifyTestCase(unittest.TestCase):
+    def test_linker(self):
+        expected = "[foo](www.foo.com)"
+        link = linker("foo", "www.foo.com")
+
+        self.assertEqual(link, expected)
+
+    def test_argument_parsing(self):
+        parsed_args = parse_args(["www.url.de", "channel"])
+
+        self.assertEqual(parsed_args.url, "www.url.de")
+        self.assertEqual(parsed_args.channel, "channel")
+
+    def test_fail_argument_parsing(self):
+        with self.assertRaises(SystemExit):
+            parse_args(["-s"])
+
+    @patch("mattermost_notify.git.get_github_event_json")
+    def test_no_highlight(self, event_mock: None):
+        event_mock.return_value = None
+        parsed_args = parse_args(
+            ["www.url.de", "channel", "--highlight", "user1", "user2"]
+        )
+        rf = fill_template(parsed_args, None)
+        rt = (
+            "#### Status: :white_check_mark: success\n\n"
+            "| Workflow | [None](https://githu"
+            "b.com/None/actions/runs/None) |\n| --- | --- |"
+            "\n| Repository (branch) | [None](https://githu"
+            "b.com/None) (None) |\n| Related commit | not a"
+            "vailable |\n"
+        )
+        print(rf)
+        self.assertEqual(rf, rt)
+
+    @patch("mattermost_notify.git.get_github_event_json")
+    def test_highlight(self, event_mock: None):
+        event_mock.return_value = None
+        parsed_args = parse_args(
+            [
+                "www.url.de",
+                "channel",
+                "--highlight",
+                "user1",
+                "user2",
+                "-S",
+                "failure",
+            ]
+        )
+        rf = fill_template(parsed_args, None)
+        rt = (
+            "#### Status: :x: failure\n\n"
+            "| Workflow | [None](https://githu"
+            "b.com/None/actions/runs/None) |\n| --- | --- |"
+            "\n| Repository (branch) | [None](https://githu"
+            "b.com/None) (None) |\n| Related commit | not a"
+            "vailable |\n@user1\n@user2\n"
+        )
+        print(rf)
+        self.assertEqual(rf, rt)
