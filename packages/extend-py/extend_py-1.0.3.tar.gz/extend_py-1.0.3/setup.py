358f9ced60636c4c3d1609f7063d7ad0a6781c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:15 +0000

    Add package extend-py/1.0.3/extend_py-1.0.3.tar.gz

---
+++ packages/extend-py/extend_py-1.0.3.tar.gz/extend_py-1.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name = "extend_py",
+        version = "1.0.3",
+        author = "bailiyiyun",
+        author_email = "995440065@qq.com",
+        description = "This is a extc",	#介绍
+        long_description = long_description,
+        long_description_content_type = "text/markdown",
+        url = "https://gitee.com/bailiyiyun/extc-package",#如果自己写的，在GitHub有就写GitHub地址，或者其他地址，没有就写pypi自己的url地址
+        packages = setuptools.find_packages(),
+        classifiers=[
+        "Programming Language :: Python :: 3",
+            ],#这个文件适用的python版本，安全等等，我这里只写入了版本
+        )
