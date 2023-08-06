c35c9e9a0dbdedb1a22a2be7fc71049730a752a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:35 +0000

    Add package easydata/0.3.3/easydata-0.3.3-py2.py3-none-any.whl

---
+++ packages/easydata/easydata-0.3.3-py2.py3-none-any.whl/easydata/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.3.3"
+
+from easydata.blocks import *  # noqa: F401 F403
+from easydata.models import ItemModel, StackedModel, StackedParser  # noqa: F401 F403
+from easydata.parsers import *  # noqa: F401 F403
+from easydata.processors import *  # noqa: F401 F403
+from easydata.queries import *  # noqa: F401 F403
