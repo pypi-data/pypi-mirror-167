319e9f416ba24541ba20c6763cc220b3ce8e1ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:19 +0000

    Add package qianli-yinyuan-hechuqian/2022.9.14.0/qianli-yinyuan-hechuqian-2022.9.14.0.tar.gz

---
+++ packages/qianli-yinyuan-hechuqian/qianli-yinyuan-hechuqian-2022.9.14.0.tar.gz/qianli-yinyuan-hechuqian-2022.9.14.0/QianliYinyuanHechuqian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qianli-yinyuan-hechuqian
+https://github.com/apachecn/qianli-yinyuan-hechuqian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.14.0"
