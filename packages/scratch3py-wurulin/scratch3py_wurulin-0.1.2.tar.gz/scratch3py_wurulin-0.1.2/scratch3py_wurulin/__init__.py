425461783563c96e766093de41e1071099cad6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:18 +0000

    Add package scratch3py-wurulin/0.1.2/scratch3py_wurulin-0.1.2.tar.gz

---
+++ packages/scratch3py-wurulin/scratch3py_wurulin-0.1.2.tar.gz/scratch3py_wurulin-0.1.2/scratch3py_wurulin/__init__.py
@@ -0,0 +1,7 @@
+print('****************************')
+print('*模块名称：scratch3py       *')
+print('*开发作者: 吴儒林           *')
+print('*作者微信：946317637        *')
+print('*作者邮箱：946317637@qq.com *')
+print('****************************')
+from scratch3py.sddd import *
