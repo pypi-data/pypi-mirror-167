d8a6e2bc2019a16587422a8265c2811a8b58ce43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:31 +0000

    Add package calciiical/0.0.1/calciiical-0.0.1.tar.gz

---
+++ packages/calciiical/calciiical-0.0.1.tar.gz/calciiical-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 10',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='calciiical',
+    version='0.0.1',
+    description='this is a trial package used for calculation.',
+    url='',
+    author='D.SaiTvaritha',
+    author_email='tvaritha999@gmail.com',
+    licence='MIT',
+    packages=['calciiical'],
+    install_requires=[]
+)
