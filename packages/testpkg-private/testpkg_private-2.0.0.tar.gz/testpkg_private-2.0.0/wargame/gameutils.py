1235e4afe769dafd9a7496c935fde16e1aa8d809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:46 +0000

    Add package testpkg-private/2.0.0/testpkg_private-2.0.0.tar.gz

---
+++ packages/testpkg-private/testpkg_private-2.0.0.tar.gz/testpkg_private-2.0.0/wargame/gameutils.py
@@ -0,0 +1,15 @@
+import random
+
+
+def weighted_random_selection(obj1, obj2):
+    weighted_list = 3 * [id(obj1)] + 7 * [id(obj2)]
+    selection = random.choice(weighted_list)
+
+    if selection == id(obj1):
+        return obj1
+
+    return obj2
+
+
+def print_bold(msg, end='\n'):
+    print("\033[1m" + msg + "\033[0m", end=end)
