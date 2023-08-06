436bd7969633d6b01036f7133f57bf42b42f45c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:05 +0000

    Add package gym-wofost/1.0.3/gym_wofost-1.0.3-py3-none-any.whl

---
+++ packages/gym-wofost/gym_wofost-1.0.3-py3-none-any.whl/gym_wofost/__init__.py
@@ -0,0 +1,20 @@
+"""
+    gym-wofost package
+    ----------------------------------
+    This package contains the gym-wofost environment, which is a wrapper around the WOFOST simulator to be used in reinforcement learning.
+"""
+__author__ = "Noureddine Ech-chouky"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Noureddine Ech-chouky", "Mohammed Hmimmou", "Hemza Lahkim"]
+__license__ = "None"
+__version__ = "0.0.7"
+__maintainer__ = "Noureddine Ech-chouky"
+__email__ = "noureddine.echchouky@um5r.ac.ma"
+__status__ = "Development"
+
+from gym.envs.registration import register
+
+register(
+    id="wofost-v0",
+    entry_point="gym_wofost.envs:WofostEnv"
+)
