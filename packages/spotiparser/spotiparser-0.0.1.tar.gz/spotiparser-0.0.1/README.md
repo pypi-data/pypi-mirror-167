2dd78ca3b8e9f226cf0dc8c355b1889ccfe064a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:24 +0000

    Add package spotiparser/0.0.1/spotiparser-0.0.1.tar.gz

---
+++ packages/spotiparser/spotiparser-0.0.1.tar.gz/spotiparser-0.0.1/README.md
@@ -0,0 +1,2 @@
+# spotiparser
+Parse and analyze Spotify streaming data downloaded from the Spotify website in your Python code.
---
+++ packages/spotiparser/spotiparser-0.0.1.tar.gz/spotiparser-0.0.1/spotiparser/spotiparser.py
@@ -0,0 +1,11 @@
+def parse(folder_names):
+    """
+    Print a line about parsing from a folder.
+    Args:
+        folder_names (list): name of folder(s) where streaming data is stored
+    Returns:
+        None
+    """
+
+    for name in folder_names:
+        print(f"I'll parse you one day, {name}.")
