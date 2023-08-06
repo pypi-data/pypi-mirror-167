6de68a19bd97cf864260f06fee79e47a00942bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:11 +0000

    Add package dvc-azure/2.20.4/dvc_azure-2.20.4-py3-none-any.whl

---
+++ packages/dvc-azure/dvc_azure-2.20.4-py3-none-any.whl/dvc_azure/tests/test_dvc.py
@@ -0,0 +1,33 @@
+import pytest
+from dvc.testing.test_api import TestAPI  # noqa, pylint: disable=unused-import
+from dvc.testing.test_remote import (  # noqa, pylint: disable=unused-import
+    TestRemote,
+)
+from dvc.testing.test_workspace import TestImport as _TestImport
+
+
+@pytest.fixture
+def remote(make_remote):
+    yield make_remote(name="upstream", typ="azure")
+
+
+@pytest.fixture
+def workspace(make_workspace):
+    yield make_workspace(name="workspace", typ="azure")
+
+
+@pytest.mark.xfail(
+    reason="waiting for https://github.com/fsspec/adlfs/pull/333"
+)
+class TestImport(_TestImport):
+    @pytest.fixture
+    def stage_md5(self):
+        return "2aa17f8daa26996b3f7a4cf8888ac9ac"
+
+    @pytest.fixture
+    def is_object_storage(self):
+        return True
+
+    @pytest.fixture
+    def dir_md5(self):
+        return "ec602a6ba97b2dd07bd6d2cd89674a60.dir"
