7b63a4b06a90f085451b01704cfc9db604788600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:56 +0000

    Add package rambler-google-activator/0.1.2/rambler_google_activator-0.1.2.tar.gz

---
+++ packages/rambler-google-activator/rambler_google_activator-0.1.2.tar.gz/rambler_google_activator-0.1.2/README.md
@@ -0,0 +1,9 @@
+# Rambler.ru Google Activator
+
+## usage: 
+
+``` 
+email =  RamblerEmail("example@rambler.ru", "xxxxxxxxx")
+(status, code) = email.get_code(2)
+```
+
---
+++ packages/rambler-google-activator/rambler_google_activator-0.1.2.tar.gz/rambler_google_activator-0.1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from rambler_google_activator.version import Version
+
+
+setuptools.setup(name='rambler_google_activator',
+                 version=Version('0.1.2').number,
+                 description='Rambler.ru Google Activator',
+                 long_description_content_type="text/markdown",
+                 long_description=open('README.md').read().strip(),
+                 author='@KM8Oz (kmoz000)',
+                 author_email='<kimo@oldi.dev>',
+                 url='https://github.com/KM8Oz/rambler-google-activator.git',
+                 py_modules=['rambler_google_activator'],
+                 install_requires=[],
+                 license='MIT License',
+                 keywords=['Rambler.ru', 'Google', 'Activator', 'Auth'],
+                 classifiers=['Development Status :: 3 - Alpha', 'Topic :: Communications :: Email', 'Topic :: Communications :: Email :: Post-Office :: IMAP'])
