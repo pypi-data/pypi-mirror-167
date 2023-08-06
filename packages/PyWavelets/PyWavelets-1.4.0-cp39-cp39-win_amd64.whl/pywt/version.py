fceff88b7a62c8ad4e406a014e2be0a455f684b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:48 +0000

    Add package PyWavelets/1.4.0/PyWavelets-1.4.0-cp39-cp39-win_amd64.whl

---
+++ packages/PyWavelets/PyWavelets-1.4.0-cp39-cp39-win_amd64.whl/pywt/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYWAVELETS SETUP.PY
+short_version = '1.4.0'
+version = '1.4.0'
+full_version = '1.4.0'
+git_revision = '401e75314b7047d6cd6400aad6df22764bf01f51'
+release = True
+
+if not release:
+    version = full_version
