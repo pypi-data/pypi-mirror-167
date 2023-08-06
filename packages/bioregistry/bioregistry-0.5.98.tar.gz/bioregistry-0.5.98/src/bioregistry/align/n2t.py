5d0c1690919585a4eb9d844090ee6925022797fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:13 +0000

    Add package bioregistry/0.5.98/bioregistry-0.5.98.tar.gz

---
+++ packages/bioregistry/bioregistry-0.5.98.tar.gz/bioregistry-0.5.98/src/bioregistry/align/n2t.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""Align N2T with the Bioregistry."""
+
+from typing import Mapping, Sequence
+
+from bioregistry.align.utils import Aligner
+from bioregistry.external.n2t import get_n2t
+
+
+class N2TAligner(Aligner):
+    """Aligner for the N2T."""
+
+    key = "n2t"
+    getter = get_n2t
+    curation_header = ("name", "homepage", "description")
+
+    def get_skip(self) -> Mapping[str, str]:
+        """Get the prefixes in N2T that should be skipped."""
+        return {
+            "zzztestprefix": "test prefix should not be considered",
+        }
+
+    def get_curation_row(self, external_id, external_entry) -> Sequence[str]:
+        """Prepare curation rows for unaligned BioPortal registry entries."""
+        return [
+            external_entry["name"].strip(),
+            external_entry.get("homepage", "").strip(),
+            external_entry.get("description", "").strip(),
+        ]
+
+
+if __name__ == "__main__":
+    N2TAligner.align()
