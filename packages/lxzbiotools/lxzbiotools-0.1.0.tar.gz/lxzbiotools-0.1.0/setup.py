829950cc1a53a6b56fb077a8c53c2ec0ae09ca25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:09 +0000

    Add package lxzbiotools/0.1.0/lxzbiotools-0.1.0.tar.gz

---
+++ packages/lxzbiotools/lxzbiotools-0.1.0.tar.gz/lxzbiotools-0.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lxzbiotools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['biopython>=1.79,<2.0',
+ 'openpyxl>=3.0.10,<4.0.0',
+ 'pandas>=1.4.4,<2.0.0',
+ 'pathlib>=1.0.1,<2.0.0',
+ 'rich>=12.5.1,<13.0.0',
+ 'typer>=0.6.1,<0.7.0']
+
+setup_kwargs = {
+    'name': 'lxzbiotools',
+    'version': '0.1.0',
+    'description': "Xingze Li's Bioinformatics Analysis Scripts",
+    'long_description': '',
+    'author': 'Xingze_Li',
+    'author_email': 'lixingzee@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
