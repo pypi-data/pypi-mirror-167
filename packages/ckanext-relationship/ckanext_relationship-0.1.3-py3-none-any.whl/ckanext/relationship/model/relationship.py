21c13d7a6a9e4193b1e40d2369eca7ed057a25bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:41 +0000

    Add package ckanext-relationship/0.1.3/ckanext_relationship-0.1.3-py3-none-any.whl

---
+++ packages/ckanext-relationship/ckanext_relationship-0.1.3-py3-none-any.whl/ckanext/relationship/model/relationship.py
@@ -0,0 +1,76 @@
+import ckan.logic as logic
+import ckan.model as model
+from ckan.model.types import make_uuid
+from sqlalchemy import Column, Text, or_
+
+from .base import Base
+
+
+class Relationship(Base):
+    __tablename__ = 'relationship_relationship'
+    id: str = Column(Text, primary_key=True, default=make_uuid)
+    subject_id: str = Column(Text, nullable=False)
+    object_id: str = Column(Text, nullable=False)
+    relation_type: str = Column(Text, nullable=False)
+
+    reverse_relation_type = {
+        'related_to': 'related_to',
+        'child_of': 'parent_of',
+        'parent_of': 'child_of'
+    }
+
+    def __repr__(self):
+        return (
+            'Relationship('
+            f'id={self.id!r},'
+            f'subject_id={self.subject_id!r},'
+            f'object_id={self.object_id!r},'
+            f'relation_type={self.relation_type!r},'
+            ')'
+        )
+
+    def as_dict(self):
+        id = self.id
+        subject_id = self.subject_id
+        object_id = self.object_id
+        relation_type = self.relation_type
+        return ({'id': id,
+                 'subject_id': subject_id,
+                 'object_id': object_id,
+                 'relation_type': relation_type
+                 })
+
+    @classmethod
+    def by_object_id(cls, subject_id, object_id, relation_type):
+        subject_name = _pkg_name_by_id(subject_id)
+        object_name = _pkg_name_by_id(object_id)
+
+        return model.Session.query(cls). \
+            filter(or_(cls.subject_id == subject_id,
+                       cls.subject_id == subject_name)). \
+            filter(or_(cls.object_id == object_id,
+                       cls.object_id == object_name)). \
+            filter(cls.relation_type == relation_type).one_or_none()
+
+    @classmethod
+    def by_object_type(cls, subject_id, object_entity, object_type, relation_type):
+        object_class = logic.model_name_to_class(model, object_entity)
+        subject_name = _pkg_name_by_id(subject_id)
+
+        return model.Session.query(cls). \
+            filter(or_(cls.subject_id == subject_id,
+                       cls.subject_id == subject_name)). \
+            filter(or_(object_class.id == cls.object_id,
+                       object_class.name == cls.object_id)). \
+            filter(object_class.type == object_type). \
+            filter(cls.relation_type == relation_type).distinct().all()
+
+
+def _pkg_name_by_id(pkg_id):
+    """
+    Returns pkg name by its id
+    """
+
+    pkg = model.Session.query(model.Package).filter(model.Package.id == pkg_id).one_or_none()
+    if pkg:
+        return pkg.name
