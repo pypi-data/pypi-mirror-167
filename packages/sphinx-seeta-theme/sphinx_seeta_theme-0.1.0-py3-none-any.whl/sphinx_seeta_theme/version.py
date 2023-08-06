e0892ca67a029b8a4e5806e2ba15458f05aae72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:48 +0000

    Add package sphinx-seeta-theme/0.1.0/sphinx_seeta_theme-0.1.0-py3-none-any.whl

---
+++ packages/sphinx-seeta-theme/sphinx_seeta_theme-0.1.0-py3-none-any.whl/sphinx_seeta_theme/version.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+version = '0.1.0'
+git_version = '5a5cb7a481d78ebfc8d116a97da497a29dc1836d'
