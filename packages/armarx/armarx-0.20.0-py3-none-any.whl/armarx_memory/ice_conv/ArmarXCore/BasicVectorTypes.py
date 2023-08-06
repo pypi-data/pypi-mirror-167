07a51acea8e80cd9364998fc4a6ccf88f1cc417f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:40 +0000

    Add package armarx/0.20.0/armarx-0.20.0-py3-none-any.whl

---
+++ packages/armarx/armarx-0.20.0-py3-none-any.whl/armarx_memory/ice_conv/ArmarXCore/BasicVectorTypes.py
@@ -0,0 +1,1 @@
+from armarx.ice_conv.armarx_core.basic_vector_types import Vector2fConv, Vector3fConv, scale
---
+++ packages/armarx/armarx-0.20.0-py3-none-any.whl/armarx_memory/ice_conv/RobotAPI/PoseBase.py
@@ -0,0 +1,2 @@
+from armarx.ice_conv.robot_api.pose_base import \
+    Vector3BaseConv, QuaternionBaseConv, PoseBaseConv
