e952f3104c8ea385a3b0f4469624ea1e6ef35e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:43 +0000

    Add package ax-platform/0.2.7.2/ax_platform-0.2.7.2-py3-none-any.whl

---
+++ packages/ax-platform/ax_platform-0.2.7.2-py3-none-any.whl/ax/benchmark/tests/test_methods.py
@@ -0,0 +1,26 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from ax.benchmark.methods.modular_botorch import get_sobol_botorch_modular_acquisition
+from ax.modelbridge.registry import Models
+from ax.utils.common.testutils import TestCase
+from botorch.acquisition.knowledge_gradient import qKnowledgeGradient
+
+
+class TestMethods(TestCase):
+    # pyre-fixme[3]: Return type must be annotated.
+    def test_mbm_acquisition(self):
+        method = get_sobol_botorch_modular_acquisition(
+            acquisition_cls=qKnowledgeGradient,
+            acquisition_options={"num_fantasies": 16},
+        )
+        self.assertEqual(method.name, "SOBOL+BOTORCH_MODULAR::qKnowledgeGradient")
+        gs = method.generation_strategy
+        sobol, kg = gs._steps
+        self.assertEqual(kg.model, Models.BOTORCH_MODULAR)
+        model_kwargs = kg.model_kwargs
+        # pyre-fixme[16]: Optional type has no attribute `__getitem__`.
+        self.assertEqual(model_kwargs["botorch_acqf_class"], qKnowledgeGradient)
+        self.assertEqual(model_kwargs["acquisition_options"], {"num_fantasies": 16})
