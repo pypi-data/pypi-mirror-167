d996ad3d21152c07544a7c1302530aed5fd05616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:15 +0000

    Add package featimp/0.1.6/featimp-0.1.6.tar.gz

---
+++ packages/featimp/featimp-0.1.6.tar.gz/featimp-0.1.6/featimp/__init__.py
@@ -0,0 +1,11 @@
+from featimp.base import (
+    get_corr_importances,
+    get_chi2_crosstab_importances,
+    get_chi2_importances,
+    get_anova_importances,
+    get_mutual_info_importances,
+    get_ml_importances,
+    get_permutation_importances,
+    get_feature_importances,
+    get_fi_plots,
+)
