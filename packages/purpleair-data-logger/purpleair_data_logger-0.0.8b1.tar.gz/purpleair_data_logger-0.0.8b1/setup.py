352edf3d231162b3194866fd4193795da3664e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:23 +0000

    Add package purpleair-data-logger/0.0.8b1/purpleair_data_logger-0.0.8b1.tar.gz

---
+++ packages/purpleair-data-logger/purpleair_data_logger-0.0.8b1.tar.gz/purpleair_data_logger-0.0.8b1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from setuptools import setup
+import os
+
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+
+setup(
+    name="purpleair_data_logger",
+    version="0.0.8b1",
+    license="MIT",
+    author="Carlos Santos",
+    author_email="27721404+carlkid1499@users.noreply.github.com",
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    packages=['purpleair_data_logger'],
+    url="https://github.com/carlkid1499/purpleair_data_logger",
+    keywords=["purpleair_data_logger", "purpleair", "data logger",
+              "PurpleAirPSQLDataLogger", "PurpleAirCSVDataLogger"],
+    install_requires=[
+        "pg8000",
+        "requests"
+    ],
+)
