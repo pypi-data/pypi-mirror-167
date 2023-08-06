44f4d58e271a970ed02f75f359f670e9a645a5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:17:52 +0000

    Add package browserist/1.1.0/browserist-1.1.0.tar.gz

---
+++ packages/browserist/browserist-1.1.0.tar.gz/browserist-1.1.0/src/browserist/helper/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["date_time", "directory", "file", "image", "operating_system", "timeout", "url", "window_handle", "xpath"]
+
+from . import date_time, directory, file, image, operating_system, timeout, url, window_handle, xpath
---
+++ packages/browserist/browserist-1.1.0.tar.gz/browserist-1.1.0/src/browserist/model/browser/base/settings.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+from ....constant import directory
+from ....helper import operating_system
+from ....model.viewport.device import DeviceViewport
+from .page_load_strategy import PageLoadStrategy
+from .timeout.settings import TimeoutSettings
+from .type import BrowserType
+
+
+@dataclass(kw_only=True, slots=True)
+class BrowserSettings:
+    """Class to configure the browser driver.
+
+    headless: If enabled, note that some interactable methods, e.g. "select", aren't supported.
+
+    screenshot_dir: Destination directory for screenshot files. If not set, default directory is from where the script is executed."""
+
+    type: BrowserType = BrowserType.EDGE if operating_system.is_windows() else BrowserType.CHROME
+    headless: bool = False
+    disable_images: bool = False
+    page_load_strategy: PageLoadStrategy = PageLoadStrategy.NORMAL
+    path_to_executable: str | None = None
+    screenshot_dir: str = directory.CURRENT
+    timeout: TimeoutSettings = TimeoutSettings()
+    viewport: DeviceViewport | tuple[int, int] | None = None
