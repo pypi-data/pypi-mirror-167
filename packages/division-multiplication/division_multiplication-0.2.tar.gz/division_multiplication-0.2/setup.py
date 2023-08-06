c70f073e380b495acd321603ddd2ba3fc245c191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:17 +0000

    Add package division-multiplication/0.2/division_multiplication-0.2.tar.gz

---
+++ packages/division-multiplication/division_multiplication-0.2.tar.gz/division_multiplication-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(name='division_multiplication',
+      version='0.2',
+      author='Love Kadiya',
+      description='Division and Multiplication by three updated',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['division_multiplication'],
+      author_email='love.kadiya@acquaintsoft.com',
+      zip_safe=False)
