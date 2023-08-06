2f4825a00a2ee7ff3e292cc8b89acae0271611ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:35 +0000

    Add package MySEAS/0.0.1.11/MySEAS-0.0.1.11.tar.gz

---
+++ packages/MySEAS/MySEAS-0.0.1.11.tar.gz/MySEAS-0.0.1.11/SEAS/Engine/Core/input.py
@@ -0,0 +1,12 @@
+from SEAS.Engine.Setup import *
+
+
+class Input:
+    def start(self):
+        self.keys = pygame.key.get_pressed()
+
+    def updateBefore(self):
+        self.keys = pygame.key.get_pressed()
+
+    def updateAfter(self):
+        pass
