5dc69653639a60f6d2b34d761697a0fda1a91d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:20 +0000

    Add package net-automation-usman-u/0.0.18/net_automation_usman_u-0.0.18-py3-none-any.whl

---
+++ packages/net-automation-usman-u/net_automation_usman_u-0.0.18-py3-none-any.whl/net_automation/erxtest.py
@@ -0,0 +1,33 @@
+import net_automation
+
+# net_automation.Vyos.deploy_yaml("erxtest.yml")
+
+# erx_lan = net_automation.EdgeOS(
+#     "ubiquiti_edgerouter",
+#     "erx.zahid.lan",
+#     "usman",
+#     "",
+#     True,
+#     r"c:\Users\Usman\.ssh\id_rsa",
+# )
+
+# erx_lan.init_ssh()
+# print (erx_lan.get_ospf_neighbours())
+# # print (erx_lan.get_ospf_route())
+
+# print (erx_lan.get_ospf_route("10.100.100.4"))
+# print (erx_lan.get_ospf_route("10.100.100.1"))
+
+
+edge = net_automation.Vyos(
+    "vyos",
+    "edge.dn42.lan",
+    "test",
+    "test",
+    False,
+    "",
+)
+print (edge.get_route_table(modidier="ospf", test="t"))
+# edge.init_ssh()
+# print (edge.get_ospf_neighbours())
+# print (edge.get_route("10.0.10.1"))
