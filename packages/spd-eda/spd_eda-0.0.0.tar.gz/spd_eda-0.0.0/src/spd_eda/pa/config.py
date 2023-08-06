8e5f633e4fd3861f8078b50e4113019da83f90c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:39 +0000

    Add package spd-eda/0.0.0/spd_eda-0.0.0.tar.gz

---
+++ packages/spd-eda/spd_eda-0.0.0.tar.gz/spd_eda-0.0.0/src/spd_eda/pa/config.py
@@ -0,0 +1,40 @@
+DB_SERVER = 'pa-va-sql02'
+PLOTS_DIR_PATH = './tmp_plots'
+INS_STATS_FORMATTING_DICT = {
+    'EExp': '#,##0',
+    'EP': '#,##0',
+    'IL': '#,##0',
+    'CC': '#,##0',
+    'CC_pct': '0.00%',
+
+    'Freq': '0.00',
+    'Sev': '#,##0',
+    'LC': '#,##0',
+    'LR': '0.00%',
+
+    'Freq_rel': '0.00',
+    'Sev_rel': '0.00',
+    'LC_rel': '0.00',
+    'LR_rel': '0.00',
+
+    'Loss': '#,##0',
+    'Eval': '#,##0',
+    'Avg_Loss': '#,##0',
+    'Avg_Eval': '#,##0',
+    'Ratio': '0.00%',
+    'Avg_Loss_rel': '0.00',
+    'Avg_Eval_rel': '0.00',
+    'Ratio_rel': '0.00',
+
+    'mean_loss': '#,##0',
+    'mean_eval': '#,##0',
+    'weighted_mean_ratios': '0.00%',
+    'unweighted_mean_ratios': '0.00%',
+
+    'mean_loss_rel': '0.00',
+    'weighted_mean_ratios_rel': '0.00',
+    'mean_eval_rel': '0.00',
+    'weighted_mean_ratios_rel': '0.00',
+    'unweighted_mean_ratios_rel': '0.00',
+
+}
