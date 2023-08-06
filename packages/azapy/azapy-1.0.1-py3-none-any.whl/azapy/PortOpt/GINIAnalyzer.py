5c9dca29f921d6f84be889c8254f817d44917531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:34 +0000

    Add package azapy/1.0.1/azapy-1.0.1-py3-none-any.whl

---
+++ packages/azapy/azapy-1.0.1-py3-none-any.whl/azapy/PortOpt/GINIAnalyzer.py
@@ -0,0 +1,532 @@
+import numpy as np
+import scipy.sparse as sps
+import warnings
+
+from ._RiskAnalyzer import _RiskAnalyzer
+from ._solvers import _lp_solver
+
+class GINIAnalyzer(_RiskAnalyzer):
+    """
+    GINI based optimal portfolio strategies.
+    
+    Methods:
+        * getWeights
+        * getRisk
+        * getPositions
+        * viewForntiers
+        * set_rrate
+        * set_mktdata
+        * set_rtype
+        * set_random_seed
+    """
+    def __init__(self, mktdata=None, colname='adjusted', freq='Q', 
+                 hlength=1.25, calendar=None, 
+                 rtype='Sharpe', method='ecos'):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        `mktdata` : `pandas.DataFrame`, optional
+            Historic daily market data for portfolio components in the format
+            returned by `azapy.mktData` function. The default is `None`.
+        `colname` : str, optional
+            Name of the price column from mktdata used in the weights 
+            calibration. The default is `'adjusted'`.
+        `freq` : str, optional
+            Rate of return horizon in number of business day. it could be 
+            'Q' for quarter or 'M' for month. The default is `'Q'`.
+        `hlength` : float, optional
+            History length in number of years used for calibration. A 
+            fractional number will be rounded to an integer number of months.
+            The default is `1.25` years.
+        `calendar` : `numpy.busdaycalendar`, optional
+            Business days calendar. If is it `None` then the calendar will 
+            be set to NYSE business calendar.
+            The default is `None`.
+        `rtype` : str, optional
+            Optimization type. Possible values \n
+                'Risk' : minimization of dispersion (risk) measure for  
+                targeted rate of return. \n
+                'Sharpe' : maximization of generalized Sharpe ratio.\n
+                'Sharpe2' : minimization of the inverse generalized Sharpe 
+                ratio.\n
+                'MinRisk' : minimum dispersion (risk) portfolio.\n
+                'InvNrisk' : optimal portfolio with the same dispersion (risk)
+                value as a benchmark portfolio 
+                (e.g. equal weighted portfolio).\n
+                'RiskAverse' : optimal portfolio for a fixed value of 
+                risk-aversion factor.
+            The default is `'Sharpe'`.
+        `method` : str, optional
+            Linear programming numerical method. 
+            Could be: 'ecos', 'highs-ds', 'highs-ipm', 'highs', 
+            'interior-point', 'glpk' and 'cvxopt'.
+            The defualt is `'ecos'`.
+            
+        Returns
+        -------
+        The object.
+        """
+        self.drate = None
+        self.nn2 = None
+        super().__init__(mktdata, colname, freq, hlength, calendar, rtype)
+        
+        self._set_method(method)
+        
+        
+    def _set_method(self, method):
+        self._set_lp_method(method)
+        
+    
+    def set_rrate(self, rrate):
+        """
+        Sets the MkT Data.
+
+        Parameters
+        ----------
+        rrate : pandas.DataFrame
+            Market data. It will overwrite the value set by the constructor.
+        Returns
+        -------
+        None.
+        """
+        self.nn, self.mm = rrate.shape
+        self.muk = rrate.mean()
+        self.rrate = rrate.copy()
+        
+        self.nn2 = int(self.nn * (self.nn - 1) / 2)
+        yy = []
+        for m in range(self.mm):
+            x = self.rrate.iloc[:,m]
+            yy.append([x[i] - x[j] \
+                       for i in range(self.nn - 1) \
+                       for j in range(i + 1, self.nn)])
+        self.drate = np.concatenate(yy)
+        
+        
+    def _risk_calc(self, prate, alpha):
+        nn = len(prate)
+ 
+        y = np.sort(prate, kind='heapsort')
+        gini = np.sum([y[i] * ( 2 * i - nn + 1) for i in range(nn)]) / nn / nn
+        # status, gini, gini
+        return 0, gini, gini
+    
+    
+    def _risk_min(self, d=1):
+        # Order of variables (mm - no of symb, nn - no of observations)
+        # w <- [0 : mm]
+        # s <- [mm : mm + nn2]
+        # in total dim = mm + nn2 
+        # where nn2 = nn * (nn - 1) / 2
+        mm = self.mm
+        nn2 = self.nn2
+        norm = 1 / self.nn ** 2
+        
+        # build c
+        c_data = [0.] * mm + [norm] * nn2
+        
+        # bild G
+        G_icol = [m for m in range(mm) for _ in range(nn2)] * 2
+        G_irow = list(range(nn2)) * mm + list(range(nn2, 2 * nn2)) * mm
+        G_data = list(self.drate) + list(-self.drate)
+        
+        G_icol += list(range(mm, mm + nn2)) * 2
+        G_irow += list(range(nn2 * 2))
+        G_data += [-1.] * (nn2 * 2)
+        
+        G_icol += list(range(mm))
+        G_irow += [nn2 * 2] * mm
+        G_data += list(-self.muk * d)
+        
+        G_icol += list(range(mm + nn2))
+        G_irow += list(range(nn2 * 2 + 1, nn2 * 3 + 1 + mm))
+        G_data += [-1.] * (mm + nn2)
+        
+        G_shape = (nn2 * 3 + 1 + mm, mm + nn2)
+        G = sps.coo_matrix((G_data, (G_irow, G_icol)), G_shape)
+   
+        # build h
+        h_data = [0.] * (nn2 * 2) + [-self.mu * d] + [0.] * (mm + nn2)
+        
+        # build A
+        A_icol = list(range(mm))
+        A_irow = [0] * mm
+        A_data = [1.] * mm
+        A_shape = (1, mm + nn2)
+        A = sps.coo_matrix((A_data, (A_irow, A_icol)), A_shape)
+        
+        # build b
+        b_data = [1.]
+        
+        # calc
+        res = _lp_solver(self.method, c_data, G, h_data, A, b_data)
+ 
+        self.status = res['status']
+        if self.status != 0:
+            warnings.warn(f"Warning {res['status']}: {res['infostring']} "
+                        + f"on calibration date {self.rrate.index[-1]}")
+            return np.array([np.nan] * mm)
+            
+        # GINI
+        self.risk = res['pcost']
+        # optimal weights
+        self.ww = np.array(res['x'][:mm])
+        self.ww.shape = mm
+        # rate of return
+        self.RR = np.dot(self.ww, self.muk)
+        # defalut to risk
+        self.primary_risk_comp = np.array([self.risk])
+        self.secondary_risk_comp = np.array([self.risk])
+        
+        return self.ww
+    
+    
+    def _sharpe_max(self):
+        # Order of variables (mm - no of symb, nn - no of observations)
+        # w <- [0 : mm]
+        # s <- [mm : mm + nn2]
+        # t <- mm + nn2
+        # in total dim = mm + nn2 + 1
+        # where nn2 = nn * (nn - 1) / 2
+        mm = self.mm
+        nn2 = self.nn2
+        norm = 1 / self.nn ** 2
+        
+        # build c
+        c_data = list(-self.muk) + [0.] * nn2 + [self.mu]
+        
+        # bild G
+        G_icol = [m for m in range(mm) for _ in range(nn2)] * 2
+        G_irow = list(range(nn2)) * mm + list(range(nn2, 2 * nn2)) * mm
+        G_data = list(self.drate) + list(-self.drate)
+        
+        G_icol += list(range(mm, mm + nn2)) * 2
+        G_irow += list(range(nn2 * 2))
+        G_data += [-1.] * (nn2 * 2)
+        
+        G_icol += list(range(mm))
+        G_irow += list(range(nn2 * 2, nn2 * 2 + mm))
+        G_data += [-1.] * mm
+        
+        G_shape = (nn2 * 2 + mm , mm + nn2 + 1)
+        G = sps.coo_matrix((G_data, (G_irow, G_icol)), G_shape)
+        
+        # build h
+        h_data = [0.] * (nn2 * 2 + mm) 
+        
+        # build A
+        A_icol = list(range(mm, mm + nn2)) + list(range(mm)) + [mm + nn2] 
+        A_irow = [0] * nn2 + [1] * (mm + 1)
+        A_data = [norm] * nn2 + [1.] * mm + [-1.]
+        
+        A_shape = (2, mm + nn2 + 1)
+        A = sps.coo_matrix((A_data, (A_irow, A_icol)), A_shape)
+        
+        # build b
+        b_data = [1., 0.]
+        
+        # calc
+        res = _lp_solver(self.method, c_data, G, h_data, A, b_data)
+ 
+        self.status = res['status']
+        if self.status != 0:
+            warnings.warn(f"Warning {res['status']}: {res['infostring']} "
+                        + f"on calibration date {self.rrate.index[-1]}")
+            return np.array([np.nan] * mm)
+            
+        t = res['x'][-1]
+        # Gini-Sharpe
+        self.sharpe = -res['pcost']
+        # GINI
+        self.risk = 1. / t
+        # optimal weights
+        self.ww = np.array(res['x'][:mm] / t)
+        self.ww.shape = mm
+        # rate of return
+        self.RR = -res['pcost'] / t + self.mu
+        # default to risk
+        self.primary_risk_comp = np.array([self.risk])
+        self.secondary_risk_comp = np.array([self.risk])
+        
+        return self.ww
+    
+    
+    def _sharpe_inv_min(self):
+        # Order of variables (mm - no of symb, nn - no of observations)
+        # w <- [0 : mm]
+        # s <- [mm : mm + nn2]
+        # t <- mm + nn2
+        # in total dim = mm + nn2 + 1
+        # where nn2 = nn * (nn - 1) / 2
+        mm = self.mm
+        nn2 = self.nn2
+        norm = 1 / self.nn ** 2
+        
+        # build c
+        c_data = [0.] * mm + [norm] * nn2 + [0.]
+        
+        # build G
+        G_icol = [m for m in range(mm) for _ in range(nn2)] * 2
+        G_irow = list(range(nn2)) * mm + list(range(nn2, 2 * nn2)) * mm
+        G_data = list(self.drate) + list(-self.drate)
+        
+        G_icol += list(range(mm, mm + nn2)) * 2
+        G_irow += list(range(nn2 * 2))
+        G_data += [-1.] * (nn2 * 2)
+        
+        G_icol += list(range(mm))
+        G_irow += list(range(nn2 * 2, nn2 * 2 + mm))
+        G_data += [-1.] * mm
+        
+        G_shape = (nn2 * 2 + mm, mm + nn2 + 1)
+        G = sps.coo_matrix((G_data, (G_irow, G_icol)), G_shape)
+        
+        # build h
+        h_data = [0.] * (nn2 * 2 + mm) 
+        
+        # build A
+        A_icol = (list(range(mm)) + [mm + nn2]) * 2
+        A_irow = [0] * (mm + 1) + [1] * (mm + 1)
+        A_data = list(self.muk) + [-self.mu] + [1.] * mm + [-1.]
+        
+        A_shape = (2, mm + nn2 + 1)
+        A = sps.coo_matrix((A_data, (A_irow, A_icol)), A_shape)
+        
+        # build b
+        b_data = [1., 0.]
+        
+        # calc
+        res = _lp_solver(self.method, c_data, G, h_data, A, b_data)
+ 
+        self.status = res['status']
+        if self.status != 0:
+            warnings.warn(f"Warning {res['status']}: {res['infostring']} "
+                        + f"on calibration date {self.rrate.index[-1]}")
+            return np.array([np.nan] * mm)
+            
+        t = res['x'][-1]
+        # Gini-Sharpe
+        self.sharpe = 1. / res['pcost']
+        # GINI
+        self.risk = res['pcost'] / t
+        # optimal weights
+        self.ww = np.array(res['x'][:mm] / t)
+        self.ww.shape = mm
+        # rate of return
+        self.RR = 1. / t + self.mu
+        # default to risk
+        self.primary_risk_comp = np.array([self.risk])
+        self.secondary_risk_comp = np.array([self.risk])
+        
+        return self.ww
+    
+    
+    def _rr_max(self):
+        # Order of variables (mm - no of symb, nn - no of observations)
+        # w <- [0 : mm]
+        # s <- [mm : mm + nn2]
+        # in total dim = mm + nn2
+        # where nn2 = nn * (nn - 1) / 2
+        mm = self.mm
+        nn2 = self.nn2
+        norm = 1 / self.nn ** 2
+        
+        # build c
+        c_data = list(-self.muk) + [0.] * nn2
+        
+        # build G
+        G_icol = [m for m in range(mm) for _ in range(nn2)] * 2
+        G_irow = list(range(nn2)) * mm + list(range(nn2, 2 * nn2)) * mm
+        G_data = list(self.drate) + list(-self.drate)
+        
+        G_icol += list(range(mm, mm + nn2)) * 2
+        G_irow += list(range(nn2 * 2))
+        G_data += [-1.] * (nn2 * 2)
+        
+        G_icol += list(range(mm))
+        G_irow += list(range(nn2 * 2, nn2 * 2 + mm))
+        G_data += [-1.] * mm
+        
+        G_shape = (nn2 * 2 + mm, mm + nn2)
+        G = sps.coo_matrix((G_data, (G_irow, G_icol)), G_shape)
+        
+        # build h
+        h_data = [0.] * (nn2 * 2 + mm) 
+        
+        # build A
+        A_icol = list(range(mm)) + list(range(mm, mm + nn2))
+        A_irow = [0] * mm + [1] * nn2
+        A_data = [1.] * mm + [norm] * nn2
+        
+        A_shape = (2, mm + nn2)
+        A = sps.coo_matrix((A_data, (A_irow, A_icol)), A_shape)
+        
+        # build b
+        b_data = [1., self.risk]
+        
+        # calc
+        res = _lp_solver(self.method, c_data, G, h_data, A, b_data)
+ 
+        self.status = res['status']
+        if self.status != 0:
+            warnings.warn(f"Warning {res['status']}: {res['infostring']} "
+                        + f"on calibration date {self.rrate.index[-1]}")
+            return np.array([np.nan] * mm)
+            
+        # optimal weights
+        self.ww = np.array(res['x'][:mm])
+        self.ww.shape = mm
+        # rate of return
+        self.RR = -res['pcost']
+        # default to risk
+        self.primary_risk_comp = np.array([self.risk])
+        self.secondary_risk_comp = np.array([self.risk])
+        
+        return self.ww
+
+
+    def _risk_averse(self):
+        # Order of variables (mm - no of symb, nn - no of observations)
+        # w <- [0 : mm]
+        # s <- [mm : mm + nn2]
+        # in total dim = mm + nn2 
+        # where nn2 = nn * (nn - 1) / 2
+        mm = self.mm
+        nn2 = self.nn2
+        norm = 1 / self.nn ** 2
+        
+        # build c
+        c_data = list(-self.muk) + [self.Lambda * norm] * nn2
+        
+        # bild G
+        G_icol = [m for m in range(mm) for _ in range(nn2)] * 2
+        G_irow = list(range(nn2)) * mm + list(range(nn2, 2 * nn2)) * mm
+        G_data = list(self.drate) + list(-self.drate)
+        
+        G_icol += list(range(mm, mm + nn2)) * 2
+        G_irow += list(range(nn2 * 2))
+        G_data += [-1.] * (nn2 * 2)
+        
+        G_icol += list(range(mm))
+        G_irow += list(range(nn2 * 2, nn2 * 2 + mm))
+        G_data += [-1.] * (mm)
+        
+        G_shape = (nn2 * 2 + mm, mm + nn2)
+        G = sps.coo_matrix((G_data, (G_irow, G_icol)), G_shape)
+        
+        # build h
+        h_data = [0.] * (nn2 * 2 + mm)
+        
+        # build A
+        A_icol = list(range(mm))
+        A_irow = [0] * mm
+        A_data = [1.] * mm
+        
+        A_shape = (1, mm + nn2)
+        A = sps.coo_matrix((A_data, (A_irow, A_icol)), A_shape)
+        
+        # build b
+        b_data = [1.]
+        
+        # calc
+        res = _lp_solver(self.method, c_data, G, h_data, A, b_data)
+ 
+        self.status = res['status']
+        if self.status != 0:
+            warnings.warn(f"Warning {res['status']}: {res['infostring']} "
+                        + f"on calibration date {self.rrate.index[-1]}")
+            return np.array([np.nan] * mm)
+            
+        # optimal weights
+        self.ww = np.array(res['x'][:mm])
+        self.ww.shape = mm
+        # rate of return
+        self.RR = np.dot(self.ww, self.muk)
+        # GINI
+        self.risk = (res['pcost'] + self.RR) / self.Lambda
+        # default to risk
+        self.primary_risk_comp = np.array([self.risk])
+        self.secondary_risk_comp = np.array([self.risk])
+        
+        return self.ww
+    
+    
+    def _risk_diversification(self, d=1):
+        # Order of variables (mm - no of symb, nn - no of observations)
+        # w <- [0 : mm]
+        # s <- [mm : mm + nn2]
+        # t <- mm + nn2
+        # in total dim = mm + nn2 + 1
+        # where nn2 = nn * (nn - 1) / 2
+        mm = self.mm
+        nn2 = self.nn2
+        norm = 1 / self.nn ** 2
+        
+        # build c
+        c_data = [0.] * mm + [norm] * nn2 + [0.]
+        
+        # build G
+        G_icol = [m for m in range(mm) for _ in range(nn2)] * 2
+        G_irow = list(range(nn2)) * mm + list(range(nn2, 2 * nn2)) * mm
+        G_data = list(self.drate) + list(-self.drate)
+        
+        G_icol += list(range(mm, mm + nn2)) * 2
+        G_irow += list(range(nn2 * 2))
+        G_data += [-1.] * (nn2 * 2)
+        
+        G_icol += list(range(mm)) + [mm + nn2]
+        G_irow += [nn2 * 2] * (mm + 1)
+        G_data += list(-self.muk * d) + [self.mu * d]
+        
+        G_icol += list(range(mm))
+        G_irow += list(range(nn2 * 2 + 1, nn2 * 2 + mm + 1))
+        G_data += [-1.] * mm
+        
+        G_shape = (nn2 * 2 + mm + 1, mm + nn2 + 1)
+        G = sps.coo_matrix((G_data, (G_irow, G_icol)), G_shape)
+        
+        # build h
+        h_data = [0.] * (nn2 * 2 + mm + 1) 
+        
+        # build A
+        A_icol = list(range(mm))
+        A_irow = [0] * mm
+        A_data = list(self.risk_comp)
+        
+        A_icol += list(range(mm)) + [mm + nn2]
+        A_irow += [1] * (mm + 1)
+        A_data += [1.] * mm + [-1.]
+        
+        A_shape = (2, mm + nn2 + 1)
+        A = sps.coo_matrix((A_data, (A_irow, A_icol)), A_shape)
+        
+        # build b
+        b_data = [1., 0.]
+        
+        # calc
+        res = _lp_solver(self.method, c_data, G, h_data, A, b_data)
+ 
+        self.status = res['status']
+        if self.status != 0:
+            warnings.warn(f"Warning {res['status']}: {res['infostring']} "
+                        + f"on calibration date {self.rrate.index[-1]}")
+            return np.array([np.nan] * mm)
+            
+        t = res['x'][-1]
+        # Gini-Divers
+        self.divers = 1. - res['pcost']
+        # Gini
+        self.risk = res['pcost'] / t
+        # optimal weights
+        self.ww = np.array(res['x'][:mm] / t)
+        self.ww.shape = mm
+        # rate of return
+        self.RR = np.dot(self.ww, self.muk)
+        # default to risk
+        self.primary_risk_comp = np.array([self.risk])
+        self.secondary_risk_comp = np.array([self.risk])
+        
+        return self.ww
+        
---
+++ packages/azapy/azapy-1.0.1-py3-none-any.whl/azapy/PortOpt/Port_MV.py
@@ -0,0 +1,88 @@
+from .Port_CVaR import Port_CVaR
+from .MVAnalyzer import MVAnalyzer
+
+class Port_MV(Port_CVaR):
+    """
+    Backtesting MV optimal portfolio strategies, periodically rebalanced.
+
+    Methods:
+        * set_model
+        * get_port
+        * get_nshares
+        * get_weights
+        * get_account
+        * get_mktdata
+        * port_view
+        * port_view_all
+        * port_drawdown
+        * port_perf
+        * port_annual_returns
+        * port_monthly_returns
+        * port_period_returns
+    """
+    def set_model(self, rtype='Sharpe', mu=None, mu0=0, aversion=None, 
+                  ww0=None, hlength=3.25, method='ecos'):
+        """
+        Sets model parameters and evaluates portfolio time-series.
+
+        Parameters
+        ----------.
+        `rtype` : : str, optional
+            Optimization type. Possible values \n
+                'Risk' : minimization of dispersion (risk) measure for a fixed 
+                vale of expected rate of return. \n
+                'Sharpe' : maximization of generalized Sharpe ratio.\n
+                'Sharpe2' : minimization of the inverse generalized Sharpe 
+                ratio.\n
+                'MinRisk' : optimal portfolio with minimum dispersion (risk) 
+                value.\n
+                'InvNRisk' : optimal portfolio with the same dispersion (risk)
+                as the targeted portfolio
+                (e.g. equal weighted portfolio). \n
+                'RiskAverse' : optimal portfolio for a fixed value of risk 
+                aversion coefficient.
+            The default is 'Sharpe'.
+        `mu` : float, optional
+            Targeted portfolio expected rate of return. 
+            Relevant only if `rtype='Risk'`
+            The default is `None`.
+        `mu0` : float, optional
+            Risk-free rate accessible to the investor.
+            Relevant only if `rype='Sharpe'` or `rtype='Sharpe2'`.
+            The default is `0`.
+        `aversion` : float, optional
+            The value of the risk-aversion coefficient.
+            Must be positive. Relevant only if `rtype='RiskAvers'`.
+            The default is `None`.
+        `ww0` : list (also 'numpy.array` or `pandas.Series`), optional
+            Targeted portfolio weights. 
+            Relevant only if `rype='InvNrisk'`.
+            Its length must be equal to the number of
+            symbols in rrate (mktdata). 
+            All weights must be >= 0 with sum > 0.
+            If it is a list or a `numpy.array` then the weights are assumed to
+            by in order of `rrate.columns`. If it is a `pandas.Series` the 
+            index should be compatible with the `rrate.columns` or mktdata 
+            symbols (same symbols, not necessary in the same order).
+            If it is `None` then it will be set to equal weights.
+            The default is `None`.
+        `hlength` : float, optional
+            The length in year of the historical calibration period relative
+            to 'Dfix'. A fractional number will be rounded to an integer number
+            of months. The default is 3.25 years.
+        `method` : str, optional
+            Numerical method to solve the SOCP and QP. It can be 'ecos' or 
+            'cvxopt'. The default is 'ecos'.
+
+        Returns
+        -------
+        pandas.DataFrame
+            The portfolio time-series in the format "date", "pcolname".
+        """
+        return super().set_model(rtype=rtype, mu=mu, mu0=mu0, 
+                                 aversion=aversion, ww0=ww0,
+                                 hlength=hlength, method=method)
+
+
+    def _wwgen(self):
+        return MVAnalyzer(rtype=self.rtype, method=self.method)
