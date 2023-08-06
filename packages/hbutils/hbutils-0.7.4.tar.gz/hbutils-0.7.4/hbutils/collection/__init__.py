609c9e883befa80d00d73763a4f566cbfb64f69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:21 +0000

    Add package hbutils/0.7.4/hbutils-0.7.4.tar.gz

---
+++ packages/hbutils/hbutils-0.7.4.tar.gz/hbutils-0.7.4/hbutils/collection/__init__.py
@@ -0,0 +1,9 @@
+"""
+Overview:
+    Collection module, include the basic utilities with collections like dict, list and tuple.
+"""
+from .dimension import *
+from .functional import *
+from .sequence import *
+from .stacked import *
+from .structural import *
