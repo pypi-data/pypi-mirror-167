3d6ecfbf4f628cea45f0100eb7f63daf7695fe58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:04 +0000

    Add package test-packaging-chmforte/0.0.1/test_packaging_chmforte-0.0.1.tar.gz

---
+++ packages/test-packaging-chmforte/test_packaging_chmforte-0.0.1.tar.gz/test_packaging_chmforte-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+        name="test_packaging_chmforte", 
+        version=VERSION,
+        author="Christopher M. Forte",
+        author_email="christopher.forte1@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],        
+        keywords=['python', 'first package'],
+        classifiers= []
+)
