89a74cb4babf4aa675124b5657cd0a2d489ea56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:34 +0000

    Add package stamdata3/1.0.2/stamdata3-1.0.2-py3-none-any.whl

---
+++ packages/stamdata3/stamdata3-1.0.2-py3-none-any.whl/stamdata3/__init__.py
@@ -0,0 +1,6 @@
+from .Employment import Employment
+from .Organisation import Organisation
+from .Relation import Relation
+from .Resource import Resource
+from .XMLDataHelper import XMLDataHelper
+from .stamdata3 import Stamdata3
