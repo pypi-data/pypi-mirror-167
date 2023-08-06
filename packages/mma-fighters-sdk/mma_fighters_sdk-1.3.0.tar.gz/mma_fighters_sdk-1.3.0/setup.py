aaa4644ff256d802300ea236ef059cd31b47fa82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:04 +0000

    Add package mma-fighters-sdk/1.3.0/mma_fighters_sdk-1.3.0.tar.gz

---
+++ packages/mma-fighters-sdk/mma_fighters_sdk-1.3.0.tar.gz/mma_fighters_sdk-1.3.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='mma_fighters_sdk',
+    version='1.3.0',
+    description='SDK for mma_fighters_sdk',
+    url='https://github.com/hallrizon-io/mma_fighters_game_sdk',
+    author='Kostiantyn Minkov',
+    author_email='hallrizon.usa@gmail.com',
+    license='BSD 2-clause',
+    packages=[],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.6"
+)
+
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
