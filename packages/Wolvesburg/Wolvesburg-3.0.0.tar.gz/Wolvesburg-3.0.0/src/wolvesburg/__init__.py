5f8482fee95ba84207ed035e03196ac522c8a507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:08 +0000

    Add package Wolvesburg/3.0.0/Wolvesburg-3.0.0.tar.gz

---
+++ packages/Wolvesburg/Wolvesburg-3.0.0.tar.gz/Wolvesburg-3.0.0/src/wolvesburg/__init__.py
@@ -0,0 +1,34 @@
+import pickle
+listy = [[]]
+global listyval
+listyval = 0
+
+
+def get(filename: str):
+    filename += '.wolvesburg'
+    f = open(filename, 'rb')
+    data = pickle.load(f)
+    f.close()
+    return data
+
+
+class Wolvesburg:
+    def __init__(self):
+        pass
+
+
+def deposit(filename: str, data: Wolvesburg):
+    filename += '.wolvesburg'
+    f = open(filename, 'wb')
+    pickle.dump(data, f)
+    f.close()
+
+
+def importxt(filename, object:Wolvesburg):
+    filename += '.txt'
+    f = open(filename, 'r')
+    for i in f:
+        listy[listyval].append(i)
+    listyval += 1
+    object.txt = listy
+
