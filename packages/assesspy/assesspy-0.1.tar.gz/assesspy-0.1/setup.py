8e8c80374f86207cd37005ac8bd82f4e2ac503ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:25 +0000

    Add package assesspy/0.1/assesspy-0.1.tar.gz

---
+++ packages/assesspy/assesspy-0.1.tar.gz/assesspy-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="assesspy",
+    version="0.1",
+    description="General purpose Python package for measuring assessment performance",
+    url="https://gitlab.com/ccao-data-science---modeling/packages/assesspy/",
+    author="CCAO",
+    author_email="assessor.data@cookcountyil.gov",
+    license="GPL-3",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "pyarrow",
+        "numpy",
+        "scipy",
+        "sklearn",
+        "statsmodels"
+        ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"],
+    include_package_data = True,
+    package_data={
+        "": ["*.parquet"],
+    }
+    )
