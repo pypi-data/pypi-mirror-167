f0fd93e0ec39fddf29951933e3af863bddc9b4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:55 +0000

    Add package schwifty/2022.9.0/schwifty-2022.9.0.tar.gz

---
+++ packages/schwifty/schwifty-2022.9.0.tar.gz/schwifty-2022.9.0/scripts/get_bank_registry_se.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import json
+import pandas
+
+
+# https://www.swedishbankers.se/fraagor-vi-arbetar-med/betalningar/ny-nordisk-betalningsinfrastruktur/iban-och-svenskt-nationellt-kontonummer/
+URL = (
+    "https://www.swedishbankers.se/media/4863/"
+    "kalkylblad-i-iban-och-svenskt-nationellt-kontonummer-2021-02-16.xlsx"
+)
+
+def process():
+    registry = []
+
+    datas = pandas.read_excel(URL, skiprows=2, sheet_name=0, dtype=str)
+    datas.fillna("", inplace=True)
+
+    for row in datas.itertuples(index=False):
+        bank_code, bic, name = row[:3]
+        registry.append(
+            {
+                "country_code": "SE",
+                "primary": True,
+                "bic": str(bic).upper(),
+                "bank_code": str(bank_code).split(".", 1)[0],
+                "name": str(name).strip(),
+                "short_name": str(name).strip(),
+            }
+        )
+
+    print(f"Fetched {len(registry)} bank records")
+    return registry
+
+
+if __name__ == "__main__":
+    with open("schwifty/bank_registry/generated_se.json", "w") as fp:
+        json.dump(process(), fp, indent=2)
