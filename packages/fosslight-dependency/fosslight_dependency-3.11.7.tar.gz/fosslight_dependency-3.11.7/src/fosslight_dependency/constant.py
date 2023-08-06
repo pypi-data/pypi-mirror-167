02fd77c46d3a933f5e12bb2c3bbc6b3758c23036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:44 +0000

    Add package fosslight-dependency/3.11.7/fosslight_dependency-3.11.7.tar.gz

---
+++ packages/fosslight-dependency/fosslight_dependency-3.11.7.tar.gz/fosslight_dependency-3.11.7/src/fosslight_dependency/constant.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021 LG Electronics Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# System platform name
+WINDOWS = 'Windows'
+LINUX = 'Linux'
+MACOS = 'Darwin'
+
+# Package manager name
+PYPI = 'pypi'
+NPM = 'npm'
+MAVEN = 'maven'
+GRADLE = 'gradle'
+PUB = 'pub'
+COCOAPODS = 'cocoapods'
+ANDROID = 'android'
+SWIFT = 'swift'
+CARTHAGE = 'carthage'
+GO = 'go'
+
+# Supported package name and manifest file
+SUPPORT_PACKAE = {
+    PYPI: ['requirements.txt', 'setup.py'],
+    NPM: 'package.json',
+    MAVEN: 'pom.xml',
+    GRADLE: 'build.gradle',
+    PUB: 'pubspec.yaml',
+    COCOAPODS: 'Podfile.lock',
+    ANDROID: 'build.gradle',
+    SWIFT: 'Package.resolved',
+    CARTHAGE: 'Cartfile.resolved',
+    GO: 'go.mod'
+}
+
+# default android app name
+default_app_name = 'app'
