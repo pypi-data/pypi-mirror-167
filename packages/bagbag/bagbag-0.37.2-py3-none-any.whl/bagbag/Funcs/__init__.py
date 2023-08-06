64b30d865fa23d391c0f8a31b1d4ba55b5a0dba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:12 +0000

    Add package bagbag/0.37.2/bagbag-0.37.2-py3-none-any.whl

---
+++ packages/bagbag/bagbag-0.37.2-py3-none-any.whl/bagbag/Funcs/__init__.py
@@ -0,0 +1,6 @@
+from .Wget import Wget
+from .IPAddressConvert import Int2IP, IP2Int
+from .ResizeImage import ResizeImage
+from .GetPublicIP import GetPublicIP
+from .Ping import Ping
+from .CutChinese import CutChinese
