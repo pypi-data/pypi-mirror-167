e2f6ab3e54d656a3fbf1ad1218536224f87b5d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:36 +0000

    Add package chex/0.1.5/chex-0.1.5-py3-none-any.whl

---
+++ packages/chex/chex-0.1.5-py3-none-any.whl/chex/_src/pytypes.py
@@ -0,0 +1,44 @@
+# Copyright 2020 DeepMind Technologies Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Pytypes for arrays and scalars."""
+
+from typing import Any, Iterable, Mapping, Union
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+Array = jnp.ndarray
+ArrayBatched = jax.interpreters.batching.BatchTracer
+ArrayNumpy = np.ndarray
+ArraySharded = jax.interpreters.pxla.ShardedDeviceArray
+# Use this type for type annotation. For instance checking,  use
+# `isinstance(x, jax.DeviceArray)`.
+# `jax.interpreters.xla._DeviceArray` appears in jax > 0.2.5
+if hasattr(jax.interpreters.xla, '_DeviceArray'):
+  ArrayDevice = jax.interpreters.xla._DeviceArray  # pylint:disable=protected-access
+else:
+  ArrayDevice = jax.interpreters.xla.DeviceArray
+
+Scalar = Union[float, int]
+Numeric = Union[Array, Scalar]
+PRNGKey = jax.random.KeyArray
+PyTreeDef = type(jax.tree_util.tree_structure(None))
+Shape = jax.core.Shape
+
+Device = jax.lib.xla_extension.Device
+
+ArrayTree = Union[Array, Iterable['ArrayTree'], Mapping[Any, 'ArrayTree']]
+
+ArrayDType = Any
