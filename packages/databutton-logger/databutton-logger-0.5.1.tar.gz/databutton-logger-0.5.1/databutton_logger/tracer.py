f3ef9c6378c0e48ce163ae48f31e2ab323c436ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:48 +0000

    Add package databutton-logger/0.5.1/databutton-logger-0.5.1.tar.gz

---
+++ packages/databutton-logger/databutton-logger-0.5.1.tar.gz/databutton-logger-0.5.1/databutton_logger/tracer.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.cloud_trace_propagator import CloudTraceFormatPropagator
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+
+def add_traces_to_fastapi(app: FastAPI):
+    set_global_textmap(CloudTraceFormatPropagator())
+
+    tracer_provider = TracerProvider()
+    cloud_trace_exporter = CloudTraceSpanExporter()
+    tracer_provider.add_span_processor(
+        # BatchSpanProcessor buffers spans and sends them in batches in a
+        # background thread. The default parameters are sensible, but can be
+        # tweaked to optimize your performance
+        BatchSpanProcessor(cloud_trace_exporter)
+    )
+
+    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer_provider)
