85a99511375a234bd2e69b30d7936457a703c662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:42 +0000

    Add package humblepy/0.5.0/humblepy-0.5.0-py3-none-any.whl

---
+++ packages/humblepy/humblepy-0.5.0-py3-none-any.whl/humblepy/validate/__init__.py
@@ -0,0 +1,4 @@
+from humblepy.validate.functions import dataframes_are_equal
+
+# https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
