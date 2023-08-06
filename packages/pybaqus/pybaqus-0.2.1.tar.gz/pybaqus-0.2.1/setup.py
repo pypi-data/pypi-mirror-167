7b0076c19bd6315d8ff7b672057b455103b6ae64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:33 +0000

    Add package pybaqus/0.2.1/pybaqus-0.2.1.tar.gz

---
+++ packages/pybaqus/pybaqus-0.2.1.tar.gz/pybaqus-0.2.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pybaqus', 'pybaqus.plot_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.2.1,<4.0.0',
+ 'numpy>=1.18.2,<2.0.0',
+ 'pyvista>=0.31.0,<0.32.0',
+ 'scipy>=1.4.1,<2.0.0',
+ 'tqdm>=4.62.3,<5.0.0',
+ 'vtk>=9.0.3,<10.0.0']
+
+setup_kwargs = {
+    'name': 'pybaqus',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Cristóbal Tapia Camú',
+    'author_email': 'crtapia@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.7,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
