f2456f6fe2afb8cda9e609b5dd82f5114758876d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:39 +0000

    Add package dl-common/0.0.1/dl_common-0.0.1.tar.gz

---
+++ packages/dl-common/dl_common-0.0.1.tar.gz/dl_common-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='dl_common',
+    version='0.0.1',
+    license='MIT',
+    author="Zaheer ud Din Faiz",
+    author_email='zaheer@datalogz.io',
+    packages=['dl_common'],
+)
