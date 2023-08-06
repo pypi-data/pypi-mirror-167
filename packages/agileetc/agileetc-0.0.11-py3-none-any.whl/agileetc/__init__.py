e047f1aa8b752d9afa1a9186519e770b64a0a29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:22 +0000

    Add package agileetc/0.0.11/agileetc-0.0.11-py3-none-any.whl

---
+++ packages/agileetc/agileetc-0.0.11-py3-none-any.whl/agileetc/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__version__ = '0.0.11'
+
+import os
+from pathlib import Path
+
+# Get the location of this module, useful for builtin file operations.
+MODULE_PATH = Path(os.path.realpath(__file__)).parent
+
+# Define exit codes.
+EXITCODE = -1
