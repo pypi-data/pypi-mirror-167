6eb344d95e971e3a023c68c6575233a0d8081633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:07 +0000

    Add package katatachi/1.0.1/katatachi-1.0.1-py3-none-any.whl

---
+++ packages/katatachi/katatachi-1.0.1-py3-none-any.whl/katatachi/mod_view/__init__.py
@@ -0,0 +1,5 @@
+from .mod_view_query import ModViewQuery
+from .mod_view_query import NamedModViewColumn
+from .mod_view_renderer import ModViewRenderer
+from .mod_view_store import ModViewStore
+from .mod_view_blueprint import mod_view_blueprint_factory
