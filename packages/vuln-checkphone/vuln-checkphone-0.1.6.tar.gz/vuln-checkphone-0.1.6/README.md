0f6617f5bdd700cbbc6b3ac94980e215028bd69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:14 +0000

    Add package vuln-checkphone/0.1.6/vuln-checkphone-0.1.6.tar.gz

---
+++ packages/vuln-checkphone/vuln-checkphone-0.1.6.tar.gz/vuln-checkphone-0.1.6/README.md
@@ -0,0 +1,5 @@
+# CheckPhone
+
+This library is a vulnerable implementation of a supposed phone number validator/checker that calls subprocess in an unsecure way if phone number is provided by the user.
+
+The reason for creating this is to simulate an RCE in a Django web app in an incident handling exercise.
---
+++ packages/vuln-checkphone/vuln-checkphone-0.1.6.tar.gz/vuln-checkphone-0.1.6/checkphone/checkphone.py
@@ -0,0 +1,24 @@
+import logging
+import subprocess
+import inspect
+import os
+
+logger = logging.getLogger(__name__)
+
+# Just simulating an RCE bug in Tienda, where we use an imaginary third party library "CheckPhone" to do some sort of
+# phone number validation. This bug is of the simplest possible kind, user input passed to subprocess
+
+
+def check_it(mobile_number: str):
+    logger.info(f"I am checking: {mobile_number}")
+    if len(mobile_number) >= 8:
+        try:
+            my_dir = os.path.dirname(inspect.getfile(check_it))
+
+            subprocess.check_output(f"{my_dir}/libs/checkphone {mobile_number}", shell=True)
+        except subprocess.CalledProcessError:
+            return False
+        return True
+
+
+check_it("123456789")
