e49642c5877a6168b6a2bd54ce596c3b27b9e37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:31 +0000

    Add package oracle-ads/2.6.4/oracle_ads-2.6.4.tar.gz

---
+++ packages/oracle-ads/oracle_ads-2.6.4.tar.gz/oracle_ads-2.6.4/ads/model/deployment/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*--
+
+# Copyright (c) 2021, 2022 Oracle and its affiliates.
+# Copyright (c) 2021, 2022 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+from .model_deployer import ModelDeployer
+from .model_deployment import (
+    DEFAULT_POLL_INTERVAL,
+    DEFAULT_WAIT_TIME,
+    DEFAULT_CONTENT_TYPE_JSON,
+    DEFAULT_CONTENT_TYPE_BYTES,
+    ModelDeployment,
+)
+from .model_deployment_properties import ModelDeploymentProperties
