65ba3eb41bf2bcfe76fa3987de19498273c62d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:14 +0000

    Add package sabiranjeDvaBroja/0.0.1/sabiranjeDvaBroja-0.0.1.tar.gz

---
+++ packages/sabiranjeDvaBroja/sabiranjeDvaBroja-0.0.1.tar.gz/sabiranjeDvaBroja-0.0.1/setup.py
@@ -0,0 +1,35 @@
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Sabiranje dva broja'
+LONG_DESCRIPTION = 'Sabiranje dva broja pypy package'
+
+# Setting up
+setup(
+    name="sabiranjeDvaBroja",
+    version=VERSION,
+    author="davidas96 (David Dujakovic)",
+    author_email="daviddujakovic996@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pytest'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
