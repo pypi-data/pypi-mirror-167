be8189abebf66afd922b1dc1d876eedaa5ff2e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:55 +0000

    Add package beat/1.2.1/beat-1.2.1-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/beat/beat-1.2.1-cp39-cp39-macosx_10_9_x86_64.whl/beat/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+project_root = '/Users/runner/work/beat/beat'
+git_sha1 = '5f6c14f6b61a49444ecde5593f4cbd3867892b5f'
+local_modifications = False
+version = '1.2.1'
+long_version = 'beat-1.2.1-5f6c14f6b61a49444ecde5593f4cbd3867892b5f-2022-09-14_15:30:57'  # noqa
+installed_date = '2022-09-14_15:30:57'
