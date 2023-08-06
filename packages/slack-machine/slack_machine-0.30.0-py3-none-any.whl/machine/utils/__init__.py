c29760944032bef737efcf229242db12b6b865fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:47 +0000

    Add package slack-machine/0.30.0/slack_machine-0.30.0-py3-none-any.whl

---
+++ packages/slack-machine/slack_machine-0.30.0-py3-none-any.whl/machine/utils/__init__.py
@@ -0,0 +1,15 @@
+def sizeof_fmt(num, suffix="B"):
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return "{:.1f}{}{}".format(num, "Yi", suffix)
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
