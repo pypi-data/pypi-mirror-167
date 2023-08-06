c7f6a3cc2d57f1fb1d1f52d5850cf8ee018d9208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:29 +0000

    Add package lib-ms-api/1.0.0/lib_ms_api-1.0.0.tar.gz

---
+++ packages/lib-ms-api/lib_ms_api-1.0.0.tar.gz/lib_ms_api-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='lib_ms_api',
+    packages=find_packages(include=['lib_ms_api']),
+    version='1.0.0',
+    description='A Python library to build simple Microservices',
+    author_email='uedsonreis@gmail.com',
+    author='Uedson Reis',
+    license='ISC',
+)
