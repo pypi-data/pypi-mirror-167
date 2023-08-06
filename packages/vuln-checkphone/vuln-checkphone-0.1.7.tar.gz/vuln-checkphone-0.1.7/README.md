be551a18b45885012449615f589907b5b75bb1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:03 +0000

    Add package vuln-checkphone/0.1.7/vuln-checkphone-0.1.7.tar.gz

---
+++ packages/vuln-checkphone/vuln-checkphone-0.1.7.tar.gz/vuln-checkphone-0.1.7/README.md
@@ -0,0 +1,10 @@
+# CheckPhone
+
+This library is a vulnerable implementation of a supposed phone number validator/checker that calls subprocess in an unsecure way if phone number is provided by the user.
+
+The reason for creating this is to simulate an RCE in a Django web app in an incident handling exercise.
+
+
+## Upload new package to PyPI
+- Bump version in setup.py
+- 
---
+++ packages/vuln-checkphone/vuln-checkphone-0.1.7.tar.gz/vuln-checkphone-0.1.7/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="vuln-checkphone",
+    version="0.1.7",
+    description="YEah",
+    packages=["checkphone"],
+    package_data={'': ['libs/checkphone']},
+    include_package_data=True,
+    author='Jarle Kittilsen',
+    author_email='jarle.spam@gmail.com',
+    license='MIT License',
+
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ]
+)
