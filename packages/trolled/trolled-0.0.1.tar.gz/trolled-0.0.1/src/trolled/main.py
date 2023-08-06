e25b766fa96cd234292d986da93bb68be07eeda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:52 +0000

    Add package trolled/0.0.1/trolled-0.0.1.tar.gz

---
+++ packages/trolled/trolled-0.0.1.tar.gz/trolled-0.0.1/src/trolled/main.py
@@ -0,0 +1,3 @@
+print("bruh why are you on the 0.0.1 release. you do realize this is called t r o l l e d right? 😵‍💫")
+print("its fine though, the next releases will be better :D")
+print("like anyone reads this ...")
