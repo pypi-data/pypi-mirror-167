65f3bb6e999539b876c89cd6620f61d247e0030b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:14 +0000

    Add package nb2workflow/1.3.34/nb2workflow-1.3.34.tar.gz

---
+++ packages/nb2workflow/nb2workflow-1.3.34.tar.gz/nb2workflow-1.3.34/tests/test_cwl.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+import pytest
+
+
+@pytest.mark.cwl
+def test_cwl(test_notebook):
+    cwl_fn = "test.cwl"
+    import nb2workflow.cwl as cwl
+
+    cwl.nb2cwl(test_notebook, cwl_fn)
+
+    subprocess.check_call(["cwl-runner", cwl_fn])
+
+
+@pytest.mark.cwl
+def test_cwl_odakb(test_notebook):
+    cwl_fn = "test.cwl"
+    import nb2workflow.cwl as cwl
+
+    cwl.nb2cwl(test_notebook, "test.cwl", nbrunner_module="odakb.evaluator")
+
+    subprocess.check_call(["cwl-runner", cwl_fn])
