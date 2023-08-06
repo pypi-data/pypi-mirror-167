8189d7c3a1acb0d7d8896215353ef263fe6cd79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:17 +0000

    Add package nandini/0.0.1/nandini-0.0.1.tar.gz

---
+++ packages/nandini/nandini-0.0.1.tar.gz/nandini-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+core_requirements = [ 
+    'numpy==1.23.2',
+    'regex==2022.9.13',
+    'tqdm==4.64.1',
+    'joblib==1.1.0',
+    'nltk==3.7',
+]
+
+setuptools.setup(
+    name='nandini',
+    description="A Scratch Module for Machine learning",
+    url="",
+    author='Sanjaypranav',
+    author_email='sanjaypriya195@gmail.com',
+    version='0.0.1',
+    install_requires=core_requirements,
+    python_requires='>=3.7,<3.10',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=['nandini']
+    )
