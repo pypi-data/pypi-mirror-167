c9febc1bb9fa51543d2eb0224d054a005ed04406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:55 +0000

    Add package nso-bridge/0.1.1/nso_bridge-0.1.1.tar.gz

---
+++ packages/nso-bridge/nso_bridge-0.1.1.tar.gz/nso_bridge-0.1.1/nso_bridge/__init__.py
@@ -0,0 +1,11 @@
+
+__version__ = '0.1.1'
+
+from nso_bridge.nsa import NintendoSwitchAccount
+from nso_bridge.nso import NintendoSwitchOnlineAPI
+
+
+__all__ = [
+    "NintendoSwitchAccount",
+    "NintendoSwitchOnlineAPI",
+]
