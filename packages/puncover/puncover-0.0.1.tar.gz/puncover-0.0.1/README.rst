c4afa485f324089bae1459a54d75bc32d32899fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:42 +0000

    Add package puncover/0.0.1/puncover-0.0.1.tar.gz

---
+++ packages/puncover/puncover-0.0.1.tar.gz/puncover-0.0.1/README.rst
@@ -0,0 +1,13 @@
+.. image:: https://img.shields.io/travis/HBehrens/puncover.svg
+    :target: https://travis-ci.org/HBehrens/puncover
+.. image:: https://img.shields.io/codecov/c/github/HBehrens/puncover.svg
+    :target: https://codecov.io/gh/HBehrens/puncover
+
+puncover
+========
+
+.. image:: https://raw.githubusercontent.com/HBehrens/puncover/master/images/overview.png
+
+Analyzes C/C++ binaries for code size, static variables and stack usages.
+It creates a report with dissambler and call-stack analysis per directory, file, or function.
+
---
+++ packages/puncover/puncover-0.0.1.tar.gz/puncover-0.0.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup, find_packages, Command
+
+__version__ = None  # Overwritten by executing version.py.
+with open('puncover/version.py') as f:
+    exec (f.read())
+
+with open('requirements.txt') as f:
+    requires = f.readlines()
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    # http://stackoverflow.com/a/3780822/196350
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
+
+setup(name='puncover',
+      version=__version__,
+      description='Analyses C/C++ build output for code size, static variables, and stack usage.',
+      long_description=open('README.rst').read(),
+      url='https://github.com/hbehrens/puncover',
+      download_url='https://github.com/hbehrens/puncover/tarball/%s' % __version__,
+      author='Heiko Behrens',
+      license='MIT',
+      packages=find_packages(exclude=['tests', 'tests.*']),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={'console_scripts': ['puncover = puncover.puncover:main']},
+      install_requires=requires,
+      test_suite='nose.collector',
+      cmdclass={
+          'clean': CleanCommand,
+      }
+      )
