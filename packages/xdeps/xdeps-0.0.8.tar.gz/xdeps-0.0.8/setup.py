ba7a3fcc4df99ea8b12e7c37d9960ccf64c6e2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:25 +0000

    Add package xdeps/0.0.8/xdeps-0.0.8.tar.gz

---
+++ packages/xdeps/xdeps-0.0.8.tar.gz/xdeps-0.0.8/setup.py
@@ -0,0 +1,26 @@
+# copyright ############################### #
+# This file is part of the Xdeps Package.   #
+# Copyright (c) CERN, 2021.                 #
+# ######################################### #
+
+from setuptools import setup, find_packages
+
+setup(
+        name='xdeps',
+        version='0.0.8',
+        description='Data dependency manager',
+        long_description=("Data dependency manager\n"
+                         "\nThis package is part of the Xsuite collection."),
+        author='Riccardo De Maria',
+        author_email='riccardo.de.maria@cern.ch',
+        packages=find_packages(),
+        install_requires=['lark'],
+        url='https://xsuite.readthedocs.io/',
+        license='Apache 2.0',
+        download_url="https://pypi.python.org/pypi/xdeps",
+        project_urls={
+            "Bug Tracker": "https://github.com/xsuite/xsuite/issues",
+            "Documentation": 'https://xsuite.readthedocs.io/',
+            "Source Code": "https://github.com/xsuite/xdeps",
+        },
+)
