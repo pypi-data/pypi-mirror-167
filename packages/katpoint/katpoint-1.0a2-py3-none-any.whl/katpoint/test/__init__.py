2e6566b6d3afdf8402029f8f0e5bd069dfc40f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:25 +0000

    Add package katpoint/1.0a2/katpoint-1.0a2-py3-none-any.whl

---
+++ packages/katpoint/katpoint-1.0a2-py3-none-any.whl/katpoint/test/__init__.py
@@ -0,0 +1,26 @@
+"""Unit test suite based on pytest."""
+
+import pytest
+
+pytest.register_assert_rewrite("katpoint.test.helper")
+
+# Automatically added by katversion
+__version__ = '1.0a1'
+
+# Automatically added by katversion
+__version__ = '0.9.dev800+spaza.296.fix.ci.6525422.dirty'
+
+# Automatically added by katversion
+__version__ = '0.9.dev801+spaza.296.fix.ci.e2971e0.dirty'
+
+# Automatically added by katversion
+__version__ = '1.1a1.dev822+spaza.287.astrometry.test.bcd0ab5'
+
+# Automatically added by katversion
+__version__ = '0.9.dev822+spaza.287.astrometry.test.bcd0ab5'
+
+# Automatically added by katversion
+__version__ = '0.9.dev833+master.7b8e351'
+
+# Automatically added by katversion
+__version__ = '1.0a2'
