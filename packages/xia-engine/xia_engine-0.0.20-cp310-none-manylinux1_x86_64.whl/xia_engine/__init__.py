3dc2b5c018b608530873ca5a9c64f746b85c95b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:34 +0000

    Add package xia-engine/0.0.20/xia_engine-0.0.20-cp310-none-manylinux1_x86_64.whl

---
+++ packages/xia-engine/xia_engine-0.0.20-cp310-none-manylinux1_x86_64.whl/xia_engine/__init__.py
@@ -0,0 +1,12 @@
+from xia_engine.base import Base, BaseDocument, BaseEmbeddedDocument
+from xia_engine.fields import EmbeddedDocumentField, ReferenceField, ListField
+from xia_engine.document import Document, EmbeddedDocument
+
+
+__all__ = [
+    "Base", "BaseDocument", "BaseEmbeddedDocument",
+    "EmbeddedDocumentField", "ReferenceField", "ListField",
+    "Document", "EmbeddedDocument",
+]
+
+__version__ = "0.0.20"
