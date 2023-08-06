d25f5d49db59d53e8267159d6659f2fc0f462276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:50 +0000

    Add package mne-nirs/0.3.0/mne_nirs-0.3.0-py3-none-any.whl

---
+++ packages/mne-nirs/mne_nirs-0.3.0-py3-none-any.whl/mne_nirs/datasets/__init__.py
@@ -0,0 +1,9 @@
+"""Download useful datasets."""
+
+# Authors: Robert Luke <mail@robertluke.net>
+#
+# License: BSD (3-clause)
+
+from . import fnirs_motor_group
+from . import block_speech_noise
+from . import audio_or_visual_speech
---
+++ packages/mne-nirs/mne_nirs-0.3.0-py3-none-any.whl/mne_nirs/io/snirf/__init__.py
@@ -0,0 +1,5 @@
+# Author: Robert Luke <mail@robertluke.net>
+#
+# License: BSD (3-clause)
+
+from ._snirf import write_raw_snirf, SPEC_FORMAT_VERSION
