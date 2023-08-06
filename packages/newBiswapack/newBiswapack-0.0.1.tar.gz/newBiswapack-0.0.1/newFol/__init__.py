26c90f5f14f7fffc0b2c1515ead2c51ae1ed9472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:32 +0000

    Add package newBiswapack/0.0.1/newBiswapack-0.0.1.tar.gz

---
+++ packages/newBiswapack/newBiswapack-0.0.1.tar.gz/newBiswapack-0.0.1/newFol/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 15 09:18:43 2022
+
+@author: bppsa
+"""
+
---
+++ packages/newBiswapack/newBiswapack-0.0.1.tar.gz/newBiswapack-0.0.1/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 15 09:45:16 2022
+
+@author: bppsa
+"""
+
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 11',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='newBiswapack',
+    version='0.0.1',
+    description='BMI TRIAL PACKAGE.',
+    url='',
+    author='BISWATOSH PURKAYASTHA',
+    author_email='bppsah@gmail.com',
+    licence='MIT',
+    packages=['newFol'],
+    install_requires=[]
+)
