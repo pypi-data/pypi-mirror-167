355533a86a960d8366ee6fca1bf67e018678d73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:57 +0000

    Add package multibotkit/0.0.8/multibotkit-0.0.8-py3-none-any.whl

---
+++ packages/multibotkit/multibotkit-0.0.8-py3-none-any.whl/multibotkit/dispatchers/telegram.py
@@ -0,0 +1,38 @@
+from multibotkit.dispatchers.base_dispatcher import BaseDispatcher
+from multibotkit.schemas.telegram.incoming import Update
+
+
+class TelegramDispatcher(BaseDispatcher):
+
+    async def process_event(
+        self, event: Update
+    ):
+        if event.message is not None:
+            sender_id = event.message.from_.id
+        if event.callback_query is not None:
+            sender_id = event.callback_query.from_.id
+        
+        state_id = f"telegram_{sender_id}"
+        state_object = await self.state_manager.get_state(state_id)
+
+        for (func, state_func, handler) in self._handlers:
+            
+            state_func_result = True
+            if state_func is not None:
+                try:
+                    state_func_result = state_func(state_object)
+                except Exception:
+                    continue
+            
+            func_result = True
+            if func is not None:
+                try:
+                    func_result = func(event)
+                except Exception:
+                    continue
+
+            summary_result = state_func_result * func_result
+            
+            if summary_result:
+                await handler(event, state_object)
+                return
---
+++ packages/multibotkit/multibotkit-0.0.8-py3-none-any.whl/multibotkit/dispatchers/viber.py
@@ -0,0 +1,32 @@
+from multibotkit.dispatchers.base_dispatcher import BaseDispatcher
+from multibotkit.schemas.viber.incoming import Callback
+
+
+class ViberDispatcher(BaseDispatcher):
+    async def process_event(
+        self, event: Callback
+    ):
+        state_id = f"viber_{event.user_id}"
+        state_object = await self.state_manager.get_state(state_id)
+
+        for (func, state_func, handler) in self._handlers:
+            
+            state_func_result = True
+            if state_func is not None:
+                try:
+                    state_func_result = state_func(state_object)
+                except Exception:
+                    continue
+            
+            func_result = True
+            if func is not None:
+                try:
+                    func_result = func(event)
+                except Exception:
+                    continue
+
+            summary_result = state_func_result * func_result
+
+            if summary_result:
+                await handler(event, state_object)
+                return
