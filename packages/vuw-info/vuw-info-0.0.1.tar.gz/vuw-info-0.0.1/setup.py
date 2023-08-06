fddf8e5a40cbe3a92cbd5ee541e8505bf5b24c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:44 +0000

    Add package vuw-info/0.0.1/vuw-info-0.0.1.tar.gz

---
+++ packages/vuw-info/vuw-info-0.0.1.tar.gz/vuw-info-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python Information'
+LONG_DESCRIPTION = 'A python module to extract computer information'
+
+# Setting up
+setup(
+    name="vuw-info",
+    version=VERSION,
+    author="Hazzahs",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['gspread', 'pywin32', 'pycryptodome'],
+    keywords=['python', 'information', 'osint', 'pentester', 'ngoto'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
