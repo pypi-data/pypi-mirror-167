2707c33b583654178cb617f720763a5e8aaaf2a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:02 +0000

    Add package TimeConvert/3.0.1/TimeConvert-3.0.1.tar.gz

---
+++ packages/TimeConvert/TimeConvert-3.0.1.tar.gz/TimeConvert-3.0.1/TimeConvert/__init__.py
@@ -0,0 +1,13 @@
+from TimeConvert.convert import TimeConvert
+from TimeConvert.month import Month
+from TimeConvert.quarter import Quarter
+from TimeConvert.stopwatch import StopWatch, TimeType
+
+
+__all__ = [
+    'TimeConvert',
+    'Month',
+    'Quarter',
+    'StopWatch',
+    'TimeType'
+]
