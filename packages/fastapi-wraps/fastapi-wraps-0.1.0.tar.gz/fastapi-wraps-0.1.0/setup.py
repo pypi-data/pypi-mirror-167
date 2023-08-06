c2415e604a4097db6350ac71ed60dda451dd9b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:40 +0000

    Add package fastapi-wraps/0.1.0/fastapi-wraps-0.1.0.tar.gz

---
+++ packages/fastapi-wraps/fastapi-wraps-0.1.0.tar.gz/fastapi-wraps-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastapi_wraps']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.83.0,<0.84.0']
+
+setup_kwargs = {
+    'name': 'fastapi-wraps',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '# fastapi-wraps\n',
+    'author': 'Paweł Rubin',
+    'author_email': 'pawelrubindev@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/pawelrubin/fastapi-wraps',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
