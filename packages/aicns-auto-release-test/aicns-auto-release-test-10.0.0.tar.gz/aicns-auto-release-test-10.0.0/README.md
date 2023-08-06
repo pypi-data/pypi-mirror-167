80618ceb770028336ca52ee305bffa5e80050ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:17 +0000

    Add package aicns-auto-release-test/10.0.0/aicns-auto-release-test-10.0.0.tar.gz

---
+++ packages/aicns-auto-release-test/aicns-auto-release-test-10.0.0.tar.gz/aicns-auto-release-test-10.0.0/README.md
@@ -0,0 +1,2 @@
+# test-pipeline-as-code
+a
---
+++ packages/aicns-auto-release-test/aicns-auto-release-test-10.0.0.tar.gz/aicns-auto-release-test-10.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+__version__ = "10.0.0"
+
+setup(
+   name="aicns-auto-release-test",
+   version=__version__,
+   # And so on...
+)
