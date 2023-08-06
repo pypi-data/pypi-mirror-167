7e60198331c2bfcf8e11ad9dcbd733cd62b7383f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:06 +0000

    Add package novelpy/1.2.1/novelpy-1.2.1.tar.gz

---
+++ packages/novelpy/novelpy-1.2.1.tar.gz/novelpy-1.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='novelpy',
+  version='1.2.1',
+  description='Calculate novelty indicators',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Pelletier Pierre and Wirtz Kevin',
+  author_email='kevin.wirtz@unistra.fr',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Novelty, scientometrics', 
+  packages=find_packages(),
+  install_requires=['pymongo','joblib','sklearn','glob2','tqdm','networkx','python-louvain',
+  'numpy','pickle-mixin','scipy','pandas','tqdm','multiprocess','pyyaml','spacy','scispacy','cycler','thinc',
+  'wosfile','seaborn'] 
+)
