edc20f4c156917483f661ca88b7ebec53cef4751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:07 +0000

    Add package DMLABBasicCal/0.0.3/DMLABBasicCal-0.0.3.tar.gz

---
+++ packages/DMLABBasicCal/DMLABBasicCal-0.0.3.tar.gz/DMLABBasicCal-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='DMLABBasicCal',
+  version='0.0.3',
+  description='A very basic function that used python wrapped C++ calculators (ADD and SUB)',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Ayush Kanyal',
+  author_email='kanyal.lavi@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
