f49009eb0dce0543db1a63e86c4428b2a9275d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:41 +0000

    Add package offchain/0.1.0/offchain-0.1.0.tar.gz

---
+++ packages/offchain/offchain-0.1.0.tar.gz/offchain-0.1.0/offchain/metadata/adapters/base_adapter.py
@@ -0,0 +1,32 @@
+from typing import Union
+
+from requests.adapters import (
+    BaseAdapter as RequestsBaseAdapter,
+    HTTPAdapter as RequestsHTTPAdapter,
+)
+from urllib3.util.retry import Retry
+
+
+class BaseAdapter(RequestsBaseAdapter):
+    """Base Adapter inheriting from requests BaseAdapter"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+
+class HTTPAdapter(RequestsHTTPAdapter):
+    """HTTP Adapter inheriting from requests HTTPAdapter"""
+
+    def __init__(
+        self,
+        pool_connections: int = ...,
+        pool_maxsize: int = ...,
+        max_retries: Union[Retry, int, None] = ...,
+        pool_block: bool = ...,
+        *args,
+        **kwargs
+    ) -> None:
+        super().__init__(pool_connections, pool_maxsize, max_retries, pool_block)
+
+
+Adapter = Union[BaseAdapter, HTTPAdapter]
