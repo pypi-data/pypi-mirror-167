ef46b958b3e55edae2c0ecd22b1aadcdce8b400a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:53 +0000

    Add package quanario/1.0.24/quanario-1.0.24.tar.gz

---
+++ packages/quanario/quanario-1.0.24.tar.gz/quanario-1.0.24/quanario/__init__.py
@@ -0,0 +1,5 @@
+from user import *
+from input_message import *
+# from .message_extensions.carousel import (Carousel, CarouselElement, )
+# from .message_extensions.keyboard import (Keyboard, VkKeyboardButton, )
+from .message_extensions import (Carousel, CarouselElement, Keyboard, VkKeyboardButton,)
