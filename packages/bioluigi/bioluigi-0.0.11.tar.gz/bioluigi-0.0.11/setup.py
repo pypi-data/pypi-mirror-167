68c3d00d72804963207848ee6740541e47dd3ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:52 +0000

    Add package bioluigi/0.0.11/bioluigi-0.0.11.tar.gz

---
+++ packages/bioluigi/bioluigi-0.0.11.tar.gz/bioluigi-0.0.11/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import sys
+
+# Luigi 3+ does not support Python 2
+if sys.version_info.major == 2:
+    luigi_dep = 'luigi~=2.8'
+else:
+    luigi_dep = 'luigi'
+
+setup(name='bioluigi',
+      version='0.0.11',
+      description='Reusable and maintained Luigi tasks to incorporate in bioinformatics pipelines',
+      long_description='file: README.md',
+      url='https://github.com/pavlidisLab/bioluigi',
+      license='Apache-2.0',
+      license_files=('LICENSE',),
+      author='Guillaume Poirier-Morency',
+      author_email='poirigui@msl.ubc.ca',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'bioluigi = bioluigi.cli:main']},
+          install_requires=['click', luigi_dep, 'requests', 'babel'])
