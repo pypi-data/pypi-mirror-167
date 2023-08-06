29ec5fc1c54d85699c2864454bfbc955e5b68fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:57 +0000

    Add package EsosaAI-probability/1.1/EsosaAI_probability-1.1.tar.gz

---
+++ packages/EsosaAI-probability/EsosaAI_probability-1.1.tar.gz/EsosaAI_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name= "EsosaAI_probability",
+        version= "1.1",
+        description = "Guassian and Binomial distributions",
+        packages= ["EsosaAI_probability"],
+        author= "Esosa Asemota",
+        author_email= "a_esosa@yahoo.com",
+        zip_safe= False)
