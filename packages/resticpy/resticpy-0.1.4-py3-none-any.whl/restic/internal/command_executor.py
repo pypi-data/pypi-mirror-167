ffd83624bd16b42ef2ad7bae91df2f0a5c2921c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:55 +0000

    Add package resticpy/0.1.4/resticpy-0.1.4-py3-none-any.whl

---
+++ packages/resticpy/resticpy-0.1.4-py3-none-any.whl/restic/internal/command_executor.py
@@ -0,0 +1,28 @@
+import logging
+import subprocess
+
+import restic.errors
+
+logger = logging.getLogger(__name__)
+
+
+def execute(cmd):
+    logger.debug('Executing restic command: %s', str(cmd))
+    try:
+        process = subprocess.run(cmd,
+                                 capture_output=True,
+                                 text=True,
+                                 encoding='utf-8')
+    except FileNotFoundError as e:
+        raise restic.errors.NoResticBinaryEror(
+            'Cannot find restic installed') from e
+
+    logger.debug('Restic command completed with return code %d',
+                 process.returncode)
+
+    if process.returncode != 0:
+        raise restic.errors.ResticFailedError(
+            'Restic failed with exit code %s: %s' %
+            (process.returncode, process.stderr))
+
+    return process.stdout
---
+++ packages/resticpy/resticpy-0.1.4-py3-none-any.whl/restic/internal/forget.py
@@ -0,0 +1,37 @@
+import json
+
+import restic.errors
+from restic.internal import command_executor
+
+
+def run(restic_base_command,
+        prune=False,
+        keep_daily=None,
+        keep_within=None,
+        group_by=None):
+    cmd = restic_base_command + ['forget']
+
+    if prune:
+        cmd.extend(['--prune'])
+
+    if keep_daily is not None:
+        cmd.extend(['--keep-daily', str(keep_daily)])
+
+    if keep_within is not None:
+        cmd.extend(['--keep-within', keep_within])
+
+    if group_by is not None:
+        cmd.extend(['--group-by', group_by])
+
+    return _parse_result(command_executor.execute(cmd))
+
+
+def _parse_result(result):
+    # The result is JSON followed by 0 or more non-JSON lines.
+    result_lines = result.split('\n')
+    try:
+        return json.loads(result_lines[0])
+    except json.decoder.JSONDecodeError as e:
+        raise restic.errors.UnexpectedResticResponse(
+            'Unexpected result from restic. Expected JSON, got: %s' %
+            result_lines[0]) from e
