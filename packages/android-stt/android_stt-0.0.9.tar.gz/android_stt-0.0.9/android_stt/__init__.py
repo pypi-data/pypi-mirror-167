3a91430a3c80bdbbe7639e87abe16842d3eac2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:30 +0000

    Add package android-stt/0.0.9/android_stt-0.0.9.tar.gz

---
+++ packages/android-stt/android_stt-0.0.9.tar.gz/android_stt-0.0.9/android_stt/__init__.py
@@ -0,0 +1,79 @@
+import time
+from jnius import autoclass
+import android
+from kivy.clock import mainthread
+from android.permissions import request_permissions, Permission
+request_permissions([Permission.RECORD_AUDIO])
+Bundle = autoclass('android.os.Bundle')
+PA = autoclass('org.kivy.android.PythonActivity')
+
+class RecognizerListenerBase:
+	def onBeginningOfSpeech(self):
+		print('onBeginningOfSpeech()')
+
+	def onPartialResults(self, partialResults):
+		print('onPartialResults() called', particalResults)
+		strArray = partialResults.getStringArrayList(SR.RESULTS_RECOGNITION)
+		return strArray
+		
+	def onReadyForSpeech(self, params):
+		print('onReadyForSpeech() called', params)
+
+	def onResults(self, results):
+		print('onResults() called', results)
+		strArray = results.getStringArrayList(SR.RESULTS_RECOGNITION)
+		return strArray
+
+	def onEndOfSpeech(self):	
+		print('onEndOfSpeech() called')
+
+	def onError(self, error):
+		if self.on_error:
+			self.on_error(error)
+
+
+class AndroidNativeSTT(object):
+	"""
+	use case:
+	tts = AndroidNativeSTT()
+	listener = RecognizerListenerBase(tts)
+	tts.setListener(listener)
+	tts.startListening()
+	....
+	tts.stopListening()
+	"""
+	@mainthread
+	def __init__(self):
+		SR = autoclass('android.speech.SpeechRecognizer')
+		self.context = PA.mActivity
+		if not SR.isRecognitionAvailable(self.context):
+			raise Exception('Recognition not available')
+		self._stt = None
+		self._stt = SR.createSpeechRecognizer(self.context)
+		print('self._stt=', self._stt)
+		self.listener = None
+
+	def setListener(self, listener):
+		self.listener = listener
+		self._stt.setRecognitionListener(listener)
+
+	def startListening(self):
+		if self.listener is None:
+			return
+		Intent = autoclass('android.speech.RecognizerIntent')
+		intent = Intent(Intent.ACTION_RECOGNIZE_SPEECH)
+		intent.putExtra(Intent.EXTRA_LANGUAGE, "en-US");
+		intent.putExtra(Intent.EXTRA_LANGUAGE_MODEL, \
+						Intent.LANGUAGE_MODEL_FREE_FORM)
+		self._stt.startListener(intent)
+
+	def stopListener(self):
+		self._stt.stopListening()
+
+	def cancel(self):
+		self._stt.cancel()
+
+	def __del__(self):
+		if self._stt:
+			self._stt.destroy()
+
