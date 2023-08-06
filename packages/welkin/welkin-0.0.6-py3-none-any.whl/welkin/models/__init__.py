5f27eed1128cc651118f8fa030feb5d4f1cc55b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:31 +0000

    Add package welkin/0.0.6/welkin-0.0.6-py3-none-any.whl

---
+++ packages/welkin/welkin-0.0.6-py3-none-any.whl/welkin/models/__init__.py
@@ -0,0 +1,43 @@
+from welkin.models.assessment import (
+    Assessment,
+    AssessmentRecord,
+    AssessmentRecordAnswers,
+    AssessmentRecords,
+    Assessments,
+)
+from welkin.models.calendar import CalendarEvent, CalendarEvents, Schedules
+from welkin.models.care_plan import CarePlan, CarePlanOverview
+from welkin.models.cdt import CDT, CDTs
+from welkin.models.chat import Chat, Chats, SearchChats
+from welkin.models.encounter import Disposition, Encounter, Encounters
+from welkin.models.formation import Formations
+from welkin.models.patient import Patient, Patients
+from welkin.models.user import User, Users
+
+# NOTE: If a class isn't imported here and added to __all__, it will not be callable
+# from a `Client` instance. Also, for legibility, keep this list alphabetical.
+__all__ = [
+    "Assessment",
+    "AssessmentRecord",
+    "AssessmentRecordAnswers",
+    "AssessmentRecords",
+    "Assessments",
+    "CalendarEvent",
+    "CalendarEvents",
+    "CarePlan",
+    "CarePlanOverview",
+    "CDT",
+    "CDTs",
+    "Chat",
+    "Chats",
+    "SearchChats",
+    "Disposition",
+    "Encounter",
+    "Encounters",
+    "Patient",
+    "Patients",
+    "Schedules",
+    "User",
+    "Users",
+    "Formations",
+]
