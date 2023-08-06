ca7aa86abcf6be6556495bccfdf2aa46a80340b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:46 +0000

    Add package owl-parser/0.1.3/owl_parser-0.1.3-py3-none-any.whl

---
+++ packages/owl-parser/owl_parser-0.1.3-py3-none-any.whl/owl_parser/mutato/svc/__init__.py
@@ -0,0 +1,5 @@
+from .perform_exact_matching import PerformExactMatching
+from .perform_span_matching import PerformSpanMatching
+from .perform_spacy_matching import PerformSpacyMatching
+from .augment_token_hierarchy import AugmentTokenHierarchy
+from .perform_hierarchy_matching import PerformHierarchyMatching
