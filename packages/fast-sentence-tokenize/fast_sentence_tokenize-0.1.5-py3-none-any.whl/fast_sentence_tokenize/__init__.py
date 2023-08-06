ff5ea8ac97e887c50025812df3d5fdac41a80877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:28 +0000

    Add package fast-sentence-tokenize/0.1.5/fast_sentence_tokenize-0.1.5-py3-none-any.whl

---
+++ packages/fast-sentence-tokenize/fast_sentence_tokenize-0.1.5-py3-none-any.whl/fast_sentence_tokenize/__init__.py
@@ -0,0 +1,20 @@
+from .bp import *
+from .svc import *
+from .dmo import *
+
+
+from baseblock import Enforcer
+from .bp.tokenizer import Tokenizer
+
+__tok = Tokenizer().input_text
+
+
+def fast_sentence_tokenize(input_text: str) -> list:
+
+    Enforcer.is_str(input_text)
+
+    tokens = __tok(input_text)
+
+    Enforcer.is_list_of_str(tokens)
+
+    return tokens
