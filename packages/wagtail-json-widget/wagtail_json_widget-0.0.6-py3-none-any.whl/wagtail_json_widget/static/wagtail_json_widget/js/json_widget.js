49680100eecbe954f985fde23cb027d9df4da05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:24 +0000

    Add package wagtail-json-widget/0.0.6/wagtail_json_widget-0.0.6-py3-none-any.whl

---
+++ packages/wagtail-json-widget/wagtail_json_widget-0.0.6-py3-none-any.whl/wagtail_json_widget/static/wagtail_json_widget/js/json_widget.js
@@ -0,0 +1,34 @@
+class JSONEditorWidget {
+
+    constructor(element) {
+        this.initEditor(element);
+    }
+
+    initEditor(element) {
+        // Initialize the JSONEditor widget
+        const textarea = element.querySelector('textarea');
+        const options = {"modes": ["text", "code", "tree", "form", "view"], "mode": "code", "search": true};
+
+        options.onChange = function () {
+            var json = editor.get();
+            textarea.value=JSON.stringify(json);
+        }        
+        const editor = new JSONEditor(element, options);
+        editor.set(JSON.parse(textarea.value));
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    createJSONFieldEditors();
+});
+
+function createJSONFieldEditors() {
+    const els = document.getElementsByClassName('json_editor_widget');
+    for (const el of els) {
+        const collection = el.getElementsByClassName('input')
+        if (collection.length === 0) {
+            continue;
+        }
+        new JSONEditorWidget(collection[0]);
+    }
+}
