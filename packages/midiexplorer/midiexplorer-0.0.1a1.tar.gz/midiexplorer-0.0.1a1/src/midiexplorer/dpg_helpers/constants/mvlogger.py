1f145a1a4af44e89adfb8f19263ff795e5b76501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:06 +0000

    Add package midiexplorer/0.0.1a1/midiexplorer-0.0.1a1.tar.gz

---
+++ packages/midiexplorer/midiexplorer-0.0.1a1.tar.gz/midiexplorer-0.0.1a1/src/midiexplorer/dpg_helpers/constants/mvlogger.py
@@ -0,0 +1,16 @@
+# This Python file uses the following encoding: utf-8
+#
+# SPDX-FileCopyrightText: 2022 Raphaël Doursenaud <rdoursenaud@free.fr>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""
+DearPyGui mvLogger levels.
+"""
+
+TRACE = 0
+DEBUG = 1
+INFO = 2
+WARNING = 3
+ERROR = 4
+CRITICAL = 5
