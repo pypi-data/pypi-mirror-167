db1c3d2174e57b2a8d3a8ca073e1a61ce1b567d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:24 +0000

    Add package net-inspect/1.3.4/net_inspect-1.3.4-py3-none-any.whl

---
+++ packages/net-inspect/net_inspect-1.3.4-py3-none-any.whl/net_inspect/plugins/input_plugin_with_console.py
@@ -0,0 +1,44 @@
+from typing import Dict, Tuple
+import re
+
+from ..domain import InputPluginAbstract, DeviceInfo
+
+# 类思科的情况
+# device>show version
+# device(config)# sh version
+similar_cisco_reg = r'^(?P<device_name>[\w\-_\.]+)(?:\(.*?\))?[#|>]\s*(?P<cmd>sh(?:o(?:w)?)?\s+.*)$'
+
+# 类华为的情况
+# <device>display version
+# [device] dis version
+simialr_huawei_reg = r'[\<|\[](?P<device_name>[\w\-_\.]+)(?:\-(?:.+?))?[\]|>]\s*(?P<cmd>dis(?:p(?:l(?:a(?:y)?)?)?)?\s+.*)$'
+
+
+class InputPluginWithConsole(InputPluginAbstract):
+    """通过Console或者vty获取命令的输出"""
+
+    def main(self, file_path: str, stream: str) -> Tuple[Dict[str, str], DeviceInfo]:
+        device_name = ''
+        command = ''
+        cmd_dict = {}
+        content = []
+        for line in stream.splitlines():
+            match = re.match(similar_cisco_reg, line) or \
+                re.match(simialr_huawei_reg, line)  # 判断是否为命令的行
+            if match:
+                if not device_name:
+                    device_name = match.group('device_name')
+
+                if content and command:  # 如果有内容，且有命令，则保存
+                    cmd_dict[command] = '\n'.join(content)
+
+                command = match.group('cmd').strip()
+                content.clear()
+                continue
+
+            content.append(line)
+
+        if content and command:
+            cmd_dict[command] = '\n'.join(content)
+
+        return cmd_dict, DeviceInfo(device_name, '')
