3dcfa70f93e5013da8764c77cfb6fc3b67406b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:54 +0000

    Add package my-random-package/0.0.1/my-random-package-0.0.1.tar.gz

---
+++ packages/my-random-package/my-random-package-0.0.1.tar.gz/my-random-package-0.0.1/random_stuff/person.py
@@ -0,0 +1,7 @@
+class Person():
+
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print(f"hi {self.name}!")
---
+++ packages/my-random-package/my-random-package-0.0.1.tar.gz/my-random-package-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.log import Log
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+setup(
+    name="my-random-package",
+    version="0.0.1",
+    author="mustafa hakimi",
+    author_email="test@test.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
