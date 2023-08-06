f0eb82c41af4ebead1ceaa45280eab398d0800d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:54 +0000

    Add package headersformat/0.0.3/headersformat-0.0.3-py3-none-any.whl

---
+++ packages/headersformat/headersformat-0.0.3-py3-none-any.whl/headersformat/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/14 18:41
+# @Author  : jia666666
+# @FileName: __init__.py.py
+
+
+from .core import headers_foramt
+
+__all__ = (
+    headers_foramt
+)
