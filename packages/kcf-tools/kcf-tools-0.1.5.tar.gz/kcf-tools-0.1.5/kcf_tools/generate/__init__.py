018dc4720d85ac191950de9e4d0286f4fa6fcd28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:18 +0000

    Add package kcf-tools/0.1.5/kcf-tools-0.1.5.tar.gz

---
+++ packages/kcf-tools/kcf-tools-0.1.5.tar.gz/kcf-tools-0.1.5/kcf_tools/generate/__init__.py
@@ -0,0 +1,6 @@
+from .readme.main import generate_readme
+from .docs.main import generate_help_doc
+from .key_definitions.main import generate_key_definitions
+from .client.main import generate_client
+from .config_schema.main import generate_config_schema
+from .ui_block.main import generate_ui_block
