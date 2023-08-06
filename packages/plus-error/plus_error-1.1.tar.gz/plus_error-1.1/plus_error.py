9e9bc770454af5ab3ddc911a7f3d799444d56bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:48 +0000

    Add package plus-error/1.1/plus_error-1.1.tar.gz

---
+++ packages/plus-error/plus_error-1.1.tar.gz/plus_error-1.1/plus_error.py
@@ -0,0 +1,44 @@
+"""
+This module helps you to squash bugs by giving you information about the error and how to fix it.
+"""
+
+import sys
+import re
+from termcolor import colored
+
+__version__ = "1.0"
+
+class example:
+    class error(Exception):
+        def __init__(self):
+            super().__init__("Example Error")
+
+class ErrorCatcher():
+    def __init__(self):
+        self.finished = False
+        self.err = ""
+    def write(self, msg):
+        if re.match(".+:", msg) and not "Traceback (most recent call last):" in msg:
+            self.finished = True
+        if self.finished:
+            self.err += msg
+            evaluate(self.err)
+            self.err = ""
+            self.finished = False
+        else:
+            self.err += msg
+
+class evaluate():
+    def __init__(self, err):
+        self.details = {}
+        self.details["type"] = re.findall(".+:", err)[1].replace(":", "")
+        self.details["message"] = re.search(": .+", err)[0]
+        self.details["line_no"] = re.search("line \d+", err)[0].replace("line ", "")
+        self.details["file"] = re.search("File \".+?\"", err)[0].replace("File ", "").replace('"', '')
+
+        print(colored("{} on line {} in file {}{}.".format( str( self.details[ "type" ] ), str( self.details[ "line_no" ] ), str( self.details[ "file" ] ), str( self.details[ "message" ] ) ) , "red" ) )
+
+
+def init():
+    """Start catching errors"""
+    sys.stderr = ErrorCatcher()
