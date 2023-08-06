96c8837534d311bea847010877649f1a82f7ffb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:19 +0000

    Add package crownstone-sse/2.0.4/crownstone-sse-2.0.4.tar.gz

---
+++ packages/crownstone-sse/crownstone-sse-2.0.4.tar.gz/crownstone-sse-2.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='crownstone-sse',
+    version="2.0.4",
+    url='https://github.com/crownstone/crownstone-lib-python-sse',
+    author='Crownstone B.V.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=['examples', 'tests']),
+    install_requires=list(package.strip() for package in open('requirements.txt')),
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    python_requires='>=3.8',
+)
