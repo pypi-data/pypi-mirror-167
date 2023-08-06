14298f5a886575974b8775183602e52b20f3ea46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:05 +0000

    Add package nansen-airflow-errors/0.0.0/nansen-airflow-errors-0.0.0.tar.gz

---
+++ packages/nansen-airflow-errors/nansen-airflow-errors-0.0.0.tar.gz/nansen-airflow-errors-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nansen-airflow-errors',
+      version='0.0.0',
+      description='Nansen Airflow Errors',
+      author='Nansen SRE',
+      packages=['nansen-airflow-errors'])
+
