7c19ec3d31bca43d26e6151727059489a6a92270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:21 +0000

    Add package m-filetypes/0.0.8/m-filetypes-0.0.8.tar.gz

---
+++ packages/m-filetypes/m-filetypes-0.0.8.tar.gz/m-filetypes-0.0.8/setup.py
@@ -0,0 +1,42 @@
+import os
+import pathlib
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+__PACKAGE_NAME__ = 'mobio'
+__TEMPLATE_DIR__ = 'libs'
+
+
+def package_data_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        p = pathlib.Path(path)
+        path = pathlib.Path(*p.parts[1:])
+        for filename in filenames:
+            paths.append(os.path.join(path, filename))
+    return paths
+
+
+data_files = package_data_files(os.path.join(__PACKAGE_NAME__, __TEMPLATE_DIR__))
+
+setup(
+    name='m-filetypes',
+    version='0.0.8',
+    author='MOBIO',
+    author_email='contact@mobio.vn',
+    description='Libs check filetype',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages(),
+    package_data={'': data_files}
+)
