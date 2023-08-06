c87a9508050c50699973bcc4283644b497479f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:36 +0000

    Add package supervisely/6.61.6/supervisely-6.61.6.tar.gz

---
+++ packages/supervisely/supervisely-6.61.6.tar.gz/supervisely-6.61.6/supervisely/app/__init__.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from supervisely.app.content import StateJson, DataJson
+from supervisely.app.content import get_data_dir
+import supervisely.app.fastapi as fastapi
+import supervisely.app.widgets as widgets
+import supervisely.app.development as development
