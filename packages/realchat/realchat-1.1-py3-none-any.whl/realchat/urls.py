b6dce94e76c61d62db7cad172fc53d9f56483081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:06 +0000

    Add package realchat/1.1/realchat-1.1-py3-none-any.whl

---
+++ packages/realchat/realchat-1.1-py3-none-any.whl/realchat/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import InboxView, UserListsView, MessagesListView
+
+app_name = 'realchat'
+
+
+urlpatterns = [
+    path('', MessagesListView.as_view(), name='message_list'),
+    path('meet/', UserListsView.as_view(), name='users_list'),
+    path('inbox/<str:username>/', InboxView.as_view(), name='inbox'),
+]
