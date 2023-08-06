ade8f97daa7c010cda592595e336d57d6f348738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:40 +0000

    Add package mypackscal/0.0.1/mypackscal-0.0.1.tar.gz

---
+++ packages/mypackscal/mypackscal-0.0.1.tar.gz/mypackscal-0.0.1/SETUP.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 11',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='mypackscal',
+    version='0.0.1',
+    description='this is a trial package used for calculation.',
+    url='',
+    author='Shreyas MR',
+    author_email='shreyasmr13@gmail.com',
+    licence='MIT',
+    packages=['mypackscal'],
+    install_requires=[]
+)
