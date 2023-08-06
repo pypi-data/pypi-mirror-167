51aaf0df819cf9e4dc3162d3da6bb75131e294b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:30 +0000

    Add package pynb-dag-runner-webui/0.0.8/pynb_dag_runner_webui-0.0.8-py3-none-any.whl

---
+++ packages/pynb-dag-runner-webui/pynb_dag_runner_webui-0.0.8-py3-none-any.whl/pynb_dag_runner_webui-0.0.8.data/data/assets/precache-manifest.45ebf770d200f923e62903fbbf71c92f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "185a7d1aa5b8a09cd57c",
+    "url": "./static/css/main.5f6e021f.chunk.css"
+  },
+  {
+    "revision": "185a7d1aa5b8a09cd57c",
+    "url": "./static/js/main.b3b207ce.chunk.js"
+  },
+  {
+    "revision": "228afff4e5cf9397e8af",
+    "url": "./static/js/runtime~main.8ddb08e0.js"
+  },
+  {
+    "revision": "104090c040871f771b2d",
+    "url": "./static/css/2.631fbddf.chunk.css"
+  },
+  {
+    "revision": "104090c040871f771b2d",
+    "url": "./static/js/2.a1d184b7.chunk.js"
+  },
+  {
+    "revision": "90ffd1a6bba3add98ec8",
+    "url": "./static/js/3.77155de0.chunk.js"
+  },
+  {
+    "revision": "224121223378720378fc",
+    "url": "./static/js/4.75324fdd.chunk.js"
+  },
+  {
+    "revision": "898a765cc98bf478163a284fc7160bd0",
+    "url": "./static/media/default.898a765c.less"
+  },
+  {
+    "revision": "f8c411a9a70c61fd7000bb3082b16e23",
+    "url": "./static/media/default-error.f8c411a9.svg"
+  },
+  {
+    "revision": "5092675c27ede31134e05ef939bb3b7f",
+    "url": "./static/media/filter-icon.5092675c.svg"
+  },
+  {
+    "revision": "d58c509937a4d6b0b74ade8a685b16cd",
+    "url": "./static/media/notebook.d58c5099.svg"
+  },
+  {
+    "revision": "8b250f7f9dfd82d346bc7c4e34fb5400",
+    "url": "./static/media/revision.8b250f7f.svg"
+  },
+  {
+    "revision": "91661969e9b84d8458d5be802ce436d6",
+    "url": "./static/media/empty.91661969.svg"
+  },
+  {
+    "revision": "0e9e57fc119af79203e8a87520122dd4",
+    "url": "./static/media/laptop.0e9e57fc.svg"
+  },
+  {
+    "revision": "673c346d4c92cca2efb614a8013994de",
+    "url": "./static/media/project.673c346d.svg"
+  },
+  {
+    "revision": "31372da50f2b97dd943a05080bbd1b0f",
+    "url": "./static/media/job.31372da5.svg"
+  },
+  {
+    "revision": "3f204b5402b58877f290f685a8e20d99",
+    "url": "./static/media/warning.3f204b54.svg"
+  },
+  {
+    "revision": "246bfb44ab6d6f0973e0516ae6436ec0",
+    "url": "./static/media/404-overflow.246bfb44.svg"
+  },
+  {
+    "revision": "27426c1d3a2e50de28af78c2017b3bf0",
+    "url": "./static/media/registered-model.27426c1d.svg"
+  },
+  {
+    "revision": "52a4e4559a79eb2745a5c05eb321b4b3",
+    "url": "./static/media/logged-model.52a4e455.svg"
+  },
+  {
+    "revision": "aee7e216079bea4ebe0816d94970b022",
+    "url": "./static/media/registryIcon.aee7e216.svg"
+  },
+  {
+    "revision": "d1cc4fe780317a855dabe7cf22e42792",
+    "url": "./static/media/expand-more.d1cc4fe7.svg"
+  },
+  {
+    "revision": "74bba15d71d39cdeae9e3edce246e3ac",
+    "url": "./static/media/permission-denied-lock.74bba15d.svg"
+  },
+  {
+    "revision": "a1bbaadbf82282f9b9a44f18d932a893",
+    "url": "./static/media/no-experiments.a1bbaadb.svg"
+  },
+  {
+    "revision": "7a9dd62d43af29b22609a5aa93325481",
+    "url": "./static/media/home-logo.7a9dd62d.png"
+  },
+  {
+    "revision": "8e7d21dc5e5453e38e86f6b5a2c25dd0",
+    "url": "./static/media/index.8e7d21dc.less"
+  },
+  {
+    "revision": "24a6085fcb8a5fd2977c376e13b22e30",
+    "url": "./static/media/sync.24a6085f.svg"
+  },
+  {
+    "revision": "c3b37d96c916d42e45e3ae8cc11c29c9",
+    "url": "./index.html"
+  }
+];
