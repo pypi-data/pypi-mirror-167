87df3f37a5b2ef9fe7dc54c288787da0165aebcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:03 +0000

    Add package Test-my-Pypi19127/1.0.21/Test_my_Pypi19127-1.0.21-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/Test-my-Pypi19127/Test_my_Pypi19127-1.0.21-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/diffcp/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.0.19"
+
+from diffcp.cone_program import say_hello_from_python_cones
+from diffcp.cones import say_hello_from_cones_python
+from diffcp.utils import say_hello_from_utils_python
---
+++ packages/Test-my-Pypi19127/Test_my_Pypi19127-1.0.21-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tests/test_scs.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+import diffcp.cone_program as cone_prog
+import diffcp.utils as utils
+
+
+def test_scs_solve():
+    n = 1
+    assert 1 == n
