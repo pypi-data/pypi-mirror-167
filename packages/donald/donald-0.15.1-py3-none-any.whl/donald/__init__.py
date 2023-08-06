07bf57a1e5d55e1289cb617738ae28db6867661e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:43 +0000

    Add package donald/0.15.1/donald-0.15.1-py3-none-any.whl

---
+++ packages/donald/donald-0.15.1-py3-none-any.whl/donald/__init__.py
@@ -0,0 +1,24 @@
+"""Donald tasks' manager for Asyncio."""
+
+import logging
+
+logger = logging.getLogger(__name__)
+handle = logging.StreamHandler()
+handle.setFormatter(
+    logging.Formatter(
+        "[%(asctime)-15s] [%(process)d] [%(levelname)s] %(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
+)
+logger.addHandler(handle)
+
+
+from .core import CronTab as crontab
+from .core import Donald, run_donald
+
+__all__ = "crontab", "Donald", "run_donald"
+
+__version__ = "0.15.1"
+__project__ = "Donald"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "BSD"
