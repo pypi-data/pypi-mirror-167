9d8a17af60bc10fc4ad81aded1e96b118d41f328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:15 +0000

    Add package xingyun-gongwei-shengjing/2022.9.14.0/xingyun_gongwei_shengjing-2022.9.14.0-py3-none-any.whl

---
+++ packages/xingyun-gongwei-shengjing/xingyun_gongwei_shengjing-2022.9.14.0-py3-none-any.whl/XingyunGongweiShengjing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xingyun-gongwei-shengjing
+https://github.com/apachecn/xingyun-gongwei-shengjing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.14.0"
