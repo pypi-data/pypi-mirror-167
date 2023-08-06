3c8010a3131759c79ede2d574b4e8cb43eb7cf56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:35 +0000

    Add package ajioz-gb-distribution/1.0/ajioz_gb_distribution-1.0.tar.gz

---
+++ packages/ajioz-gb-distribution/ajioz_gb_distribution-1.0.tar.gz/ajioz_gb_distribution-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ajioz_gb_distribution',
+      version='1.0',
+      description='A Gaussian and Binomial distributions',
+      packages=['ajioz_gb_distribution'],
+      author= 'Ajiroghene Sunday',
+      author_email= 'sunny@ajiozi.com',
+      zip_safe=False)
