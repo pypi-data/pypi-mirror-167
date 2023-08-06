3f8d3389bbd56b4c79c3a7643283540ab05b7db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:24 +0000

    Add package apmpython/0.1.11/apmpython-0.1.11.tar.gz

---
+++ packages/apmpython/apmpython-0.1.11.tar.gz/apmpython-0.1.11/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+
+    name="apmpython",
+    version="0.1.11",
+
+    author="Nidhi_Sharma",
+    description="This package is use to check the RAM and CPU Usage of Current Device.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/plain',
+    packages=["apmpythonpackage"],
+    url = "https://github.com/middleware-labs/agent-apm-python.git",
+)
