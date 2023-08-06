3f0c474c144af093fe691926f871825fa3ee1483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package concat-package/0.0.1/concat_package-0.0.1.tar.gz

---
+++ packages/concat-package/concat_package-0.0.1.tar.gz/concat_package-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name="concat_package",
+    version="0.0.1",
+    author="suman choudhary",
+    description="string upper concat",
+    python_requires='>=3.6',
+    packages=["concat_package"]
+
+)
