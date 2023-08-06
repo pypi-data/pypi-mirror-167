01948cffe5612fd0124e227a24509a3633d65ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:57 +0000

    Add package distri-probability-tests/1.1/distri_probability_tests-1.1.tar.gz

---
+++ packages/distri-probability-tests/distri_probability_tests-1.1.tar.gz/distri_probability_tests-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distri_probability_tests',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['distri_probability_tests'],
+      author = 'Miguel',
+      author_email = 'name@gmail.com',
+      zip_safe=False)
