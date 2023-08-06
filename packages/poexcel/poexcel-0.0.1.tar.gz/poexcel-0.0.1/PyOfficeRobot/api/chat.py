8f8fad8bfc18d457ffde1978070114a483774b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:32 +0000

    Add package poexcel/0.0.1/poexcel-0.0.1.tar.gz

---
+++ packages/poexcel/poexcel-0.0.1.tar.gz/poexcel-0.0.1/PyOfficeRobot/api/chat.py
@@ -0,0 +1,21 @@
+from PyOfficeRobot.core.WeChatType import WeChat
+from PyOfficeRobot.lib.CONST import ACT_TYPE
+from PyOfficeRobot.lib.dec.act_dec import act_info
+
+wx = WeChat()
+
+
+@act_info(ACT_TYPE.MESSAGE)
+def send_message(who, message):
+    """
+    给指定人，发送一条消息
+    :param who:
+    :param message:
+    :return:
+    """
+
+    # 获取会话列表
+    wx.GetSessionList()
+    wx.ChatWith(who)  # 打开`文件传输助手`聊天窗口
+    # for i in range(10):
+    wx.SendMsg(message)  # 向`文件传输助手`发送消息：你好~
