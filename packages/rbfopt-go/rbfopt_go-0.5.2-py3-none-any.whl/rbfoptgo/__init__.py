64e221ca0b794ea3eccdeeddabb88ea4c9d89ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:38 +0000

    Add package rbfopt-go/0.5.2/rbfopt_go-0.5.2-py3-none-any.whl

---
+++ packages/rbfopt-go/rbfopt_go-0.5.2-py3-none-any.whl/rbfoptgo/__init__.py
@@ -0,0 +1,4 @@
+#  Copyright (c) New Cloud Technologies, Ltd. 2013-2022.
+#  Author: Vitaly Isaev <vitaly.isaev@myoffice.team>
+#  License: https://github.com/newcloudtechnologies/rbfopt-go/blob/master/LICENSE
+
