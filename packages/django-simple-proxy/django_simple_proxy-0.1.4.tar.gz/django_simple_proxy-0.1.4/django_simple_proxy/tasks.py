6a93bd119b5d2aa3ce73994d6fb68b024df1445c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:44 +0000

    Add package django-simple-proxy/0.1.4/django_simple_proxy-0.1.4.tar.gz

---
+++ packages/django-simple-proxy/django_simple_proxy-0.1.4.tar.gz/django_simple_proxy-0.1.4/django_simple_proxy/tasks.py
@@ -0,0 +1,23 @@
+from .models import Proxy
+from .tools.check import check_proxy
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
+def check_proxy_task(queryset=None):
+    dead_proxy = []
+
+    for proxy in queryset or Proxy.objects.all():
+        print(f"Check {proxy.url}")
+        if not check_proxy(proxy):
+            logger.error(f"PROXY DEAD {proxy.url}")
+            dead_proxy.append(proxy.url)
+            proxy.check = False
+            proxy.save(update_fields=["check"])
+        else:
+            proxy.check = True
+            proxy.save(update_fields=["check"])
+
+    if dead_proxy:
+        raise RuntimeError(f"Some proxies are dead: {dead_proxy}")
