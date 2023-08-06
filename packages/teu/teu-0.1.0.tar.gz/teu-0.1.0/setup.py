96f68a5f80a557fff64a1a1b69e0703156c10518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:28 +0000

    Add package teu/0.1.0/teu-0.1.0.tar.gz

---
+++ packages/teu/teu-0.1.0.tar.gz/teu-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['teu']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'teu',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Pritam Soni',
+    'author_email': 'pritam@grid.ai',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
