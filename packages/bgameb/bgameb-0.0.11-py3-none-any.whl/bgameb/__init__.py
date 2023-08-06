65e91db5c74bf3234d964e3351df435ea47f13c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:53 +0000

    Add package bgameb/0.0.11/bgameb-0.0.11-py3-none-any.whl

---
+++ packages/bgameb/bgameb-0.0.11-py3-none-any.whl/bgameb/__init__.py
@@ -0,0 +1,8 @@
+from bgameb.game import Game
+from bgameb.players import Human, Bot
+from bgameb.stuff import Dice, Coin, Card
+from bgameb.utils import log_enable
+from bgameb._version import __version__
+
+
+PUB_VERSION = __version__.public()
---
+++ packages/bgameb/bgameb-0.0.11-py3-none-any.whl/bgameb/players.py
@@ -0,0 +1,21 @@
+"""Game players
+"""
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+    """Base class to create a player
+    """
+
+
+@dataclass
+class Human(Player):
+    """Base class to create a human palyer
+    """
+
+
+@dataclass
+class Bot(Player):
+    """Base class to create a bot player
+    """
