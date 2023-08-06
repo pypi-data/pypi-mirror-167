57e1b05df5374c3babf4c586cd9b320c0cf15090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:45 +0000

    Add package snowflake-snowpark-python/0.10.0/snowflake_snowpark_python-0.10.0-py3-none-any.whl

---
+++ packages/snowflake-snowpark-python/snowflake_snowpark_python-0.10.0-py3-none-any.whl/snowflake/snowpark/_internal/analyzer/grouping_set.py
@@ -0,0 +1,27 @@
+#
+# Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
+#
+from typing import List
+
+from snowflake.snowpark._internal.analyzer.expression import Expression
+
+
+class GroupingSet(Expression):
+    def __init__(self, group_by_exprs: List[Expression]) -> None:
+        super().__init__()
+        self.group_by_exprs = group_by_exprs
+        self.children = group_by_exprs
+
+
+class Cube(GroupingSet):
+    pass
+
+
+class Rollup(GroupingSet):
+    pass
+
+
+class GroupingSetsExpression(Expression):
+    def __init__(self, args: List[List[Expression]]) -> None:
+        super().__init__()
+        self.args = args
