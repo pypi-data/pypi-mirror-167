6aea251ef28be4bbef243c61e58e1234e8fba96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:57 +0000

    Add package edc-vitals/0.1.9/edc-vitals-0.1.9.tar.gz

---
+++ packages/edc-vitals/edc-vitals-0.1.9.tar.gz/edc-vitals-0.1.9/edc_vitals/models/fields/__init__.py
@@ -0,0 +1,5 @@
+from .blood_pressure import DiastolicPressureField, SystolicPressureField
+from .height import HeightField
+from .temperature import TemperatureField
+from .waist_circumference import WaistCircumferenceField
+from .weight import WeightField
