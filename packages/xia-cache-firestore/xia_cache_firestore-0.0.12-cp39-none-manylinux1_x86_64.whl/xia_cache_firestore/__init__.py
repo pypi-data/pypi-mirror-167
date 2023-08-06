697034a1a66c1e9d4221017f1dd6f925ca329f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:03 +0000

    Add package xia-cache-firestore/0.0.12/xia_cache_firestore-0.0.12-cp39-none-manylinux1_x86_64.whl

---
+++ packages/xia-cache-firestore/xia_cache_firestore-0.0.12-cp39-none-manylinux1_x86_64.whl/xia_cache_firestore/__init__.py
@@ -0,0 +1,10 @@
+from xia_cache_firestore.connection import connect
+from xia_cache_firestore.cache import FirestoreCache
+
+
+__all__ = [
+    "connect",
+    "FirestoreCache"
+]
+
+__version__ = "0.0.12"
