7be134f203be61a281c844b8fc3c29237b396930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:38 +0000

    Add package fooDemoDC/0.0.2/fooDemoDC-0.0.2.tar.gz

---
+++ packages/fooDemoDC/fooDemoDC-0.0.2.tar.gz/fooDemoDC-0.0.2/README.md
@@ -0,0 +1,2 @@
+# FooDemoDC
+Test
---
+++ packages/fooDemoDC/fooDemoDC-0.0.2.tar.gz/fooDemoDC-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name="fooDemoDC",
+    version="0.0.2",
+    description="Guide",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/dance858/FooDemoDC",
+    author="Daniel Cederberg",
+    author_email="roberto.prevato@gmail.com",
+    keywords="core utilities",
+    license="MIT",
+    packages=["my_foo"],
+    install_requires=[],
+    include_package_data=True,
+)
