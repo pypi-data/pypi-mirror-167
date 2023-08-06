617f9821d356449a89d22de76d540997abeb0b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:17 +0000

    Add package absense/0.1.4/abSENSE-0.1.4-py3-none-any.whl

---
+++ packages/absense/abSENSE-0.1.4-py3-none-any.whl/abSENSE/__init__.py
@@ -0,0 +1,12 @@
+# pylint: disable=invalid-name
+
+"""
+Copyright (c) 2022 Troy Comi. All rights reserved.
+
+abSENSE: Perform homolog detectability
+"""
+from __future__ import annotations
+
+__version__ = "0.1.0"
+
+__all__ = ("__version__",)
---
+++ packages/absense/abSENSE-0.1.4-py3-none-any.whl/abSENSE/constants.py
@@ -0,0 +1,5 @@
+"""String constants for module."""
+from __future__ import annotations
+
+ORANGE = "#fc8123"
+GREY = "#a3a29b"
