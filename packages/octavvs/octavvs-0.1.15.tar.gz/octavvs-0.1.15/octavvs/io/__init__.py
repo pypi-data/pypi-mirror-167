54c474ed653b900e606a3247e1c00219148eb47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:37 +0000

    Add package octavvs/0.1.15/octavvs-0.1.15.tar.gz

---
+++ packages/octavvs/octavvs-0.1.15.tar.gz/octavvs-0.1.15/octavvs/io/__init__.py
@@ -0,0 +1,6 @@
+from .spectraldata import SpectralData
+from .decompositiondata import DecompositionData
+from .opusreader import OpusReader
+from .ptirreader import PtirReader
+from .image import Image
+from .parameters import Parameters
