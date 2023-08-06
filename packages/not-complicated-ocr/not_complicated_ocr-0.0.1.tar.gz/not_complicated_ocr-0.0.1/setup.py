99966bff27a387612ec74133d2c646513d50256d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:58 +0000

    Add package not-complicated-ocr/0.0.1/not_complicated_ocr-0.0.1.tar.gz

---
+++ packages/not-complicated-ocr/not_complicated_ocr-0.0.1.tar.gz/not_complicated_ocr-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name='not_complicated_ocr',
+    version='0.0.1',
+    author='写BUG专家',
+    author_email='',
+    description='一个简单的验证码识别库，使用中遇到任何的问题都可以加q群，有众多大佬解答:738389368',
+    long_description='text/markdown',
+    url='https://github.com/',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
