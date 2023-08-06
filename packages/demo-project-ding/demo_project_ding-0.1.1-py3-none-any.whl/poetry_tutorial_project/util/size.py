c0d0b492c1e5571ff338424a6eaceef420f1ec12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:10 +0000

    Add package demo-project-ding/0.1.1/demo_project_ding-0.1.1-py3-none-any.whl

---
+++ packages/demo-project-ding/demo_project_ding-0.1.1-py3-none-any.whl/poetry_tutorial_project/util/size.py
@@ -0,0 +1,15 @@
+"""Format bytes to human-readable format."""
+
+
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
