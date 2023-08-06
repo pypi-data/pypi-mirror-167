2b52e968f950f2cc7089c16c29e068cc25819cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:00 +0000

    Add package quanario/1.0.30/quanario-1.0.30.tar.gz

---
+++ packages/quanario/quanario-1.0.30.tar.gz/quanario-1.0.30/quanario/__init__.py
@@ -0,0 +1,6 @@
+from .bot import (Bot, )
+from .user import *
+from .send import (Send, )
+from .upload import (Upload, )
+from .input_message import *
+from .message_extensions import (Carousel, CarouselElement, Keyboard, VkKeyboardButton, )
