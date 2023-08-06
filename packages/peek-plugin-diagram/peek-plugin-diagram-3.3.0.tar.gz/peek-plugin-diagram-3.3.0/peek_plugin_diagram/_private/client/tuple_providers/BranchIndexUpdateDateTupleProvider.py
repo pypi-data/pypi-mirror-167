247e8040114937c8420dcc69cb3776c56366b36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:28 +0000

    Add package peek-plugin-diagram/3.3.0/peek-plugin-diagram-3.3.0.tar.gz

---
+++ packages/peek-plugin-diagram/peek-plugin-diagram-3.3.0.tar.gz/peek-plugin-diagram-3.3.0/peek_plugin_diagram/_private/client/tuple_providers/BranchIndexUpdateDateTupleProvider.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Union
+
+from twisted.internet.defer import Deferred
+from twisted.internet.defer import inlineCallbacks
+from vortex.Payload import Payload
+from vortex.TupleSelector import TupleSelector
+from vortex.handler.TupleDataObservableHandler import TuplesProviderABC
+
+from peek_plugin_diagram._private.client.controller.BranchIndexCacheController import (
+    BranchIndexCacheController,
+)
+from peek_plugin_diagram._private.tuples.branch.BranchIndexUpdateDateTuple import (
+    BranchIndexUpdateDateTuple,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class BranchIndexUpdateDateTupleProvider(TuplesProviderABC):
+    def __init__(self, cacheHandler: BranchIndexCacheController):
+        self._cacheHandler = cacheHandler
+
+    @inlineCallbacks
+    def makeVortexMsg(
+        self, filt: dict, tupleSelector: TupleSelector
+    ) -> Union[Deferred, bytes]:
+        tuple_ = BranchIndexUpdateDateTuple()
+        tuple_.updateDateByChunkKey = {
+            key: self._cacheHandler.encodedChunk(key).lastUpdate
+            for key in self._cacheHandler.encodedChunkKeys()
+        }
+        payload = Payload(filt, tuples=[tuple_])
+        payloadEnvelope = yield payload.makePayloadEnvelopeDefer()
+        vortexMsg = yield payloadEnvelope.toVortexMsg()
+        return vortexMsg
---
+++ packages/peek-plugin-diagram/peek-plugin-diagram-3.3.0.tar.gz/peek-plugin-diagram-3.3.0/peek_plugin_diagram/_private/client/tuple_providers/ClientLocationIndexUpdateDateTupleProvider.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Union
+
+from twisted.internet.defer import Deferred
+from twisted.internet.defer import inlineCallbacks
+from vortex.Payload import Payload
+from vortex.TupleSelector import TupleSelector
+from vortex.handler.TupleDataObservableHandler import TuplesProviderABC
+
+from peek_plugin_diagram._private.client.controller.LocationIndexCacheController import (
+    LocationIndexCacheController,
+)
+from peek_plugin_diagram._private.tuples.location_index.LocationIndexUpdateDateTuple import (
+    LocationIndexUpdateDateTuple,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class ClientLocationIndexUpdateDateTupleProvider(TuplesProviderABC):
+    def __init__(self, cacheHandler: LocationIndexCacheController):
+        self._cacheHandler = cacheHandler
+
+    @inlineCallbacks
+    def makeVortexMsg(
+        self, filt: dict, tupleSelector: TupleSelector
+    ) -> Union[Deferred, bytes]:
+        tuple_ = LocationIndexUpdateDateTuple()
+        tuple_.updateDateByChunkKey = {
+            key: self._cacheHandler.encodedChunk(key).lastUpdate
+            for key in self._cacheHandler.encodedChunkKeys()
+        }
+        payload = Payload(filt, tuples=[tuple_])
+        payloadEnvelope = yield payload.makePayloadEnvelopeDefer()
+        vortexMsg = yield payloadEnvelope.toVortexMsg()
+        return vortexMsg
