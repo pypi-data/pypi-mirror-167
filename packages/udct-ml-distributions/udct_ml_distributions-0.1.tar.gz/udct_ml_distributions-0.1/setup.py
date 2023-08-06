77ea44ea0d7150f83f771ef5313e5bb9cdcf9125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:44 +0000

    Add package udct-ml-distributions/0.1/udct_ml_distributions-0.1.tar.gz

---
+++ packages/udct-ml-distributions/udct_ml_distributions-0.1.tar.gz/udct_ml_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udct_ml_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udct_ml_distributions'],
+      zip_safe=False)
