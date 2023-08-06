b14d41a56c99161fd55680cfb5e06736603d5b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:32 +0000

    Add package rgtracker/0.0.1.1.212/rgtracker-0.0.1.1.212.tar.gz

---
+++ packages/rgtracker/rgtracker-0.0.1.1.212.tar.gz/rgtracker-0.0.1.1.212/src/jobs/sandbox/sandbox.py
@@ -0,0 +1,34 @@
+import json
+import math
+import pandas as pd
+
+
+GB("StreamReader"). \
+    aggregate([],
+              lambda a, r: a + [r['value']],
+              lambda a, r: a + r). \
+    run('ST:5MINUTES:P:::PG', trimStream=False)
+
+
+# GB("StreamReader"). \
+#     foreach(lambda record: log(f'Raw - {record}')). \
+#     aggregateby(lambda e: e['value']['ts'],
+#                 [],
+#                 lambda k, a, r: a + [r['value']],
+#                 lambda k, a, r: a + r). \
+#     foreach(lambda record: log(f'Agg - {record}')). \
+#     run('ST:1MINUTE:W:::UD', trimStream=False)
+
+
+
+# register(
+# prefix=f'ST:1MINUTE:{dimension}:::PG',
+# convertToStr=True,
+# collect=True,
+# onFailedPolicy='abort',
+# onFailedRetryInterval=1,
+# batch=1,
+# duration=0,
+# trimStream=False)
+
+# run('ST:1MINUTE::::PG', trimStream=False)
