642ecd87a095b8076482eceeee416a1478c53aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:09 +0000

    Add package gayathripack/0.0.1/gayathripack-0.0.1.tar.gz

---
+++ packages/gayathripack/gayathripack-0.0.1.tar.gz/gayathripack-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 11',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='gayathripack',
+    version='0.0.1',
+    description='this is a trial package used for calculation.',
+    url='',
+    author='Gayathri Sunil',
+    author_email='gayathrisachusunil@gmail.com',
+    licence='MIT',
+    packages=['gayathripack'],
+    install_requires=[]
+)# -*- coding: utf-8 -*-
+
