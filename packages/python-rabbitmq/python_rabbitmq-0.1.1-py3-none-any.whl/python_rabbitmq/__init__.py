1f382679108c414d451db5e830c03d8da9031791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:38 +0000

    Add package python-rabbitmq/0.1.1/python_rabbitmq-0.1.1-py3-none-any.whl

---
+++ packages/python-rabbitmq/python_rabbitmq-0.1.1-py3-none-any.whl/python_rabbitmq/__init__.py
@@ -0,0 +1,23 @@
+__version__ = '0.1.1'
+
+
+import logging
+
+from python_rabbitmq.connection import RMQConnection  # noqa
+from python_rabbitmq.consumer import RMQConsumer  # noqa
+from python_rabbitmq.producer import RMQProducer  # noqa
+from python_rabbitmq.defs import (  # noqa
+    QueueParams,
+    ExchangeParams,
+    QueueBindParams,
+    ConsumeParams,
+    PublishParams,
+    ConsumeOK,
+    ConfirmModeOK,
+    DeliveryError,
+    DEFAULT_EXCHANGE,
+    MandatoryError
+)
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
