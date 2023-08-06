2918c98102d7a61092b473a9acf66080a438bbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:13 +0000

    Add package crazy-thursday/1.0.3.0/crazy-thursday-1.0.3.0.tar.gz

---
+++ packages/crazy-thursday/crazy-thursday-1.0.3.0.tar.gz/crazy-thursday-1.0.3.0/setup.py
@@ -0,0 +1,22 @@
+"""
+this is the setup of this package.
+"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+from crazy_thursday import VERSION
+
+with open('crazy_thursday/requirements.txt', 'r', encoding='utf8') as file:
+    install_requires = list(map(lambda x: x.strip(), file.readlines()))
+
+setup(
+    name='crazy-thursday',
+    version='.'.join(map(str, VERSION)),
+    author='kinopico',
+    author_email='zqmillet@qq.com',
+    url='https://github.com/zqmillet/sphinx-console',
+    description='an extension for sphinx to display console in sphinx documents',
+    packages=find_packages(),
+    install_requires=install_requires,
+)
