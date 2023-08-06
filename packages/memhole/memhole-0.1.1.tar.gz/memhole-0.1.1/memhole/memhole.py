64cf0e3e2e70a788c889d4a8d6ef0e544e85cdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:41 +0000

    Add package memhole/0.1.1/memhole-0.1.1.tar.gz

---
+++ packages/memhole/memhole-0.1.1.tar.gz/memhole-0.1.1/memhole/memhole.py
@@ -0,0 +1,24 @@
+import ctypes
+
+memhole = ctypes.cdll.LoadLibrary("./memhole.so")
+memhole.create_memhole.restype = ctypes.c_void_p
+memhole.delete_memhole.argtypes = [ctypes.c_void_p]
+memhole.attach_to_pid.argtypes = [ctypes.c_void_p, ctypes.c_int]
+memhole.attach_to_pid.restype = ctypes.c_long
+memhole.set_buffer_size.argtypes = [ctypes.c_void_p, ctypes.c_int]
+memhole.set_buffer_size.restype = ctypes.c_int
+memhole.get_memory_position.argtypes = [ctypes.c_void_p]
+memhole.get_memory_position.restype = ctypes.c_void_p
+memhole.set_memory_position.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int]
+memhole.set_memory_position.restype = ctypes.c_void_p
+memhole.read_memory.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_long, ctypes.c_int]
+memhole.read_memory.restype = ctypes.c_long
+memhole.connect_memhole.argtypes = [ctypes.c_void_p]
+memhole.buffer_creator.argtypes = [ctypes.c_void_p, ctypes.c_long]
+memhole.buffer_creator.restype = ctypes.c_char_p
+memhole.printf_wrapper.argtypes = [ctypes.c_char_p, ctypes.c_long]
+memhole.disconnect_memhole.argtypes = [ctypes.c_void_p]
+memhole.disconnect_memhole.restype = ctypes.c_int
+memhole.buffer_deleter.argtypes = [ctypes.c_void_p]
+
+print("finished import")
