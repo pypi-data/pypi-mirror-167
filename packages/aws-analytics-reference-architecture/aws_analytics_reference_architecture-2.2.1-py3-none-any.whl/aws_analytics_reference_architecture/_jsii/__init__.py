686e84533b55323570dc26ea9a2236bf7a12e74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:25 +0000

    Add package aws-analytics-reference-architecture/2.2.1/aws_analytics_reference_architecture-2.2.1-py3-none-any.whl

---
+++ packages/aws-analytics-reference-architecture/aws_analytics_reference_architecture-2.2.1-py3-none-any.whl/aws_analytics_reference_architecture/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import aws_cdk.aws_glue_alpha._jsii
+import aws_cdk.aws_redshift_alpha._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "aws-analytics-reference-architecture",
+    "2.2.1",
+    __name__[0:-6],
+    "aws-analytics-reference-architecture@2.2.1.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
