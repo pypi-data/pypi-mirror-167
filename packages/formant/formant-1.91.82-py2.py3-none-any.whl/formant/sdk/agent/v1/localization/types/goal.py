934c8be608217d3d440253ef6232c8e7498c1576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:42 +0000

    Add package formant/1.91.82/formant-1.91.82-py2.py3-none-any.whl

---
+++ packages/formant/formant-1.91.82-py2.py3-none-any.whl/formant/sdk/agent/v1/localization/types/goal.py
@@ -0,0 +1,16 @@
+from formant.protos.model.v1 import navigation_pb2
+from .transform import Transform
+
+
+class Goal:
+    def __init__(
+        self, pose: Transform = Transform(), transform_to_world: Transform = Transform()
+    ):
+        self.pose = pose
+        self.transform_to_world = transform_to_world
+
+    def to_proto(self):
+        return navigation_pb2.Goal(
+            pose=self.pose.to_proto(),
+            world_to_local=self.transform_to_world.to_proto(),
+        )
