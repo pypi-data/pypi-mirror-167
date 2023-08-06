575cb4cf4fbf889f79a8707c1fba7eab276db89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:02 +0000

    Add package lifter-api-wrapper/0.3.1/lifter_api_wrapper-0.3.1-py3-none-any.whl

---
+++ packages/lifter-api-wrapper/lifter_api_wrapper-0.3.1-py3-none-any.whl/lifter_api/__init__.py
@@ -0,0 +1,6 @@
+"""Lifter API Wrapper."""
+from .main import LifterAPI
+
+__all__ = ["LifterAPI"]
+
+__version__ = "0.3.1"
---
+++ packages/lifter-api-wrapper/lifter_api_wrapper-0.3.1-py3-none-any.whl/lifter_api/utils/decorators.py
@@ -0,0 +1,42 @@
+"""Decorators for mixins."""
+
+from collections.abc import Callable
+from functools import wraps
+
+
+def _check_id(func: Callable) -> Callable:
+    """Check competition ID - Decorator.
+
+    An incorrect id will return a dictionary containing the key "detail".
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> dict[str, str] | Callable:
+        """Wrap function."""
+        not_exists = {}
+
+        athlete_id = kwargs.get("athlete_id")
+        if athlete_id and func.__name__ != "get_athlete":
+            not_exists["athlete"] = self.get_athlete(
+                athlete_id=athlete_id
+            ).get("detail", False)
+
+        competition_id = kwargs.get("competition_id")
+        if competition_id and func.__name__ != "get_competition":
+            not_exists["competition"] = self.get_competition(
+                competition_id=competition_id
+            ).get("detail", False)
+
+        lift_id = kwargs.get("lift_id")
+        if lift_id and func.__name__ != "get_lift":
+            not_exists["lift"] = self.get_lift(
+                competition_id=competition_id, lift_id=lift_id
+            ).get("detail", False)
+
+        cleaned_not_exists = list(not_exists.values())
+        if any(cleaned_not_exists):
+            return {"detail": " ".join(cleaned_not_exists)}
+
+        return func(self, *args, **kwargs)
+
+    return wrapper
