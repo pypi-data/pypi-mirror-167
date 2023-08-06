d5ffaa2ab19bfa336ef9dfc83706e44872c019bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:31 +0000

    Add package BaseNN/0.0.2/BaseNN-0.0.2.tar.gz

---
+++ packages/BaseNN/BaseNN-0.0.2.tar.gz/BaseNN-0.0.2/BaseNN/__init__.py
@@ -0,0 +1,6 @@
+from .BaseNN import BaseNN
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from .version import __version__ 
+__all__    = ['BaseNN','torch', 'nn', 'Variable',' __version__'] 
