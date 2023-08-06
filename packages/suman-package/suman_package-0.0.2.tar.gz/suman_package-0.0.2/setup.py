7522914b9f624e461bb0cd4a7c886b3abb571ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package suman-package/0.0.2/suman_package-0.0.2.tar.gz

---
+++ packages/suman-package/suman_package-0.0.2.tar.gz/suman_package-0.0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name="suman_package",
+    version="0.0.2",
+    author="suman choudhary",
+    description="string upper concat",
+    python_requires='>=3.6',
+    packages=["suman_package"]
+
+)
---
+++ packages/suman-package/suman_package-0.0.2.tar.gz/suman_package-0.0.2/suman_package/uppercase_string.py
@@ -0,0 +1,5 @@
+def string_upper_concat(a,b):
+    a = a.upper()
+    b = b.upper()
+    result = a + b
+    return result
