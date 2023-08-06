a0847ee16cb3e74e57151f59e0c0b7239e1113d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:08 +0000

    Add package pyoccad/0.5.0/pyoccad-0.5.0.tar.gz

---
+++ packages/pyoccad/pyoccad-0.5.0.tar.gz/pyoccad-0.5.0/pyoccad/transform/scale_test.py
@@ -0,0 +1,26 @@
+from OCC.Core.TopoDS import TopoDS_Solid
+
+from pyoccad.create import CreateBox
+from pyoccad.measure import solid
+from pyoccad.tests import TestCase
+from pyoccad.transform import Scale
+
+import pytest
+
+
+class ScaleTest(TestCase):
+
+    def test_using_transformation(self):
+        box = CreateBox.from_dimensions_and_center((1., 1., 1.))
+        self.assertAlmostSameCoord(solid.center(box), (0, 0, 0))
+        self.assertAlmostEqual(1, solid.volume(box))
+
+        with pytest.raises(RuntimeError):
+            Scale.from_factor(box, 10, inplace=True)
+
+        result = Scale.from_factor(box, 10, inplace=False)
+        self.assertAlmostSameCoord(solid.center(box), (0, 0, 0))
+        self.assertAlmostEqual(1, solid.volume(box))
+        self.assertIsInstance(result, TopoDS_Solid)
+        self.assertAlmostSameCoord(solid.center(result), (0, 0, 0))
+        self.assertAlmostEqual(1000, solid.volume(result))
