144eebd24e800d4e82b4283cde734e6c76682bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:42 +0000

    Add package ufwio/0.0.1/ufwio-0.0.1.tar.gz

---
+++ packages/ufwio/ufwio-0.0.1.tar.gz/ufwio-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+    # import lmdb
+    # from scipy.ndimage import zoom
+    # from skimage.transform import resize
+    # import skimage.io
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="ufwio",
+  version="0.0.1",
+  author="Wangyang.zuo",
+  author_email="wangyang.zuo@sophon.com",
+  description="A package for LMDB database operations",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/pypa/sampleproject",
+  packages=setuptools.find_packages(),
+  install_requires=[
+      "numpy",
+      "lmdb",
+      "scipy",
+      "scikit-image",
+      "google",
+      "protobuf",
+  ],
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
