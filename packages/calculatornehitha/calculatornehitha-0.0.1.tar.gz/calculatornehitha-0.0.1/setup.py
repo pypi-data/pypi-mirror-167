9e1ee75303d2c313e7bb81379a380a8207a5c8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:05 +0000

    Add package calculatornehitha/0.0.1/calculatornehitha-0.0.1.tar.gz

---
+++ packages/calculatornehitha/calculatornehitha-0.0.1.tar.gz/calculatornehitha-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 10',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='calculatornehitha',
+    version='0.0.1',
+    description='this is a trial package used for calculation.',
+    url='',
+    author='Neha Pappachan',
+    author_email='nehapappachan@gmail.com',
+    licence='MIT',
+    packages=['ncal'],
+    install_requires=[]
+)
