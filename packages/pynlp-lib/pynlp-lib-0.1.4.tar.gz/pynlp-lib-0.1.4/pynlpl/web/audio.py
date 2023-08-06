f5be12a25c817eb4e7b409d7f9b0f6eaac901949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:38 +0000

    Add package pynlp-lib/0.1.4/pynlp-lib-0.1.4.tar.gz

---
+++ packages/pynlp-lib/pynlp-lib-0.1.4.tar.gz/pynlp-lib-0.1.4/pynlpl/web/audio.py
@@ -0,0 +1,10 @@
+from deepgram import Deepgram
+
+class Deepgram(Deepgram):
+    """Implements the most recent Deepgram SDK."""
+    def __init__(self, apikey):
+        super().__init__(apikey)
+
+# TODO: implement Google
+# TODO: implement Microsoft
+# TODO: implement Amazon
---
+++ packages/pynlp-lib/pynlp-lib-0.1.4.tar.gz/pynlp-lib-0.1.4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
+
+# from setuptools import find_packages, setup
+
+# basic setup, to be improved in the future
+# setup(
+#     name="pynlp-lib",
+#     packages=find_packages(),
+#     version='0.1.4',
+#     description="A Comprehensive NLP Library for Python",
+#     author="Yujian Tang",
+#     license="MIT",
+#     install_requires=["requests", "deepgram-sdk", "spacy", "spacytextblob"],
+#     setup_requires=[],
+#     tests_requires=[],
+#     test_suites='tests',
+#     keywords='nlp text processing transcription ai nlu nlg asr'
+# )
