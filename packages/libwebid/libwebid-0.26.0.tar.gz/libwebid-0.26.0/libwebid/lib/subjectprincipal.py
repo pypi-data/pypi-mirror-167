26e945f9f55ab047f29af1d29c76d254b80c722e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:19 +0000

    Add package libwebid/0.26.0/libwebid-0.26.0.tar.gz

---
+++ packages/libwebid/libwebid-0.26.0.tar.gz/libwebid-0.26.0/libwebid/lib/subjectprincipal.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2022 Cochise Ruhulessin
+# 
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from ckms.types import ClaimSet
+from cbra.ext.oauth2.types import RFC9068Token
+from cbra.ext.oauth2.types import IPrincipal
+
+
+class SubjectPrincipal(IPrincipal, RFC9068Token): # type: ignore
+    __module__: str = 'libwebid.lib'
+    sub: int
+
+    @classmethod
+    def fromclaimset(cls, claims: ClaimSet) -> 'SubjectPrincipal':
+        return cls.parse_obj(claims.dict())
+
+    def is_authenticated(self) -> bool:
+        return True
