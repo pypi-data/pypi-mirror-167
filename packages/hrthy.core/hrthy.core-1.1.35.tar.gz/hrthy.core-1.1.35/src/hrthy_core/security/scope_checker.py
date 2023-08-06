0a5adc77109b23ad56b3767a29ca5cf176f74364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:04 +0000

    Add package hrthy.core/1.1.35/hrthy.core-1.1.35.tar.gz

---
+++ packages/hrthy.core/hrthy.core-1.1.35.tar.gz/hrthy.core-1.1.35/src/hrthy_core/security/scope_checker.py
@@ -0,0 +1,38 @@
+from typing import List, Optional
+from uuid import UUID
+
+from hrthy_core.security.exceptions import MissingPermissionException, MissingScopeConfiguration
+from hrthy_core.security.scopes_hierarchy import HIERARCHY_SCOPES
+from hrthy_core.security.security import Requester
+from hrthy_core.security.types import ScopeType
+
+
+class ScopeChecker:
+    @staticmethod
+    def has_all_permissions(requester: Requester, scopes: List[str]) -> bool:
+        return all(s in requester.scopes for s in scopes)
+
+    @staticmethod
+    def has_at_least_one_permission(requester: Requester, scopes: List[str]) -> bool:
+        return any(s in requester.scopes for s in scopes)
+
+    @staticmethod
+    def get_first_match_permission(requester: Requester, scopes: List[str]) -> Optional[str]:
+        for s in scopes:
+            if s in requester.scopes:
+                return s
+        return None
+
+    @staticmethod
+    def get_target_company(requester: Requester, priority_scopes: List[str], company_id: UUID = None) -> UUID:
+        scope = ScopeChecker.get_first_match_permission(requester=requester, scopes=priority_scopes)
+        if scope:
+            scope_configuration = HIERARCHY_SCOPES.get(scope, None)
+            if not scope_configuration:
+                raise MissingScopeConfiguration()
+            if scope_configuration.get('type') != ScopeType.GLOBAL \
+                    and company_id is not None \
+                    and company_id != requester.company_id:
+                raise MissingPermissionException()
+            return company_id if company_id is not None else requester.company_id
+        raise MissingPermissionException()
