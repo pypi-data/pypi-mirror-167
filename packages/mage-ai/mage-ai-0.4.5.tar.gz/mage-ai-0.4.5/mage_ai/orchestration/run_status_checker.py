21067b66545314c1672b2c7a032d6f381ccc05a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:30 +0000

    Add package mage-ai/0.4.5/mage-ai-0.4.5.tar.gz

---
+++ packages/mage-ai/mage-ai-0.4.5.tar.gz/mage-ai-0.4.5/mage_ai/orchestration/run_status_checker.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from mage_ai.orchestration.db.models import BlockRun, PipelineRun, PipelineSchedule
+
+
+def check_status(
+    pipeline_uuid: str,
+    execution_date: datetime,
+    block_uuid: str = None,
+) -> bool:
+    pipeline_run = (
+        PipelineRun
+        .query
+        .join(PipelineRun.pipeline_schedule)
+        .filter(PipelineSchedule.pipeline_uuid == pipeline_uuid)
+        .filter(PipelineRun.execution_date == execution_date)
+        .first()
+    )
+    if pipeline_run is None:
+        return False
+
+    pipeline_run.refresh()
+
+    if block_uuid is not None:
+        block_run = next(filter(lambda run: run.block_uuid == block_uuid, pipeline_run.block_runs))
+        if block_run is None:
+            return False
+        return block_run.status == BlockRun.BlockRunStatus.COMPLETED
+    else:
+        return pipeline_run.status == PipelineRun.PipelineRunStatus.COMPLETED
