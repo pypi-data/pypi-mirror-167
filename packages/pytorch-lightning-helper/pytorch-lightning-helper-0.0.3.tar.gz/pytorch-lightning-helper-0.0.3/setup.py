f70fa400f587585e0050a556d307b70206dd1069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:11 +0000

    Add package pytorch-lightning-helper/0.0.3/pytorch-lightning-helper-0.0.3.tar.gz

---
+++ packages/pytorch-lightning-helper/pytorch-lightning-helper-0.0.3.tar.gz/pytorch-lightning-helper-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import *
+
+LONG_DESC = """
+This is pytorch lightning helper
+"""
+
+setup(name='pytorch-lightning-helper',
+	  version='0.0.3',
+	  description='Pytorch lightning helper',
+	  long_description=LONG_DESC,
+	  author='Sang Ki Kwon',
+	  url='https://github.com/automatethem/pytorch-lightning-helper',
+	  #install_requires=['pytorch-lightning', 'torchsummaryX'],
+	  install_requires=[],
+	  author_email='automatethem@gmail.com',
+	  license='MIT',
+	  packages=find_packages(),
+	  zip_safe=False)
