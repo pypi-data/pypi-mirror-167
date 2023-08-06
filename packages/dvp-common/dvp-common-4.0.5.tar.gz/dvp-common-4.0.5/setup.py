4c75b7c066a6ae812c6ee4bb8fbcabfe48d783cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:25 +0000

    Add package dvp-common/4.0.5/dvp-common-4.0.5.tar.gz

---
+++ packages/dvp-common/dvp-common-4.0.5.tar.gz/dvp-common-4.0.5/setup.py
@@ -0,0 +1,19 @@
+import os
+import setuptools
+
+PYTHON_SRC = 'src/main/python'
+
+install_requires = [
+  "dvp-api == 1.7.0",
+]
+
+with open(os.path.join(PYTHON_SRC, 'dlpx/virtualization/common/VERSION')) as version_file:
+    version = version_file.read().strip()
+
+setuptools.setup(name='dvp-common',
+                 version=version,
+                 install_requires=install_requires,
+                 package_dir={'': PYTHON_SRC},
+                 packages=setuptools.find_packages(PYTHON_SRC),
+                 python_requires='>=2.7, <3.9, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, !=3.7.*',
+)
