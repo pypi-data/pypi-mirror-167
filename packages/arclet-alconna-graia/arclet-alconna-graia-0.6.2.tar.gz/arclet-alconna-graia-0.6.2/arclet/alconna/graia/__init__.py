ec8c0c59241ca74d352660f8b2951878fe775c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:02 +0000

    Add package arclet-alconna-graia/0.6.2/arclet-alconna-graia-0.6.2.tar.gz

---
+++ packages/arclet-alconna-graia/arclet-alconna-graia-0.6.2.tar.gz/arclet-alconna-graia-0.6.2/arclet/alconna/graia/__init__.py
@@ -0,0 +1,27 @@
+"""
+Alconna 对于 Graia 系列的支持
+
+"""
+from arclet.alconna import Alconna
+from .analyser import GraiaCommandAnalyser
+from .dispatcher import AlconnaDispatcher, AlconnaOutputMessage, success_record
+from .saya import AlconnaSchema, AlconnaBehaviour
+from .model import AlconnaProperty, Query, Match
+from .utils import (
+    ImgOrUrl,
+    fetch_name,
+    match_path,
+    AtID,
+    alcommand,
+    match_value,
+    from_command,
+    shortcuts,
+    assign,
+    startswith,
+    endswith,
+    MatchPrefix,
+    MatchSuffix,
+)
+
+Alconna.config(analyser_type=GraiaCommandAnalyser)
+Alc = AlconnaDispatcher
