f981db69580bfd04ed9c70004c2ac29c8817d822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:33 +0000

    Add package djangoplicity-mailman/0.4.2/djangoplicity-mailman-0.4.2.tar.gz

---
+++ packages/djangoplicity-mailman/djangoplicity-mailman-0.4.2.tar.gz/djangoplicity-mailman-0.4.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name = 'djangoplicity-mailman',
+    version = '0.4.2',
+    packages = ['django_mailman',],
+    platforms = ['any'],
+    license = 'GNU LGPL v2.1',
+    author = "Bernd Schlapsi",
+    author_email = 'albert@albertoconnor.ca',
+    description = 'Interface to Mailman Web-API',
+    long_description = open('README.rst').read(),
+    url = 'https://github.com/djangoplicity/djangoplicity-mailman',
+    download_url = 'https://github.com/djangoplicity/djangoplicity-mailman/archive/refs/tags/0.4.2.tar.gz'
+)
