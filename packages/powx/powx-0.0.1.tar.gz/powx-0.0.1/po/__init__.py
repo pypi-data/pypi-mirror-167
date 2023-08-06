e3a9bec1394caecc1803541f4e30004c21f19b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:44 +0000

    Add package powx/0.0.1/powx-0.0.1.tar.gz

---
+++ packages/powx/powx-0.0.1.tar.gz/powx-0.0.1/po/__init__.py
@@ -0,0 +1,4 @@
+from po.api import chat
+from po.api import file
+
+print('聊天机器人，开始运行：https://github.com/CoderWanFeng/PyOfficeRobot/blob/main/README.md')
---
+++ packages/powx/powx-0.0.1.tar.gz/powx-0.0.1/tests/test.py
@@ -0,0 +1,4 @@
+import os
+
+print(os.environ.keys())
+print(os.environ.get('USERNAME'))
