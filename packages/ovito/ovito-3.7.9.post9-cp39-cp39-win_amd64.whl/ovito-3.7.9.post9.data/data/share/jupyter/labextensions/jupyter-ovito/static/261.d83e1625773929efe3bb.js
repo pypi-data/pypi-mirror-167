d82e7dd72f885806370f415e974ce2ba820d65ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:23 +0000

    Add package ovito/3.7.9.post9/ovito-3.7.9.post9-cp39-cp39-win_amd64.whl

---
+++ packages/ovito/ovito-3.7.9.post9-cp39-cp39-win_amd64.whl/ovito-3.7.9.post9.data/data/share/jupyter/labextensions/jupyter-ovito/static/261.d83e1625773929efe3bb.js
@@ -0,0 +1,1 @@
+(self.webpackChunkjupyter_ovito=self.webpackChunkjupyter_ovito||[]).push([[261],{261:(e,t,r)=>{var i=r(568),o=r(382);e.exports={id:"jupyter-ovito:plugin",requires:[o.IJupyterWidgetRegistry],activate:function(e,t){t.registerWidget({name:"jupyter-ovito",version:i.version,exports:i})},autoStart:!0}}}]);
