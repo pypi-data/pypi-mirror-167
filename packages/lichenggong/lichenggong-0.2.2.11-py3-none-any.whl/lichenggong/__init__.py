c6d2f429e48b2010d1921e7d9a37d944e1ac56ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:21 +0000

    Add package lichenggong/0.2.2.11/lichenggong-0.2.2.11-py3-none-any.whl

---
+++ packages/lichenggong/lichenggong-0.2.2.11-py3-none-any.whl/lichenggong/__init__.py
@@ -0,0 +1,7 @@
+print("""
+It's a set of Python modules designed for OS(No,it's not)
+programs in the python language. The package can't portable,
+just can run with Windows
+
+les's say that!       'Noodows!'       
+""")
