2b4ca72cf8ca54b0890ad9bb3a61bff0a3927350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:38 +0000

    Add package easyplib/0.6/easyplib-0.6.tar.gz

---
+++ packages/easyplib/easyplib-0.6.tar.gz/easyplib-0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='easyplib',
+    version='0.6',
+    author="Alexander Timofeev",
+    author_email="tam2511@mail.ru",
+    python_requires=">=3.6",
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tam2511/EasyPL",
+    install_requires=[
+        'pytorch-lightning',
+        'pandas',
+        'dill',
+        'opencv-python',
+        'matplotlib',
+        'albumentations',
+        'torchvision',
+        'torchmetrics'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
