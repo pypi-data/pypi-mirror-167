c623eae85051e54693effe7b1e4a827e897b5d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:07 +0000

    Add package charSn/0.0.5/charSn-0.0.5.tar.gz

---
+++ packages/charSn/charSn-0.0.5.tar.gz/charSn-0.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['charsn']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'charsn',
+    'version': '0.0.5',
+    'description': 'some functions to calculate group characters of S_n',
+    'long_description': '# Characters_Sn\nFunctions for calculation group characters of the symmetric group Sn\n\n\nThis code was written by Lizzie Hernandez \nfor Math 68 Algebraic Combinatorics course in Fall 2021 \nat Darmouth College \n\n[GitHub Link] (https://github.com/lizziehv/math68-final)\n[Google Colab Link] (https://colab.research.google.com/drive/1GMeXdseX97ZfMtfsO6Tvf5pijP4Zslc6?usp=sharing)',
+    'author': 'Vladislav Kargin',
+    'author_email': 'slavakargin@yahoo.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': '',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
