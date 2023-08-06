5e268865aa2ce0785d3e072f4b1b65852ed25fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:57 +0000

    Add package ols-client/0.1.2/ols_client-0.1.2.tar.gz

---
+++ packages/ols-client/ols_client-0.1.2.tar.gz/ols_client-0.1.2/tests/test_ols_client.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for the client."""
+
+from ols_client.client import EBIClient
+from tests import cases
+
+
+class TestEbi(cases.TestClient):
+    """Tests for the EBI client."""
+
+    client_cls = EBIClient
+    test_ontology = "aro"
+    test_label = "CMY-167"
