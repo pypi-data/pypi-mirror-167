ececf2206a207b874b85a6b012f307bbbcdf450f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:43 +0000

    Add package jupyter-scheduler/0.1.0/jupyter_scheduler-0.1.0.tar.gz

---
+++ packages/jupyter-scheduler/jupyter_scheduler-0.1.0.tar.gz/jupyter_scheduler-0.1.0/src/components/notebook-jobs-navigation-tab.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { JobsPanelView } from '../notebook-jobs-panel';
+
+export interface tabClickProps {
+  (event: React.MouseEvent<HTMLElement, MouseEvent>, view: JobsPanelView): void;
+}
+
+export function NotebookJobsNavigationTab(props: {
+  onClick: tabClickProps;
+  id: JobsPanelView;
+  title: string;
+  active: boolean;
+}) {
+  return (
+    <li
+      id={props.id}
+      className={
+        'jp-notebook-job-navigation-tab' + (props.active ? ' active' : '')
+      }
+      onClick={props.active ? () => {} : e => props.onClick(e, props.id)}
+    >
+      {props.title}
+    </li>
+  );
+}
