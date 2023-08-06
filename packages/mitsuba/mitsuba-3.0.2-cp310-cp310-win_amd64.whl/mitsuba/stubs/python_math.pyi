ca309745fd8db2c1cb0f781a75a8533ed4742079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:57 +0000

    Add package mitsuba/3.0.2/mitsuba-3.0.2-cp310-cp310-win_amd64.whl

---
+++ packages/mitsuba/mitsuba-3.0.2-cp310-cp310-win_amd64.whl/mitsuba/stubs/python_math.pyi
@@ -0,0 +1,10 @@
+from typing import Any, Callable, Iterable, Iterator, Tuple, List, TypeVar, Union, overload, ModuleType
+import mitsuba
+import mitsuba as mi
+
+def rlgamma(a, x):
+    """
+    Regularized lower incomplete gamma function based on CEPHES
+    """
+    ...
+
---
+++ packages/mitsuba/mitsuba-3.0.2-cp310-cp310-win_amd64.whl/mitsuba/stubs/util.pyi
@@ -0,0 +1,36 @@
+from typing import Any, Callable, Iterable, Iterator, Tuple, List, TypeVar, Union, overload, ModuleType
+import mitsuba
+import mitsuba as mi
+
+def core_count() -> int:
+    """
+    Determine the number of available CPU cores (including virtual cores)
+    """
+    ...
+
+def mem_string(size: int, precise: bool = False) -> str:
+    """
+    Turn a memory size into a human-readable string
+    """
+    ...
+
+def time_string(time: float, precise: bool = False) -> str:
+    """
+    Convert a time difference (in seconds) to a string representation
+    
+    Parameter ``time``:
+        Time difference in (fractional) sections
+    
+    Parameter ``precise``:
+        When set to true, a higher-precision string representation is
+        generated.
+    """
+    ...
+
+def trap_debugger() -> None:
+    """
+    Generate a trap instruction if running in a debugger; otherwise,
+    return.
+    """
+    ...
+
