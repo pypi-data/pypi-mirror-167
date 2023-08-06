97b6714296757045fbb9f1b593541e404b7bd4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:04 +0000

    Add package exception-details/0.0.2/exception_details-0.0.2.tar.gz

---
+++ packages/exception-details/exception_details-0.0.2.tar.gz/exception_details-0.0.2/exception_details/__init__.py
@@ -0,0 +1,12 @@
+from .exception_details import (
+    print_exception_details,
+    get_exception_details_dict,
+    get_exception_details_str,
+)
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+from . import _version
+__version__ = _version.get_versions()['version']
