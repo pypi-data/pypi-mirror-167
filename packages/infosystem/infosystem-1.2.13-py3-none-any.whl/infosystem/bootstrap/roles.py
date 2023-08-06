9ab4a13612bdf2ea245fe19d36547fe9651607b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:03 +0000

    Add package infosystem/1.2.13/infosystem-1.2.13-py3-none-any.whl

---
+++ packages/infosystem/infosystem-1.2.13-py3-none-any.whl/infosystem/bootstrap/roles.py
@@ -0,0 +1,31 @@
+from infosystem.common.subsystem.apihandler import Api
+import uuid
+
+from typing import Dict, List
+
+from infosystem.common.subsystem import Subsystem
+from infosystem.subsystem.role.resource import Role
+
+
+class BootstrapRoles(object):
+
+    def __init__(self, api: Api) -> None:
+        self.role_manager = api.roles()
+
+    def execute(self):
+        roles = self.role_manager.list()
+        if not roles:
+            default_roles = self._default_roles()
+            roles = self.role_manager.create_roles(roles=default_roles)
+        return roles
+
+    def _get_role(self, name: str) -> Role:
+        role = Role(id=uuid.uuid4().hex, name=name)
+        return role
+
+    def _default_roles(self) -> List[Role]:
+        user = self._get_role(Role.USER)
+        sysadmin = self._get_role(Role.SYSADMIN)
+        admin = self._get_role(Role.ADMIN)
+
+        return [user, sysadmin, admin]
