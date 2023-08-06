d8e3aa1f8b5858b36c3e8e1431fbee5c632add65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:35 +0000

    Add package stackcli/0.1.14/stackcli-0.1.14-py3-none-any.whl

---
+++ packages/stackcli/stackcli-0.1.14-py3-none-any.whl/src/core/core.py
@@ -0,0 +1,464 @@
+import sys
+sys.path.append( '../..' )
+from src.core.init import Initializer
+import socket
+import string
+import json
+import io
+from datetime import datetime
+import time
+import difflib
+import re
+import os
+from pathlib import Path
+from src.comm.docker_ver import *
+path_home = os.getenv('LCP_DKR')+'/' if docker_ver() else str(Path.home())
+
+def commit(init, comment = ''):
+	# get new files
+	new_files, new_lm, old_files, old_lm = init.getListtoCompare()
+
+	if comment != '':
+		comment = ', ' + comment
+
+	init.storage.resetBuffer()
+	# checks who is the user
+	idx = 0
+	if init.user == None:
+		username = socket.gethostbyname(socket.gethostname())
+	else:
+		username = init.user.username
+
+	commits = []
+	toadd = []
+	toremove = []
+
+	for idx, f in enumerate(old_files):
+		# relative file location
+		f_relative = f.replace(init.prefix_curr,"")
+
+		# checks if the file was modified or remove
+		if f_relative in new_files:
+			if new_lm[new_files.index(f_relative)] != old_lm[idx]:
+				# checks the latest diff and sets the new path
+				n = init.getLatestDiffNumber(f_relative) + 1
+				diff = init.prefix_diffs + f_relative + '/' + str(n).zfill(10)
+
+				# computes the diff and stores it
+				if init.storage.type == 'local':
+					storeDiff(init.storage,diff,f_relative,f)
+				else:
+					storeDiff(init.storage,diff,f_relative,f)
+
+				commit = {
+					'key'	: f_relative,
+					'diff'	: diff,
+					'version' : n,
+					'type'	: 'modified',
+					'date'	: new_lm[idx],
+					'source': username,
+					'comment' : 'modified '+ f_relative + comment,
+				}
+
+				print('-- modified '+ f_relative)
+
+				toadd.append(f_relative)
+	
+				commitpath = init.prefix_commit + f_relative + '/' + str(n).zfill(10)
+				commits.append(commitpath)
+				init.storage.addFileFromBinaryGlobal(commitpath,io.BytesIO(json.dumps(commit).encode('ascii')))
+				updateFileHistory(init,f_relative,commit)
+		else:
+			# checks the latest diff and sets the new path
+			n = init.getLatestDiffNumber(f_relative) + 1
+			diff = init.prefix_diffs + f_relative + '/' + str(n).zfill(10)
+			init.storage.addFileFromBinaryGlobal(diff,io.BytesIO("".encode('ascii')))
+
+			commit = {
+				'key'	: f_relative,
+				'diff'	: diff,
+				'version' : n,
+				'type'	: 'remove',
+				'date'	: old_lm[idx],
+				'source': username,
+				'comment' : 'removed '+ f_relative+comment,
+			}
+
+			print('-- removed '+ f_relative)
+
+			toremove.append(f_relative)
+
+			prefix_commit = init.prefix_commit
+
+			commitpath = prefix_commit + f_relative + '/' + str(n).zfill(10)
+			commits.append(commitpath)
+			init.storage.addFileFromBinaryGlobal(commitpath,io.BytesIO(json.dumps(commit).encode('ascii')))
+			updateFileHistory(init,f_relative,commit)
+
+	init.storage.resetBuffer()
+
+	old_rel_files = [x.replace(init.prefix_curr,"") for x in old_files]
+
+	for idx, f in enumerate(new_files):
+		# checks if the file was modified or remove
+		if not (f in old_rel_files):
+
+			if not '.DS_Store' in f:
+				# checks the latest diff and sets the new path
+				n = init.getLatestDiffNumber(f) + 1
+				diff = init.prefix_diffs + f + '/' + str(n).zfill(10)
+
+				if init.storage.type == 'local':
+					storeDiff(init.storage,diff,f,f)
+				else:
+					storeDiff(init.storage,diff,f,f)
+
+				commit = {
+					'key'	: f,
+					'diff'	: diff,
+					'version' : n,
+					'type'	: 'add',
+					'date'	: new_lm[idx],
+					'source': username,
+					'comment' : 'added '+ f+comment,
+				}
+
+				print('-- added '+ f)			
+
+				toadd.append(f)
+
+				prefix_commit = init.prefix_commit
+
+				commitpath = prefix_commit + f + '/' + str(n).zfill(10)
+				commits.append(commitpath)
+				init.storage.addFileFromBinaryGlobal(commitpath,io.BytesIO(json.dumps(commit).encode('ascii')))
+				updateFileHistory(init,f,commit)
+	init.storage.resetBuffer()	
+
+	# updates the current version in .stack
+	if len(commits):
+		init.copyCurrentCommit(toadd,toremove)
+		updateHistory(init,commits)
+	init.storage.resetBuffer()
+
+	return (len(commits) > 0)
+
+def computeDiff(bin1,bin2):
+	"""
+	Get unified string diff between two strings. Trims top two lines.
+	Returns empty string if strings are identical.
+	"""
+	_no_eol = "\ No newline at end of file"
+	diffs = difflib.unified_diff(bin1.splitlines(True),bin2.splitlines(True),n=0)
+	try: 
+		_,_ = next(diffs),next(diffs)
+	except StopIteration: 
+		pass
+	return ''.join([d if d[-1] == '\n' else d+'\n'+_no_eol+'\n' for d in diffs])
+
+def applyDiff(bin1,patch):
+	s = s.splitlines(True)
+	p = patch.splitlines(True)
+	t = ''
+	i = sl = 0
+	_hdr_pat = re.compile("^@@ -(\d+),?(\d+)? \+(\d+),?(\d+)? @@$")
+	(midx,sign) = (1,'+') if not revert else (3,'-')
+	while i < len(p) and p[i].startswith(("---","+++")): 
+		i += 1 # skip header lines
+	while i < len(p):
+		m = _hdr_pat.match(p[i])
+		if not m: 
+			raise Exception("Bad patch -- regex mismatch [line "+str(i)+"]")
+		l = int(m.group(midx))-1 + (m.group(midx+1) == '0')
+		if sl > l or l > len(s):
+		  raise Exception("Bad patch -- bad line num [line "+str(i)+"]")
+		t += ''.join(s[sl:l])
+		sl = l
+		i += 1
+		while i < len(p) and p[i][0] != '@':
+			if i+1 < len(p) and p[i+1][0] == '\\':
+				line = p[i][:-1]
+				i += 2
+			else: 
+				line = p[i]
+				i += 1
+			if len(line) > 0:
+				if line[0] == sign or line[0] == ' ':
+					t += line[1:]
+				sl += (line[0] != sign)
+	t += ''.join(s[sl:])
+	return t
+
+def storeDiff(storage, diff_path, key_old, key_new):
+	# loads files
+	# bin_old = storage.loadFileGlobal(key_old)
+	# bin_new = storage.loadFileGlobal(key_new)
+
+	# diff surrogate, save latest snapshot
+	storage.copyFileGlobal(key_new,diff_path)
+	return True
+
+def updateHistory(init,commits):
+	# loads file global
+	metapath = init.prefix_meta+'history.json'
+	history = json.load(init.storage.loadFileGlobal(metapath))
+	init.storage.resetBuffer()
+
+	# adds to the latest commits
+	time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+	history[str(len(history)+1)] = {'commits': commits, 'date': time}
+
+	init.storage.addFileFromBinaryGlobal(metapath,io.BytesIO(json.dumps(history).encode('ascii')))
+	init.storage.resetBuffer()
+	return True
+
+def updateFileHistory(init,key,commit):
+	# loads file global
+	metapath = init.prefix_history+key+'/history.json'
+	if init.storage.checkIfEmpty(init.prefix_history+key+'/'): 
+		history = {}
+		history[1] = commit
+	else:
+		history = json.load(init.storage.loadFileGlobal(metapath))
+		history[str(len(history)+1)] = commit
+		init.storage.resetBuffer()
+		
+	init.storage.addFileFromBinaryGlobal(metapath,io.BytesIO(json.dumps(history).encode('ascii')))
+	init.storage.resetBuffer()
+	return True
+
+def add(init, files=[], location='?'):
+	if location == '?':
+		location = ''
+	
+	# adds each file
+	for file in files:
+		print('adding '+file+'...')
+		init.storage.addFile(file, '', location)
+	init.storage.resetBuffer()
+	return True
+
+def add_from_binary(init, filename='', binary='', location='?'):
+	if location == '?':
+		location = ''
+	
+	# adds each file
+	print('adding '+filename+'...')
+	init.storage.addFileFromBinary(filename, binary)
+	init.storage.resetBuffer()
+	return True
+
+def remove(init, files=[], location='?'):
+	if location == '?':
+		location = ''
+
+	# adds each file
+	for file in files:
+		print('deleting file '+location+file+'...')
+		init.storage.removeFile(location+file)
+	return True
+
+def removeGlobal(init, files=[], location='?'):
+	if location == '?':
+		location = ''
+
+	# adds each file
+	for file in files:
+		print('deleting file '+location+file+'...')
+		init.storage.removeFileGlobal(location+file)
+	return True
+
+def remove_full(init, key):
+	# diff location
+	n_versions = init.getLatestDiffNumber(key)
+	init.storage.removeFile(key)
+	for version in range(1,n_versions+1):
+		diff = init.prefix_diffs + ke + '/' + str(version).zfill(10)
+
+		# generates an empty diff
+		init.storage.addFileFromBinaryGlobal(diff,io.BytesIO("".encode('ascii')))
+		init.storage.resetBuffer()
+
+	return True
+
+def remove_diff(init, key, version):
+	# diff location
+	diff = init.prefix_diffs + key + '/' + str(version).zfill(10)
+
+	# generates an empty diff
+	init.storage.addFileFromBinaryGlobal(diff,io.BytesIO("".encode('ascii')))
+	init.storage.resetBuffer()
+	return True
+
+def revertFile(init, key, version):
+	# finds the commit version
+	diff = init.prefix_diffs + key + '/' + str(version).zfill(10)
+	init.storage.copyFileGlobal(diff,key)
+	print('reverted file ' + key + ' to version ' + str(version))
+	return True
+
+def revertCommit(init, target_version):
+	if int(target_version) == 0:
+		return False
+
+	# finds the commit version
+	metapath = init.prefix_meta+'history.json'
+	history = json.load(init.storage.loadFileGlobal(metapath))
+
+	print('reverting dataset to version ' + str(target_version))
+
+	for i in range(len(history),int(target_version),-1):
+		for commit_ in history[str(i)]['commits']:
+			if init.storage.type == 'local':
+				cmit = json.load(init.storage.loadFileGlobal(commit_))
+			else:
+				cmit = json.load(init.storage.loadFileGlobal(commit_))
+			init.storage.resetBuffer()
+			if cmit['type'] == 'add':
+				removeGlobal(init, [cmit['key']])
+			elif cmit['type'] == 'remove':
+				revertFile(init,cmit['key'],cmit['version']-1)
+			else:
+				revertFile(init,cmit['key'],cmit['version'])
+			init.storage.resetBuffer()
+	return True
+
+def get_key_history(init, key):
+	# finds the commit version
+	metapath = init.prefix_history+key+'/history.json'
+	history = json.load(init.storage.loadFileGlobal(metapath))
+	return history
+
+def pull(init, files=[],version='current'):
+
+	if version == 'current':
+		# saves each file
+		for key in files:
+			newFile = open(os.path.basename(key), "wb")
+			binary = init.storage.loadFileGlobal(key)
+			newFile.write(binary.read())
+	else:
+		gtfo = False
+		# finds the commit of interest
+		metapath = init.prefix_meta+'history.json'
+		history = json.load(init.storage.loadFileGlobal(metapath))
+		for key in files:
+			if key[-1] == '/':
+				print('Do not pull directories')
+				return False
+			for i in range(len(history),int(version)-1,-1):
+				for commit in history[str(i)]['commits']:
+					# reads each file version
+					if init.storage.type == 'local':
+						cmit = json.load(init.storage.loadFileGlobal(commit))
+					else:
+						cmit = json.load(init.storage.loadFileGlobal(commit))
+					if str(cmit['version']) == version and cmit['key'] == key:
+						if cmit['type'] != 'remove':
+							newFile = open(os.path.basename(key), "wb")
+							key = init.prefix_diffs + key + '/' + str(cmit['version']).zfill(10)
+							newFile.write(init.storage.loadFileGlobal(key).read())
+							init.storage.resetBuffer()
+						gtfo = True
+					if gtfo:
+						break
+			if gtfo:
+				gtfo = False
+				break
+
+	init.storage.resetBuffer()
+
+	return True
+
+def printHistory(init):
+	# loads file global
+	metapath = init.prefix_meta+'history.json'
+	history = json.load(init.storage.loadFileGlobal(metapath))
+	init.storage.resetBuffer()
+
+	print('\n--------------------------')
+	print('- History of the dataset -')
+	print('--------------------------\n')
+
+	# prints history
+	for i in range(len(history),0,-1):
+		print('Version: '+str(i)+' Date: '+ history[str(i)]['date'])
+		idx = 0
+		for commit in history[str(i)]['commits']:
+			# reads each file version
+			if init.storage.type == 'local':
+				cmit = json.load(init.storage.loadFileGlobal(commit))
+			else:
+				cmit = json.load(init.storage.loadFileGlobal(commit))
+			print('-- '+cmit['comment']+' by '+cmit['source'])
+			idx += 1
+			if idx > 4:
+				print('-- ('+str(len(history[str(i)]['commits'])-idx)+' more changes not showed)')
+				break
+
+	print('')
+
+	init.storage.resetBuffer()
+
+def printDiff(init, v2, v1, file=''):
+	# loads file global
+	metapath = init.prefix_meta+'history.json'
+	history = json.load(init.storage.loadFileGlobal(metapath))
+	init.storage.resetBuffer()
+
+	v2 = int(v2)
+	v1 = int(v1) 
+
+	if file == '':
+		# prints history
+		print('Comparing commit '+str(v2)+' and commit '+str(v1))
+		print('Commit: '+str(v2)+' Date: '+ history[str(v2)]['date'])
+		print('Commit: '+str(v1)+' Date: '+ history[str(max(v1,1))]['date'])
+
+		for i in range(v2,v1,-1):
+			for commit in history[str(i)]['commits']:
+				# reads each file version
+				if init.storage.type == 'local':
+					cmit = json.load(init.storage.loadFileGlobal(commit))
+				else:
+					cmit = json.load(init.storage.loadFileGlobal(commit))
+				print('-- '+cmit['comment']+' by '+cmit['source'])
+	else:
+		print('Comparing file '+file+' between commit '+str(v2)+' and commit '+str(v1))
+		idx = 0;
+		# prints history
+		for i in range(v2,v1,-1):
+			for commit in history[str(i)]['commits']:
+				# reads each file version
+				if init.storage.type == 'local':
+					cmit = json.load(init.storage.loadFileGlobal(commit))
+				else:
+					cmit = json.load(init.storage.loadFileGlobal(commit))
+				# print(cmit['comment']+' by '+cmit['source'])
+				if cmit['key'] == init.storage.dataset+file:
+					print('Version: '+str(cmit['version'])+' Date: '+ history[str(i)]['date'])
+					print('-- '+cmit['comment']+' by '+cmit['source'])
+				if str(cmit['version']) == '1':
+					init.storage.resetBuffer()
+					return True
+					break
+		init.storage.resetBuffer()
+
+def printStatus(init):
+	# loads file global
+	metapath = init.prefix_meta+'current.json'
+	current = json.load(init.storage.loadFileGlobal(metapath))
+	init.storage.resetBuffer()
+	if len(current['keys']) > 0:
+		print('\n-------------------------')
+		print('- Status of the dataset -')
+		print('-------------------------\n')
+		print('List of files in dataset:')
+		for i in range(len(current['keys'])):
+			print('\t-- '+current['keys'][i] + '\tlast modified: '+str(current['lm'][i]))
+
+		init.storage.resetBuffer()
+		print()
+	else:
+		print('everything is ok!')
+		print('please add or commit a file')
---
+++ packages/stackcli/stackcli-0.1.14-py3-none-any.whl/src/storage/classes/s3.py
@@ -0,0 +1,362 @@
+# from src.storage import Storage
+import os
+import boto3
+import boto3.s3.transfer as s3transfer
+import botocore
+import configparser
+import maskpass
+import io
+from pathlib import Path
+import sys
+sys.path.append( '../../../' )
+from src.comm.docker_ver import *
+path_home = os.getenv('LCP_DKR')+'/' if docker_ver() else str(Path.home())
+
+class S3Bucket(object):
+	"""docstring for Storage"""
+	def __init__(self, BUCKET_NAME):
+		# self = Cloud().__init__()
+		self.type = "s3"
+		self.dataset = ""
+		self.BUCKET_NAME = BUCKET_NAME
+		self.credentials = {
+			"aws_access_key_id": "",
+			"aws_secret_access_key": "",
+			"region": "us-east-2",
+		}
+		self.resource = None
+		self.s3t = None
+		self.bucket = None
+
+	def connectBucket(self, verbose=False):
+		# checks if the credentials are in the computer
+		home_dir = path_home
+		if not os.path.isfile(home_dir+'/.aws/credentials'):
+
+			print("AWS is not setup in this machine. Please follow the following setups:")
+			print('\n')
+			print("1) Go to https://console.aws.amazon.com/iamv2/")
+			print("2) Choose 'Users' and click on 'Add user'.")
+			print("3) Give the user a name (for example, StackBot).")
+			print("4) Check 'Enable programmatic access'.")
+			print("5) go to 'Set permissions', click 'Attach Existing Policies'.")
+			print("6) Check 'AmazonS3FullAccess'")
+			print('\n')
+			print('Please copy your keys...')
+			print('\n')
+
+			self.credentials['aws_access_key_id'] = maskpass.askpass(prompt="Access key ID: ", mask="#")
+			self.credentials['aws_secret_access_key'] = maskpass.askpass(prompt="Secret access key: ", mask="#")
+			print('\n')
+
+			config = configparser.ConfigParser()
+			config['default'] = {}
+			config['default']['aws_access_key_id'] = self.credentials['aws_access_key_id']
+			config['default']['aws_secret_access_key'] = self.credentials['aws_secret_access_key']
+			os.mkdir(home_dir+'/.aws/')
+			with open(home_dir+'/.aws/credentials', 'w') as configfile:
+				config.write(configfile)
+		else:
+			config = configparser.ConfigParser()
+			config.read(home_dir+'/.aws/credentials')
+
+			self.credentials['aws_access_key_id'] = config['default']['aws_access_key_id']
+			self.credentials['aws_secret_access_key'] = config['default']['aws_secret_access_key']
+
+		# checks if the config is there
+		if not os.path.isfile(home_dir+'/.aws/config'):
+			print('Please select your region (https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region)')
+			print('[Press enter for us-east-2]')
+
+			self.credentials['region'] = input("region: ")
+
+			if self.credentials['region'] == "":
+				self.credentials['region'] = 'us-east-2'
+
+			config = configparser.ConfigParser()
+			config['default'] = {}
+			config['default']['region'] = self.credentials['region']
+
+			with open(home_dir+'/.aws/config', 'w') as configfile:
+				config.write(configfile)
+		else:
+			config = configparser.ConfigParser()
+			config.read(home_dir+'/.aws/config')
+			self.credentials['region'] = config['default']['region']
+
+		if verbose:
+			print('Connecting to your bucket...')
+		self.resource = boto3.resource('s3',aws_access_key_id=self.credentials['aws_access_key_id'],aws_secret_access_key=self.credentials['aws_secret_access_key'])
+		self.resetBuffer()
+
+		# checks if the target bucket exist
+		found = False
+		buckets = self.resource.buckets.all()
+
+		try:
+			for bucket in buckets:
+				if bucket.name == self.BUCKET_NAME:
+					found = True
+					break
+		except:
+			print('invalid credentials')
+			print('removing config files')
+			os.remove(home_dir+'/.aws/credentials')
+			os.remove(home_dir+'/.aws/config')
+			os.rmdir(home_dir+'/.aws/')
+
+			print('try again...')
+
+			return self.connectBucket()
+
+		# creates a new bucket or picks another bucket
+
+		if found == False:
+			if verbose:
+				print('could not find your desired bucket')
+				print('connection failed')
+				raise Exception('Could not find your desired bucket')
+		else:
+			self.bucket = self.resource.Bucket(name=self.BUCKET_NAME)
+
+		return found
+
+	def connect_bucket_api(self,keys_dict):
+		# checks if the credentials are in the computer
+		self.credentials['aws_access_key_id'] = keys_dict['key1']
+		self.credentials['aws_secret_access_key'] = keys_dict['key2']
+		self.credentials['region'] = keys_dict['key3']
+
+		config = configparser.ConfigParser()
+		config['default'] = {}
+		config['default']['aws_access_key_id'] = self.credentials['aws_access_key_id']
+		config['default']['aws_secret_access_key'] = self.credentials['aws_secret_access_key']
+		config['default']['region'] = self.credentials['region']
+
+		print('saving credentials')
+		with open(path_home+'/.aws/credentials', 'w') as configfile:
+			config.write(configfile)
+
+		config = configparser.ConfigParser()
+		config['default'] = {}
+		config['default']['region'] = self.credentials['region']
+
+		print('saving config')
+		with open(path_home+'/.aws/config', 'w') as configfile:
+			config.write(configfile)
+
+		print('Connecting to your bucket...')
+		self.resource = boto3.resource('s3',aws_access_key_id=self.credentials['aws_access_key_id'],aws_secret_access_key=self.credentials['aws_secret_access_key'])
+		self.resetBuffer()
+
+		# checks if the target bucket exist
+		buckets = self.resource.buckets.all()
+
+		for bucket in buckets:
+			if bucket.name == self.BUCKET_NAME:
+				found = True
+				self.bucket = self.resource.Bucket(name=self.BUCKET_NAME)
+				return True
+
+		return False
+
+	def createDataset(self,location):
+		# assigns location
+		if location[-1] != '/':
+			location = location + '/'
+		self.dataset = location
+		self.raw_location = location
+		return True
+
+	def addFile(self,filepath,target_name='',subpath=''):
+		# adding new file
+		if target_name == '':
+			target_name = os.path.basename(filepath)
+		if filepath[-1] == '/':
+			for root,dirs,files in os.walk(filepath):
+				for file in files:
+					self.s3t.upload(os.path.join(root,file),self.BUCKET_NAME,self.dataset+subpath+file)
+			self.resetBuffer()
+		else:
+			self.s3t.upload(filepath,self.BUCKET_NAME,self.dataset+subpath+os.path.basename(filepath))
+		return True
+
+	def addFileFromBinary(self,filename,f):
+		return self.addFileFromBinaryGlobal(self.dataset+filename,f)
+
+	def addFileFromBinaryGlobal(self,filename,f):
+		self.s3t.upload(f,self.BUCKET_NAME,filename)
+		return True
+
+	def removeFile(self,filename):
+		# deletes files in path
+		return self.removeFileGlobal(self.dataset+filename)
+
+	def removeFileGlobal(self,filename):
+		# deletes files in path
+		if filename[-1] == '/':
+			for obj in self.bucket.objects.filter(Prefix=filename):
+				self.s3t.delete(self.BUCKET_NAME, obj.key)
+			self.resetBuffer()
+		elif filename[-1] == '*':
+			for obj in self.bucket.objects.filter(Prefix=filename[:-1]):
+				self.s3t.delete(self.BUCKET_NAME, obj.key)
+			self.resetBuffer()
+		else:
+			self.s3t.delete(self.BUCKET_NAME, filename)
+
+		return True
+
+	def loadFile(self,filename):
+		return self.loadFileGlobal(self.dataset+filename)
+
+	def loadFileBytes(self,filename, b_i, b_f):
+		return self.loadFileGlobalBytes(self.dataset+filename,b_i,b_f)
+
+	def loadFileGlobal(self,filename):
+		return self.resource.meta.client.get_object(Bucket=self.BUCKET_NAME,Key=filename)['Body']
+
+	def loadFileGlobalBytes(self,filename,b_i,b_f):
+		bytes_rage = 'bytes=' + str(bi) + '-' + str(b_f)
+		return self.resource.meta.client.get_object(Bucket=self.BUCKET_NAME,Key=filename,Range=bytes_rage)['Body']
+
+	def loadFileMetadata(self,filename,debug=False):
+		# gets the metadata
+		obj = self.resource.Object(self.BUCKET_NAME,self.dataset+filename).get()
+
+		metadata = {
+			'key' : self.dataset+filename,
+			'date_loaded' : obj['ResponseMetadata']['HTTPHeaders']['date'],
+			'date_added' : obj['ResponseMetadata']['HTTPHeaders']['date'],
+			'last_modified' : obj['ResponseMetadata']['HTTPHeaders']['last-modified'],
+			'HostId' : obj['ResponseMetadata']['HostId'],
+		}
+
+		return metadata
+
+	def listFilesinPath(self,path):
+		for obj in self.bucket.objects.filter(Prefix=path):
+			print(f'-- {obj.key}')
+		return True
+
+	def loadDataset(self):
+		# loads all the metadata associated with the dataset
+		files = []
+		params = {"Bucket" : self.BUCKET_NAME, "Prefix" : self.dataset}
+		objects = self.resource.meta.client.list_objects_v2(**params)
+
+		# labels each file
+		if 'Contents' in objects.keys():
+			for obj in objects['Contents']:
+				if obj["Key"][-1] != '/':
+					metadata = {
+						'key' : obj['Key'],
+						'last_modified' : obj["LastModified"].strftime("%m/%d/%Y, %H:%M:%S"),
+					}
+					files.append(metadata)
+			return files
+		else:
+			return []
+
+	def loadDatasetList(self):
+		# loads all the metadata associated with the dataset
+		return self.loadListInPath(self.dataset)
+
+	def loadListInPath(self,path):
+		# loads all the metadata associated within a path
+		keys = []
+		last_m = []
+
+		params = {"Bucket" : self.BUCKET_NAME, "Prefix" : path}
+		objects = self.resource.meta.client.list_objects_v2(**params)
+
+		# loads each file
+		if 'Contents' in objects.keys():
+			for obj in objects['Contents']:
+				if obj['Key'][-1] != '/':
+					keys.append(obj['Key'])
+					last_m.append(obj["LastModified"].strftime("%m/%d/%Y, %H:%M:%S"))
+
+			return keys, last_m
+		else:
+			return [],[]
+
+	def checkIfEmpty(self,path):
+		# checks the number of files in a directory
+		summary = self.bucket.objects.filter(Prefix=path)
+		count = 0
+		for obj in summary:
+			if obj.key[-1] != '/':
+				count += 1
+		return (count == 0)
+
+	def listFilesinDataset(self):
+		# lists all the files in the main path of the dataset
+		return self.listFilesinPath(self.dataset)
+
+	def copyFile(self,filepath,full_target_name):
+		# adding new file
+		return self.copyFileGlobal(self.dataset+filepath,self.dataset+full_target_name)
+
+	def copyFileGlobal(self,filepath,full_target_name):
+		# adding new file
+		copy_source = {
+			'Bucket' : self.BUCKET_NAME,
+			'Key' : filepath
+		}
+		self.s3t.copy(copy_source=copy_source,bucket=self.BUCKET_NAME, key=full_target_name)
+		return True
+
+	def resetBuffer(self):
+		if self.s3t != None:
+			self.s3t.shutdown()
+		botocore_config = botocore.config.Config(max_pool_connections=80)
+		self.resource.meta.client.config = botocore_config
+		transfer_config = s3transfer.TransferConfig(use_threads=True,max_concurrency=80,multipart_threshold=25*1024,multipart_chunksize=25*1024)
+		self.s3t = s3transfer.create_transfer_manager(self.resource.meta.client, transfer_config)
+
+def main():
+	# connects a bucket
+	cloud = S3Bucket('stacktest123')
+	cloud.connectBucket()
+
+	# connects to a dataset in the bucket
+	cloud.createDataset('dataset1/')
+
+	# list the files in the bucket
+	cloud.listFilesinPath('')
+	cloud.listFilesinDataset()
+
+	# addes on file
+	cloud.addFile('../../tests/image.png','image.png')
+	cloud.addFile('../../tests/image.png','base/image.png',subpath='base/')
+	cloud.resetBuffer()
+
+	# opens one file
+	body = cloud.loadFile('image.png')
+	print(body)
+
+	meta = cloud.loadFileMetadata('image.png')
+	print(meta)
+
+	# adds two file from binaries
+	cloud.addFileFromBinary('extra/image2.png',open("../../tests/image.png", "rb"))
+	cloud.addFileFromBinary('extra/image3.png',cloud.loadFile('base/image.png'))
+	cloud.resetBuffer()
+
+	# removes a file
+	cloud.removeFile('extra/image2.png')
+	cloud.resetBuffer()
+
+	# loads the dataset in memory
+	dataset = cloud.loadDataset()
+	print(cloud.loadDataset())
+
+	cloud.listFilesinDataset()
+
+	cloud.copyFile('extra/image3.png','extra/image4.png')
+	cloud.listFilesinPath('')
+	print('success')
+
+if __name__ == '__main__':
+	main()
