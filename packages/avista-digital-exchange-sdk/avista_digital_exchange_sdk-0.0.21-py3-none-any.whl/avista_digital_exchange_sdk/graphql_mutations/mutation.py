e76f582cc98ab59ae4154318f22212d791170e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:23 +0000

    Add package avista-digital-exchange-sdk/0.0.21/avista_digital_exchange_sdk-0.0.21-py3-none-any.whl

---
+++ packages/avista-digital-exchange-sdk/avista_digital_exchange_sdk-0.0.21-py3-none-any.whl/avista_digital_exchange_sdk/graphql_mutations/mutation.py
@@ -0,0 +1,31 @@
+from ..exceptions import *
+from ..common import *
+
+
+class Mutation:
+    def __init__(self, client, mutationName, resultType):
+        self._client = client
+        self._result = None
+        self.mutationName = mutationName
+        self.resultType = resultType
+        return
+
+    def _getMutationString(self):
+        return f"mutation {self.mutationName} {{ {self.mutationName}() {{ {self.resultType.getQueryString(None, 4)} }} }}"
+
+    def _processResult(self):
+        if debug:
+            print(f'processing mutation result: {self.mutationName}')
+            print(self._result)
+        if 'errors' in self._result and len(self._result['errors']) > 0:
+            print('Mutation encountered error.')
+            print(self._result)
+            if 'errorType' in self._result['errors'][0] and ['errorType'] == "UnauthorizedException":
+                raise UnauthorizedException
+            elif 'message' in self._result['errors'][0]['message'] and self._result['errors'][0]['message'] == "Unauthorized":
+                raise Unauthorized
+            else:
+                raise MutationFailed(f"Mutation {self.mutationName} failed.")
+
+        if self._result['data'][self.mutationName] is None:
+            raise MissingDataInResultException
