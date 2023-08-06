a11d43a5102d2fff03788f032984e60e9ddc65a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:13 +0000

    Add package pochu-nixingde-mozhou/2022.9.14.0/pochu_nixingde_mozhou-2022.9.14.0-py3-none-any.whl

---
+++ packages/pochu-nixingde-mozhou/pochu_nixingde_mozhou-2022.9.14.0-py3-none-any.whl/PochuNixingdeMozhou/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pochu-nixingde-mozhou
+https://github.com/apachecn/pochu-nixingde-mozhou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.14.0"
