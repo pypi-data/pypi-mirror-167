f7c68b5fae3c8e617c1f77c242ea45452906564e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:12 +0000

    Add package colubrid-helpers/1.0.25/colubrid-helpers-1.0.25.tar.gz

---
+++ packages/colubrid-helpers/colubrid-helpers-1.0.25.tar.gz/colubrid-helpers-1.0.25/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+# specify requirements of your package here
+REQUIREMENTS = []
+
+# some more details
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Internet',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.10',
+    ]
+
+# calling the setup function 
+setup(name='colubrid-helpers',
+      version='1.0.25',
+      description='Colubrid libraries',
+      url='https://github.com/rhonalejandro/Colubrid-Framework',
+      author='Rhonald Alejandro Brito Querales',
+      author_email='rhonalejandro@gmail.com',
+      license='MIT',
+      packages=["src", 
+                "src.routestructure"],
+      classifiers=CLASSIFIERS,
+      install_requires=REQUIREMENTS,
+      keywords='colubrid-helpers',
+      entry_points = {
+        "console_scripts": [
+            "colubrid = src.__main__:main"
+        ]
+    }
+      )
