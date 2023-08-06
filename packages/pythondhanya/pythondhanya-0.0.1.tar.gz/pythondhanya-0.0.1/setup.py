46811b64f8df1762a772243e0c284fdf3be9e137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:32 +0000

    Add package pythondhanya/0.0.1/pythondhanya-0.0.1.tar.gz

---
+++ packages/pythondhanya/pythondhanya-0.0.1.tar.gz/pythondhanya-0.0.1/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 11',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='pythondhanya',
+    version='0.0.1',
+    description='this is a trial package used for calculation.',
+    url='',
+    author='Dhanya Shree',
+    author_email='dhanya02.ps@gmail.com',
+    licence='MIT',
+    packages=['pythondhanya'],
+    install_requires=[]
+)
