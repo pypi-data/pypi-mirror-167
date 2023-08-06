53f5d744b9d9ab79fda38cf1e4de0fed6d24b24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:08 +0000

    Add package meili-xingzuo-shizizuo/2022.9.14.0/meili-xingzuo-shizizuo-2022.9.14.0.tar.gz

---
+++ packages/meili-xingzuo-shizizuo/meili-xingzuo-shizizuo-2022.9.14.0.tar.gz/meili-xingzuo-shizizuo-2022.9.14.0/MeiliXingzuoShizizuo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""meili-xingzuo-shizizuo
+https://github.com/apachecn/meili-xingzuo-shizizuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.14.0"
