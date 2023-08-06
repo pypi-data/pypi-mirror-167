9234e3f3bbf14fabf348a99169cacc3e1d0b1710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:49 +0000

    Add package cv2-helper/0.0.1/cv2-helper-0.0.1.tar.gz

---
+++ packages/cv2-helper/cv2-helper-0.0.1.tar.gz/cv2-helper-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import *
+
+LONG_DESC = """
+This is cv2 helper
+"""
+
+setup(name='cv2-helper',
+	  version='0.0.1',
+	  description='Cv2 helper',
+	  long_description=LONG_DESC,
+	  author='Sang Ki Kwon',
+	  url='https://github.com/automatethem/cv2-helper',
+	  install_requires=[],
+	  author_email='automatethem@gmail.com',
+	  license='MIT',
+	  packages=find_packages(),
+	  zip_safe=False)
