e6f029c202aa8faab3428174f6955da516b9807e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:57 +0000

    Add package mono-client/0.1/mono_client-0.1.tar.gz

---
+++ packages/mono-client/mono_client-0.1.tar.gz/mono_client-0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    version='0.1',
+    name='mono_client',
+    packages=['mono_client'],
+    author='Abdulrasheed Ibrahim',
+    description='Access high-quality financial data and perform direct bank payments with mono.co'
+)
