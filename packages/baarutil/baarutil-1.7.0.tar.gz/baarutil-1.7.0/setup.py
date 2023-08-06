9f94c92042e7bfe3c73ae129a51755bb50cfbf1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:23 +0000

    Add package baarutil/1.7.0/baarutil-1.7.0.tar.gz

---
+++ packages/baarutil/baarutil-1.7.0.tar.gz/baarutil-1.7.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="baarutil",
+    version="1.7.0",
+    author="Zhaoyu Xu, Souvik Roy",
+    author_email="zhaoyu.xu@alliedmedia.com, souvik.roy@alliedmedia.com",
+    description="Utility functions for BAAR developers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Allied-Media/baarutil",
+    project_urls={
+        "Bug Tracker": "https://github.com/Allied-Media/baarutil/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    instrall_requires=[
+        "numpy >=1.18.4",
+        "pandas >=1.0.3",
+        "pycryptodome>=3.9",
+        "pybase64==1.2.0",
+        "asn1crypto==0.24.0"
+        "robotframework-crypto==0.3.0"
+    ],
+    python_requires=">=3.6.8",
+)
