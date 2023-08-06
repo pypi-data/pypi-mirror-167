795e7cdad5f72fa1c4e0be86c4efb518310cae76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:19 +0000

    Add package ToNNi/0.1/ToNNi-0.1.tar.gz

---
+++ packages/ToNNi/ToNNi-0.1.tar.gz/ToNNi-0.1/README.rst
@@ -0,0 +1,3 @@
+ToNNi neural network
+Adjust network details in ToNNi.py
+Create model object and fit data in main.py
---
+++ packages/ToNNi/ToNNi-0.1.tar.gz/ToNNi-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ToNNi',
+    version='0.1',
+    license='MIT',
+    author="Antonio Kezele",
+    author_email='akezele35@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://gitlab.com/big-projects1/tonni/',
+    keywords='neural network',
+    install_requires=[
+          'numpy',
+      ],
+
+)
