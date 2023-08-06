05ef45f1a9441accda4e53debea8866a63a3a776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:44 +0000

    Add package avista-digital-exchange-sdk/0.0.20/avista_digital_exchange_sdk-0.0.20.tar.gz

---
+++ packages/avista-digital-exchange-sdk/avista_digital_exchange_sdk-0.0.20.tar.gz/avista_digital_exchange_sdk-0.0.20/src/avista_digital_exchange_sdk/client.py
@@ -0,0 +1,48 @@
+import requests
+from .common import *
+
+
+class Client:
+
+    def __init__(self, token):
+        APPSYNC_API_ENDPOINT_URL_dev = 'https://annsvlcb4vew7msipwjyzzvyhi.appsync-api.us-west-2.amazonaws.com/graphql'
+        APPSYNC_API_ENDPOINT_URL_prod = 'https://rrfs7pb7ancybo7bom7uxcsaxq.appsync-api.us-west-2.amazonaws.com/graphql'
+        self.token = token
+        if stage == "PRODUCTION":
+            self.APPSYNC_API_ENDPOINT_URL = APPSYNC_API_ENDPOINT_URL_prod
+        else:
+            self.APPSYNC_API_ENDPOINT_URL = APPSYNC_API_ENDPOINT_URL_dev
+        return
+
+    def performQuery(self, queryString):
+        response = None
+        try:
+            with requests.Session() as session:
+                response = session.request(
+                    url=self.APPSYNC_API_ENDPOINT_URL,
+                    method='POST',
+                    headers={'Accept': 'application/json',
+                             'Content-Type': 'application/json',
+                             'Authorization': self.token},
+                    json={'query': queryString}
+                )
+        except Exception as e:
+            raise e
+        return response.json()
+
+    def performMutation(self, mutationString):
+        response = None
+        try:
+            with requests.Session() as session:
+                # Now we can simply post the request...
+                response = session.request(
+                    url=self.APPSYNC_API_ENDPOINT_URL,
+                    method='POST',
+                    headers={'Accept': 'application/json',
+                             'Content-Type': 'application/json',
+                             'Authorization': self.token},
+                    json={'query': mutationString}
+                )
+        except Exception as error:
+            raise error
+        return response.json()
