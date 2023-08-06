ed5ce22c99c2b50a8ee85fdf07ad0e0ebc7aa281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:39 +0000

    Add package MYPRACT/0.0.1/MYPRACT-0.0.1.tar.gz

---
+++ packages/MYPRACT/MYPRACT-0.0.1.tar.gz/MYPRACT-0.0.1/SETUP.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 10',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='MYPRACT',
+    version='0.0.1',
+    description='this is a trial package used for calculation.',
+    url='',
+    author='ARYAN SAMSUKHA',
+    author_email='ARYANSAMSUKHA@gmail.com',
+    licence='MIT',
+    packages=['MYPRACT'],
+    install_requires=[]
+)
