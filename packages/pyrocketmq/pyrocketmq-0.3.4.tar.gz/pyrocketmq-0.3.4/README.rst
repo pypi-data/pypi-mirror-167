ffc4c88b4b7fa79957fb574cce93f469bbc55097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:41 +0000

    Add package pyrocketmq/0.3.4/pyrocketmq-0.3.4.tar.gz

---
+++ packages/pyrocketmq/pyrocketmq-0.3.4.tar.gz/pyrocketmq-0.3.4/README.rst
@@ -0,0 +1,114 @@
+PyRocketMQ
+==========
+
+1. Introduce
+------------
+
+1.1 Why another python RocketMQ API ?
+*************************************
+
+Currently the existing apis are too outdated. For example:
+
+- `The official python API <https://github.com/apache/rocketmq-client-python>`_, which you can install via ``pip install rocketmq-client-python``, is for RocketMQ 2.0 and below, and does not support PullConsumer.
+- `An unofficial python API written by messense <https://github.com/messense/rocketmq-python>`_, which you can install via ``pip install rocketmq``, wraps the same cpp dynamic libraries as the official API, does not support RocketMQ 4.0+ either.
+- `The official cpp API <https://github.com/apache/rocketmq-client-cpp>`_ now has v5.0-rc1 version, but does not provide pre-compiled binaries, which means you have to compile the project your self. If you're not familiar bazel tool and cpp class export, it may be a little difficult.
+
+1.2 What's the features of this API ?
+*************************************
+
+- It's developed and tested on RocketMQ 4.0+, so it supports many recent features. For example, custom queue selector when producing messages, custom message selector by tag/sql when consuming messages, etc.
+- It exposes the java API as it is as possible, via `JPype <https://github.com/jpype-project/jpype>`_. You can directly reference the java doc in most cases.
+- The get method of any java class are wrapped as property of the coresponding python class. For example, calling ``msg.getTopic()`` in java is equivalent to calling ``msg.topic`` in python.
+
+2. Installation
+---------------
+
+- Download RocketMQ binary release, for example: https://archive.apache.org/dist/rocketmq/4.3.2/rocketmq-all-4.3.2-bin-release.zip.
+- Extract it to somewhere as you like, for example: /path/to/rocketmq-all-4.3.2-bin-release
+- Install pyrocketmq via pip: ``pip install pyrocketmq``
+- Make sure the jvm is started **BEFORE** you import and use pyrocketmq, for example:
+
+.. code-block::  python
+
+    import jpype
+    import jpype.imports
+    jpype.startJVM(classpath=['/path/to/rocketmq-all-4.3.2-bin-release/lib/*',])
+    from pyrocketmq import *
+    # do something
+    jpype.shutdownJVM()
+
+
+3. QuickStart
+-------------
+
+3.1 Producer
+************
+
+.. code-block::  python
+
+    import json
+    from pyrocketmq.common.message import Message
+    from pyrocketmq.client.producer import Producer, SendStatus
+    pr = Producer('test_producer')
+    pr.setNamesrvAddr('localhost:9876')
+    pr.start()
+    body = json.dumps({'name':'Alice', 'age':1}).encode('utf-8')
+    msg = Message(topic='test_topic', body=body, tags='girl')
+    # send, tcp-like, return sendStatus
+    sr = pr.send(msg)
+    assert(sr.sendStatus == SendStatus.SEND_OK)
+    pr.shutdown()
+
+3.2 PullConsumer
+****************
+
+.. code-block::  python
+
+    import json
+    from pyrocketmq.client.consumer.consumer import PullConsumer, PullStatus
+    cs = PullConsumer('test_pull_consumer')
+    cs.setNamesrvAddr('localhost:9876')
+    topic = 'test_topic'
+    cs.start()
+    # pull messages from each queue
+    mqs = cs.fetchSubscribeMessageQueues(topic)
+    for mq in mqs:
+        ofs = cs.minOffset(mq)
+        pr = cs.pull(mq, subExpression='girl', offset=ofs, maxNums=1)
+        if pr.pullStatus == PullStatus.FOUND:
+            # iterate msg in pull result
+            for msg in pr:
+                print(json.loads(msg.body))
+    cs.shutdown()
+
+3.3 PushConsumer
+****************
+
+.. code-block::  python
+
+    import json
+    import time
+    from typing import List
+    from pyrocketmq.client.consumer.listener import ConsumeConcurrentlyContext, ConsumeConcurrentlyStatus, MessageListenerConcurrently
+    from pyrocketmq.client.consumer.consumer import MessageSelector, PushConsumer
+    from pyrocketmq.common.common import ConsumeFromWhere
+    from pyrocketmq.common.message import MessageExt
+    
+    # subclass MessageListenerConcurrently to write your own consume action
+    class MyMessageListenerConcurrently(MessageListenerConcurrently):
+        def _consumeMessage(self, msgs:List[MessageExt], context:ConsumeConcurrentlyContext) -> ConsumeConcurrentlyStatus:
+            print('Concurrently', context.ackIndex)
+            for msg in msgs:
+                print(json.loads(msg.body))
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS
+
+    cs = PushConsumer('test_push_consumer')
+    cs.setNamesrvAddr('localhost:9876')
+    selector = MessageSelector.byTag('girl')
+    ml = MyMessageListenerConcurrently()
+    cs.registerMessageListener(ml)
+    cs.subscribe('test_topic', selector)
+    cs.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET)
+    cs.start()
+    time.sleep(5)
+    cs.shutdown()
---
+++ packages/pyrocketmq/pyrocketmq-0.3.4.tar.gz/pyrocketmq-0.3.4/src/pyrocketmq/client/consumer/rebalance.py
@@ -0,0 +1,131 @@
+from abc import abstractmethod
+from typing import List, Optional
+
+from jpype import JImplements, JOverride
+from java.util import ArrayList, HashSet
+from org.apache.rocketmq.client.consumer import AllocateMessageQueueStrategy as JAllocateMessageQueueStrategy
+from org.apache.rocketmq.client.consumer.rebalance import AllocateMachineRoomNearby as JAllocateMachineRoomNearby
+from org.apache.rocketmq.client.consumer.rebalance import AllocateMessageQueueAveragely as JAllocateMessageQueueAveragely
+from org.apache.rocketmq.client.consumer.rebalance import AllocateMessageQueueAveragelyByCircle as JAllocateMessageQueueAveragelyByCircle
+from org.apache.rocketmq.client.consumer.rebalance import AllocateMessageQueueByConfig as JAllocateMessageQueueByConfig
+from org.apache.rocketmq.client.consumer.rebalance import AllocateMessageQueueByMachineRoom as JAllocateMessageQueueByMachineRoom
+from org.apache.rocketmq.client.consumer.rebalance import AllocateMessageQueueConsistentHash as JAllocateMessageQueueConsistentHash
+from org.apache.rocketmq.client.consumer.rebalance.AllocateMachineRoomNearby import MachineRoomResolver as JMachineRoomResolver
+from org.apache.rocketmq.common.consistenthash import HashFunction as JHashFunction
+
+from ...common.message import MessageQueue
+
+
+@JImplements(JMachineRoomResolver)
+class MachineRoomResolver:
+    @JOverride
+    def brokerDeployIn(self, messageQueue):
+        return self._brokerDeployIn(MessageQueue(messageQueue))
+
+    @JOverride
+    def consumerDeployIn(self, clientID):
+        return self._consumerDeployIn(str(clientID))
+
+    @abstractmethod
+    def _brokerDeployIn(self, messageQueue:MessageQueue) -> str:
+        pass
+
+    @abstractmethod
+    def _consumerDeployIn(self, clientID:str) -> str:
+        pass
+
+class BaseAllocateMessageQueueStrategy:
+    def __init__(self,
+        allocate_message_queue_strategy:Optional[JAllocateMessageQueueStrategy] = None,
+        AllocateMessageQueueStrategyClass:Optional[type] = None,
+    ):
+        if AllocateMessageQueueStrategyClass is None and allocate_message_queue_strategy is None:
+            raise Exception('At least one of AllocateMessageQueueStrategyClass or allocate_message_queue_strategy must be specified.')
+        elif allocate_message_queue_strategy is not None:
+            self.this = allocate_message_queue_strategy
+        else:
+            self.this = AllocateMessageQueueStrategyClass()
+    
+    def allocate(self, consumerGroup:str, currentCID:str, mqAll:List[MessageQueue], cidAll:List[str]) -> List[MessageQueue]:
+        return [
+            MessageQueue(mq) for mq in
+            self.this.allocate(consumerGroup, currentCID, ArrayList([mq.this for mq in mqAll]), ArrayList(cidAll)) or []
+        ]
+
+    @property
+    def name(self) -> str:
+        return str(self.this.getName())
+
+class AllocateMachineRoomNearby(BaseAllocateMessageQueueStrategy):
+    def __init__(self,
+        allocate_machine_room_nearby:Optional[JAllocateMachineRoomNearby] = None,
+        allocate_message_queue_strategy:Optional[BaseAllocateMessageQueueStrategy] = None,
+        machine_room_resolver:Optional[JMachineRoomResolver] = None
+    ):
+        if allocate_machine_room_nearby is None == (allocate_message_queue_strategy is None or machine_room_resolver is None):
+            raise Exception('Exactly one of allocate_machine_room_nearby and allocate_message_queue_strategy+machine_room_resolver must be specified')
+        elif allocate_machine_room_nearby is not None:
+            self.this = allocate_machine_room_nearby
+        else:
+            self.this = JAllocateMachineRoomNearby(allocate_message_queue_strategy.this, machine_room_resolver)
+
+class AllocateMessageQueueAveragely(BaseAllocateMessageQueueStrategy):
+    def __init__(self, allocate_message_queue_strategy:Optional[JAllocateMessageQueueStrategy]=None):
+        BaseAllocateMessageQueueStrategy.__init__(self, allocate_message_queue_strategy, JAllocateMessageQueueAveragely)
+
+class AllocateMessageQueueAveragelyByCircle(BaseAllocateMessageQueueStrategy):
+    def __init__(self, allocate_message_queue_strategy:Optional[JAllocateMessageQueueStrategy]=None):
+        BaseAllocateMessageQueueStrategy.__init__(self, allocate_message_queue_strategy, JAllocateMessageQueueAveragelyByCircle)
+
+class AllocateMessageQueueByConfig(BaseAllocateMessageQueueStrategy):
+    def __init__(self, allocate_message_queue_strategy:Optional[JAllocateMessageQueueStrategy]=None):
+        BaseAllocateMessageQueueStrategy.__init__(self, allocate_message_queue_strategy, JAllocateMessageQueueByConfig)
+    
+    @property
+    def messageQueueList(self) -> List[MessageQueue]:
+        return [MessageQueue(mq) for mq in self.this.getMessageQueueList() or []]
+    
+    def setMessageQueueList(self, messageQueueList:List[MessageQueue]):
+        self.this.setMessageQueueList(ArrayList([mq.this for mq in messageQueueList]))
+
+class AllocateMessageQueueByMachineRoom(BaseAllocateMessageQueueStrategy):
+    def __init__(self, allocate_message_queue_strategy:Optional[JAllocateMessageQueueStrategy]=None):
+        BaseAllocateMessageQueueStrategy.__init__(self, allocate_message_queue_strategy, JAllocateMessageQueueByMachineRoom)
+    
+    @property
+    def consumeridcs(self) -> List[str]:
+        return [str(idc) for idc in self.this.getConsumeridcs() or []]
+    
+    def setConsumeridcs(self, consumeridcs:List[str]):
+        self.this.setConsumeridcs(HashSet(consumeridcs))
+
+class AllocateMessageQueueConsistentHash(BaseAllocateMessageQueueStrategy):
+    def __init__(self,
+        allocate_message_queue_strategy:Optional[JAllocateMessageQueueStrategy] = None,
+        virtualNodecnt:int = 10,
+        customHashFunction:Optional[JHashFunction] = None):
+        if allocate_message_queue_strategy is not None == (virtualNodecnt is not None or customHashFunction is not None):
+            raise Exception('Exactly one of allocate_message_queue_strategy and virtualNodecnt+customHashFunction must be specified')
+        elif allocate_message_queue_strategy is not None:
+            self.this = allocate_message_queue_strategy
+        else:
+            self.this = JAllocateMessageQueueConsistentHash(virtualNodecnt, customHashFunction)
+
+AllocateMessageQueueStrategyMap = {
+    JAllocateMachineRoomNearby: AllocateMachineRoomNearby,
+    JAllocateMessageQueueAveragely: AllocateMessageQueueAveragely,
+    JAllocateMessageQueueAveragelyByCircle: AllocateMessageQueueAveragelyByCircle,
+    JAllocateMessageQueueByConfig: AllocateMessageQueueByConfig,
+    JAllocateMessageQueueByMachineRoom: AllocateMessageQueueByMachineRoom,
+    JAllocateMessageQueueConsistentHash: AllocateMessageQueueConsistentHash,
+}
+
+@JImplements(JHashFunction)
+class HashFunction:
+    @JOverride
+    def hash(self, key):
+        return self._hash(key)
+
+    @abstractmethod
+    def _hash(self, key:str) -> int:
+        pass
