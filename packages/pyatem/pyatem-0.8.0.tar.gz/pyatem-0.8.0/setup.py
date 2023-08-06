dc0f191ae494b28529dc6efb7a57389ded2df896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:04 +0000

    Add package pyatem/0.8.0/pyatem-0.8.0.tar.gz

---
+++ packages/pyatem/pyatem-0.8.0.tar.gz/pyatem-0.8.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension
+
+setup(
+    name='pyatem',
+    version='0.8.0',
+    packages=['pyatem'],
+    ext_modules=[Extension('pyatem.mediaconvert', ['pyatem/mediaconvertmodule.c'])],
+    url='https://git.sr.ht/~martijnbraam/pyatem',
+    license='LGPL3',
+    author='Martijn Braam',
+    author_email='martijn@brixit.nl',
+    description='Library implementing the Blackmagic Design Atem switcher protocol',
+    long_description=open("README.md").read(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: POSIX :: Linux',
+    ],
+)
