eae71296da56426173b1bd893536a8f419a4a2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:55 +0000

    Add package dreem-nap/2.0.6/dreem_nap-2.0.6-py2.py3-none-any.whl

---
+++ packages/dreem-nap/dreem_nap-2.0.6-py2.py3-none-any.whl/dreem_nap/util.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import datetime
+from os.path import exists
+import os
+import matplotlib.pyplot as plt
+from typing import Tuple, List
+import dreem_nap as nap
+import yaml
+
+class OutputPlot(object):
+    def __init__(self,data, figsize=None, dpi=None) -> None:
+        self.fig = plt.figure(figsize=figsize, dpi=dpi)
+        self.fig.patch.set_facecolor('white')
+        self.ax = plt.axes()
+        self.data = data
+
+def make_path(path:str)->str:
+    """Create directories until path exists on your computer. Turns the keyword 'date' into today's date.
+
+    Args:
+        path: series of directories that you want to create.
+    
+    Returns:
+        Updated path with today's date instead of the keyword 'date'  
+    """
+
+    path = os.path.normpath(path)
+    path=path.split(os.sep)
+    try:
+        path[path.index('date')] = str(datetime.datetime.now())[:10]
+    except:
+        'No date in path'
+    full_path = ''
+    for repo in path:
+        full_path = full_path + f"{repo}/"
+        if not exists(full_path):
+            os.mkdir(full_path)
+    return full_path
+
+
+
+def gini(x:np.array)->float:
+    """Returns Gini index
+
+    Args:
+        x (np.array): the array you want the Gini index from
+
+    Returns:
+        float: Gini index of the input array
+    """
+    # (Warning: This is a concise implementation, but it is O(n**2)
+    # in time and memory, where n = len(x).  *Don't* pass in huge
+    # samples!)
+
+    # Mean absolute difference
+    mad = np.abs(np.subtract.outer(x, x)).mean()
+    # Relative mean absolute difference
+    rmad = mad/np.mean(x)
+    # Gini coefficient
+    g = 0.5 * rmad
+    return g
+
+def savefig(file:str, close=True)->None:
+    """Save a matplotlib figure and create the directory if it doesn't exists.
+
+    Args:
+        file: path+title.
+        facecolor: color of the background 
+    """
+
+    path = make_path('/'.join(file.split('/')[:-1]))
+    plt.savefig(path+file.split('/')[-1])
+    if close:
+        # Clear the current axes.
+        plt.cla() 
+        # Clear the current figure.
+        plt.clf() 
+        # Closes all the figure windows.
+        plt.close('all')   
+
+
+def define_figure(title:str, xlabel:str, ylabel:str, figsize:Tuple[float, float])->plt.figure:
+    """Define title, labels and size of your figure.
+
+    Args:
+        title: matplotlib title
+        xlabel: matplotlib xlabel
+        ylabel: matplotlib ylabel
+        figsize: matplotlib figsize
+    """
+
+    fig = plt.figure(figsize=figsize)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    return fig
+
+
+
