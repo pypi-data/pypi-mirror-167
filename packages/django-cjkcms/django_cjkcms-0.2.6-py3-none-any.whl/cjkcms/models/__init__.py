405188b93016685f67568712c1b2ac345512906f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:11 +0000

    Add package django-cjkcms/0.2.6/django_cjkcms-0.2.6-py3-none-any.whl

---
+++ packages/django-cjkcms/django_cjkcms-0.2.6-py3-none-any.whl/cjkcms/models/__init__.py
@@ -0,0 +1,6 @@
+from .admin_sidebar import *
+from .integration_models import *
+from .page_models import *
+from .snippet_models import *
+from .wagtailsettings_models import *
+from .cms_models import *
---
+++ packages/django-cjkcms/django_cjkcms-0.2.6-py3-none-any.whl/cjkcms/tests/__init__.py
@@ -0,0 +1,10 @@
+from cjkcms.tests.test_templatetags import TemplateTagTests
+from cjkcms.tests.test_settings import SettingsTests
+from cjkcms.tests.test_gravatar import (
+    GravatarTests,
+    GravatarWithDefaultGravatarUrlTests,
+)
+from cjkcms.tests.test_articlepages import ArticlePageTests
+from cjkcms.tests.test_webpage import WebPageTests
+from cjkcms.tests.test_urls import TestFavicon, TestSiteURLs
+from cjkcms.tests.test_search_blocks import TestSearchBlocks
