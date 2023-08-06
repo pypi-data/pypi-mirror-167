4a5896ccba008d4d70400b8a61fe0574efc330fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:30 +0000

    Add package sjtu-lib-booker/0.0.0/sjtu-lib-booker-0.0.0.tar.gz

---
+++ packages/sjtu-lib-booker/sjtu-lib-booker-0.0.0.tar.gz/sjtu-lib-booker-0.0.0/sjtu-lib-booker/__init__.py
@@ -0,0 +1,15 @@
+
+
+__all__=[
+
+]
+
+
+__version__='0.0.0'
+__license__='GPL-2.0 License'
+__author__='Tiancheng Jiao'
+__url__='https://github.com/jtc1246'
+__author_email__='jtc1246@outlook.com'
+__description__='Library seat checker for SJTU'
+
+
