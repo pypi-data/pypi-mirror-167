4402cde03f579206da14f110b4c21cbd7c7724ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:28 +0000

    Add package lisaorbits/2.0/lisaorbits-2.0-py3-none-any.whl

---
+++ packages/lisaorbits/lisaorbits-2.0-py3-none-any.whl/lisaorbits/meta.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-module-docstring
+
+__version__ = '2.0'
+__author__ = 'Jean-Baptiste Bayle'
+__email__ = 'j2b.bayle@gmail.com'
+__copyright__ = '2020, Jean-Baptiste Bayle'
