58300c76919783bb28ebdd6ca8bdc63100fc888e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:42 +0000

    Add package freqlearning/0.3/freqlearning-0.3.tar.gz

---
+++ packages/freqlearning/freqlearning-0.3.tar.gz/freqlearning-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='freqlearning',
+      version='0.03',
+      description='frequency domain learning, GAFL',
+      packages=['freqlearning'],
+      url='https://github.com/YaroslavBespalov/freqlearning',
+      author='YaroslavBespalov, ViktorShipitsin',
+      author_email='ja.bespalov@yandex.ru',
+      long_description=open('./README.md', 'rt').read(),
+      zip_safe=False)
