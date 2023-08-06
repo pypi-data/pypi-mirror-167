a6436e05c9be231a1d91090c52b942e65b03c175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:51 +0000

    Add package test-pypi-zgl/0.0.3/test-pypi-zgl-0.0.3.tar.gz

---
+++ packages/test-pypi-zgl/test-pypi-zgl-0.0.3.tar.gz/test-pypi-zgl-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+setup(
+    name='test-pypi-zgl',
+    version='0.0.3',
+    description='test-pypi',
+    classifiers=[],
+    keywords='test-pypi',
+    author='zgl',
+    author_email='',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=[]),
+    package_data={'': ['*.*']},
+    include_package_data=True,
+    install_requires=[],
+    long_description='test-pypi'
+)
