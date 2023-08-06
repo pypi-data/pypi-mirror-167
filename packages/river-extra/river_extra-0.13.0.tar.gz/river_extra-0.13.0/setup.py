deedf8ba3fe5b99e587e07a773e85aa149a272de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:26 +0000

    Add package river-extra/0.13.0/river_extra-0.13.0.tar.gz

---
+++ packages/river-extra/river_extra-0.13.0.tar.gz/river_extra-0.13.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['river_extra',
+ 'river_extra.compat',
+ 'river_extra.metrics',
+ 'river_extra.metrics.cluster']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['river==0.12.1']
+
+setup_kwargs = {
+    'name': 'river-extra',
+    'version': '0.13.0',
+    'description': 'Additional estimators for the River package',
+    'long_description': None,
+    'author': 'MaxHalford',
+    'author_email': 'maxhalford25@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<4.0',
+}
+
+
+setup(**setup_kwargs)
