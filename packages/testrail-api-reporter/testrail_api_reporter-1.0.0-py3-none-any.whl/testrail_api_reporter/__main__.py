ea12552fa581bc79a681af02be84a536731a40d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:41 +0000

    Add package testrail-api-reporter/1.0.0/testrail_api_reporter-1.0.0-py3-none-any.whl

---
+++ packages/testrail-api-reporter/testrail_api_reporter-1.0.0-py3-none-any.whl/testrail_api_reporter/__main__.py
@@ -0,0 +1,10 @@
+# Engines
+from testrail_api_reporter.engines.at_coverage_reporter import ATCoverageReporter
+from testrail_api_reporter.engines.results_reporter import TestRailResultsReporter
+from testrail_api_reporter.engines.plotly_reporter import PlotlyReporter
+# Publishers
+from testrail_api_reporter.publishers.confluence_sender import ConfluenceSender
+from testrail_api_reporter.publishers.email_sender import EmailSender
+from testrail_api_reporter.publishers.slack_sender import SlackSender
+# Utils
+from testrail_api_reporter.utils.reporter_utils import upload_image
