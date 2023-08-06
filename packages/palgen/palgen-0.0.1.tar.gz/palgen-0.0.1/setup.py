7b6c1ed9780214b461af7eab99190fb4064fa09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:43 +0000

    Add package palgen/0.0.1/palgen-0.0.1.tar.gz

---
+++ packages/palgen/palgen-0.0.1.tar.gz/palgen-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="palgen",
+    version="0.0.1",
+    author="Tsche",
+    author_email="contact@palliate.io",
+    description="Code generation utility",
+    long_description="Code generation utility",
+    long_description_content_type="text/markdown",
+    url="https://github.com/palliate/palgen",
+    project_urls={
+        "Bug Tracker": "https://github.com/palliate/palgen/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=[],
+)
