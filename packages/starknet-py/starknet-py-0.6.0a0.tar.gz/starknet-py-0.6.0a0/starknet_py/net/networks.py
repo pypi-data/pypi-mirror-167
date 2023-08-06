2f8af6c4d07302c15ab26810fd747a7a085100b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:13 +0000

    Add package starknet-py/0.6.0a0/starknet-py-0.6.0a0.tar.gz

---
+++ packages/starknet-py/starknet-py-0.6.0a0.tar.gz/starknet-py-0.6.0a0/starknet_py/net/networks.py
@@ -0,0 +1,27 @@
+from typing import TypedDict, Union
+
+MAINNET = "mainnet"
+TESTNET = "testnet"
+
+try:
+    # noinspection PyUnresolvedReferences
+    from typing import Literal  # pylint: disable=no-name-in-module
+
+    PredefinedNetwork = Literal["mainnet", "testnet"]
+except ImportError:
+    PredefinedNetwork = str
+
+
+class CustomGatewayUrls(TypedDict):
+    feeder_gateway_url: str
+    gateway_url: str
+
+
+Network = Union[PredefinedNetwork, str, CustomGatewayUrls]
+
+
+def net_address_from_net(net: Network) -> str:
+    return {
+        MAINNET: "https://alpha-mainnet.starknet.io",
+        TESTNET: "https://alpha4.starknet.io",
+    }.get(net, net)
