53363e5089bdb07516e1166f9bcdc9b363f79d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:23 +0000

    Add package GML-distributions/0.1/GML_distributions-0.1.tar.gz

---
+++ packages/GML-distributions/GML_distributions-0.1.tar.gz/GML_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GML_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['GML_distributions'],
+      author='Siqi Huang',
+      author_email='siqi.huang@boehringer-ingelheim.com',
+      zip_safe=False)
