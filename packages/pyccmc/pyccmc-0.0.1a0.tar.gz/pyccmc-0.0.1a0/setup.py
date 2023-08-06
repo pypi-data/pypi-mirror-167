a0627b573bf36130b012f9d01ebcf123c2c8ec06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:22 +0000

    Add package pyccmc/0.0.1a0/pyccmc-0.0.1a0.tar.gz

---
+++ packages/pyccmc/pyccmc-0.0.1a0.tar.gz/pyccmc-0.0.1a0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyccmc',
+    version='0.0.1a',
+    packages=['pyccmc'],
+    install_requires=[
+        'requests',
+        'tqdm',
+        'colorama',
+    ],
+    url='https://tigabeatz.net',
+    author='tigabeatz',
+    author_email='tigabeatz@cccwi.de',
+    description='Download from CC Mixter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha"
+    ],
+    entry_points={
+        'console_scripts': [
+            'ccmclient=pyccmc.cli:main'
+        ]
+    },
+)
+
