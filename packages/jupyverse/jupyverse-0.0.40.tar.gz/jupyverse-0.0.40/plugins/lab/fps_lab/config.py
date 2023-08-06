37b263eb73d0f742cb89ffb408a64882b77c5a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:59 +0000

    Add package jupyverse/0.0.40/jupyverse-0.0.40.tar.gz

---
+++ packages/jupyverse/jupyverse-0.0.40.tar.gz/jupyverse-0.0.40/plugins/lab/fps_lab/config.py
@@ -0,0 +1,14 @@
+from fps.config import PluginModel, get_config  # type: ignore
+from fps.hooks import register_config  # type: ignore
+
+
+class LabConfig(PluginModel):
+    base_url: str = "/"
+    collaborative: bool = False
+
+
+def get_lab_config():
+    return get_config(LabConfig)
+
+
+c = register_config(LabConfig)
