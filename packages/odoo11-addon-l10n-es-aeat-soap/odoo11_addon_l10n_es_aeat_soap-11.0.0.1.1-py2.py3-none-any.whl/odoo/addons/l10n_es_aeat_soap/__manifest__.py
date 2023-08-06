3d482b39a71a66d2368c2b79f70e8609c51be899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:13 +0000

    Add package odoo11-addon-l10n-es-aeat-soap/11.0.0.1.1/odoo11_addon_l10n_es_aeat_soap-11.0.0.1.1-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-l10n-es-aeat-soap/odoo11_addon_l10n_es_aeat_soap-11.0.0.1.1-py2.py3-none-any.whl/odoo/addons/l10n_es_aeat_soap/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Acysos S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'AEAT - SOAP Webservice',
+    'author': 'Acysos S.L.,'
+              'Odoo Community Association (OCA)',
+    "website": "https://github.com/OCA/l10n-spain",
+    'category': 'Accounting',
+    'version': '11.0.0.1.1',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_es_aeat_certificate'
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
