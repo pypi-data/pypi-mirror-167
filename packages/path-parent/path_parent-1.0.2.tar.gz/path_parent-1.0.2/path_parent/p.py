37c2ba4f057c7072cf9b66e103dce6b5f0823f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:38 +0000

    Add package path-parent/1.0.2/path_parent-1.0.2.tar.gz

---
+++ packages/path-parent/path_parent-1.0.2.tar.gz/path_parent-1.0.2/path_parent/p.py
@@ -0,0 +1,4 @@
+﻿
+from .tools import append
+
+append("..")
---
+++ packages/path-parent/path_parent-1.0.2.tar.gz/path_parent-1.0.2/setup.py
@@ -0,0 +1,16 @@
+﻿from setuptools import setup
+
+setup(
+    name="path_parent",
+    version="1.0.2",
+    keywords=("path", "parent"),
+    description="Append '..' to path setting.",
+    long_description="Append string '..' to the path setting when being imported.",
+    license="MIT Licence",
+    url="https://github.com/adf0001/path_parent",
+    author="fwg",
+    author_email="adf0001@163.com",
+    packages =["path_parent"],
+    include_package_data=True,
+    platforms="any",
+)
