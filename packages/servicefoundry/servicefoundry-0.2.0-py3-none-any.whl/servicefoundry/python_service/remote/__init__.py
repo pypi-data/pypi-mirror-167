0d7e5c6d43ab057a62b03a05c3272659427328bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:31 +0000

    Add package servicefoundry/0.2.0/servicefoundry-0.2.0-py3-none-any.whl

---
+++ packages/servicefoundry/servicefoundry-0.2.0-py3-none-any.whl/servicefoundry/python_service/remote/__init__.py
@@ -0,0 +1,6 @@
+from servicefoundry.python_service.remote.context import QUAL_NAME_TO_URL_MAP
+from servicefoundry.python_service.remote.remote import (
+    RemoteClass,
+    RemoteFunction,
+    remote,
+)
