a4b26e564a77611777c6f25154a11b0bbafa5bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:53 +0000

    Add package py-stresser/2.0.0a0/py-stresser-2.0.0a0.tar.gz

---
+++ packages/py-stresser/py-stresser-2.0.0a0.tar.gz/py-stresser-2.0.0a0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='py-stresser',
+    version='2.0.0-alpha',
+    packages= find_packages(),
+    
+)
+
+# pypi-AgENdGVzdC5weXBpLm9yZwIkZDA3NGNhZTItZjM2Yy00NDY4LWE3ZjctOWUzZTM5NmIxYjY5AAIqWzMsIjcwOGVkY2RiLTAxZjgtNDc5Ny04MzM1LTAxNjJlOTU1MjI3NSJdAAAGILeLkpyw9gy8y5ZP0v1XY5slsLGMN4Z4yjhDg3X-H5Cz
