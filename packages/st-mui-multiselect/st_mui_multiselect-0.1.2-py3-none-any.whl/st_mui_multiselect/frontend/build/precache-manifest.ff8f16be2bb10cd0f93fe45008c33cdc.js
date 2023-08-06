eaeb064b854f1dc2aee58ee3b3d4d7c2d87be2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:32 +0000

    Add package st-mui-multiselect/0.1.2/st_mui_multiselect-0.1.2-py3-none-any.whl

---
+++ packages/st-mui-multiselect/st_mui_multiselect-0.1.2-py3-none-any.whl/st_mui_multiselect/frontend/build/precache-manifest.ff8f16be2bb10cd0f93fe45008c33cdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae730c12ec577ac383721ecdfce11dae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b1ee50c5d9694a2c142",
+    "url": "./static/js/2.32345768.chunk.js"
+  },
+  {
+    "revision": "fbabe484400fd249a0740ba2c0d294dd",
+    "url": "./static/js/2.32345768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc5ccd2a2313942815e",
+    "url": "./static/js/main.73032e85.chunk.js"
+  },
+  {
+    "revision": "43ec314e7be372be36f2",
+    "url": "./static/js/runtime-main.25638943.js"
+  }
+]);
