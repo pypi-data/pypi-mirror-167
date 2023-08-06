208a739584f53cc1cced77bd40f5e7af2c6cf5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:23 +0000

    Add package bowtie-json-schema/0.15.1/bowtie_json_schema-0.15.1.tar.gz

---
+++ packages/bowtie-json-schema/bowtie_json_schema-0.15.1.tar.gz/bowtie_json_schema-0.15.1/bowtie/_cli.py
@@ -0,0 +1,151 @@
+from contextlib import AsyncExitStack
+from fnmatch import fnmatch
+import asyncio
+import json
+import os
+import sys
+
+import aiodocker
+import click
+import jinja2
+import structlog
+
+from bowtie._core import Implementation, Reporter, report_on
+
+
+@click.group(context_settings=dict(help_option_names=["--help", "-h"]))
+@click.version_option(prog_name="bowtie")
+def main():
+    """
+    A meta-validator for the JSON Schema specifications.
+    """
+
+
+@main.command()
+@click.argument(
+    "input",
+    default="-",
+    type=click.File(mode="r"),
+)
+@click.option(
+    "--out", "-o", "output",
+    help="Where to write the outputted report HTML.",
+    default="bowtie-report.html",
+    type=click.File("w"),
+)
+def report(input, output):
+    """
+    Generate a Bowtie report from a previous run.
+    """
+
+    env = jinja2.Environment(
+        loader=jinja2.PackageLoader("bowtie", "template"),
+        undefined=jinja2.StrictUndefined,
+        keep_trailing_newline=True,
+    )
+    template = env.get_template("report.html.j2")
+    output.write(template.render(**report_on(input)))
+
+
+@main.command()
+@click.pass_context
+@click.option(
+    "--implementation", "-i", "implementations",
+    help="A docker image which implements the bowtie IO protocol.",
+    multiple=True,
+)
+@click.option(
+    "-k", "filter",
+    type=lambda pattern: f"*{pattern}*",
+    help="Only run cases whose description match the given glob pattern.",
+)
+@click.option(
+    "--hide-expected-results/--include-expected-results",
+    "hide_expected_results",
+    default=True,
+    help=(
+        "Don't pass expected results to implementations under test. "
+        "Doing so generally should have no expected change in behavior."
+    ),
+)
+@click.argument(
+    "input",
+    default="-",
+    type=click.File(mode="rb"),
+)
+def run(context, input, filter, **kwargs):
+    """
+    Run a sequence of cases provided on standard input.
+    """
+
+    out = sys.stderr
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(
+                fmt="%Y-%m-%d %H:%M.%S", utc=False,
+            ),
+            structlog.processors.StackInfoRenderer(),
+            structlog.dev.set_exc_info,
+            structlog.dev.ConsoleRenderer(
+                colors=getattr(out, "isatty", lambda: False)(),
+            ),
+        ],
+        logger_factory=structlog.PrintLoggerFactory(out),
+    )
+    cases = (json.loads(line) for line in input)
+    if filter:
+        cases = (
+            case for case in cases
+            if fnmatch(case["description"], filter)
+        )
+
+    reporter = Reporter()
+    count = asyncio.run(_run(**kwargs, reporter=reporter, cases=cases))
+    if not count:
+        context.exit(os.EX_DATAERR)
+
+
+async def _run(implementations, reporter, cases, hide_expected_results):
+    reporter.run_starting(implementations=implementations)
+
+    async with AsyncExitStack() as stack:
+        docker = await stack.enter_async_context(aiodocker.Docker())
+        runners = [
+            await stack.enter_async_context(
+                Implementation.start(docker=docker, image_name=each),
+            ) for each in implementations
+        ]
+        reporter.ready(implementations=runners)
+
+        seq = 0
+        for seq, case, case_reporter in sequenced(cases, reporter):
+            if hide_expected_results:
+                for test in case["tests"]:
+                    # TODO: Re-emit me later
+                    test.pop("valid", None)
+
+            responses = [each.run_case(seq=seq, case=case) for each in runners]
+            for each in asyncio.as_completed(responses):
+                response = await each
+                if not response["succeeded"]:
+                    if response.get("backoff"):
+                        case_reporter.backoff(response)
+                    else:
+                        case_reporter.errored_uncaught(response)
+                    continue
+
+                if response["response"].get("errored"):
+                    case_reporter.errored(response)
+                    continue
+
+                case_reporter.got_results(
+                    implementation=response["implementation"],
+                    response=response["response"],
+                )
+        reporter.finished(count=seq)
+    return seq
+
+
+def sequenced(cases, reporter):
+    for seq, case in enumerate(cases, 1):
+        yield seq, case, reporter.case_started(seq=seq, case=case)
---
+++ packages/bowtie-json-schema/bowtie_json_schema-0.15.1.tar.gz/bowtie_json_schema-0.15.1/implementations/js-ajv/bowtie_ajv.js
@@ -0,0 +1,58 @@
+const readline = require("readline");
+
+const Ajv = require("ajv");
+
+const stdio = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+function send(data) {
+  console.log(JSON.stringify(data));
+}
+
+var started = false;
+
+const cmds = {
+  start: (args) => {
+    console.assert(args.version === 1, { args });
+    started = true;
+    return {
+      ready: true,
+      version: 1,
+      implementation: {
+        language: "javascript",
+        name: "ajv",
+        homepage: "https://ajv.js.org/",
+        issues: "https://github.com/ajv-validator/ajv/issues",
+      },
+    };
+  },
+
+  run: (args) => {
+    console.assert(started, "Not started!");
+
+    const testCase = args.case;
+    const validate = ajv.compile(testCase.schema);
+    return {
+      seq: args.seq,
+      results: testCase.tests.map((test) => ({
+        valid: validate(test.instance),
+      })),
+    };
+  },
+
+  stop: (_) => {
+    console.assert(started, "Not started!");
+    process.exit(0);
+  },
+};
+
+const ajv = new Ajv();
+
+stdio.on("line", (line) => {
+  const request = JSON.parse(line);
+  const response = cmds[request.cmd](request);
+  send(response);
+});
