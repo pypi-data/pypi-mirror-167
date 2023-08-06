2d0a75ca6a08f9dcd6a4c3d42d3e1da2af54a49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:46 +0000

    Add package pil-helper/0.0.1/pil-helper-0.0.1.tar.gz

---
+++ packages/pil-helper/pil-helper-0.0.1.tar.gz/pil-helper-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import *
+
+LONG_DESC = """
+This is pil helper
+"""
+
+setup(name='pil-helper',
+	  version='0.0.1',
+	  description='Pil helper',
+	  long_description=LONG_DESC,
+	  author='Sang Ki Kwon',
+	  url='https://github.com/automatethem/pil-helper',
+	  install_requires=[],
+	  author_email='automatethem@gmail.com',
+	  license='MIT',
+	  packages=find_packages(),
+	  zip_safe=False)
