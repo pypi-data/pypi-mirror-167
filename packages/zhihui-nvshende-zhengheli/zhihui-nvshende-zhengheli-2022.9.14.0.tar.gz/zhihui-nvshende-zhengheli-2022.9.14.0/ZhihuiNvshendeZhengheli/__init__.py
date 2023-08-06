310bd7a763c7803f54ca878503742f4aa073a724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:28 +0000

    Add package zhihui-nvshende-zhengheli/2022.9.14.0/zhihui-nvshende-zhengheli-2022.9.14.0.tar.gz

---
+++ packages/zhihui-nvshende-zhengheli/zhihui-nvshende-zhengheli-2022.9.14.0.tar.gz/zhihui-nvshende-zhengheli-2022.9.14.0/ZhihuiNvshendeZhengheli/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhihui-nvshende-zhengheli
+https://github.com/apachecn/zhihui-nvshende-zhengheli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.14.0"
