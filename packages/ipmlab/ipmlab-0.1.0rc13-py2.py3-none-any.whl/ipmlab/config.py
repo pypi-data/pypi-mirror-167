5ed91612a6fc4ccead3a3a8e5360823d36018a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:43 +0000

    Add package ipmlab/0.1.0rc13/ipmlab-0.1.0rc13-py2.py3-none-any.whl

---
+++ packages/ipmlab/ipmlab-0.1.0rc13-py2.py3-none-any.whl/ipmlab/config.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+"""Shared configuration constants"""
+
+version = ""
+inDevice = ""
+aaruBin = ""
+rootDir = ""
+batchFolder = ""
+batchManifest = ""
+secondsToTimeout = ""
+prefixBatch = ""
+socketHost = "127.0.0.1"
+socketPort = "65432"
+enablePPNLookup = True
+enableSocketAPI = False
+quitFlag = False
+batchIsOpen = False
+readyToStart = False
+finishedMedium = False
+processingMedium = False
+finishedBatch = False
