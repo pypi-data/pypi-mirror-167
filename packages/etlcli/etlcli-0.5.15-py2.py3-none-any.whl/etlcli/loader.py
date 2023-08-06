9cc81dfd619feeb026f8aa9afa3d9c69451953ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:51 +0000

    Add package etlcli/0.5.15/etlcli-0.5.15-py2.py3-none-any.whl

---
+++ packages/etlcli/etlcli-0.5.15-py2.py3-none-any.whl/etlcli/loader.py
@@ -0,0 +1,31 @@
+import sqlite3
+import csv
+
+
+class Loader:
+    def __init__(self, database) -> None:
+        self.conn = sqlite3.connect(database)
+        self.cur = self.conn.cursor()
+    
+    def load_data(self, input, target) -> None:
+        with open(input, "r") as f:
+            reader = csv.reader(f, delimiter=",")
+            for i, line in enumerate(reader):
+                if i == 0:
+                    continue
+                query = self._prepare_query(target, len(line))
+                # print(query, line)
+                self.conn.execute(query, line)
+            self.conn.commit()
+
+    def _prepare_query(self, target, length):
+        query = f"INSERT or REPLACE into {target} VALUES ({','.join(['?'] * length)});"
+        return query
+    
+    def close_conn(self) -> None:
+        self.conn.close()
+
+if __name__ == "__main__":
+    loader = Loader("target.db")
+    loader.load_data("transactions_2010-12-02.csv", "transactions")
+    loader.close_conn()
