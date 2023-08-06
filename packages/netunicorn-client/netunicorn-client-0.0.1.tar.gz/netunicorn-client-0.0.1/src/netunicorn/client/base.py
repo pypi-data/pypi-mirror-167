d453a7e73fa538a03dc8c0b583de90a7081f3aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:05 +0000

    Add package netunicorn-client/0.0.1/netunicorn-client-0.0.1.tar.gz

---
+++ packages/netunicorn-client/netunicorn-client-0.0.1.tar.gz/netunicorn-client-0.0.1/src/netunicorn/client/base.py
@@ -0,0 +1,60 @@
+from typing import Tuple, Union, Optional, List
+
+from netunicorn.base.experiment import Experiment, ExperimentStatus, ExperimentExecutionResult
+from netunicorn.base.minions import MinionPool
+
+
+class BaseClient:
+
+    def get_minion_pool(self) -> MinionPool:
+        """
+        This method returns description of available minions in the system.
+        :return: MinionPool with available minions
+        """
+        raise NotImplementedError()
+
+    def prepare_experiment(self, experiment: Experiment, experiment_id: str) -> str:
+        """
+        Prepares a deployment map. Server will start compiling and distributing the environment among nodes.
+        You can check status of preparation by calling 'get_deployment_status' function and checking if it's in
+        "DeploymentStatus.READY" status.
+        You need to provide a per-user unique deployment id.
+        This method is network-failure-safe: subsequent calls with the same deployment id
+        will not create additional deployment process.
+        :param experiment: map to be prepared for deployment
+        :param experiment_id: user-wide unique deployment id
+        :return: the same deployment_id if preparation already in progress or finished.
+        """
+        raise NotImplementedError()
+
+    def start_execution(self, experiment_id: str) -> str:
+        """
+        Starts execution of prepared deployment map.
+        You can check status of deployment by calling 'get_deployment_status' function and checking if it's in
+        "DeploymentStatus.FINISHED" status.
+        You can retrieve results by calling 'get_deployment_result' function.
+        You need to provide a per-user unique deployment id.
+        This method is network-failure-safe: subsequent calls with the same deployment id
+        will not create additional deployment process.
+        :param experiment_id: prepared deployment id
+        :return: the same deployment_id if execution already in progress or finished
+        """
+        raise NotImplementedError()
+
+    def get_experiment_status(self, experiment_id: str) -> Tuple[
+        ExperimentStatus,
+        Optional[Experiment],
+        Union[
+            None,
+            Exception,
+            List[ExperimentExecutionResult]
+        ]
+    ]:
+        """
+        Returns status and results of experiment.
+        If experiment preparation succeed, you can explore map to see what minions are prepared for deployment.
+        If experiment finished, you can explore results of the experiment
+        :param experiment_id: id of the experiment returned by 'deploy_map' function
+        :return: current status of the experiment, optionally experiment definition, optionally experiment results
+        """
+        raise NotImplementedError()
---
+++ packages/netunicorn-client/netunicorn-client-0.0.1.tar.gz/netunicorn-client-0.0.1/src/netunicorn/client/remote.py
@@ -0,0 +1,103 @@
+import json
+
+import requests as req
+from typing import Union, Tuple, Optional, List
+
+from netunicorn.base.experiment import Experiment, ExperimentExecutionResult, ExperimentStatus
+from netunicorn.base.minions import MinionPool
+from netunicorn.base.utils import UnicornEncoder
+
+from .base import BaseClient
+
+
+class RemoteClientException(Exception):
+    pass
+
+
+class RemoteClient(BaseClient):
+    def __init__(self, endpoint: str, login: str, password: str):
+        """
+        Remote client for Unicorn.
+        :param endpoint: Unicorn installation endpoint.
+        :param login: Unicorn installation login.
+        :param password: Unicorn installation password.
+        """
+        if endpoint.endswith("/"):
+            endpoint = endpoint[:-1]
+        self.endpoint = endpoint
+        self.login = login
+        self.password = password
+
+    def get_minion_pool(self) -> MinionPool:
+        result = req.get(f"{self.endpoint}/api/v1/minion_pool", auth=(self.login, self.password))
+        if result.status_code == 200:
+            return MinionPool.from_json(result.json())
+
+        raise RemoteClientException(
+            f"Failed to get minion pool. "
+            f"Status code: {result.status_code}, content: {result.content}"
+        )
+
+    def prepare_experiment(self, experiment: Experiment, experiment_id: str) -> str:
+        data = json.dumps(experiment, cls=UnicornEncoder)
+        result = req.post(
+            f"{self.endpoint}/api/v1/experiment/{experiment_id}/prepare",
+            auth=(self.login, self.password),
+            data=data,
+            headers={"Content-Type": "application/json"}
+        )
+        if result.status_code == 200:
+            return result.json()
+
+        raise RemoteClientException(
+            "Failed to prepare an experiment. "
+            f"Status code: {result.status_code}, content: {result.content}"
+        )
+
+    def start_execution(self, experiment_id: str) -> str:
+        result = req.post(
+            f"{self.endpoint}/api/v1/experiment/{experiment_id}/start",
+            auth=(self.login, self.password)
+        )
+        if result.status_code == 200:
+            return result.json()
+
+        raise RemoteClientException(
+            "Failed to start experiment execution. "
+            f"Status code: {result.status_code}, content: {result.content}"
+        )
+
+    def get_experiment_status(self, experiment_id: str) -> Tuple[
+        ExperimentStatus,
+        Optional[Experiment],
+        Union[
+            None,
+            Exception,
+            List[ExperimentExecutionResult]
+        ]
+    ]:
+        result_data = req.get(f"{self.endpoint}/api/v1/experiment/{experiment_id}", auth=(self.login, self.password))
+        if result_data.status_code == 200:
+            result = result_data.json()
+            if not (isinstance(result, list) and len(result) == 3):
+                raise RemoteClientException(f"Invalid response from the server. Result: {result}")
+
+            # decode experiment
+            result[0] = ExperimentStatus.from_json(result[0])
+
+            if result[1] is not None:
+                result[1] = Experiment.from_json(result[1])
+
+            # decode execution results
+            if isinstance(result[2], str):
+                result[2] = Exception(result[2])
+
+            if isinstance(result[2], list):
+                result[2] = [ExperimentExecutionResult.from_json(r) for r in result[2]]
+
+            return tuple(result)
+
+        raise RemoteClientException(
+            "Failed to get experiment status. "
+            f"Status code: {result_data.status_code}, content: {result_data.content}"
+        )
