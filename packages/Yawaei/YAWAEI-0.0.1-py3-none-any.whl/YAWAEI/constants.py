83696d7b3457205f4fcef86932a6100377884ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:13 +0000

    Add package Yawaei/0.0.1/YAWAEI-0.0.1-py3-none-any.whl

---
+++ packages/Yawaei/YAWAEI-0.0.1-py3-none-any.whl/YAWAEI/constants.py
@@ -0,0 +1,11 @@
+PROMOTIONS = {
+    'msc1': 'msc4',
+    'msc2': 'msc5',
+    'premsc': 'msc3',
+    'wac1': 'wac1',
+    'wac2': 'wac2'
+}
+
+courses = 'course[]=Code-And-Go&course[]=Dev-And-Go&course[]=msc&course[]=premsc&course[]=webacademie&course[]=other'
+
+INTRA_LINK = r'intra\.epitech\.eu'
