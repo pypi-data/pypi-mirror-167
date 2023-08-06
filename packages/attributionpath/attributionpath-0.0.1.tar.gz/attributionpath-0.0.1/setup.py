b90b307165c87ae397aff87d4980f94d4e563610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:35 +0000

    Add package attributionpath/0.0.1/attributionpath-0.0.1.tar.gz

---
+++ packages/attributionpath/attributionpath-0.0.1.tar.gz/attributionpath-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="attributionpath",
+    version="0.0.1",
+    author="Ayman Elkassas",
+    author_email="aymanelkassas@mans.edu.eg",
+    description="A small package for integrated gradients based on attribution method",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
