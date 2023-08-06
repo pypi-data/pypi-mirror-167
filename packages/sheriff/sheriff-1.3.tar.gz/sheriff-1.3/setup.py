7d72f830b3724ea2c0b4c932a6eaf89b85138b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:24 +0000

    Add package sheriff/1.3/sheriff-1.3.tar.gz

---
+++ packages/sheriff/sheriff-1.3.tar.gz/sheriff-1.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from setuptools import setup, find_packages
+from src.sheriff.__init__ import __version__
+
+VERSION = __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sheriff",
+    version=VERSION,
+    author="Gautam Gambhir",
+    author_email="ggambhir1919@gmail.com",
+    description="An AI enabled program to detect car speed",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Programming-Hero-1313/Sheriff",
+    project_urls={
+        "Bug Tracker": "https://github.com/Programming-Hero-1313/Sheriff/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    package_dir={'':"src"},
+    packages=find_packages('src'),
+    python_requires=">=3.10",
+    keywords=['speed detector', 'sheriff', 'ai', 'opencv'],
+    include_package_data=True
+)
