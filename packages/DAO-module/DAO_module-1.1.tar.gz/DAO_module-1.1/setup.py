66769041939454c6eeae1d90cec3f8b484674bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package DAO-module/1.1/DAO_module-1.1.tar.gz

---
+++ packages/DAO-module/DAO_module-1.1.tar.gz/DAO_module-1.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="DAO_module",
+    version="1.1",
+    author="BuldakovN",
+    author_email="nikitabuldakov@mail.ru",
+    description="Модуль для работы с удаленной БД",
+    url="https://github.com/SADT-Boting/DAO-Module",
+    packages=['dao_module'],
+    python_requires='>=3.5',
+    install_requires=["pymysql"],
+    requirements=['pymysql>=1.0.2']
+)
