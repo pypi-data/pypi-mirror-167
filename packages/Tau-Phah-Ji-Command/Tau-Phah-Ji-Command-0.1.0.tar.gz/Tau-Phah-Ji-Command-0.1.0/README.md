faa5498d410cfdb3a441e0ed1962bce390010d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:29 +0000

    Add package Tau-Phah-Ji-Command/0.1.0/Tau-Phah-Ji-Command-0.1.0.tar.gz

---
+++ packages/Tau-Phah-Ji-Command/Tau-Phah-Ji-Command-0.1.0.tar.gz/Tau-Phah-Ji-Command-0.1.0/README.md
@@ -0,0 +1,25 @@
+# Tau-Phah-Ji-Command
+鬥拍字的指令直接包做一个套件
+
+## Tau
+因為新版本猶未deploy到PyPI，暫時先用zip。
+```bash
+pip install https://github.com/i3thuan5/TauPhahJi-Command/archive/refs/heads/master.zip
+```
+
+## Ing
+```python
+>>> from tauphahji_cmd import tàuphahjī
+>>> tàuphahjī('gau5 tsa2\nlâi-khì.')
+{
+  # Tsuân-pōo
+  'kiatko': [
+    {'漢字': '𠢕早', 'KIP': 'gâu-tsá', '分詞': '𠢕-早｜gâu-tsá'},
+    {'漢字': '來去。', 'KIP': 'lâi-khì.', '分詞': '來-去｜lâi-khì 。｜.'}
+  ],
+  # Thâu-tsi̍t-kú
+  '漢字': '𠢕早',
+  'KIP': 'gâu-tsá',
+  '分詞': '𠢕-早｜gâu-tsá'
+}
+```
---
+++ packages/Tau-Phah-Ji-Command/Tau-Phah-Ji-Command-0.1.0.tar.gz/Tau-Phah-Ji-Command-0.1.0/tauphahji_cmd/__init__.py
@@ -0,0 +1,42 @@
+import json
+from urllib.parse import urlencode
+from http.client import HTTPSConnection
+
+
+def tàuphahjī(漢羅, **tshamsoo):
+    conn = HTTPSConnection(
+        "hokbu.ithuan.tw"
+    )
+    tshamsoo = urlencode({
+        'taibun': 漢羅,
+        **tshamsoo,
+    })
+    headers = {
+        "Content-type": "application/x-www-form-urlencoded",
+        "Accept": "text/plain"
+    }
+    conn.request(
+        "POST",
+        "/tau",
+        tshamsoo,
+        headers,
+    )
+
+    responseStr = conn.getresponse().read().decode('utf-8')
+    conn.close()
+    return json.loads(responseStr)
+
+
+def liânKù(多元書寫, 欲連的key):
+    if 欲連的key == '漢字':
+        return liânHànjī(多元書寫)
+    elif 欲連的key == '臺羅':
+        return liânTâilô(多元書寫)
+
+
+def liânHànjī(多元書寫):
+    return ''.join([i['漢字'].replace('.', '。') for i in 多元書寫])
+
+
+def liânTâilô(多元書寫):
+    return '. '.join([i['臺羅'] for i in 多元書寫])
