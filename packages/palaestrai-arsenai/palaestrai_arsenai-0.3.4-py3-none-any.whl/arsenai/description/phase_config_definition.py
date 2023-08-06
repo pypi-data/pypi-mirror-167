441b8f4466a1ab1e66302b6f7b3f51165b13c715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:22 +0000

    Add package palaestrai-arsenai/0.3.4/palaestrai_arsenai-0.3.4-py3-none-any.whl

---
+++ packages/palaestrai-arsenai/palaestrai_arsenai-0.3.4-py3-none-any.whl/arsenai/description/phase_config_definition.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PhaseConfigDefinition:
+
+    uid: str
+    mode: str
+    worker: int
+    episodes: int
---
+++ packages/palaestrai-arsenai/palaestrai_arsenai-0.3.4-py3-none-any.whl/arsenai/description/sensor_definition.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class SensorDefinition:
+
+    uid: str
+    sensor_ids: List[str]
