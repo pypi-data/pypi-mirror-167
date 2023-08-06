c9af484a3ab9e5624b3a623f748f40fff6019a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:46 +0000

    Add package bjgrader/0.0.5/bjgrader-0.0.5.tar.gz

---
+++ packages/bjgrader/bjgrader-0.0.5.tar.gz/bjgrader-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="bjgrader",
+    version="0.0.5",
+    author="Dooho Lee",
+    author_email="dhdev5ba@gmail.com",
+    url="https://github.com/BlueYellowGreen/bjgrader",
+    description="Grade a solution using baekjoon online sample inputs and outputs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=['requests', 'beautifulsoup4'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=['bjgrader'],
+)
