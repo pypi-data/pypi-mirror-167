1f0a066a8bf0d454a3cbc5e8e786e9cdba52b6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:43 +0000

    Add package registtro/1.1.0/registtro-1.1.0-py2.py3-none-any.whl

---
+++ packages/registtro/registtro-1.1.0-py2.py3-none-any.whl/registtro/__init__.py
@@ -0,0 +1,5 @@
+from ._protocol import RegistryProtocol
+from ._registry import Registry, RegistryEvolver
+from ._exceptions import RegisttroException, EntryNotFoundError
+
+__all__ = ["RegistryProtocol", "Registry", "RegistryEvolver", "RegisttroException", "EntryNotFoundError"]
