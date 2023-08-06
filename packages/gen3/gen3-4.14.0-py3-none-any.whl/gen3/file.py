d4df6638eae5aa7024c3c0321e640f6278211c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:05 +0000

    Add package gen3/4.14.0/gen3-4.14.0-py3-none-any.whl

---
+++ packages/gen3/gen3-4.14.0-py3-none-any.whl/gen3/file.py
@@ -0,0 +1,122 @@
+import json
+import requests
+
+
+class Gen3FileError(Exception):
+    pass
+
+
+class Gen3File:
+    """For interacting with Gen3 file management features.
+
+    A class for interacting with the Gen3 file download services.
+    Supports getting presigned urls right now.
+
+    Args:
+        auth_provider (Gen3Auth): A Gen3Auth class instance.
+
+    Examples:
+        This generates the Gen3File class pointed at the sandbox commons while
+        using the credentials.json downloaded from the commons profile page.
+
+        >>> auth = Gen3Auth(refresh_file="credentials.json")
+        ... file = Gen3File(auth)
+
+    """
+
+    def __init__(self, endpoint=None, auth_provider=None):
+        # auth_provider legacy interface required endpoint as 1st arg
+        self._auth_provider = auth_provider or endpoint
+        self._endpoint = self._auth_provider.endpoint
+
+    def get_presigned_url(self, guid, protocol=None):
+        """Generates a presigned URL for a file.
+
+        Retrieves a presigned url for a file giving access to a file for a limited time.
+
+        Args:
+            guid (str): The GUID for the object to retrieve.
+            protocol (:obj:`str`, optional): The protocol to use for picking the available URL for generating the presigned URL.
+
+        Examples:
+
+            >>> Gen3File.get_presigned_url(query)
+
+        """
+        api_url = "{}/user/data/download/{}".format(self._endpoint, guid)
+        if protocol:
+            api_url += "?protocol={}".format(protocol)
+        output = requests.get(api_url, auth=self._auth_provider).text
+
+        try:
+            data = json.loads(output)
+        except:
+            return output
+        return data
+
+    def delete_file(self, guid):
+        """
+        This method is DEPRECATED. Use delete_file_locations() instead.
+        Delete all locations of a stored data file and remove its record from indexd
+
+        Args:
+            guid (str): provide a UUID for file id to delete
+        Returns:
+            text: requests.delete text result
+        """
+        print("This method is DEPRECATED. Use delete_file_locations() instead.")
+        api_url = "{}/user/data/{}".format(self._endpoint, guid)
+        output = requests.delete(api_url, auth=self._auth_provider).text
+
+        return output
+
+    def delete_file_locations(self, guid):
+        """
+        Delete all locations of a stored data file and remove its record from indexd
+
+        Args:
+            guid (str): provide a UUID for file id to delete
+        Returns:
+            requests.Response : requests.delete result
+        """
+        api_url = "{}/user/data/{}".format(self._endpoint, guid)
+        output = requests.delete(api_url, auth=self._auth_provider)
+
+        return output
+
+    def upload_file(self, file_name, authz=None, protocol=None, expires_in=None):
+        """
+        Get a presigned url for a file to upload
+
+        Args:
+            file_name (str): file_name to use for upload
+            authz (list): authorization scope for the file as list of paths, optional.
+            protocol (str): Storage protocol to use for upload: "s3", "az".
+                If this isn't set, the default will be "s3"
+            expires_in (int): Amount in seconds that the signed url will expire from datetime.utcnow().
+                Be sure to use a positive integer.
+                This value will also be treated as <= MAX_PRESIGNED_URL_TTL in the fence configuration.
+        Returns:
+            Document: json representation for the file upload
+        """
+        api_url = f"{self._endpoint}/user/data/upload"
+        body = {}
+        if protocol:
+            body["protocol"] = protocol
+        if authz:
+            body["authz"] = authz
+        if expires_in:
+            body["expires_in"] = expires_in
+        if file_name:
+            body["file_name"] = file_name
+
+        headers = {"Content-Type": "application/json"}
+        output = requests.post(
+            api_url, auth=self._auth_provider, json=body, headers=headers
+        ).text
+        try:
+            data = json.loads(output)
+        except:
+            return output
+
+        return data
