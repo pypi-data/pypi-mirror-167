76fcc30935fe2710016c14df7e8ffe9d110d87dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:19:37 +0000

    Add package distributions-vul3rmp/0.1/distributions_vul3rmp-0.1.tar.gz

---
+++ packages/distributions-vul3rmp/distributions_vul3rmp-0.1.tar.gz/distributions_vul3rmp-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_vul3rmp',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_vul3rmp'],
+      author='Hsiao-Chun Hu',
+      author_email='hsiaochun.hu@outlook.com',
+      zip_safe=False)
