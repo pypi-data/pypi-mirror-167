8b01b4a51b9e9ffb205624e84b2735902b205ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:41 +0000

    Add package avimu-probability/1.0/avimu_probability-1.0.tar.gz

---
+++ packages/avimu-probability/avimu_probability-1.0.tar.gz/avimu_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='avimu_probability',
+      version='1.0',
+      description='Binomial and Gaussian distributions',
+      packages=['avimu_probability'],
+      author='Avi Mukesh',
+      zip_safe=False)
